fd9521dbfebecc5a4f7d9990419ff0c368a8966e
David Li <dawei.li@gmx.net>
2020-03-04 16:16:03 +0100

    hi

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ const Home = () => {
       <Nav />
 
       <div className="hero">
-        <h1 className="title">Welcome to Next!</h1>
+        <h1 className="title">Welcome to Next 123!</h1>
         <p className="description">
           To get started, edit the <code>pages/index.js</code> or{' '}
           <code>pages/api/date.js</code> files, then save to reload.
