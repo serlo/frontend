5f43bcf12273dcc6b37d6dd54b35c9fb9b465f6c
David Li <dawei.li@gmx.net>
2020-03-17 23:14:14 +0100

    make spoiler reusabe

--- src/components/spoiler.tsx
+++ src/components/spoiler.tsx
@@ -8,28 +8,39 @@ function Spoiler(props) {
   const [open, setOpen] = React.useState(defaultOpen)
   return (
     <StyledSpoiler>
-      <SpoilerTitle onClick={() => setOpen(!open)}>
-        {open ? (
-          <FontAwesomeIcon icon={faCaretDown} />
-        ) : (
-          <FontAwesomeIcon icon={faCaretRight} />
-        )}{' '}
-        {title}
-      </SpoilerTitle>
+      <SpoilerHeader open={open} onClick={() => setOpen(!open)} title={title} />
       {open && <SpoilerContent>{children}</SpoilerContent>}
     </StyledSpoiler>
   )
 }
 
-const StyledSpoiler = styled.div`
+export function SpoilerHeader(props) {
+  const { open, onClick, title, children } = props
+  return (
+    <SpoilerTitle onClick={onClick}>
+      {open ? (
+        <FontAwesomeIcon icon={faCaretDown} />
+      ) : (
+        <FontAwesomeIcon icon={faCaretRight} />
+      )}{' '}
+      {title}
+      {children}
+    </SpoilerTitle>
+  )
+}
+
+export const StyledSpoiler = styled.div<{ hide?: boolean }>`
   width: 100%
   display: flex;
   flex-direction: column;
   border-left: 4px solid ${props => props.theme.colors.gray};
   margin-bottom: 38px;
+  > :not(:first-child) {
+    display: ${props => (props.hide ? 'none' : 'block')};
+  }
 `
 
-const SpoilerTitle = styled.button`
+export const SpoilerTitle = styled.button`
   border: none;
   background-color: transparent;
   margin: 0;
@@ -43,7 +54,7 @@ const SpoilerTitle = styled.button`
   text-align: left;
 `
 
-const SpoilerContent = styled.div`
+export const SpoilerContent = styled.div`
   margin-top: 15px;
   margin-left: 10px;
   margin-right: 0;
