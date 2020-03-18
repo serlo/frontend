b2be484bdd4bb534925dad8e1c6b3d10167ef435
David Li <dawei.li@gmx.net>
2020-03-18 19:54:37 +0100

    small fixes

--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -5,56 +5,63 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons'
 
 interface BreadcrumbProps {
-    entries?: BreadcrumbEntry[]
+  entries?: BreadcrumbEntry[]
 }
 
 interface BreadcrumbEntry {
-    label: string
-    url: string
+  label: string
+  url: string
 }
 
-export default function Breadcrumbs(entries : BreadcrumbEntry[]) {
-    if (!entries || entries.length < 1) {
-        return <p>""</p>
-    }
+export default function Breadcrumbs(props: BreadcrumbProps) {
+  const { entries } = props
+  if (!entries || entries.length < 1) {
+    return <p>""</p>
+  }
 
-    return (
-        <BreadcrumbWrapper>
-            {entries.map((bcEntry, i, l) => {
-                return (
-                    <BreadcrumbEntries bcEntry={bcEntry} i={i} l={l}></BreadcrumbEntries>
-                )
-            })}
-        </BreadcrumbWrapper>
-    )
+  return (
+    <BreadcrumbWrapper>
+      {entries.map((bcEntry, i, l) => {
+        return (
+          <BreadcrumbEntries
+            bcEntry={bcEntry}
+            i={i}
+            l={l}
+            key={i}
+          ></BreadcrumbEntries>
+        )
+      })}
+    </BreadcrumbWrapper>
+  )
 }
 
 function BreadcrumbEntries(props) {
-    const { bcEntry, i, l } = props
-    return (
-        (l.length !== i + 1) ?
-            <React.Fragment>
-                <Breadcrumb href={bcEntry.url}>{bcEntry.label}</Breadcrumb>
-                <Seperator>></Seperator>
-            </React.Fragment>
-        : <BreadcrumbLast href={bcEntry.url}>
-            <Icon icon={faArrowCircleLeft} size="1x" />
-            {bcEntry.label}
-          </BreadcrumbLast>
-        
-    )
+  const { bcEntry, i, l } = props
+  return l.length !== i + 1 ? (
+    <>
+      <Breadcrumb href={bcEntry.url}>{bcEntry.label}</Breadcrumb>
+      <Seperator>></Seperator>
+    </>
+  ) : (
+    <BreadcrumbLast href={bcEntry.url}>
+      <Icon>
+        <FontAwesomeIcon icon={faArrowCircleLeft} size="1x" />
+      </Icon>
+      {bcEntry.label}
+    </BreadcrumbLast>
+  )
 }
 
 const BreadcrumbWrapper = styled.div`
-    margin: 10px;
+  margin: 10px;
 `
 
 const Seperator = styled.span`
-    color: ${props => props.theme.colors.dark1};
+  color: ${props => props.theme.colors.dark1};
 
-    @media (max-width: ${props => props.theme.breakpoints.sm}) {
-      display: none;
-    }
+  @media (max-width: ${props => props.theme.breakpoints.sm}) {
+    display: none;
+  }
 `
 
 const Breadcrumb = styled.a`
@@ -79,20 +86,21 @@ const Breadcrumb = styled.a`
 const BreadcrumbLast = styled(Breadcrumb)`
   @media (max-width: ${props => props.theme.breakpoints.sm}) {
     display: inline-flex;
-    background: ${props => transparentize(0.35, props.theme.colors.lightBlueBackground)};
+    background: ${props =>
+      transparentize(0.35, props.theme.colors.lightBlueBackground)};
     border-radius: 12px;
     padding-left: 2px;
 
     &:hover {
-        background: ${props => transparentize(0.35, props.theme.colors.brand)};
-        color: ${props => props.theme.colors.bluewhite};
+      background: ${props => transparentize(0.35, props.theme.colors.brand)};
+      color: ${props => props.theme.colors.bluewhite};
     }
   }
 `
 
-const Icon = styled(FontAwesomeIcon)`
-    @media (min-width: ${props => props.theme.breakpoints.sm}) {
-        display: none;
-    }
+const Icon = styled.span`
+  @media (min-width: ${props => props.theme.breakpoints.sm}) {
+    display: none;
+  }
   margin-right: 10px;
 `
