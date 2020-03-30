157f313a5826ad4def1eabbb40851fa4b5320f1a
Botho <1258870+elbotho@users.noreply.github.com>
2020-03-30 19:23:29 +0200

    add button helper
    paddings in em because it should be relative to font size

--- src/helper/csshelper.ts
+++ src/helper/csshelper.ts
@@ -3,3 +3,13 @@ export const makeMargin = props =>
 
 export const makePadding = props =>
   props.full ? '' : 'padding-left:15px;padding-right:15px;'
+
+export const makeButton = props =>
+  `
+    font-weight: bold;
+    transition: all 0.2s ease-in-out 0s;
+    border-radius: 2em;
+    padding: 0.27em 0.6em;
+    text-decoration: none;
+    cursor: pointer;
+  `
