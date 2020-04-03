8d7a2c2bbabd16ed129b043802da94ed948b89c4
theMetalkate <>
2020-04-03 18:28:01 +0200

    feature notifications

--- src/components/content/Notifications.tsx
+++ src/components/content/Notifications.tsx
@@ -2,83 +2,82 @@ import React from 'react'
 import styled from 'styled-components'
 
 interface User {
-    id: string
-    username: string
-  }
+  id: string
+  username: string
+}
 
 interface NotificationProp {
-    author: User,
-    body: string,
-    timestamp: string,
-    readed: boolean
+  author: User
+  body: string
+  timestamp: string
+  readed: boolean
 }
 
 export default function Notifications(entries: NotificationProp[]) {
-    return (
-        <Wrapper>
-            {entries.map((entry, index) => {
-                return (
-                    <Item readed={entry.readed} className={ ( !entry.readed ? "unreaded" : "") }>
-                        <Link
-                            href={`https://serlo.org/${entry.author.id}`}>
-                                {entry.author.username}
-                        </Link>
-                        <Body dangerouslySetInnerHTML={{__html: entry.body}}></Body>
-                        <TimestampText>
-                            {entry.timestamp}
-                        </TimestampText>
-                    </Item>
-                )
-            })}
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      {entries.map((entry, i, l) => {
+        return (
+          <Item readed={entry.readed}>
+            <Link href={`https://serlo.org/${entry.author.id}`}>
+              {entry.author.username}
+            </Link>
+            <Body dangerouslySetInnerHTML={{ __html: entry.body }}></Body>
+            <TimestampText>{entry.timestamp}</TimestampText>
+          </Item>
+        )
+      })}
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
-    margin: 50px 0;
+  margin: 50px 0;
 `
 
 const Link = styled.a`
-    color: ${props => props.theme.colors.brand};
-    text-decoration: none;
-    margin-right: 5px;
-    &:hover {
-        color: ${props => props.theme.colors.lightblue};
-    }
+  color: ${props => props.theme.colors.brand};
+  text-decoration: none;
+  margin-right: 5px;
+  &:hover {
+    color: ${props => props.theme.colors.lightblue};
+  }
 `
 
 const TimestampText = styled.span`
-    color: ${props => props.theme.colors.gray};
-    margin-left: 5px;
+  color: ${props => props.theme.colors.gray};
+  margin-left: 5px;
 `
 
-const Item = styled.div<{ readed: boolean }>`
-    margin: 10px 0;
-    padding: 24px;
-    &:nth-child(even) {
-        background: ${props => props.theme.colors.bluewhite};
+const Item = styled.div<{ readed: boolean; theme: any }>`
+  margin: 10px 0;
+  padding: 24px;
+  &:nth-child(even) {
+    background: ${props => props.theme.colors.bluewhite};
+  }
+  ${props =>
+    props.readed
+      ? ''
+      : `
+    font-weight: 600;
+    &:before {
+        content: '';
+        display: inline-block;
+        background: ${props.theme.colors.brand};
+        border-radius: 50%;
+        width: 10px;
+        height: 10px;
+        margin-right: 7px;
     }
-    &.unreaded {
-        font-weight: 600;
-        &:before {
-            content: "";
-            display: inline-block;
-            background: ${props => props.theme.colors.brand};
-            border-radius: 50%;
-            width: 10px;
-            height: 10px;
-            margin-right: 7px;
-        }
-    }
-    
+  `}
 `
 
 const Body = styled.span`
-    a {
-        color: ${props => props.theme.colors.brand};
-        text-decoration: none;
-    }
-    a:hover {
-        color: ${props => props.theme.colors.lightblue};
-    }
+  a {
+    color: ${props => props.theme.colors.brand};
+    text-decoration: none;
+  }
+  a:hover {
+    color: ${props => props.theme.colors.lightblue};
+  }
 `
