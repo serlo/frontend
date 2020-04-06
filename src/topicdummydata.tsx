94d5838d669e6ced5563b27c8ba8fdc9638d43a2
David Li <dawei.li@gmx.net>
2020-04-06 13:39:52 +0200

    extracting topic data

---
+++ src/topicdummydata.tsx
@@ -0,0 +1,111 @@
+import { TopicPurposes } from './components/content/Topic'
+
+export const topic = {
+  title: 'Satzgruppe des Pythagoras',
+  url: 'https://de.serlo.org/mathe/geometrie/satzgruppe-pythagoras',
+  description: {
+    img:
+      'https://assets.serlo.org/legacy/56efc1b684b67_0b712ac6f94fcb971ecdc341af21717403d92fb2.png',
+    text:
+      'Hier findest du alle Artikel, Aufgaben, Videos und Kurse zu der Satzgruppe des Pythagoras. Zur Satzgruppe des Pythagoras gehören der Satz des Pythagoras, der Höhen- und der Kathetensatz.'
+  },
+  purpose: TopicPurposes.detail,
+  links: {
+    courses: [
+      {
+        title: 'Überblick zum Satz des Pythagoras',
+        url: 'https://de.serlo.org/18521/formel'
+      }
+    ],
+    articles: [
+      {
+        title: 'Hypotenuse ',
+        url: 'https://de.serlo.org/18521/formel'
+      },
+      {
+        title: 'Kathete ',
+        url: 'https://de.serlo.org/18521/formel'
+      },
+      {
+        title: 'Höhen- und Kathetensatz ',
+        url: 'https://de.serlo.org/18521/formel'
+      },
+      {
+        title: 'Satz des Pythagoras ',
+        url: 'https://de.serlo.org/18521/formel'
+      }
+    ],
+    videos: [
+      {
+        title: 'Hypotenuse ',
+        url: 'https://de.serlo.org/18521/formel'
+      },
+      {
+        title: 'Kathete ',
+        url: 'https://de.serlo.org/18521/formel'
+      }
+    ],
+    applets: [
+      {
+        title: 'Hypotenuse ',
+        url: 'https://de.serlo.org/18521/formel'
+      },
+      {
+        title: 'Kathete ',
+        url: 'https://de.serlo.org/18521/formel'
+      }
+    ],
+    excercises: [
+      {
+        title: 'Hypotenuse ',
+        url: 'https://de.serlo.org/18521/formel'
+      },
+      {
+        title: 'Kathete ',
+        url: 'https://de.serlo.org/18521/formel'
+      }
+    ]
+  }
+}
+
+export const smallTopic = {
+  title: 'Lange ueberschrift fuer den Satzgruppe des Pythagoras',
+  url: 'https://de.serlo.org/mathe/geometrie/satzgruppe-pythagoras',
+  description: {
+    img:
+      'https://assets.serlo.org/legacy/56efc1b684b67_0b712ac6f94fcb971ecdc341af21717403d92fb2.png',
+    text:
+      'Hier findest du alle Artikel, Aufgaben, Videos und Kurse zu der Satzgruppe des Pythagoras. Zur Satzgruppe des Pythagoras gehören der Satz des Pythagoras, der Höhen- und der Kathetensatz.'
+  },
+  purpose: TopicPurposes.overview,
+  links: {
+    subfolders: [
+      {
+        title: 'Hypotenuse ',
+        url: 'https://de.serlo.org/18521/formel'
+      },
+      {
+        title: 'Kathete ',
+        url: 'https://de.serlo.org/18521/formel'
+      },
+      {
+        title: 'Höhen- und Kathetensatz ',
+        url: 'https://de.serlo.org/18521/formel'
+      },
+      {
+        title: 'Satz des Pythagoras ',
+        url: 'https://de.serlo.org/18521/formel'
+      }
+    ],
+    articles: [
+      {
+        title: 'Hypotenuse ',
+        url: 'https://de.serlo.org/18521/formel'
+      },
+      {
+        title: 'Satz des Pythagoras ',
+        url: 'https://de.serlo.org/18521/formel'
+      }
+    ]
+  }
+}
