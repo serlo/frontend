ae7d6d458b78c092df36033221e48b3fe3431c35
Botho <1258870+elbotho@users.noreply.github.com>
2020-04-06 21:27:16 +0200

    proof of concept

--- src/components/navigation/SearchInput.tsx
+++ src/components/navigation/SearchInput.tsx
@@ -7,22 +7,60 @@ import { inputFontReset } from '../../helper/csshelper'
 
 export default function SearchInput() {
   const [focused, setFocused] = React.useState(false)
+  const [searchLoaded, setSearchLoaded] = React.useState(false)
+  const [searchActive, setSearchActive] = React.useState(false)
+
   //const [showSettings, setShowSettings] = React.useState(false)
   const inputRef = React.useRef(null)
   const [value, setValue] = React.useState('')
 
-  function onButtonClick(e) {
-    e.preventDefault()
-
-    if (focused && value.length > 0)
-      window.location.href = '/search?q=' + encodeURIComponent(value)
-
-    inputRef.current.focus()
+  const checkElement = async selector => {
+    while (document.querySelector(selector) === null) {
+      await new Promise(resolve => requestAnimationFrame(resolve))
+    }
+    return document.querySelector(selector)
   }
 
+  // Experiment: "lazy load" scripts and build input on the fly when using the search for the first time
+  // function activateSearch() {
+  //   // setActive(true)
+
+  //   if (!searchLoaded) {
+  //     // var cx = '016022363195733463411:78jhtkzhbhc'
+  //     var cx = '017461339636837994840:ifahsiurxu4' //current serlo search
+  //     var gcse = document.createElement('script')
+  //     gcse.type = 'text/javascript'
+  //     gcse.async = true
+  //     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx
+  //     var s = document.getElementsByTagName('script')[0]
+  //     s.parentNode.insertBefore(gcse, s)
+
+  //     setSearchLoaded(true)
+  //   }
+
+  //   checkElement('#gsc-i-id1').then(input => {
+  //     input.focus()
+  //     input.addEventListener('input', function(e) {
+  //       inputRef.current.value = e.target.value
+  //     })
+  //     setSearchActive(true)
+  //     // setActive(true)
+  //   })
+  // }
+
+  // function onFocus() {
+  //   activateSearch()
+  //   setFocused(true)
+  // }
+
+  function onChange(e) {
+    setValue(e.target.value)
+    const input = document.getElementById('gsc-i-id1') as HTMLInputElement
+    input.value = e.target.value
+  }
   return (
     <>
-      <SearchForm id="searchform" name="searchform">
+      <SearchForm id="searchform">
         {/* <Settings
           onClick={e => {
             e.preventDefault()
@@ -31,32 +69,15 @@ export default function SearchInput() {
         >
           <FontAwesomeIcon icon={faSlidersH} size="lg" />
         </Settings> */}
-
-        <Input
-          type="search"
-          name="searchtext"
-          ref={inputRef}
-          placeholder="Suche"
-          aria-label="Alle Inhalte durchsuchen"
-          value={value}
-          focused={focused}
-          onFocus={() => {
-            setFocused(true)
-          }}
-          onBlur={() => {
-            setFocused(false)
-          }}
-          onChange={e => setValue(e.target.value)}
-        />
-        <Button
-          onMouseDown={e => e.preventDefault()}
-          onClick={onButtonClick}
-          type="submit"
-          focused={focused}
-        >
-          <FontAwesomeIcon icon={faSearch} size="lg" />
-        </Button>
+        <div
+          className="gcse-searchbox"
+          data-autocompletemaxcompletions="5"
+        ></div>
       </SearchForm>
+
+      <SearchResultsWrap>
+        <div className="gcse-searchresults"></div>
+      </SearchResultsWrap>
       {/* {showSettings && (
         <SearchSettings>Hier kommen Sucheinstellungen hin</SearchSettings>
       )} */}
@@ -71,11 +92,101 @@ export default function SearchInput() {
 //   border-top: thin solid ${props => props.theme.colors.brand};
 // `
 
-const SearchForm = styled.form`
+const SearchForm = styled.div`
+  #___gcse_0,
+  & > div {
+    /* display: none; */
+    flex: 1;
+  }
+
+  .gcse-search {
+    display: none;
+  }
+
+  .gsc-input-box {
+    border: 0;
+    background: none;
+  }
+
+  .gsc-control-cse {
+    /* opacity: 1; */
+    background-color: white;
+    border: 0;
+    /* padding-top: 0;
+    padding-left: 0;
+    padding-bottom: 0; */
+    padding: 0;
+    /* position: absolute; */
+    z-index: 20;
+    /* display: none; */
+  }
+
+  input.gsc-input {
+    background: transparent !important;
+
+    &, &::placeholder {
+      font-size: 1rem !important;
+      ${inputFontReset}
+      font-weight: bold;
+      color: ${props => props.theme.colors.brand};
+      font-size: 1rem !important;
+      text-indent: 0 !important;
+    }
+
+    &::placeholder {
+      text-indent: 50px !important;
+    }
+  }
+
+  .gsib_a {
+    padding: 0;
+  }
+
+  form.gsc-search-box,
+  table.gsc-search-box {
+    margin-bottom: 0 !important;
+  }
+/* 
+  .gsc-results-wrapper-overlay {
+    top: 220px;
+  }
+
+  .gsc-modal-background-image {
+    top: 210px;
+    opacity: 1;
+  } */
+
+  .gsc-search-button {
+
+    background-color: ${props => props.theme.colors.brand};
+    transition: background-color 0.2s ease-in;
+
+    color: 'white';
+    height: 40px;
+    min-width: 40px;
+    padding: 0;
+    border: 0;
+    outline: none;
+    cursor: pointer;
+    font-size: 0.8em;
+
+    & > svg {
+      width: 19px;
+    }
+/* 
+  @media (min-width: ${props => props.theme.breakpoints.sm}) {
+    color: white;
+    min-width: 35px;
+    height: 35px;
+    border-radius: 17px;
+  } */
+  }
+
   background-color: ${props => lighten(0.1, props.theme.colors.lighterblue)};
   display: flex;
-  justify-content: center;
+  /* justify-content: center; */
   transition: background-color 0.4s ease;
+  min-height: 38px;
 
   &:focus-within {
     background-color: ${props => lighten(0.1, props.theme.colors.lighterblue)};
@@ -104,79 +215,18 @@ const SearchForm = styled.form`
   }
 `
 
-// const Settings = styled.button`
-//   width: 45px;
-//   height: 40px;
-//   background-color: transparent;
-//   border: none;
-//   color: ${props => lighten(0.5, props.theme.colors.darkgray)};
-//   cursor: pointer;
-// `
-
-const Button = styled.button<{ focused: boolean }>`
-  background-color: ${props =>
-    props.focused
-      ? props.theme.colors.brand
-      : lighten(0.1, props.theme.colors.lighterblue)};
-  transition: background-color 0.2s ease-in;
-
-  color: ${props => (props.focused ? 'white' : props.theme.colors.brand)};
-  height: 40px;
-  min-width: 40px;
-  padding: 0;
-  border: 0;
-  outline: none;
-  cursor: pointer;
-  font-size: 0.8em;
-
-  &:hover {
-    background-color: ${props => props.theme.colors.brand};
-    color: white;
-  }
-
-  @media (min-width: ${props => props.theme.breakpoints.sm}) {
-    color: white;
-    min-width: 35px;
-    height: 35px;
-    border-radius: 17px;
-  }
-`
-const Input = styled.input<{ focused: boolean }>`
-  ${inputFontReset}
-  color: ${props => props.theme.colors.brand};
-  font-weight: bold;
-  
-  padding-left: 50px;
-  padding-right: 7px;
-
-  background-color: transparent;
-
-  flex-grow: 1;
-  flex-shrink: 1;
-  width: 100px;
-
-  border: 0;
-  outline: none;
-
-  cursor: ${props => (props.focused ? 'auto' : 'pointer')};
-
-  &::placeholder {
-    color: ${props => props.theme.colors.brand};
-    opacity: 1;
-  }
-
-  &:focus::placeholder {
-    opacity: 0;
-  }
-
-  @media (min-width: ${props => props.theme.breakpoints.sm}) {
-    margin-left: 0;
-    padding-left: 12px;
-    padding-right: 6px;
-    width: 96px;
-    &::placeholder {
-      color: ${props => props.theme.colors.lightblue};
-      text-align: right;
+const SearchResultsWrap = styled.div`
+  .gsc-control-wrapper-cse {
+    .gsc-results-wrapper-visible::before {
+      content: 'Custom Search';
+      font-weight: bold;
+      display: block;
+      color: ${props => props.theme.colors.brand};
+      width: 100%;
+      padding: 10px 0;
+      background: url('http://www.google.com/cse/static/images/1x/googlelogo_lightgrey_46x16dp.png')
+        left center no-repeat;
+      text-indent: 50px;
     }
   }
 `
