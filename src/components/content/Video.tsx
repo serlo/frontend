f72329e2d1ad9d703235f68302fcd9418154f903
David Li <dawei.li@gmx.net>
2020-04-16 09:24:04 +0200

    implementation of video

--- src/components/content/Video.tsx
+++ src/components/content/Video.tsx
@@ -1,6 +1,78 @@
 import StyledP from '../tags/StyledP'
+import styled from 'styled-components'
+import { makeMargin } from '../../helper/csshelper'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilm } from '@fortawesome/free-solid-svg-icons'
 
 export default function Video(props) {
   const { url } = props
-  return <StyledP>Video an Bord!! {url}</StyledP>
+
+  if (/^(https?:\/\/)?(.*?upload\.wikimedia\.org\/)(.+)/.exec(url)) {
+    // render wikimedia as video tag
+    return (
+      <VideoWrapper>
+        <video controls src={url} />
+      </VideoWrapper>
+    )
+  }
+
+  let iframeUrl
+
+  const yt = /^(https?:\/\/)?(.*?youtube\.com\/watch\?(.*&)?v=|.*?youtu\.be\/)(.+)/.exec(
+    url
+  )
+  if (yt) {
+    iframeUrl = `https://www.youtube-nocookie.com/embed/${yt[4]}?html5=1`
+  }
+
+  const vimeo = /^(https?:\/\/)?(.*?vimeo\.com\/)(.+)/.exec(url)
+  if (vimeo) {
+    iframeUrl = `https://player.vimeo.com/video/${vimeo[3]}`
+  }
+
+  const br = /^(https?:\/\/)?(.*?br\.de\/)(.+)/.exec(url)
+  if (br) {
+    iframeUrl = `https://www.br.de/mediathek/embed/${br[3]}`
+  }
+
+  if (iframeUrl) {
+    return (
+      <VideoWrapper>
+        <iframe src={iframeUrl} allowFullScreen />
+      </VideoWrapper>
+    )
+  }
+
+  return (
+    <VideoPlaceholder>
+      <FontAwesomeIcon icon={faFilm} size="5x" />
+      <StyledP>Es gab ein Problem mit dem Video: {url}</StyledP>
+    </VideoPlaceholder>
+  )
 }
+
+const VideoWrapper = styled.div`
+  ${makeMargin}
+  padding: 0;
+  /* Player ratio: 100 / (1280 / 720) */
+  padding-top: 56.25%;
+  height: 0;
+  overflow: hidden;
+  position: relative;
+  margin-bottom: ${props => props.theme.spacing.mb.block};
+
+  & > video,
+  & > iframe,
+  & > div {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
+`
+
+const VideoPlaceholder = styled.div`
+  text-align: center;
+`
