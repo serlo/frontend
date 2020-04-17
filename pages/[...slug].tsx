98dd3bb7f1c42063e185f62b69eb295f6e53b6e5
Botho <1258870+elbotho@users.noreply.github.com>
2020-04-17 13:37:52 +0200

    map new to old content type meta tags

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -31,12 +31,31 @@ function PageView(props) {
     title
   } = data
 
-  const metaContentType = contentType.toLowerCase()
+  // Artikel -> more:pagemap:metatags-content_type:article
+  // Applets -> more:p:metatags-content_type:applet
+  // Videos -> more:p:metatags-content_type:video
+  // Kurse -> more:p:metatags-content_type:course,course*page
+  // Themenbereiche -> more:p:metatags-content_type:topic,curriculum*topic -more:p:metatags-content_type:topic*folder
+  // Aufgaben -> more:p:metatags-content_type:topic*folder,text*exercise*group,text*exercise,curriculum*topic*folder
+
+  /* TODO:
+    Collection of Exercises as extra contentType if possible
+  */
+
+  function buildMetaContentType() {
+    if (contentType === undefined) return ''
+    if (contentType === 'Exercise') return 'text-exercise'
+    if (contentType === 'CoursePage') return 'course-page'
+    if (contentType === 'TaxonomyTerm') return 'topic'
+    //Article, Video, Applet
+    else return contentType.toLowerCase()
+  }
+
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta name="content_type" content={metaContentType} />
+        <meta name="content_type" content={buildMetaContentType()} />
         <meta property="og:title" content={title} />
       </Head>
       <Header />
