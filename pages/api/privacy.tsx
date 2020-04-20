9876931490f6ce8d4e135e26d8e7e83e5bc644f3
David Li <dawei.li@gmx.net>
2020-04-20 14:19:42 +0200

    basic cookie banner implementation

---
+++ pages/api/privacy.tsx
@@ -0,0 +1,8 @@
+import fetch from 'isomorphic-unfetch'
+
+// need this to bypass CORS and cache respnses
+export default async function privacy(req, res) {
+  const data = await fetch('https://de.serlo.org/privacy/json')
+  const json = await data.json()
+  res.json(json)
+}
