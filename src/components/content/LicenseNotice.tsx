ee928e9a284cebc28fcc33be1e1e5924361b9591
Botho <1258870+elbotho@users.noreply.github.com>
2020-04-24 00:36:17 +0200

    design for LicenseNotice

--- src/components/content/LicenseNotice.tsx
+++ src/components/content/LicenseNotice.tsx
@@ -1,14 +1,68 @@
-import StyledP from '../tags/StyledP'
+import styled from 'styled-components'
 import StyledA from '../tags/StyledA'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCreativeCommons,
+  faCreativeCommonsBy,
+  faCreativeCommonsSa
+} from '@fortawesome/free-brands-svg-icons'
+import { makePadding } from '../../helper/csshelper'
 
 export default function LicenseNotice({ data }) {
+  //only link license
+  let titleParts = data.title.split('CC')
+  const text = titleParts.length === 2 ? titleParts[0] : ''
+  const licenseName =
+    titleParts.length === 2 ? 'CC' + titleParts[1] : data.title
+
   return (
-    <StyledP>
-      <StyledA href={data.url}>{data.title}</StyledA> (
-      <StyledA href={`https://de.serlo.org/license/detail/${data.id}`}>
-        Information
-      </StyledA>
-      )
-    </StyledP>
+    <Wrapper>
+      <FontAwesomeIcon icon={faCreativeCommons} size="2x" />{' '}
+      <FontAwesomeIcon icon={faCreativeCommonsBy} size="2x" />{' '}
+      <FontAwesomeIcon icon={faCreativeCommonsSa} size="2x" />
+      <br />
+      <StyledSmall>
+        {' '}
+        {text}
+        <br />
+        <StyledA href={data.url}>{licenseName}</StyledA>
+        {' → '}
+        <StyledA href={`https://de.serlo.org/license/detail/${data.id}`}>
+          <b>Was bedeutet das?</b>
+        </StyledA>
+      </StyledSmall>
+    </Wrapper>
   )
 }
+
+const StyledSmall = styled.span`
+  @media (min-width: ${props => props.theme.breakpoints.mobile}) {
+    margin-left: 12px;
+  }
+`
+
+const Wrapper = styled.div`
+  ${makePadding}
+  padding-top: 10px;
+  padding-bottom: 10px;
+  font-size: 0.9rem;
+  margin-top: 40px;
+  border-top: 2px solid ${props => props.theme.colors.lightBlueBackground};
+  color: ${props => props.theme.colors.dark1};
+  > svg {
+    color: ${props => props.theme.colors.lighterblue};
+    width: 1.4rem !important;
+    margin-bottom: 1px;
+  }
+
+  @media (min-width: ${props => props.theme.breakpoints.mobile}) {
+    display: flex;
+
+    > svg {
+      width: 2rem !important;
+      height: 2rem !important;
+      margin-top: 1px;
+      margin-right: 2px;
+    }
+  }
+`
