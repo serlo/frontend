e3ed71675006150ea374bfc0f929b8b3e773168b
Botho <1258870+elbotho@users.noreply.github.com>
2020-04-24 13:02:55 +0200

    simplify SpoilerToogle

--- src/components/content/SpoilerToggle.tsx
+++ src/components/content/SpoilerToggle.tsx
@@ -1,18 +1,7 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCaretDown, faCaretRight } from '@fortawesome/free-solid-svg-icons'
-
 interface SpoilerToggleProps {
   open: boolean
 }
 
 export default function SpoilerToggle({ open }: SpoilerToggleProps) {
-  return (
-    <>
-      {open ? (
-        <FontAwesomeIcon icon={faCaretDown} />
-      ) : (
-        <FontAwesomeIcon icon={faCaretRight} />
-      )}{' '}
-    </>
-  )
+  return <>{open ? '▾ ' : '▸ '} </>
 }
