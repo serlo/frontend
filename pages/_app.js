fe6d3727328326f9055ea8e69f19c83d136513df
Jonas Keinholz <git@inyono.dev>
2020-04-27 05:01:51 +0200

    sentry: fix release name

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,7 @@ const { version } = require('../package.json')
 if (process.env.SENTRY_DSN !== undefined) {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
-    release: `serlo-org-client@${version}-${process.env.VERCEL_GITHUB_COMMIT_SHA}`
+    release: `frontend@${version}-${process.env.VERCEL_GITHUB_COMMIT_SHA}`
   })
 }
 
