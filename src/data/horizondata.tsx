3e425902bbab6c87d65905b8c61cfd664409d6fc
Botho <1258870+elbotho@users.noreply.github.com>
2020-05-07 09:43:33 +0200

    hardcode horizon links, replace call to comment

--- src/data/horizondata.tsx
+++ src/data/horizondata.tsx
@@ -5,7 +5,8 @@ export const horizonData = [
     title: 'Öfters hier?',
     text:
       'Wenn serlo.org deine Lieblingslernplattform ist freuen wir uns von dir zu erfahren, wieso! Klicke hier um uns eine Nachricht zu hinterlassen. ',
-    url: '/horizon/37/out'
+    url:
+      'https://docs.google.com/forms/d/e/1FAIpQLSd0UO6KI_Q81zCP7i8qwNiA8QZZtofN7L1DWr1TqZ0gqC_l7g/viewform'
   },
   {
     imageUrl:
@@ -13,14 +14,22 @@ export const horizonData = [
     title: 'Mitmachen bei Serlo',
     text:
       'Hinter serlo.org stehen viele engagierte Menschen, die Bildung besser und gerechter machen wollen. Auch du kannst mitmachen! Klicke hier, um zu erfahren, wie du Teil der Serlo Community werden kannst.',
-    url: '/horizon/24/out'
+    url: '/mitmachen'
   },
   {
     imageUrl:
-      'https://assets.serlo.org/5de528a9d79cd_7bbd720d6d5702451bd1c4ed760a603151035912.jpg',
-    title: 'Gib uns Feedback!',
+      'https://assets.serlo.org/5de528cc0563b_99194d51f618c80abf1f51667223e6647449f21e.jpg',
+    title: 'Was heißt eigentlich “Serlo”?',
     text:
-      'Mit der Kommentar-Funktion kannst du uns zu jedem Inhalt sagen was dir gefällt - und was besser sein könnte. Du kannst auch Fragen stellen, wenn etwas unklar ist. Wir freuen uns über deinen Input! ',
-    url: '/horizon/23/out'
+      'Wusstest du schon, dass serlo.org nach einem Kloster in Nepal benannt ist? Dort hatte der Gründer von serlo.org die Idee für eine freie Lernplattform. Klick hier, um mehr über unsere Geschichte zu erfahren!',
+    url: '/serlo-geschichte'
   }
+  // {
+  //   imageUrl:
+  //     'https://assets.serlo.org/5de528a9d79cd_7bbd720d6d5702451bd1c4ed760a603151035912.jpg',
+  //   title: 'Gib uns Feedback!',
+  //   text:
+  //     'Mit der Kommentar-Funktion kannst du uns zu jedem Inhalt sagen was dir gefällt - und was besser sein könnte. Du kannst auch Fragen stellen, wenn etwas unklar ist. Wir freuen uns über deinen Input! ',
+  //   url: '/horizon/23/out'
+  // }
 ]
