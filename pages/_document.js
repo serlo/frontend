a98f73807b369a5e3c79ba85f852e929409de99b
Botho <1258870+elbotho@users.noreply.github.com>
2020-05-19 23:47:08 +0200

    add favicons sizes etc

--- pages/_document.js
+++ pages/_document.js
@@ -50,6 +50,30 @@ export default class MyDocument extends Document {
         <Head>
           <meta property="og:site_name" content="Serlo" />
           <meta property="og:type" content="website" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0b7ec2" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#0b7ec2" />
+          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta name="theme-color" content="#ff0000"></meta>
         </Head>
         <body style={bodyStyles}>
           <Main />
