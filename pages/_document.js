d30b7abe36e7a634ab943177b1a882149f14ed75
Botho <1258870+elbotho@users.noreply.github.com>
2020-05-27 20:05:08 +0200

    move icons and files to _assets

--- pages/_document.js
+++ pages/_document.js
@@ -53,28 +53,35 @@ export default class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="/_assets/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="/_assets/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/_assets/favicon-16x16.png"
           />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0b7ec2" />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="manifest" href="/_assets/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/_assets/safari-pinned-tab.svg"
+            color="#0b7ec2"
+          />
+          <link rel="shortcut icon" href="/_assets/favicon.ico" />
           <meta name="msapplication-TileColor" content="#0b7ec2" />
-          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta
+            name="msapplication-config"
+            content="/_assets/browserconfig.xml"
+          />
           <meta name="theme-color" content="#ff0000"></meta>
           <link
-            href="/opensearch.de.xml"
+            href="/_assets/opensearch.de.xml"
             rel="search"
             type="application/opensearchdescription+xml"
             title="Serlo (de)"
