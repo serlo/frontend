5b6284aec956a80ec6ef639a7c9e88cca4e81c4e
Jonas Keinholz <git@inyono.dev>
2020-05-27 13:14:35 +0200

    change ESLint configuration to JSON in pages/ so that NextJS doesn't fail

--- src/pages/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-commonjs */
-module.exports = {
-  rules: {
-    // eslint-plugin-import
-    'import/prefer-default-export': 'error',
-    'import/no-default-export': 'off',
-  },
-}
