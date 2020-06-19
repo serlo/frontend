276d936495d24a46f3d0d06b19217a820ff85f53
Botho <1258870+elbotho@users.noreply.github.com>
2020-06-19 14:54:29 +0200

    type article-renderer.tsx

--- src/components/content/math.tsx
+++ src/components/content/math.tsx
@@ -6,6 +6,7 @@ import KaTeXSpan from '../../../external/katexstyles'
 export interface MathProps {
   formula: string
   inline?: boolean
+  alignLeft?: boolean
 }
 
 export function Math(props: MathProps) {
