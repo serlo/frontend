c28cc3739148ac4e4ec9d1b19062f192030ec130
Jonas Keinholz <git@inyono.dev>
2020-07-02 09:50:24 +0200

    debug staging

--- src/pages/api/auth/callback.ts
+++ src/pages/api/auth/callback.ts
@@ -2,17 +2,12 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 import { getAuthorizationCode, getClientCredentials } from '@/auth/oauth2'
 
-export default async function callback(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+async function callback(req: NextApiRequest, res: NextApiResponse) {
   const oauth2ClientCredentials = getClientCredentials()
   const oauth2AuthorizationCode = getAuthorizationCode()
 
   if (oauth2ClientCredentials === null || oauth2AuthorizationCode === null) {
-    res.status(500)
-    res.send('Auth not configured correctly')
-    return
+    return fail()
   }
 
   const { code, scope, state } = req.query as {
@@ -20,24 +15,45 @@ export default async function callback(
     scope: string
     state: string
   }
-  const { csrf, referer } = JSON.parse(state)
-  if (csrf === req.cookies['auth-csrf']) {
-    const { origin } = new URL(referer)
-    const result = await oauth2AuthorizationCode.getToken({
-      code,
-      redirect_uri: `${origin}/api/auth/callback`,
-      scope,
+
+  try {
+    const { csrf, referer } = JSON.parse(state)
+    if (csrf === req.cookies['auth-csrf']) {
+      const { origin } = new URL(referer)
+      const result = await oauth2AuthorizationCode.getToken({
+        code,
+        redirect_uri: `${origin}/api/auth/callback`,
+        scope,
+      })
+      try {
+        const { token } = oauth2ClientCredentials.createToken(result)
+        res.setHeader(
+          'Set-Cookie',
+          `auth-token=${JSON.stringify(
+            token.token
+          )}; Path=/; SameSite=Lax; Max-Age=2592000;`
+        )
+      } catch (e) {
+        console.log(e)
+        console.log('Failed to create token from result', result)
+        return fail()
+      }
+    }
+    res.writeHead(302, {
+      Location: `${referer ?? '/'}#auth`,
     })
-    const { token } = oauth2ClientCredentials.createToken(result)
-    res.setHeader(
-      'Set-Cookie',
-      `auth-token=${JSON.stringify(
-        token.token
-      )}; Path=/; SameSite=Lax; Max-Age=2592000;`
-    )
+    res.end()
+  } catch (e) {
+    console.log(e)
+    console.log('Failed to parse state', state)
+    console.log('Query', req.query)
+    return fail()
   }
-  res.writeHead(302, {
-    Location: `${referer ?? '/'}#auth`,
-  })
-  res.end()
+
+  function fail() {
+    res.status(500)
+    res.send('Auth not configured correctly')
+  }
 }
+
+export default callback
