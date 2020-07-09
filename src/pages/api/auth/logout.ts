6606a4ab5d3058beb7f066c5e9b0de54f09638dc
Jonas Keinholz <git@inyono.dev>
2020-07-09 06:11:33 +0200

    auth: add logout workflow

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,39 +1,56 @@
-import cookie from 'cookie'
+import { randomBytes } from 'crypto'
 import { NextApiRequest, NextApiResponse } from 'next'
+import util from 'util'
 
-import { getClientCredentials } from '@/auth/oauth2'
+import { getClientCredentials, getLogoutUrl } from '@/auth/oauth2'
 
-export default async function logout(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+const generateSecret = util.promisify(randomBytes)
+
+async function logout(req: NextApiRequest, res: NextApiResponse) {
   const clientCredentials = getClientCredentials()
   if (clientCredentials === null) {
     return fail('Auth not configured correctly')
   }
 
-  const cookieHeaderValue = req.headers.cookie
-  if (cookieHeaderValue === undefined) return fail('Missing cookie header')
-
   const referer = req.headers.referer
   if (referer === undefined) return fail('Missing referer header')
 
-  const cookies = cookie.parse(cookieHeaderValue)
+  let idToken: string
 
   try {
-    const token = JSON.parse(cookies['auth-token'])
-    const accessToken = clientCredentials.createToken(token)
-    await accessToken.revokeAll()
+    const token = JSON.parse(req.cookies['auth-token']) as { id_token: string }
+    idToken = token['id_token']
   } catch (e) {
-    // Token does not exist or already revoked
+    // Token does not exist or user already logged out
+    res.setHeader(
+      'Set-Cookie',
+      `auth-token=; Path=/; Expires=${new Date(0).toUTCString()};`
+    )
+    res.writeHead(302, {
+      Location: `${referer ?? '/'}#auth`,
+    })
+    res.end()
+    return
   }
 
-  res.setHeader(
-    'Set-Cookie',
-    `auth-token=; Path=/; Expires=${new Date(0).toUTCString()};`
-  )
+  const csrf = await generateCsrf()
+  const state = JSON.stringify({
+    csrf,
+    referer: `${referer ?? '/'}#auth`,
+  })
+  const { origin } = new URL(referer)
+
+  const logoutUrl = getLogoutUrl({
+    idToken,
+    state,
+    callback: `${origin}/api/auth/logout-callback`,
+  })
+
+  if (logoutUrl === null) return fail('Auth not configured correctly')
+
+  res.setHeader('Set-Cookie', `auth-csrf=${csrf}; Path=/; SameSite=Lax;`)
   res.writeHead(302, {
-    Location: `${referer ?? '/'}#auth`,
+    Location: logoutUrl,
   })
   res.end()
 
@@ -42,3 +59,10 @@ export default async function logout(
     res.send(message)
   }
 }
+
+async function generateCsrf(): Promise<string> {
+  const buffer = await generateSecret(32)
+  return buffer.toString('hex')
+}
+
+export default logout
