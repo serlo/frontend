8db0aa2b546f0ea1e4d4acaa5474947da7e440e7
Botho <1258870+elbotho@users.noreply.github.com>
2020-08-03 21:10:22 +0200

    New translations index.ts (German)

--- src/data/de/index.ts
+++ src/data/de/index.ts
@@ -70,7 +70,7 @@ export const instanceData: InstanceData = {
   }
 };
 export const instanceLandingData: InstanceLandingData = {
-  lang: 'en',
+  lang: "de",
   strings: {
     vision: 'It is our vision to enable personalized learning and provide high quality educational resources worldwide – completely free of charge. Serlo is a grassroots organization inspired by Wikipedia. We already provide thousands of articles, videos and solved exercises for five million German students every year. Now it’s time to go international.',
     learnMore: 'Learn more',
@@ -80,7 +80,7 @@ export const instanceLandingData: InstanceLandingData = {
     openlyLicensed: 'openly licensed',
     adFree: 'ad-free',
     freeOfCharge: 'free of charge',
-    wikiTitle: 'Serlo is the Wikipedia for Learning',
+    wikiTitle: "Serlo.org ist die Wikipedia fürs Lernen",
     wikiText: 'Just like Wikipedia, this platform is created by an engaged community of authors. Serlo Education is run and owned by decentralized teams of volunteers and professionals all over the world.',
     movementTitle: 'Become a Part of Our Movement for Open Education',
     callForAuthors: 'We are looking for teachers and enthusiastic educators who are passionate about their subject. Become part of our community to create new learning material and help us improve existing content.',
