720ddb303067f9b7f2ca3f455f9cd07e53441f1f
Botho <1258870+elbotho@users.noreply.github.com>
2020-08-03 17:26:44 +0200

    New translations index.ts (English)

--- src/data/en/index.ts
+++ src/data/en/index.ts
@@ -1,11 +1,5 @@
-import { headerData, footerData } from './menu-data'
-import {
-  InstanceData,
-  ServerSideStrings,
-  LoggedInData,
-  InstanceLandingData,
-} from '@/data-types'
-
+import { headerData, footerData } from './menu-data';
+import { InstanceData, ServerSideStrings, LoggedInData, InstanceLandingData } from '@/data-types';
 export const instanceData: InstanceData = {
   lang: 'en',
   headerData: headerData,
@@ -14,16 +8,15 @@ export const instanceData: InstanceData = {
     header: {
       slogan: 'The Open Learning Platform',
       search: 'Search',
-      login: 'Login',
+      login: 'Login'
     },
     footer: {
       summaryHeading: 'Serlo.org is the Wikipedia for learning.',
-      summaryText:
-        'We are a community of visionaries working tirelessly to make great education freely available to everyone.',
+      summaryText: 'We are a community of visionaries working tirelessly to make great education freely available to everyone.',
       learnMore: 'Learn more',
       participate: 'Join the cause',
       donate: 'Donate',
-      toTop: 'To Top',
+      toTop: 'To Top'
     },
     categories: {
       article: 'Article',
@@ -31,28 +24,28 @@ export const instanceData: InstanceData = {
       video: 'Video',
       applet: 'Applet',
       folder: 'Folder',
-      exercises: 'Exercises',
+      exercises: 'Exercises'
     },
     share: {
       button: 'Share',
       title: 'Share!',
       copyLink: 'Copy link',
       copySuccess: 'Link copied! ',
-      close: 'Close',
+      close: 'Close'
     },
     edit: {
-      button: 'Edit',
+      button: 'Edit'
     },
     license: {
-      readMore: 'Info',
+      readMore: 'Info'
     },
     course: {
       showPages: 'Show course overview',
       pages: 'Course overview',
-      next: 'Next',
+      next: 'Next'
     },
     taxonomy: {
-      topicFolder: 'Exercise folder',
+      topicFolder: 'Exercise folder'
     },
     content: {
       show: 'show',
@@ -64,7 +57,7 @@ export const instanceData: InstanceData = {
       wrong: 'Wrong',
       check: 'Check',
       yourAnswer: 'Your answer…',
-      chooseOption: 'Click on one of the options.',
+      chooseOption: 'Click on one of the options.'
     },
     cookie: {
       part1: 'By using this website you declare that you agree with our',
@@ -72,16 +65,14 @@ export const instanceData: InstanceData = {
       part3: '.',
       link1: 'Privacy Policy',
       link2: 'Terms of use',
-      button: 'Agree',
-    },
-  },
-}
-
+      button: 'Agree'
+    }
+  }
+};
 export const instanceLandingData: InstanceLandingData = {
   lang: 'en',
   strings: {
-    vision:
-      'It is our vision to enable personalized learning and provide high quality educational resources worldwide – completely free of charge. Serlo is a grassroots organization inspired by Wikipedia. We already provide thousands of articles, videos and solved exercises for five million German students every year. Now it’s time to go international.',
+    vision: 'It is our vision to enable personalized learning and provide high quality educational resources worldwide – completely free of charge. Serlo is a grassroots organization inspired by Wikipedia. We already provide thousands of articles, videos and solved exercises for five million German students every year. Now it’s time to go international.',
     learnMore: 'Learn more',
     democraticallyStructured: 'democratically structured',
     nonProfit: 'non-profit',
@@ -90,55 +81,44 @@ export const instanceLandingData: InstanceLandingData = {
     adFree: 'ad-free',
     freeOfCharge: 'free of charge',
     wikiTitle: 'Serlo is the Wikipedia for Learning',
-    wikiText:
-      'Just like Wikipedia, this platform is created by an engaged community of authors. Serlo Education is run and owned by decentralized teams of volunteers and professionals all over the world.',
+    wikiText: 'Just like Wikipedia, this platform is created by an engaged community of authors. Serlo Education is run and owned by decentralized teams of volunteers and professionals all over the world.',
     movementTitle: 'Become a Part of Our Movement for Open Education',
-    callForAuthors:
-      'We are looking for teachers and enthusiastic educators who are passionate about their subject. Become part of our community to create new learning material and help us improve existing content.',
+    callForAuthors: 'We are looking for teachers and enthusiastic educators who are passionate about their subject. Become part of our community to create new learning material and help us improve existing content.',
     communityLink: 'Visit the landing page for authors',
-    callForOther:
-      'We offer a diverse range of jobs and volunteering opportunities in the fields of software development, design, translation, communications, project management and more.',
-    getInvolved: 'Get involved!',
-  },
-}
-
+    callForOther: 'We offer a diverse range of jobs and volunteering opportunities in the fields of software development, design, translation, communications, project management and more.',
+    getInvolved: 'Get involved!'
+  }
+};
 export const serverSideStrings: ServerSideStrings = {
-  title: 'learn with Serlo!',
-}
-
+  title: 'learn with Serlo!'
+};
 export const loggedInData: LoggedInData = {
-  authMenu: [
-    {
-      url: '/user/notifications',
-      title: 'Benachrichtigungen',
-      icon: 'notifications',
-    },
-    {
-      url: '',
-      title: 'User',
-      icon: 'user',
-      children: [
-        { url: '/user/public', title: 'Public profile' },
-        {
-          url: '/user/settings',
-          title: 'Edit profile',
-        },
-        {
-          url: '/auth/password/change',
-          title: 'Change password',
-        },
-        {
-          url: '/event/history/user/me',
-          title: 'Recent activities',
-        },
-        {
-          url: '/api/auth/logout',
-          title: 'Log out',
-        },
-      ],
-    },
-  ],
+  authMenu: [{
+    url: '/user/notifications',
+    title: 'Benachrichtigungen',
+    icon: 'notifications'
+  }, {
+    url: '',
+    title: 'User',
+    icon: 'user',
+    children: [{
+      url: '/user/public',
+      title: 'Public profile'
+    }, {
+      url: '/user/settings',
+      title: 'Edit profile'
+    }, {
+      url: '/auth/password/change',
+      title: 'Change password'
+    }, {
+      url: '/event/history/user/me',
+      title: 'Recent activities'
+    }, {
+      url: '/api/auth/logout',
+      title: 'Log out'
+    }]
+  }],
   strings: {
-    tools: 'Other Tools',
-  },
-}
+    tools: 'Other Tools'
+  }
+};
