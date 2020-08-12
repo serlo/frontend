57994cd88740153b789d67246891563e25afbdb3
David Li <dawei.li@gmx.net>
2020-08-12 09:56:59 +0200

    dynamic load

--- src/components/content/author-tools.tsx
+++ src/components/content/author-tools.tsx
@@ -1,17 +1,24 @@
 import { faTools } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Tippy from '@tippyjs/react'
+import dynamic from 'next/dynamic'
 import styled from 'styled-components'
 
 import {
-  AuthorToolsHoverMenu,
   AuthorToolsData,
+  AuthorToolsHoverMenuProps,
 } from '../navigation/author-tools-hover-menu'
 import { makeDefaultButton } from '@/helper/css'
 
 export interface AuthorToolsProps {
   data: AuthorToolsData
 }
+
+const AuthorToolsHoverMenu = dynamic<AuthorToolsHoverMenuProps>(() =>
+  import('../navigation/author-tools-hover-menu').then(
+    (mod) => mod.AuthorToolsHoverMenu
+  )
+)
 
 export function AuthorTools({ data }: AuthorToolsProps) {
   return (
