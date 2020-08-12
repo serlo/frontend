1d76006d7933420cd70bf0c5f908fce176784b18
David Li <dawei.li@gmx.net>
2020-08-12 09:42:53 +0200

    add functions

--- src/components/navigation/author-tools-hover-menu.tsx
+++ src/components/navigation/author-tools-hover-menu.tsx
@@ -19,54 +19,10 @@ export function AuthorToolsHoverMenu({ data }: AuthorToolsHoverMenuProps) {
   if (data.type == 'Page') {
     return (
       <HoverDiv>
-        <Tippy
-          interactive
-          placement="left-end"
-          content={
-            <HoverDiv>
-              <Li>
-                <SubLink href={`/subscribe/${data.id}/0`} noCSR>
-                  <SubButtonStyle>Benachrichtigungen erhalten</SubButtonStyle>
-                </SubLink>
-              </Li>
-
-              <Li>
-                <SubLink href={`/subscribe/${data.id}/1`} noCSR>
-                  <SubButtonStyle>
-                    Benachrichtigungen und E-Mail erhalten
-                  </SubButtonStyle>
-                </SubLink>
-              </Li>
-            </HoverDiv>
-          }
-        >
-          <Li>
-            <SubLink as="div">
-              <SubButtonStyle>Abonnieren</SubButtonStyle>
-            </SubLink>
-          </Li>
-        </Tippy>
-
-        <Li>
-          <SubLink
-            href={`/page/revision/create/${data.id}/${data.revisionId}`}
-            noCSR
-          >
-            <SubButtonStyle>Konvertieren (beta)</SubButtonStyle>
-          </SubLink>
-        </Li>
-
-        <Li>
-          <SubLink href={`/page/revision/revisions/${data.id}`} noCSR>
-            <SubButtonStyle>Bearbeitungsverlauf</SubButtonStyle>
-          </SubLink>
-        </Li>
-
-        <Li>
-          <SubLink href={`/event/history/${data.id}`} noCSR>
-            <SubButtonStyle>Aktivitätenlog</SubButtonStyle>
-          </SubLink>
-        </Li>
+        {abo()}
+        {convert()}
+        {history()}
+        {log()}
       </HoverDiv>
     )
   }
@@ -79,65 +35,12 @@ export function AuthorToolsHoverMenu({ data }: AuthorToolsHoverMenuProps) {
   ) {
     return (
       <HoverDiv>
-        <Tippy
-          interactive
-          placement="left-end"
-          content={
-            <HoverDiv>
-              <Li>
-                <SubLink href={`/subscribe/${data.id}/0`} noCSR>
-                  <SubButtonStyle>Benachrichtigungen erhalten</SubButtonStyle>
-                </SubLink>
-              </Li>
-
-              <Li>
-                <SubLink href={`/subscribe/${data.id}/1`} noCSR>
-                  <SubButtonStyle>
-                    Benachrichtigungen und E-Mail erhalten
-                  </SubButtonStyle>
-                </SubLink>
-              </Li>
-            </HoverDiv>
-          }
-        >
-          <Li>
-            <SubLink as="div">
-              <SubButtonStyle>Abonnieren</SubButtonStyle>
-            </SubLink>
-          </Li>
-        </Tippy>
-
-        <Li>
-          <SubLink href={`/page/revision/revisions/${data.id}`} noCSR>
-            <SubButtonStyle>Bearbeitungsverlauf</SubButtonStyle>
-          </SubLink>
-        </Li>
-
-        <Li>
-          <SubLink href={`/entity/taxonomy/update/${data.id}`} noCSR>
-            <SubButtonStyle>
-              Zuweisung zu Themen und Lehrplänen bearbeiten
-            </SubButtonStyle>
-          </SubLink>
-        </Li>
-
-        <Li>
-          <SubLink href={`/flag/add/${data.id}`} noCSR>
-            <SubButtonStyle>Inhalt melden</SubButtonStyle>
-          </SubLink>
-        </Li>
-
-        <Li>
-          <SubLink href={`/event/history/${data.id}`} noCSR>
-            <SubButtonStyle>Aktivitätenlog</SubButtonStyle>
-          </SubLink>
-        </Li>
-
-        <Li>
-          <SubLink href={`/uuid/trash/${data.id}`} noCSR>
-            <SubButtonStyle>In den Papierkorb verschieben (?)</SubButtonStyle>
-          </SubLink>
-        </Li>
+        {abo()}
+        {history()}
+        {curriculum()}
+        {flag()}
+        {log()}
+        {trash()}
       </HoverDiv>
     )
   }
@@ -150,41 +53,8 @@ export function AuthorToolsHoverMenu({ data }: AuthorToolsHoverMenuProps) {
           placement="left-end"
           content={
             <HoverDiv>
-              <Tippy
-                interactive
-                placement="left-end"
-                content={
-                  <HoverDiv>
-                    <Li>
-                      <SubLink href={`/subscribe/${data.id}/0`} noCSR>
-                        <SubButtonStyle>
-                          Benachrichtigungen erhalten
-                        </SubButtonStyle>
-                      </SubLink>
-                    </Li>
-
-                    <Li>
-                      <SubLink href={`/subscribe/${data.id}/1`} noCSR>
-                        <SubButtonStyle>
-                          Benachrichtigungen und E-Mail erhalten
-                        </SubButtonStyle>
-                      </SubLink>
-                    </Li>
-                  </HoverDiv>
-                }
-              >
-                <Li>
-                  <SubLink as="div">
-                    <SubButtonStyle>Abonnieren</SubButtonStyle>
-                  </SubLink>
-                </Li>
-              </Tippy>
-
-              <Li>
-                <SubLink href={`/page/revision/revisions/${data.id}`} noCSR>
-                  <SubButtonStyle>Bearbeitungsverlauf</SubButtonStyle>
-                </SubLink>
-              </Li>
+              {abo()}
+              {history()}
 
               <Li>
                 <SubLink
@@ -198,25 +68,9 @@ export function AuthorToolsHoverMenu({ data }: AuthorToolsHoverMenuProps) {
                 </SubLink>
               </Li>
 
-              <Li>
-                <SubLink href={`/flag/add/${data.id}`} noCSR>
-                  <SubButtonStyle>Inhalt melden</SubButtonStyle>
-                </SubLink>
-              </Li>
-
-              <Li>
-                <SubLink href={`/event/history/${data.id}`} noCSR>
-                  <SubButtonStyle>Aktivitätenlog</SubButtonStyle>
-                </SubLink>
-              </Li>
-
-              <Li>
-                <SubLink href={`/uuid/trash/${data.id}`} noCSR>
-                  <SubButtonStyle>
-                    In den Papierkorb verschieben (?)
-                  </SubButtonStyle>
-                </SubLink>
-              </Li>
+              {flag()}
+              {log()}
+              {trash()}
             </HoverDiv>
           }
         >
@@ -231,93 +85,23 @@ export function AuthorToolsHoverMenu({ data }: AuthorToolsHoverMenuProps) {
           placement="left-end"
           content={
             <HoverDiv>
-              <Tippy
-                interactive
-                placement="left-end"
-                content={
-                  <HoverDiv>
-                    <Li>
-                      <SubLink href={`/subscribe/${data.courseId}/0`} noCSR>
-                        <SubButtonStyle>
-                          Benachrichtigungen erhalten
-                        </SubButtonStyle>
-                      </SubLink>
-                    </Li>
-
-                    <Li>
-                      <SubLink href={`/subscribe/${data.courseId}/1`} noCSR>
-                        <SubButtonStyle>
-                          Benachrichtigungen und E-Mail erhalten
-                        </SubButtonStyle>
-                      </SubLink>
-                    </Li>
-                  </HoverDiv>
-                }
-              >
-                <Li>
-                  <SubLink as="div">
-                    <SubButtonStyle>Abonnieren</SubButtonStyle>
-                  </SubLink>
-                </Li>
-              </Tippy>
-
-              <Li>
-                <SubLink
-                  href={`/page/revision/revisions/${data.courseId}`}
-                  noCSR
-                >
-                  <SubButtonStyle>Bearbeitungsverlauf</SubButtonStyle>
-                </SubLink>
-              </Li>
+              {abo(data.courseId)}
+              {history(data.courseId)}
 
               <Li>
                 <SubLink
                   href={`/entity/create/course-page?link%5Btype%5D=link&link%5Bchild%5D=${data.courseId}`}
                   noCSR
                 >
-                  <SubButtonStyle>course-page hinzufügen</SubButtonStyle>
+                  <SubButtonStyle>Kursseite hinzufügen</SubButtonStyle>
                 </SubLink>
               </Li>
 
-              <Li>
-                <SubLink
-                  href={`/entity/link/order/${data.courseId}/link`}
-                  noCSR
-                >
-                  <SubButtonStyle>Unterelemente sortieren</SubButtonStyle>
-                </SubLink>
-              </Li>
-
-              <Li>
-                <SubLink
-                  href={`/entity/taxonomy/update/${data.courseId}`}
-                  noCSR
-                >
-                  <SubButtonStyle>
-                    Zuweisung zu Themen und Lehrplänen bearbeiten
-                  </SubButtonStyle>
-                </SubLink>
-              </Li>
-
-              <Li>
-                <SubLink href={`/flag/add/${data.id}`} noCSR>
-                  <SubButtonStyle>Inhalt melden</SubButtonStyle>
-                </SubLink>
-              </Li>
-
-              <Li>
-                <SubLink href={`/event/history/${data.id}`} noCSR>
-                  <SubButtonStyle>Aktivitätenlog</SubButtonStyle>
-                </SubLink>
-              </Li>
-
-              <Li>
-                <SubLink href={`/uuid/trash/${data.id}`} noCSR>
-                  <SubButtonStyle>
-                    In den Papierkorb verschieben (?)
-                  </SubButtonStyle>
-                </SubLink>
-              </Li>
+              {sort(data.courseId)}
+              {curriculum(data.courseId)}
+              {flag(data.courseId)}
+              {log(data.courseId)}
+              {trash(data.courseId)}
             </HoverDiv>
           }
         >
@@ -334,33 +118,7 @@ export function AuthorToolsHoverMenu({ data }: AuthorToolsHoverMenuProps) {
   if (data.type == 'Taxonomy') {
     return (
       <HoverDiv>
-        <Tippy
-          interactive
-          placement="left-end"
-          content={
-            <HoverDiv>
-              <Li>
-                <SubLink href={`/subscribe/${data.id}/0`} noCSR>
-                  <SubButtonStyle>Benachrichtigungen erhalten</SubButtonStyle>
-                </SubLink>
-              </Li>
-
-              <Li>
-                <SubLink href={`/subscribe/${data.id}/1`} noCSR>
-                  <SubButtonStyle>
-                    Benachrichtigungen und E-Mail erhalten
-                  </SubButtonStyle>
-                </SubLink>
-              </Li>
-            </HoverDiv>
-          }
-        >
-          <Li>
-            <SubLink as="div">
-              <SubButtonStyle>Abonnieren</SubButtonStyle>
-            </SubLink>
-          </Li>
-        </Tippy>
+        {abo()}
 
         <Li>
           <SubLink href={`/taxonomy/term/organize/${data.id}`} noCSR>
@@ -368,17 +126,8 @@ export function AuthorToolsHoverMenu({ data }: AuthorToolsHoverMenuProps) {
           </SubLink>
         </Li>
 
-        <Li>
-          <SubLink href={`/event/history/${data.id}`} noCSR>
-            <SubButtonStyle>Aktivitätenlog</SubButtonStyle>
-          </SubLink>
-        </Li>
-
-        <Li>
-          <SubLink href={`/taxonomy/term/sort/entities/${data.id}`} noCSR>
-            <SubButtonStyle>Inhalt sortieren</SubButtonStyle>
-          </SubLink>
-        </Li>
+        {log()}
+        {sort()}
 
         <Li>
           <SubLink href={`/taxonomy/term/copy/batch/${data.id}`} noCSR>
@@ -402,45 +151,10 @@ export function AuthorToolsHoverMenu({ data }: AuthorToolsHoverMenuProps) {
   ) {
     return (
       <HoverDiv>
-        <Li>
-          <SubLink href={`/entity/repository/add-revision/${data.id}`} noCSR>
-            <SubButtonStyle>Bearbeiten</SubButtonStyle>
-          </SubLink>
-        </Li>
+        {edit()}
+        {abo()}
 
-        <Tippy
-          interactive
-          placement="left-end"
-          content={
-            <HoverDiv>
-              <Li>
-                <SubLink href={`/subscribe/${data.id}/0`} noCSR>
-                  <SubButtonStyle>Benachrichtigungen erhalten</SubButtonStyle>
-                </SubLink>
-              </Li>
-
-              <Li>
-                <SubLink href={`/subscribe/${data.id}/1`} noCSR>
-                  <SubButtonStyle>
-                    Benachrichtigungen und E-Mail erhalten
-                  </SubButtonStyle>
-                </SubLink>
-              </Li>
-            </HoverDiv>
-          }
-        >
-          <Li>
-            <SubLink as="div">
-              <SubButtonStyle>Abonnieren</SubButtonStyle>
-            </SubLink>
-          </Li>
-        </Tippy>
-
-        <Li>
-          <SubLink href={`/page/revision/revisions/${data.id}`} noCSR>
-            <SubButtonStyle>Bearbeitungsverlauf</SubButtonStyle>
-          </SubLink>
-        </Li>
+        {history()}
 
         {data.type == '_ExerciseGroupInline' && (
           <Li>
@@ -453,13 +167,7 @@ export function AuthorToolsHoverMenu({ data }: AuthorToolsHoverMenuProps) {
           </Li>
         )}
 
-        {data.type != '_SolutionInline' && (
-          <Li>
-            <SubLink href={`/entity/link/order/${data.id}/link`} noCSR>
-              <SubButtonStyle>Unterelemente sortieren</SubButtonStyle>
-            </SubLink>
-          </Li>
-        )}
+        {data.type != '_SolutionInline' && sort()}
 
         {data.type == '_SolutionInline' ? (
           <Li>
@@ -473,33 +181,129 @@ export function AuthorToolsHoverMenu({ data }: AuthorToolsHoverMenuProps) {
             </SubLink>
           </Li>
         ) : (
-          <Li>
-            <SubLink href={`/entity/taxonomy/update/${data.id}`} noCSR>
-              <SubButtonStyle>
-                Zuweisung zu Themen und Lehrplänen bearbeiten
-              </SubButtonStyle>
-            </SubLink>
-          </Li>
+          curriculum()
         )}
 
-        <Li>
-          <SubLink href={`/flag/add/${data.id}`} noCSR>
-            <SubButtonStyle>Inhalt melden</SubButtonStyle>
-          </SubLink>
-        </Li>
-
-        <Li>
-          <SubLink href={`/event/history/${data.id}`} noCSR>
-            <SubButtonStyle>Aktivitätenlog</SubButtonStyle>
-          </SubLink>
-        </Li>
-
-        <Li>
-          <SubLink href={`/uuid/trash/${data.id}`} noCSR>
-            <SubButtonStyle>In den Papierkorb verschieben (?)</SubButtonStyle>
-          </SubLink>
-        </Li>
+        {flag()}
+        {log()}
+        {trash()}
       </HoverDiv>
+    )
+  }
+
+  function abo(id = data.id) {
+    // todo: check if entity is already subscribed
+    return (
+      <Tippy
+        interactive
+        placement="left-end"
+        content={
+          <HoverDiv>
+            <Li>
+              <SubLink href={`/subscribe/${id}/0`} noCSR>
+                <SubButtonStyle>Benachrichtigungen erhalten</SubButtonStyle>
+              </SubLink>
+            </Li>
+
+            <Li>
+              <SubLink href={`/subscribe/${id}/1`} noCSR>
+                <SubButtonStyle>
+                  Benachrichtigungen und E-Mail erhalten
+                </SubButtonStyle>
+              </SubLink>
+            </Li>
+          </HoverDiv>
+        }
+      >
+        <Li>
+          <SubLink as="div">
+            <SubButtonStyle>Abonnieren</SubButtonStyle>
+          </SubLink>
+        </Li>
+      </Tippy>
+    )
+  }
+
+  function convert(id = data.id, rev = data.revisionId) {
+    return (
+      <Li>
+        <SubLink href={`/page/revision/create/${id}/${rev}`} noCSR>
+          <SubButtonStyle>Konvertieren (beta)</SubButtonStyle>
+        </SubLink>
+      </Li>
+    )
+  }
+
+  function history(id = data.id) {
+    return (
+      <Li>
+        <SubLink href={`/page/revision/revisions/${id}`} noCSR>
+          <SubButtonStyle>Bearbeitungsverlauf</SubButtonStyle>
+        </SubLink>
+      </Li>
+    )
+  }
+
+  function log(id = data.id) {
+    return (
+      <Li>
+        <SubLink href={`/event/history/${id}`} noCSR>
+          <SubButtonStyle>Aktivitätenlog</SubButtonStyle>
+        </SubLink>
+      </Li>
+    )
+  }
+
+  function curriculum(id = data.id) {
+    return (
+      <Li>
+        <SubLink href={`/entity/taxonomy/update/${id}`} noCSR>
+          <SubButtonStyle>
+            Zuweisung zu Themen und Lehrplänen bearbeiten
+          </SubButtonStyle>
+        </SubLink>
+      </Li>
+    )
+  }
+
+  function flag(id = data.id) {
+    return (
+      <Li>
+        <SubLink href={`/flag/add/${id}`} noCSR>
+          <SubButtonStyle>Inhalt melden</SubButtonStyle>
+        </SubLink>
+      </Li>
+    )
+  }
+
+  function trash(id = data.id) {
+    // todo: use graphql mutation
+    return (
+      <Li>
+        <SubLink href={`/uuid/trash/${id}`} noCSR>
+          <SubButtonStyle>In den Papierkorb verschieben (?)</SubButtonStyle>
+        </SubLink>
+      </Li>
+    )
+  }
+
+  function sort(id = data.id) {
+    return (
+      <Li>
+        <SubLink href={`/entity/link/order/${id}/link`} noCSR>
+          <SubButtonStyle>Unterelemente sortieren</SubButtonStyle>
+        </SubLink>
+      </Li>
+    )
+  }
+
+  function edit(id = data.id) {
+    return (
+      <Li>
+        <SubLink href={`/entity/repository/add-revision/${id}`} noCSR>
+          <SubButtonStyle>Bearbeiten</SubButtonStyle>
+        </SubLink>
+      </Li>
     )
   }
 
