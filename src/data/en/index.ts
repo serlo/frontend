8345bb9f8766db1f5f60be55ea333f2478bf255a
Botho <1258870+elbotho@users.noreply.github.com>
2020-08-13 15:48:30 +0200

    Update source file index.ts

--- src/data/en/index.ts
+++ src/data/en/index.ts
@@ -1,4 +1,5 @@
-import { headerData, footerData } from './menu-data';
+import { headerData, footerData } from './menu-data'
+
 export const instanceData = {
   lang: 'en',
   headerData: headerData,
@@ -7,15 +8,16 @@ export const instanceData = {
     header: {
       slogan: 'The Open Learning Platform',
       search: 'Search',
-      login: 'Login'
+      login: 'Login',
     },
     footer: {
       summaryHeading: 'Serlo.org is the Wikipedia for learning.',
-      summaryText: 'We are a community of visionaries working tirelessly to make great education freely available to everyone.',
+      summaryText:
+        'We are a community of visionaries working tirelessly to make great education freely available to everyone.',
       learnMore: 'Learn more',
       participate: 'Join the cause',
       donate: 'Donate',
-      toTop: 'To Top'
+      toTop: 'To Top',
     },
     categories: {
       article: 'Article',
@@ -23,31 +25,31 @@ export const instanceData = {
       video: 'Video',
       applet: 'Applet',
       folder: 'Folder',
-      exercises: 'Exercises'
+      exercises: 'Exercises',
     },
     entities: {
       topicFolder: 'Exercise folder',
       comment: 'Comment',
       revision: 'Revision',
-      thread: 'Thread'
+      thread: 'Thread',
     },
     share: {
       button: 'Share',
       title: 'Share!',
       copyLink: 'Copy link',
       copySuccess: 'Link copied! ',
-      close: 'Close'
+      close: 'Close',
     },
     edit: {
-      button: 'Edit'
+      button: 'Edit',
     },
     license: {
-      readMore: 'Info'
+      readMore: 'Info',
     },
     course: {
       showPages: 'Show course overview',
       pages: 'Course overview',
-      next: 'Next'
+      next: 'Next',
     },
     content: {
       show: 'show',
@@ -59,7 +61,7 @@ export const instanceData = {
       wrong: 'Wrong',
       check: 'Check',
       yourAnswer: 'Your answer…',
-      chooseOption: 'Click on one of the options.'
+      chooseOption: 'Click on one of the options.',
     },
     cookie: {
       part1: 'By using this website you declare that you agree with our',
@@ -67,19 +69,20 @@ export const instanceData = {
       part3: '.',
       link1: 'Privacy Policy',
       link2: 'Terms of use',
-      button: 'Agree'
+      button: 'Agree',
     },
     notifications: {
       notifications: 'Notifications',
       pleaseLogInLink: 'Bitte melde dich an',
-      pleaseLogInText: 'um deine Benachrichtigungen zu sehen.'
-    }
-  }
-};
+      pleaseLogInText: 'um deine Benachrichtigungen zu sehen.',
+    },
+  },
+}
 export const instanceLandingData = {
   lang: 'en',
   strings: {
-    vision: 'It is our vision to enable personalized learning and provide high quality educational resources worldwide – completely free of charge. Serlo is a grassroots organization inspired by Wikipedia. We already provide thousands of articles, videos and solved exercises for five million German students every year. Now it’s time to go international.',
+    vision:
+      'It is our vision to enable personalized learning and provide high quality educational resources worldwide – completely free of charge. Serlo is a grassroots organization inspired by Wikipedia. We already provide thousands of articles, videos and solved exercises for five million German students every year. Now it’s time to go international.',
     learnMore: 'Learn more',
     democraticallyStructured: 'democratically structured',
     nonProfit: 'non-profit',
@@ -88,48 +91,61 @@ export const instanceLandingData = {
     adFree: 'ad-free',
     freeOfCharge: 'free of charge',
     wikiTitle: 'Serlo is the Wikipedia for Learning',
-    wikiText: 'Just like Wikipedia, this platform is created by an engaged community of authors. Serlo Education is run and owned by decentralized teams of volunteers and professionals all over the world.',
+    wikiText:
+      'Just like Wikipedia, this platform is created by an engaged community of authors. Serlo Education is run and owned by decentralized teams of volunteers and professionals all over the world.',
     movementTitle: 'Become a Part of Our Movement for Open Education',
-    callForAuthors: 'We are looking for teachers and enthusiastic educators who are passionate about their subject. Become part of our community to create new learning material and help us improve existing content.',
+    callForAuthors:
+      'We are looking for teachers and enthusiastic educators who are passionate about their subject. Become part of our community to create new learning material and help us improve existing content.',
     communityLink: 'Visit the landing page for authors',
-    callForOther: 'We offer a diverse range of jobs and volunteering opportunities in the fields of software development, design, translation, communications, project management and more.',
-    getInvolved: 'Get involved!'
-  }
-};
+    callForOther:
+      'We offer a diverse range of jobs and volunteering opportunities in the fields of software development, design, translation, communications, project management and more.',
+    getInvolved: 'Get involved!',
+  },
+}
 export const serverSideStrings = {
-  title: 'learn with Serlo!'
-};
+  title: 'learn with Serlo!',
+}
 export const loggedInData = {
-  authMenu: [{
-    url: '/user/notifications',
-    title: 'Notifications',
-    icon: 'notifications'
-  }, {
-    url: '',
-    title: 'User',
-    icon: 'user',
-    children: [{
-      url: '/user/public',
-      title: 'Public profile'
-    }, {
-      url: '/user/settings',
-      title: 'Edit profile'
-    }, {
-      url: '/auth/password/change',
-      title: 'Change password'
-    }, {
-      url: '/event/history/user/me',
-      title: 'Recent activities'
-    }, {
-      url: '/api/auth/logout',
-      title: 'Log out'
-    }]
-  }],
+  authMenu: [
+    {
+      url: '/user/notifications',
+      title: 'Notifications',
+      icon: 'notifications',
+    },
+    {
+      url: '',
+      title: 'User',
+      icon: 'user',
+      children: [
+        {
+          url: '/user/public',
+          title: 'Public profile',
+        },
+        {
+          url: '/user/settings',
+          title: 'Edit profile',
+        },
+        {
+          url: '/auth/password/change',
+          title: 'Change password',
+        },
+        {
+          url: '/event/history/user/me',
+          title: 'Recent activities',
+        },
+        {
+          url: '/api/auth/logout',
+          title: 'Log out',
+        },
+      ],
+    },
+  ],
   strings: {
     tools: 'Other Tools',
     notifications: {
       loadMore: 'Weitere laden',
-      unknownProblem: 'Es gibt ein Problem beim laden der Benachrichtigungen, bitte versuche es später noch einmal.',
+      unknownProblem:
+        'Es gibt ein Problem beim laden der Benachrichtigungen, bitte versuche es später noch einmal.',
       loading: 'Benachrichtigungen werden geladen',
       hide: 'Benachrichtigungen für diesen Inhalt nicht mehr anzeigen.',
       setThreadStateArchived: '%actor% hat einen %thread% archiviert.',
@@ -139,19 +155,26 @@ export const loggedInData = {
       createEntity: '%actor% hat %object% erstellt.',
       setLicense: '%actor% hat die Lizenz von %repository% geändert.',
       createEntityLink: '%actor% hat %child% mit %parent% verknüpft.',
-      removeEntityLink: '%actor% hat die Verknüpfung von %child% mit %parent% entfernt.',
-      createEntityRevision: '%actor% hat eine %revision% von %entity% erstellt.',
-      checkoutRevision: '%actor% hat eine %revision% von %repository% übernommen.',
+      removeEntityLink:
+        '%actor% hat die Verknüpfung von %child% mit %parent% entfernt.',
+      createEntityRevision:
+        '%actor% hat eine %revision% von %entity% erstellt.',
+      checkoutRevision:
+        '%actor% hat eine %revision% von %repository% übernommen.',
       rejectRevision: '%actor% hat %revision% für %repository% abgelehnt.',
       createTaxonomyLink: '%actor% hat %child% in %parent% eingeordnet.',
       removeTaxonomyLink: '%actor% hat %child% aus %parent% entfernt.',
       createTaxonomyTerm: '%actor% hat den %term% erstellt.',
       setTaxonomyTerm: '%actor% hat den %term% geändert.',
-      setTaxonomyParentDeleted: '%actor% hat den Elternknoten von %child% entfernt.',
-      setTaxonomyParentChangedFrom: '%actor% hat den Elternknoten von %child% von %previousparent% auf %parent% geändert.',
-      setTaxonomyParentChanged: '%actor% hat den Elternknoten von %child% auf %parent% geändert.',
+      setTaxonomyParentDeleted:
+        '%actor% hat den Elternknoten von %child% entfernt.',
+      setTaxonomyParentChangedFrom:
+        '%actor% hat den Elternknoten von %child% von %previousparent% auf %parent% geändert.',
+      setTaxonomyParentChanged:
+        '%actor% hat den Elternknoten von %child% auf %parent% geändert.',
       setUuidStateTrashed: '%actor% hat %object% in den Papierkorb verschoben.',
-      setUuidStateRestored: '%actor% hat %object% aus dem Papierkorb wieder hergestellt.',
+      setUuidStateRestored:
+        '%actor% hat %object% aus dem Papierkorb wieder hergestellt.',
       entityPlaceholderPage: 'Seite',
       entityPlaceholderArticle: 'Artikel',
       entityPlaceholderVideo: 'Video',
@@ -163,7 +186,7 @@ export const loggedInData = {
       entityPlaceholderEvent: 'Event',
       entityPlaceholderCourse: 'Kurs',
       entityPlaceholderTaxonomyTerm: 'Begriff',
-      entityPlaceholderFallback: 'Inhalt'
-    }
-  }
-};
+      entityPlaceholderFallback: 'Inhalt',
+    },
+  },
+}
