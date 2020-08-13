2de06276aca7fda8af5534f9397283a923c0ddd2
Botho <1258870+elbotho@users.noreply.github.com>
2020-08-13 15:48:37 +0200

    New translations index.ts (Tamil)

--- src/data/ta/index.ts
+++ src/data/ta/index.ts
@@ -1,29 +1,29 @@
 import { headerData, footerData } from './menu-data';
 export const instanceData = {
-  lang: 'en',
+  lang: "ta",
   headerData: headerData,
   footerData: footerData,
   strings: {
     header: {
-      slogan: 'The Open Learning Platform',
-      search: 'Search',
-      login: 'Login'
+      slogan: "அனைவருக்கும் திறந்த உரிமம் உள்ள ஓர் இணையத்தளம்",
+      search: "தேடுக",
+      login: "உள்நுழை"
     },
     footer: {
-      summaryHeading: 'Serlo.org is the Wikipedia for learning.',
-      summaryText: 'We are a community of visionaries working tirelessly to make great education freely available to everyone.',
-      learnMore: 'Learn more',
-      participate: 'Join the cause',
-      donate: 'Donate',
-      toTop: 'To Top'
+      summaryHeading: "கற்பதற்கு விக்கிபீடியா போன்றது Serlo.org.",
+      summaryText: "நாங்கள் கல்வியை அனைவருக்கும் இலவசமாகக் கிடைக்கச் செய்ய அயராது உழைக்கிறோம்",
+      learnMore: "மேலும் அறிக",
+      participate: "சேருங்கள்",
+      donate: "தானம் செய்",
+      toTop: "மேல் நோக்கி"
     },
     categories: {
-      article: 'Article',
-      course: 'Course',
-      video: 'Video',
-      applet: 'Applet',
-      folder: 'Folder',
-      exercises: 'Exercises'
+      article: "கட்டுரை",
+      course: "வகுப்பு",
+      video: "காணொளி",
+      applet: "ஆப்லெட்",
+      folder: "அடைவு",
+      exercises: "பயிற்சிகள்"
     },
     entities: {
       topicFolder: 'Exercise folder',
@@ -32,42 +32,42 @@ export const instanceData = {
       thread: 'Thread'
     },
     share: {
-      button: 'Share',
-      title: 'Share!',
-      copyLink: 'Copy link',
-      copySuccess: 'Link copied! ',
-      close: 'Close'
+      button: "பகிர்க",
+      title: "பகிர்!",
+      copyLink: "இணைப்பை நகலெடுக்கவும்",
+      copySuccess: "இணைப்பு நகலெடுக்கப்பட்டது!",
+      close: "நெருக்கமான"
     },
     edit: {
-      button: 'Edit'
+      button: "தொகு"
     },
     license: {
-      readMore: 'Info'
+      readMore: "தகவல்"
     },
     course: {
       showPages: 'Show course overview',
       pages: 'Course overview',
-      next: 'Next'
+      next: "அடுத்து"
     },
     content: {
-      show: 'show',
-      hide: 'hide',
+      show: "காட்டு",
+      hide: "மறை",
       prerequisite: 'For this task you need the following basic knowledge:',
-      solution: 'Solution',
-      exerciseGroup: 'Exercise group',
-      right: 'Right',
-      wrong: 'Wrong',
-      check: 'Check',
-      yourAnswer: 'Your answer…',
+      solution: "தீர்வு",
+      exerciseGroup: "பயிற்சிப் பதிவுகள்",
+      right: "சரி",
+      wrong: "பிழை",
+      check: "சரிபார்க்கவும்",
+      yourAnswer: "உங்கள் பதில்:",
       chooseOption: 'Click on one of the options.'
     },
     cookie: {
-      part1: 'By using this website you declare that you agree with our',
-      part2: 'and',
+      part1: "இந்த வலைத்தளத்தைப் பயன்படுத்துவதன் மூலம் நீங்கள் எங்களுடன் உடன்படுகிறீர்கள் என்று அறிவிக்கிறீர்கள்",
+      part2: "மற்றும்",
       part3: '.',
-      link1: 'Privacy Policy',
-      link2: 'Terms of use',
-      button: 'Agree'
+      link1: "தனியுரிமைக் கொள்கை",
+      link2: "பயன்பாட்டு விதிமுறைகளை",
+      button: "ஒப்புக்கொள்கிறேன்"
     },
     notifications: {
       notifications: 'Notifications',
@@ -79,25 +79,25 @@ export const instanceData = {
 export const instanceLandingData = {
   lang: "ta",
   strings: {
-    vision: 'It is our vision to enable personalized learning and provide high quality educational resources worldwide – completely free of charge. Serlo is a grassroots organization inspired by Wikipedia. We already provide thousands of articles, videos and solved exercises for five million German students every year. Now it’s time to go international.',
-    learnMore: 'Learn more',
-    democraticallyStructured: 'democratically structured',
-    nonProfit: 'non-profit',
-    transparent: 'transparent',
-    openlyLicensed: 'openly licensed',
-    adFree: 'ad-free',
-    freeOfCharge: 'free of charge',
-    wikiTitle: 'Serlo is the Wikipedia for Learning',
-    wikiText: 'Just like Wikipedia, this platform is created by an engaged community of authors. Serlo Education is run and owned by decentralized teams of volunteers and professionals all over the world.',
-    movementTitle: 'Become a Part of Our Movement for Open Education',
-    callForAuthors: 'We are looking for teachers and enthusiastic educators who are passionate about their subject. Become part of our community to create new learning material and help us improve existing content.',
-    communityLink: 'Visit the landing page for authors',
-    callForOther: 'We offer a diverse range of jobs and volunteering opportunities in the fields of software development, design, translation, communications, project management and more.',
-    getInvolved: 'Get involved!'
+    vision: "நாம் சமமான கல்வி வாய்ப்புகளை நோக்கி இணைந்து பணிபுரியும் ஒரு குழு. இந்த இணையத்தளத்தில் எண்ணற்ற விவரக் கட்டுரைகள், பயிற்சிகள் மற்றும் ஒலிப் பேழைகள் அனைத்துப் பாடங்களுக்கும் வழங்கப்பட்டுவருகின்றன. இவை அனைத்தும் இலவசமாக உலகம் முழுவதும் உள்ள மாணவர்களுக்காக உருவாக்கப்பட்டுவருகின்றன. இனி வரும் காலங்களில், தமிழ்மொழியிலும் இவ்வாறான இலவசப் பாடத்திட்டங்களை உருவாக்க நீங்களும் எம்முடன் இணைந்து பணியாற்றலாம்.",
+    learnMore: "மேலும் அறிக",
+    democraticallyStructured: "ஜனநாயக ரீதியாக",
+    nonProfit: "இலாப நோக்கற்றது",
+    transparent: "ஒளி புகும்",
+    openlyLicensed: "திறந்த உரிமம்",
+    adFree: "விளம்பரமின்றி",
+    freeOfCharge: "இலவசம்",
+    wikiTitle: "Serlo ஓர் கற்றலுக்கான விக்கிபீடியா",
+    wikiText: "Serlo.org விக்கிபீடியாபோல திறந்த உரிமம் கொண்ட ஓர் இணையத்தளம். இது எம் எழுத்தாளர் குழுவால் உருவாக்கப்படுகின்றது.",
+    movementTitle: "நீங்களும் இதில் பணியாற்றலாம்",
+    callForAuthors: "ஆசிரியர்களும் ஆர்வமுள்ள எழுத்தாளர்களும் பாடங்களை உருவாக்க பல வழிகளில் உதவலாம். புதுப் பயிற்சிகளை உருவாக்குவதற்கும் இந்தத் தளத்தின் சில உள்ளடக்கங்களை இன்னும் மேம்படுத்துவதற்கும் நீங்கள் உதவலாம். அதற்கு கீழுள்ள இணையத்திற்குச் செல்லவும்.",
+    communityLink: "எழுத்தாளருக்கான பக்கத்தைப் பார்வையிடவும்",
+    callForOther: "நாங்கள் பல வகையான வேலைவாய்ப்புகளையும் பொதுச்சேவையாகப் பணியாற்றும் வாய்ப்புகளையும் வழங்குகின்றோம். இந்த இணையத்தளதிற்கு மொழிபெயர்ப்பாளர்கள், வடிவமைப்பாளர்கள், தொலைத்தொடர்பாளர்கள் போன்ற துறை சார்ந்தவர்களை நாங்கள் தேடி நிற்கின்றோம். இணைந்து கொள்ளுங்கள்.",
+    getInvolved: "நீங்களும் ஈடுபடுங்கள்!"
   }
 };
 export const serverSideStrings = {
-  title: 'learn with Serlo!'
+  title: "Serlo உடன் கற்றுக்கொள்ளுங்கள்!"
 };
 export const loggedInData = {
   authMenu: [{
