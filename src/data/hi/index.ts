a366d5865f2b3472ac8ec4cfa6c48f69e5afbfc4
Botho <1258870+elbotho@users.noreply.github.com>
2020-08-28 16:20:19 +0200

    New translations index.ts (Hindi)

--- src/data/hi/index.ts
+++ src/data/hi/index.ts
@@ -54,6 +54,7 @@ export const instanceData = {
       hide: 'hide',
       prerequisite: 'For this task you need the following basic knowledge:',
       solution: 'Solution',
+      task: 'Task',
       exerciseGroup: 'Exercise group',
       right: 'Right',
       wrong: 'Wrong',
@@ -71,8 +72,21 @@ export const instanceData = {
     },
     notifications: {
       notifications: 'Notifications',
-      pleaseLogInLink: 'Bitte melde dich an',
-      pleaseLogInText: 'um deine Benachrichtigungen zu sehen.'
+      pleaseLogInLink: 'Please log in',
+      pleaseLogInText: 'to see your notifications.'
+    },
+    comments: {
+      question: 'Do you have a question?',
+      commentsOne: 'Comment',
+      commentsMany: 'Comments',
+      submit: 'Submit',
+      reportComment: 'Report comment',
+      archiveThread: 'Archive thread',
+      deleteThread: 'Delete thread',
+      deleteComment: 'Delete comment',
+      postedOn: 'Posted on',
+      placeholder: "Your question or suggestion…",
+      placeholderReply: "Your answer…"
     }
   }
 };
