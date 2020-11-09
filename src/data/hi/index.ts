87c0edc3bc862ed009e9e303c617bcd336f036bd
Botho <1258870+elbotho@users.noreply.github.com>
2020-11-09 13:17:05 +0100

    New translations index.ts (Hindi)

--- src/data/hi/index.ts
+++ src/data/hi/index.ts
@@ -1,9 +1,8 @@
 import { headerData, footerData, landingSubjectsData } from './menu-data';
 export const instanceData = {
-  lang: "hi",
+  lang: 'en',
   headerData: headerData,
   footerData: footerData,
-  landingData: landingSubjectsData,
   strings: {
     header: {
       slogan: "ओपन लर्निंग प्लेटफॉर्म",
@@ -60,7 +59,7 @@ export const instanceData = {
       button: "संपादित करें"
     },
     license: {
-      readMore: "सूचना"
+      readMore: 'Info'
     },
     course: {
       showPages: "पाठ्यक्रम का अवलोकन करें",
@@ -68,13 +67,13 @@ export const instanceData = {
       next: "अगला"
     },
     content: {
-      show: "दिखाएँ",
+      show: 'show',
       hide: "छिपाएं",
       prerequisite: "इस कार्य के लिए आपको निम्नलिखित बुनियादी ज्ञान की आवश्यकता है",
       task: "कार्य",
       right: "सही!",
       wrong: "गलत",
-      check: "जाँच करें",
+      check: 'Check',
       yourAnswer: "आपका उत्तर",
       chooseOption: "किसी एक विकल्प पर क्लिक करें",
       trashedNotice: "यह सामग्री हटाने के लिए चिह्नित है"
@@ -137,21 +136,21 @@ export const instanceLandingData = {
   lang: "hi",
   subjectsData: landingSubjectsData,
   strings: {
-    vision: 'It is our vision to enable personalized learning and provide high quality educational resources worldwide – completely free of charge. Serlo is a grassroots organization inspired by Wikipedia. We already provide thousands of articles, videos and solved exercises for five million German students every year. Now it’s time to go international.',
-    learnMore: 'Learn more',
-    democraticallyStructured: 'democratically structured',
-    nonProfit: 'non-profit',
-    transparent: 'transparent',
-    openlyLicensed: 'openly licensed',
-    adFree: 'ad-free',
-    freeOfCharge: 'free of charge',
-    wikiTitle: 'Serlo is the Wikipedia for Learning',
-    wikiText: 'Just like Wikipedia, this platform is created by an engaged community of authors. Serlo Education is run and owned by decentralized teams of volunteers and professionals all over the world.',
-    movementTitle: 'Become a Part of Our Movement for Open Education',
-    callForAuthors: 'We are looking for teachers and enthusiastic educators who are passionate about their subject. Become part of our community to create new learning material and help us improve existing content.',
-    communityLink: 'Visit the landing page for authors',
-    callForOther: 'We offer a diverse range of jobs and volunteering opportunities in the fields of software development, design, translation, communications, project management and more.',
-    getInvolved: 'Get involved!'
+    vision: "हम नॉनप्रॉफिट आर्गेनाइजेशन संगठन हैं जो व्यक्तिगत शिक्षा का समर्थन करते हैं और समान शैक्षणिक अवसरों के लिए काम करते हैं। यह सीखने का मंच दुनिया भर के लाखों छात्रों के लिए हजारों निर्देशक लेख, वीडियो के माध्यम से सीखना और अभ्यास प्रदान करता है - पूरी तरह से नि: शुल्क। अब हिंदी में हमसे जुड़ने का समय है।",
+    learnMore: "और जानो",
+    democraticallyStructured: "लोकतांत्रिक",
+    nonProfit: "नॉनप्रॉफिट",
+    transparent: "पारदर्शक",
+    openlyLicensed: "खुले तौर पर लाइसेंस प्राप्त",
+    adFree: "विज्ञापन मुक्त",
+    freeOfCharge: "निःशुल्क",
+    wikiTitle: "सेर्लो सीखने के लिए विकिपीडिया है।",
+    wikiText: "विकिपीडिया की तरह, यह मंच लेखकों के एक व्यस्त समुदाय द्वारा बनाया गया है। सेर्लो शिक्षा पूरी दुनिया में स्वयंसेवकों और पेशेवरों की विकेन्द्रीकृत टीमों द्वारा संचालित और स्वामित्व में है।",
+    movementTitle: "ओपन एजुकेशन के लिए हमारे आंदोलन का एक हिस्सा बनें",
+    callForAuthors: "हम उत्साही शिक्षकों की तलाश में हैं जो अपने विषय के बारे में भावुक हैं। Serlo.org पर एक लेखक बनें! आप नई सीखने की सामग्री बना सकते हैं और मौजूदा सामग्री को बेहतर बनाने में हमारी सहायता कर सकते हैं।",
+    communityLink: "लेखकों के लिए लैंडिंग पृष्ठ पर जाएं",
+    callForOther: "हम सॉफ्टवेयर विकास, डिजाइन, अनुवाद, संचार, परियोजना प्रबंधन और अन्य क्षेत्रों के क्षेत्र में नौकरियों और स्वयंसेवी अवसरों की विविध श्रेणी प्रदान करते हैं।",
+    getInvolved: "सेर्लो में शामिल हो जाएं"
   }
 };
 export const serverSideStrings = {
@@ -199,20 +198,20 @@ export const loggedInData = {
       subscribe: "सदस्य बनें",
       subscribeNotifications: "सूचनाएं प्राप्त करें",
       subscribeNotificationsAndMail: "ईमेल के माध्यम से सूचनाएं प्राप्त करें ।",
-      unsubscribeNotifications: 'Unsubscribe',
-      convert: 'Convert (beta)',
-      history: 'History',
-      editAssignments: 'Edit topic and curriculum assignments',
-      flagContent: 'Flag content',
-      moveToTrash: 'Move to trash',
-      restoreContent: 'Restore from trash',
-      sort: 'Sort children',
-      edit: 'Edit',
-      organize: 'Organize',
-      moveToGroupedTextExercise: 'Move content to other grouped-text-exercise',
-      moveToTextExercise: 'Move content to other text-exercise',
-      sortEntities: 'Sort content',
-      newEntity: 'New Entity'
+      unsubscribeNotifications: "सदस्यता रद्द",
+      convert: "धर्मांतरित (बीटा)",
+      history: "इतिहास",
+      editAssignments: "विषय और पाठ्यचर्या असाइनमेंट संपादित करें",
+      flagContent: "सामग्री फ्लैग करें",
+      moveToTrash: "रद्दी में डालें",
+      restoreContent: "कचरे से पुनर्स्थापित करें",
+      sort: "सोर्ट चिल्ड्रन",
+      edit: "संपादित",
+      organize: "आयोजन",
+      moveToGroupedTextExercise: "सामग्री को अन्य समूहीकृत-पाठ-व्यायाम में ले जाएँ",
+      moveToTextExercise: "सामग्री को अन्य पाठ-व्यायाम में ले जाएँ",
+      sortEntities: "सामग्री को क्रमबद्ध करें",
+      newEntity: "नई इकाई"
     },
     notifications: {
       loadMore: "अधिक लोड करें",
