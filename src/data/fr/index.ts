654f25ff0511ed8dbad0cd2503ebedaa5c21d267
Botho <1258870+elbotho@users.noreply.github.com>
2020-11-12 16:40:26 +0100

    New translations index.ts (French)

--- src/data/fr/index.ts
+++ src/data/fr/index.ts
@@ -86,6 +86,13 @@ export const instanceData = {
       link2: "nos conditions d'utilisation.",
       button: "Je suis d'accord"
     },
+    embed: {
+      part1: 'By clicking on the image or button below you agree that external content from',
+      part2: 'will be loaded. Also personal data may be transferred to this service in accordance with our',
+      link: 'Privacy Policy',
+      video: 'Play Video',
+      applet: 'Load Applet'
+    },
     notifications: {
       notifications: 'Notifications',
       pleaseLogInLink: 'Please log in',
