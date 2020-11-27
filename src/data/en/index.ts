6f33973751decfd9b3b54a20796bca75f85fce61
Botho <1258870+elbotho@users.noreply.github.com>
2020-11-27 15:00:10 +0100

    Update source file index.ts

--- src/data/en/index.ts
+++ src/data/en/index.ts
@@ -114,7 +114,8 @@ export const instanceData = {
       deleteComment: 'Delete comment',
       postedOn: 'Posted on',
       placeholder: "Your question or suggestion…",
-      placeholderReply: "Your answer…"
+      placeholderReply: "Your answer…",
+      loading: 'Looking for comments ...'
     },
     revisions: {
       toOverview: "Back to overview",
