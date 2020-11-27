a33e33299179a75c187a5faa0c28e2e628daf3ac
Botho <1258870+elbotho@users.noreply.github.com>
2020-11-27 16:00:12 +0100

    add canonical tag try

--- src/components/head-tags.tsx
+++ src/components/head-tags.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 import { useOrigin } from '@/contexts/origin-context'
 import { HeadData } from '@/data-types'
@@ -10,12 +11,15 @@ interface HeadTagsProps {
 export function HeadTags({ data }: HeadTagsProps) {
   const { title, contentType, metaDescription, metaImage } = data
   const origin = useOrigin()
+  const router = useRouter()
+
   return (
     <Head>
       <title>{title}</title>
       {contentType && <meta name="content_type" content={contentType} />}
       {metaDescription && <meta name="description" content={metaDescription} />}
       <meta property="og:title" content={title} />
+      <link rel="canonical" href={origin + router.asPath} />
       <meta
         property="og:image"
         content={metaImage ? metaImage : origin + '/_assets/img/meta/serlo.jpg'}
