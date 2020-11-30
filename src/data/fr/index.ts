c0d480606e3d0c9ebfce761bb21922c001f700ed
Botho <1258870+elbotho@users.noreply.github.com>
2020-11-30 22:10:14 +0100

    New translations index.ts (French)

--- src/data/fr/index.ts
+++ src/data/fr/index.ts
@@ -108,7 +108,6 @@ export const instanceData = {
       commentsOne: "Commentaire",
       commentsMany: "Commentaires",
       submit: "Soumettre",
-      reportComment: 'Report comment',
       archiveThread: 'Archive thread',
       deleteThread: 'Delete thread',
       deleteComment: 'Delete comment',
