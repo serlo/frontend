e5d12deffaa4f408f9cf7709b9b641753274c3e4
David Li <dawei.li@gmx.net>
2021-01-11 12:39:11 +0100

    fix protocol

--- src/helper/frontent-origin.ts
+++ src/helper/frontent-origin.ts
@@ -2,5 +2,8 @@ export const frontendOrigin =
   process.env.NEXT_PUBLIC_ENV === 'production'
     ? // we are on production branch
       'https://frontend.serlo.org'
-    : // use preview deployment url, else we are on local deployment
-      process.env.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000'
+    : process.env.NEXT_PUBLIC_VERCEL_URL
+    ? // use preview deployment url
+      `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : //  else we are on local deployment
+      'http://localhost:3000'
