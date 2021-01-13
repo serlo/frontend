7c2be27ffa191e62e876378217c3568df4c44446
David Li <dawei.li@gmx.net>
2021-01-13 13:59:00 +0100

    no patch needed, it just "magically" works

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-const serloDomain =
-  process.env.NEXT_PUBLIC_ENV === 'production'
-    ? process.env.NEXT_PUBLIC_SERLO_DOMAIN_PRODUCTION
-    : process.env.NEXT_PUBLIC_SERLO_DOMAIN_STAGING
-
 module.exports = withSourceMaps(
   withBundleAnalyzer({
     webpack(config) {
