0b9b43c7162e6ab7c96d7af5cf569ace97c0ee60
Botho <1258870+elbotho@users.noreply.github.com>
2021-01-16 12:21:10 +0100

    markup links in comments and make sure they break

--- src/components/comments/comment.tsx
+++ src/components/comments/comment.tsx
@@ -4,6 +4,7 @@ import styled, { css } from 'styled-components'
 
 import { StyledP } from '../tags/styled-p'
 import { MetaBar } from './meta-bar'
+import { htmlEscapeString } from '@/helper/html-escape'
 
 interface CommentProps {
   isParent?: boolean
@@ -12,10 +13,20 @@ interface CommentProps {
 
 export function Comment({ data, isParent }: CommentProps) {
   const { author, createdAt, content } = data
+
+  const urlFinder = /https?:\/\/(www\.)?([-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b)([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g
+
+  //could content end up here unescaped?
+  const escapedContent = htmlEscapeString(content)
+
+  const contentWithLinks = escapedContent.replace(urlFinder, (match) => {
+    return `<a href="${match}" rel="ugc nofollow">${match}</a>`
+  })
+
   return (
     <Wrapper isParent={isParent}>
       <MetaBar user={author} timestamp={createdAt} isParent={isParent} />
-      <StyledP>{content}</StyledP>
+      <StyledP dangerouslySetInnerHTML={{ __html: contentWithLinks }}></StyledP>
     </Wrapper>
   )
 }
@@ -34,6 +45,13 @@ const Wrapper = styled.div<{ isParent?: boolean }>`
   > p {
     margin-bottom: 0;
     white-space: pre-line;
+    overflow-wrap: break-word;
+    word-break: break-all;
+    hyphens: auto;
     padding-left: 8px;
+
+    > a {
+      color: ${(props) => props.theme.colors.brand};
+    }
   }
 `
