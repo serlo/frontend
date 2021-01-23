ffaa056253ec66db193d1c04f9dc9ec9dc2d60c4
David Li <dawei.li@gmx.net>
2021-01-23 10:33:09 +0100

    Revert "need to access fresh data"
    This reverts commit 02d1e1e623d33e83e45c78bb97487f8604f6d824.

--- src/api/endpoint.ts
+++ src/api/endpoint.ts
@@ -1,3 +1,3 @@
 import { serloDomain } from '@/helper/serlo-domain'
 
-export const endpoint = 'https://api.serlo.org/graphql' //`https://api.${serloDomain}/graphql`
+export const endpoint = `https://api.${serloDomain}/graphql`
