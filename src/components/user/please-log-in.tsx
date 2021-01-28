f280585ff3964a8f055a6498eb68036cef843150
Botho <1258870+elbotho@users.noreply.github.com>
2021-01-28 12:22:18 +0100

    use right link component; fixes #841

--- src/components/user/please-log-in.tsx
+++ src/components/user/please-log-in.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link'
 import React from 'react'
 
+import { Link } from '../content/link'
 import { StyledP } from '@/components/tags/styled-p'
 import { useInstanceData } from '@/contexts/instance-context'
 
