29cf26c34e492058c7b2ca696ca3aff60be5c90b
Botho <1258870+elbotho@users.noreply.github.com>
2021-02-05 23:25:50 +0100

    add filter to subscription manager

--- src/pages/subscriptions/manage.tsx
+++ src/pages/subscriptions/manage.tsx
@@ -1,12 +1,17 @@
 import { gql } from 'graphql-request'
+import { useState } from 'react'
+import styled from 'styled-components'
 
 import { useGraphqlSwrWithAuth } from '@/api/use-graphql-swr'
 import { PageTitle } from '@/components/content/page-title'
 import { FrontendClientBase } from '@/components/frontend-client-base'
 import { Guard } from '@/components/guard'
 import { ManageSubscriptions } from '@/components/pages/manage-subscriptions'
+import { StyledP } from '@/components/tags/styled-p'
 import { useInstanceData } from '@/contexts/instance-context'
 import { QueryResponse } from '@/fetcher/query-types'
+import { makeLightButton, makePrimaryButton } from '@/helper/css'
+import { getEntityStringByTypename } from '@/helper/feature-i18n'
 import { renderedPageNoHooks } from '@/helper/rendered-page'
 
 export default renderedPageNoHooks(() => (
@@ -16,14 +21,50 @@ export default renderedPageNoHooks(() => (
   </FrontendClientBase>
 ))
 
+const filters = [
+  'Article',
+  'Video',
+  'Applet',
+  'CoursePage',
+  'Exercise',
+  'GroupedExercise',
+  'ExerciseGroup',
+  'Solution',
+  'User',
+  'Course',
+  'TaxonomyTerm',
+]
+
 function Content() {
   const response = useFetch()
+  const { strings } = useInstanceData()
+  const [showTypename, setShowTypename] = useState<typeof filters[number]>(
+    'Article'
+  )
+
+  const filtered = response.data?.subscriptions.nodes.filter(
+    (node) => node.__typename === showTypename
+  )
+
   return (
-    <Guard {...response} needsAuth>
-      <ManageSubscriptions
-        subscriptions={response.data?.subscriptions.nodes!}
-      />
-    </Guard>
+    <>
+      <StyledP>
+        {/* //blur-hack, use https://caniuse.com/#feat=css-focus-visible when supported*/}
+        {filters.map((typename) => (
+          <TabButton
+            key={typename}
+            active={showTypename == typename}
+            onPointerUp={(e) => e.currentTarget.blur()}
+            onClick={() => setShowTypename(typename)}
+          >
+            {getEntityStringByTypename(typename, strings)}
+          </TabButton>
+        ))}
+      </StyledP>
+      <Guard {...response} needsAuth>
+        <ManageSubscriptions subscriptions={filtered!} />
+      </Guard>
+    </>
   )
 }
 
@@ -44,6 +85,12 @@ function useFetch() {
     },
   })
 }
+
+const TabButton = styled.button<{ active: boolean }>`
+  ${(props) => (props.active ? makePrimaryButton : makeLightButton)}
+  margin-right: 8px;
+  margin-bottom: 10px;
+`
 
 const query = gql`
   query {
