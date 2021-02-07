8ba21a7330a78f5a716dd8f0dfbf18c27ef866f8
Botho <1258870+elbotho@users.noreply.github.com>
2021-02-07 19:49:29 +0100

    input-ex: add custom feedback, closes #877

--- src/components/content/exercises/input-exercise.tsx
+++ src/components/content/exercises/input-exercise.tsx
@@ -7,7 +7,7 @@ import { useInstanceData } from '@/contexts/instance-context'
 import { EdtrPluginInputExercise } from '@/data-types'
 import { makeMargin, makePrimaryButton, inputFontReset } from '@/helper/css'
 import { submitEventWithPath } from '@/helper/submit-event'
-import { NodePath } from '@/schema/article-renderer'
+import { NodePath, renderArticle } from '@/schema/article-renderer'
 
 export interface InputExerciseProps {
   data: EdtrPluginInputExercise['state']
@@ -16,7 +16,7 @@ export interface InputExerciseProps {
 
 interface FeedbackData {
   correct: boolean
-  message: string
+  message: JSX.Element
 }
 
 export function InputExercise({ data, path }: InputExerciseProps) {
@@ -58,7 +58,7 @@ export function InputExercise({ data, path }: InputExerciseProps) {
     </Wrapper>
   )
 
-  function checkAnswer() {
+  function checkAnswer(): FeedbackData {
     const answers = data.answers
     const filteredAnswers = answers.filter((answer) => {
       try {
@@ -80,10 +80,21 @@ export function InputExercise({ data, path }: InputExerciseProps) {
         return false
       }
     })
+    const hasCustomFeedback = filteredAnswers[0]?.feedback.length > 0
+    const customFeedbackNode =
+      hasCustomFeedback &&
+      (renderArticle(filteredAnswers[0]?.feedback) as JSX.Element)
+
     if (filteredAnswers.length !== 1 || !filteredAnswers[0].isCorrect) {
-      return { correct: false, message: strings.content.wrong }
+      return {
+        correct: false,
+        message: customFeedbackNode || <>{strings.content.wrong}</>,
+      }
     } else {
-      return { correct: true, message: strings.content.right }
+      return {
+        correct: true,
+        message: customFeedbackNode || <>{strings.content.right}</>,
+      }
     }
   }
 
