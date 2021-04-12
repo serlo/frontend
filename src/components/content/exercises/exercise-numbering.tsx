9495bdd73ff67b1407ce1c55777b292c5b316e22
David Li <dawei.li@gmx.net>
2021-04-12 18:48:33 +0200

    fix after content

--- src/components/content/exercises/exercise-numbering.tsx
+++ src/components/content/exercises/exercise-numbering.tsx
@@ -32,27 +32,18 @@ export function ExerciseNumbering({
     )
   }
   return (
-    <>
-      <Link
-        href={href}
-        path={['numbering', index]}
-        className={clsx(
-          'numbering-parent',
-          'block h-12 w-12 mx-4 mb-5 pt-1 rounded-full',
-          'sm:absolute sm:-mt-2.5 sm:-ml-10 md:-ml-14',
-          'text-white text-4xl text-center font-bold bg-brand',
-          'hover:no-underline hover:bg-brand-lighter'
-        )}
-      >
-        {index + 1}
-      </Link>
-      <style jsx>{`
-        @media print {
-          .numbering-parent:after {
-            content: '' !important;
-          }
-        }
-      `}</style>
-    </>
+    <Link
+      href={href}
+      path={['numbering', index]}
+      className={clsx(
+        'block h-12 w-12 mx-4 mb-5 pt-1 rounded-full',
+        'sm:absolute sm:-mt-2.5 sm:-ml-10 md:-ml-14',
+        'text-white text-4xl text-center font-bold bg-brand',
+        'hover:no-underline hover:bg-brand-lighter',
+        'serlo-no-after-content'
+      )}
+    >
+      {index + 1}
+    </Link>
   )
 }
