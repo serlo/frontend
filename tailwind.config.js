3274db12707bd78d57de149b69bccc13073fe840
David Li <dawei.li@gmx.net>
2021-04-12 12:00:13 +0200

    baseline tailwind installation

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  jit: true,
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
