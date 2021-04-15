6d067a14fbdee233115e9a48066b31a39a5c9211
Stephan Kulla <git.mail@kulla.me>
2021-04-15 18:54:20 +0200

    user-profile: Revert avatar fix
    It does not work to lowercase all usernames

--- src/fetcher/user/request.ts
+++ src/fetcher/user/request.ts
@@ -21,7 +21,7 @@ export async function requestUser(
       newsletterPopup: false,
       userData: {
         ...uuid,
-        imageUrl: `https://community.serlo.org/avatar/${uuid.username.toLowerCase()}`,
+        imageUrl: `https://community.serlo.org/avatar/${uuid.username}`,
         description: getDescription(uuid),
       },
     }
