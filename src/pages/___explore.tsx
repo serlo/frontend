dc54563cda3804857efab728c28b8fc84b8841d5
David Li <dawei.li@gmx.net>
2021-04-17 18:59:05 +0200

    cleanup: remove unused files and packages remove explore experiment

--- src/pages/___explore.tsx
+++
@@ -1,9 +0,0 @@
-import { FrontendClientBase } from '@/components/frontend-client-base'
-import { Explore } from '@/components/pages/explore'
-import { renderedPageNoHooks } from '@/helper/rendered-page'
-
-export default renderedPageNoHooks(() => (
-  <FrontendClientBase entityId={-1}>
-    <Explore />
-  </FrontendClientBase>
-))
