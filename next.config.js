f9e49bc109e8bfd69dce7b41f251ccad33b81b13
David Li <dawei.li@gmx.net>
2021-04-19 12:08:24 +0200

    migrate to webpack 5

--- next.config.js
+++ next.config.js
@@ -1,43 +1,44 @@
-const withSourceMaps = require('@zeit/next-source-maps')()
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withSourceMaps(
-  withBundleAnalyzer({
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              svgo: false,
-            },
+module.exports = withBundleAnalyzer({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: false,
           },
-        ],
-      })
+        },
+      ],
+    })
 
-      for (const plugin of config.plugins) {
-        if (plugin.constructor.name === 'UglifyJsPlugin') {
-          plugin.options.sourceMap = true
-          break
-        }
+    for (const plugin of config.plugins) {
+      if (plugin.constructor.name === 'UglifyJsPlugin') {
+        plugin.options.sourceMap = true
+        break
       }
+    }
 
-      return config
-    },
-    devIndicators: {
-      autoPrerender: false,
-    },
-    async rewrites() {
-      return [{ source: '/user/public', destination: '/user/me' }]
-    },
-    i18n: {
-      locales: ['de', 'en', 'ta', 'hi', 'fr', 'es'],
-      defaultLocale: 'de',
-      localeDetection: false,
-    },
-    reactStrictMode: true,
-  })
-)
+    return config
+  },
+  devIndicators: {
+    autoPrerender: false,
+  },
+  async rewrites() {
+    return [{ source: '/user/public', destination: '/user/me' }]
+  },
+  i18n: {
+    locales: ['de', 'en', 'ta', 'hi', 'fr', 'es'],
+    defaultLocale: 'de',
+    localeDetection: false,
+  },
+  reactStrictMode: true,
+  future: {
+    webpack5: true,
+  },
+  productionBrowserSourceMaps: true,
+})
