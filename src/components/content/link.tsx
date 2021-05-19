0b71182387fdf973db9cba3739ba95fc7e450970
Botho <1258870+elbotho@users.noreply.github.com>
2021-05-19 16:20:58 +0200

    remove CSR for subscription manager route

--- src/components/content/link.tsx
+++ src/components/content/link.tsx
@@ -45,6 +45,7 @@ export function isLegacyLink(_href: string) {
     _href.startsWith('/page') ||
     _href.startsWith('/taxonomy') ||
     _href.startsWith('/discussions') ||
+    _href.startsWith('/subscriptions') || //disabled for zwischenrelease
     _href.startsWith('/subscription/update') ||
     _href.startsWith('/unsubscribe') ||
     _href.startsWith('/user/profile/') ||
