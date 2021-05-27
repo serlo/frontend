28963041e2947475a67f9d0086c357451e7ebcda
Stephan Kulla <git.mail@kulla.me>
2021-05-27 14:48:13 +0200

    lightbox: Use StyledModal as base for Modal

--- src/components/content/light-box.tsx
+++ src/components/content/light-box.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-import { Modal } from '../modal'
+import { StyledModal } from '../user-tools/share-modal'
 import { makeMargin } from '@/helper/css'
 
 export interface LightBoxProps {
@@ -15,28 +15,18 @@ export function LightBox(props: LightBoxProps) {
   if (!open) return null
 
   return (
-    <StyledModal isOpen={open} onRequestClose={onClose}>
+    <LightboxStyledModal isOpen={open} onRequestClose={onClose}>
       <_StyledImg onClick={onClose} src={src} alt={label || 'Bild'} />
       <p>{label}</p>
-    </StyledModal>
+    </LightboxStyledModal>
   )
 }
 
-//this is overriding the styles of the modal-content only. see doc to change overlay etc.
-export const StyledModal = styled(Modal)`
-  position: absolute;
+export const LightboxStyledModal = styled(StyledModal)`
   text-align: center;
   top: 50%;
-  left: 50%;
-  margin-right: -50%;
-  transform: translate(-50%, -50%);
-  border-radius: 12px;
-  max-width: 85%;
-  border: 0;
-  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
-  padding: 10px 10px 10px 10px;
-  background-color: #fff;
-  outline: none;
+  padding-bottom: 30px;
+  width: inherit;
 
   pointer-events: none;
   > p {
