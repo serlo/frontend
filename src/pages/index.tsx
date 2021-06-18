c8b95309646fba24435b61b4f15a6e28ee06f385
Jonas Keinholz <git@inyono.dev>
2021-06-18 20:02:29 +0200

    debug: add refreshToken button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,28 @@ import { LandingInternational } from '@/components/pages/landing-international'
 import { LandingProps } from '@/data-types'
 import { getLandingData } from '@/helper/feature-i18n'
 import { renderedPageNoHooks } from '@/helper/rendered-page'
+import refreshToken from './api/auth/refresh-token'
+import { useAuthentication } from '@/auth/use-authentication'
+
+function RefreshToken() {
+  const auth = useAuthentication()
+  return (
+    <>
+      <button onClick={() => {
+        console.log('refreshing token')
+        auth.current?.refreshToken()
+      }}>Refresh Token</button>
+      </>
+  )
+}
 
 export default renderedPageNoHooks<LandingProps>(({ pageData }, { router }) => (
   <FrontendClientBase noContainers>
     {router.locale == 'de' ? (
+      <>
+      <RefreshToken />
       <LandingDE data={pageData.landingData} />
+      </>
     ) : (
       <LandingInternational data={pageData.landingData} />
     )}
