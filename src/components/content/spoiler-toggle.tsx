468f4fee04fdc1abab2c5b4a276b8dc0b3c04d86
Stephan Kulla <git.mail@kulla.me>
2021-07-19 15:39:48 +0200

    Move everything spoiler related into one file

--- src/components/content/spoiler-toggle.tsx
+++
@@ -1,7 +0,0 @@
-interface SpoilerToggleProps {
-  open: boolean
-}
-
-export function SpoilerToggle({ open }: SpoilerToggleProps) {
-  return <span className="inline w-4">{open ? '▾ ' : '▸ '} </span>
-}
--- src/components/content/spoiler.tsx
+++ src/components/content/spoiler.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, useState } from 'react'
+import styled, { css } from 'styled-components'
 
-import { SpoilerTitle } from './spoiler-title'
-import { SpoilerToggle } from './spoiler-toggle'
+import { inputFontReset } from '@/helper/css'
 import { submitEventWithPath } from '@/helper/submit-event'
 import { NodePath } from '@/schema/article-renderer'
 
@@ -31,3 +31,63 @@ export function Spoiler({ body, title, path }: SpoilerProps) {
     </div>
   )
 }
+
+function SpoilerTitle({
+  open,
+  children,
+  onClick,
+  disabled,
+}: {
+  open: boolean
+  children: {}
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  disabled?: boolean
+}) {
+  return (
+    <StyledSpoilerTitle
+      onClick={!disabled ? onClick : undefined}
+      onPointerUp={(e) => e.currentTarget.blur()} //hack, use https://caniuse.com/#feat=css-focus-visible when supported
+      open={open}
+      interactive={!disabled}
+    >
+      {children}
+    </StyledSpoilerTitle>
+  )
+}
+
+function SpoilerToggle({ open }: { open: boolean }) {
+  return <span className="inline w-4">{open ? '▾ ' : '▸ '} </span>
+}
+
+const StyledSpoilerTitle = styled.button<{
+  open: boolean
+  interactive: boolean
+}>`
+  ${inputFontReset}
+  border: none;
+  margin: 0;
+  padding: 0;
+  font-size: 1.125rem;
+  line-height: 1.3;
+  padding: 10px 15px;
+  cursor: ${(props) => (props.interactive ? 'pointer' : 'auto')};
+  text-align: left;
+  color: ${(props) => (props.open ? '#fff' : props.theme.colors.dark1)};
+  background-color: ${(props) =>
+    props.open ? props.theme.colors.brand : props.theme.colors.bluewhite};
+
+  &:hover {
+    background-color: ${(props) =>
+      props.open
+        ? props.theme.colors.brand
+        : props.theme.colors.lightBlueBackground};
+  }
+
+  ${(props) =>
+    !props.interactive &&
+    css`
+      color: ${(props) => props.theme.colors.dark1};
+      background-color: ${(props) =>
+        props.theme.colors.lightBlueBackground} !important;
+    `}
+`
