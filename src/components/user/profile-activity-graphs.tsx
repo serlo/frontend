7c085c0fe39a3d2f067a21f40056a624841028ed
Stephan Kulla <git.mail@kulla.me>
2021-07-20 17:32:50 +0200

    Refactor src/components/user/profile-activity-graphs.tsx

--- src/components/user/profile-activity-graphs.tsx
+++ src/components/user/profile-activity-graphs.tsx
@@ -1,44 +1,27 @@
-import { Fragment } from 'react'
+import * as R from 'ramda'
 
 import { ProfileActivityGraph } from '@/components/user/profile-activity-graph'
 import { useInstanceData } from '@/contexts/instance-context'
 import { User } from '@/fetcher/query-types'
 
-interface ProfileActivityGraphsProps {
+export function ProfileActivityGraphs({
+  values,
+}: {
   values: User['activityByType']
-}
-
-export function ProfileActivityGraphs({ values }: ProfileActivityGraphsProps) {
+}) {
   const { strings } = useInstanceData()
-
-  const graphs = Object.keys(values).map((key) => {
-    const typedKey = key as keyof typeof values
-    const value = values[typedKey]
-    if (value > 0) {
-      return (
-        <ProfileActivityGraph
-          key={key}
-          title={strings.profiles.activityGraph[typedKey]}
-          value={value}
-        />
-      )
-    }
-    return null
-  })
 
   return (
     <section className="flex justify-center sm:justify-between flex-wrap sm:flex-nowrap my-20">
-      {graphs.map((graph, index) => {
-        if (!graph) return null
-        return (
-          <Fragment key={index}>
-            {graph}
-            {index === 1 && (
-              <div style={{ flexBasis: '100%' }} className="sm:hidden" />
-            )}
-          </Fragment>
-        )
-      })}
+      {R.toPairs(values)
+        .filter(([_type, value]) => value > 0)
+        .map(([type, value]) => (
+          <ProfileActivityGraph
+            key={type}
+            title={strings.profiles.activityGraph[type]}
+            value={value}
+          />
+        ))}
     </section>
   )
 }
