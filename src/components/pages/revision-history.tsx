bdb3c1519b11601ed2396712f14f19437e5ab190
Botho <1258870+elbotho@users.noreply.github.com>
2021-07-27 15:47:44 +0200

    refactor rev history

--- src/components/pages/revision-history.tsx
+++ src/components/pages/revision-history.tsx
@@ -6,7 +6,11 @@ import { UserLink } from '../user/user-link'
 import { Link } from '@/components/content/link'
 import { TimeAgo } from '@/components/time-ago'
 import { useInstanceData } from '@/contexts/instance-context'
-import type { CompBaseProps, HistoryRevisionsData } from '@/data-types'
+import type {
+  CompBaseProps,
+  HistoryRevisionData,
+  HistoryRevisionsData,
+} from '@/data-types'
 import { theme } from '@/theme'
 
 export interface RevisionHistoryProps {
@@ -17,66 +21,69 @@ export function RevisionHistory({ data }: RevisionHistoryProps) {
   const { strings } = useInstanceData()
   if (!data) return null
 
+  const { changes, status, author, date, view, edit } = strings.revisionHistory
+
   return (
     <table className="mx-side border-collapse w-full relative">
       <thead>
         <tr>
-          <Th>{strings.revisionHistory.changes}</Th>
-          <Th>{strings.revisionHistory.status}</Th>
-          <Th>{strings.revisionHistory.author}</Th>
-          <Th>{strings.revisionHistory.date}</Th>
-          <Th>{strings.revisionHistory.view}</Th>
-          <Th>{strings.revisionHistory.new}</Th>
+          {renderTh(changes)}
+          {renderTh(status)}
+          {renderTh(author)}
+          {renderTh(date)}
+          {renderTh(view)}
+          {renderTh(edit)}
         </tr>
       </thead>
-      <tbody>
-        {data.revisions?.nodes.map((entry) => {
-          const isCurrent = entry.id === data.currentRevision?.id
-          const viewUrl = `/entity/repository/compare/${data.id}/${entry.id}`
-
-          return (
-            <tr
-              key={entry.id}
-              className={isCurrent ? 'bg-brand-50' : undefined}
-            >
-              <Td>
-                <Link title={strings.revisionHistory.viewLabel} href={viewUrl}>
-                  <span className={isCurrent ? 'font-bold' : undefined}>
-                    {entry.changes || '–'}
-                  </span>
-                </Link>
-              </Td>
-              <Td centered>{getStatus(entry.trashed, isCurrent)}</Td>
-              <Td>
-                <UserLink user={entry.author} />
-              </Td>
-              <Td>
-                <TimeAgo datetime={new Date(entry.date)} dateAsTitle />
-              </Td>
-              <Td centered>
-                <Link
-                  className="serlo-button serlo-make-interactive-light my-0 mx-auto text-base"
-                  title={strings.revisionHistory.viewLabel}
-                  href={viewUrl}
-                >
-                  <FontAwesomeIcon icon={faEye} size="1x" />
-                </Link>
-              </Td>
-              <Td centered>
-                <Link
-                  className="serlo-button serlo-make-interactive-light my-0 mx-auto text-base"
-                  title={strings.revisionHistory.newLabel}
-                  href={`/entity/repository/add-revision/${data.id}/${entry.id}`}
-                >
-                  <FontAwesomeIcon icon={faPencilAlt} size="1x" />
-                </Link>
-              </Td>
-            </tr>
-          )
-        })}
-      </tbody>
+      <tbody>{data.revisions?.nodes.map(renderRow)}</tbody>
     </table>
   )
+
+  function renderRow(entry: HistoryRevisionData) {
+    const isCurrent = entry.id === data!.currentRevision?.id
+    const viewUrl = `/entity/repository/compare/${data!.id}/${entry.id}`
+
+    return (
+      <tr key={entry.id} className={isCurrent ? 'bg-brand-50' : undefined}>
+        <Td>
+          <Link title={strings.revisionHistory.viewLabel} href={viewUrl}>
+            <span className={isCurrent ? 'font-bold' : undefined}>
+              {entry.changes || '–'}
+            </span>
+          </Link>
+        </Td>
+        <Td centered>{getStatus(entry.trashed, isCurrent)}</Td>
+        <Td>
+          <UserLink user={entry.author} />
+        </Td>
+        <Td>
+          <TimeAgo datetime={new Date(entry.date)} dateAsTitle />
+        </Td>
+        <Td centered>
+          <Link
+            className="serlo-button serlo-make-interactive-light my-0 mx-auto text-base"
+            title={strings.revisionHistory.viewLabel}
+            href={viewUrl}
+          >
+            <FontAwesomeIcon icon={faEye} size="1x" />
+          </Link>
+        </Td>
+        <Td centered>
+          <Link
+            className="serlo-button serlo-make-interactive-light my-0 mx-auto text-base"
+            title={strings.revisionHistory.editLabel}
+            href={`/entity/repository/add-revision/${data!.id}/${entry.id}`}
+          >
+            <FontAwesomeIcon icon={faPencilAlt} size="1x" />
+          </Link>
+        </Td>
+      </tr>
+    )
+  }
+
+  function renderTh(text: string) {
+    return <th className="serlo-th sticky top-0 bg-white border-0">{text}</th>
+  }
 
   function getStatus(trashed?: boolean, isCurrent?: boolean) {
     return (
@@ -96,10 +103,6 @@ export function RevisionHistory({ data }: RevisionHistoryProps) {
     )
   }
 }
-
-const Th: CompBaseProps = ({ children }) => (
-  <th className="serlo-th sticky top-0 bg-white border-0">{children}</th>
-)
 
 const Td: CompBaseProps<{
   centered?: boolean
