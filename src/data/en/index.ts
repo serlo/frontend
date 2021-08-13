697d03c87c0c69bba9446b7fb96a85ccf7ce9126
Botho <1258870+elbotho@users.noreply.github.com>
2021-08-13 18:09:41 +0200

    Update source file index.ts

--- src/data/en/index.ts
+++ src/data/en/index.ts
@@ -28,7 +28,8 @@ export const instanceData = {
       applets: 'Applets',
       folders: 'Folders',
       exercises: 'Exercises',
-      events: 'Events'
+      events: 'Events',
+      unrevised: 'Unrevised'
     },
     entities: {
       applet: 'Applet',
@@ -72,7 +73,9 @@ export const instanceData = {
       title: 'Share!',
       copyLink: 'Copy link',
       copySuccess: 'Link copied! ',
-      close: 'Close'
+      close: 'Close',
+      pdf: 'Download as PDF',
+      pdfNoSolutions: 'PDF without solutions'
     },
     edit: {
       button: 'Edit',
@@ -101,7 +104,9 @@ export const instanceData = {
       check: 'Check',
       yourAnswer: 'Your answer…',
       chooseOption: 'Click on one of the options.',
+      printModeChooseOption: 'Check one of the options.',
       trashedNotice: 'This content is marked for deletion.',
+      unrevisedNotice: 'This content has no accepted revision yet. Please use the %link% to preview.',
       strategy: 'Solution Strategy',
       picture: 'Picture',
     },
@@ -182,6 +187,7 @@ export const instanceData = {
       backToHome: 'To our home page'
     },
     print: {
+      preparingNotice: 'Preparing print!',
       warning: 'IMPORTANT: To make sure all images and formulas print, please scroll down to the end of the page once BEFORE you open this dialog. Thank you!'
     },
     profiles: {
@@ -399,7 +405,8 @@ export const loggedInData = {
         title: 'Reject Revision',
         explanation: 'Please tell the author why you will not accept the submission.'
       },
-      confirm: 'Confirm'
+      confirm: 'Confirm',
+      unrevisedTaxNote: 'New content, not accepted yet'
     }
   }
 };
