e4e686f509e877fa25af93f4c2f36c5324572598
Botho <1258870+elbotho@users.noreply.github.com>
2021-08-13 20:10:08 +0200

    debug: try with more headers

--- src/pages/api/locale/[lang].ts
+++ src/pages/api/locale/[lang].ts
@@ -5,5 +5,14 @@ import { getLoggedInData } from '@/helper/feature-i18n'
 // need this to bypass CORS and cache responses
 export default function de(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
   res.json(getLoggedInData(req.query.lang as string))
 }
