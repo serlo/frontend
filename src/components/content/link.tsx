4f04a279c85058cacbf15189bc16f033677e4b27
Botho <1258870+elbotho@users.noreply.github.com>
2021-08-19 19:57:30 +0200

    use cf-worker for /ref links

--- src/components/content/link.tsx
+++ src/components/content/link.tsx
@@ -45,13 +45,14 @@ export function isLegacyLink(_href: string) {
     _href.startsWith('/api/auth') ||
     _href.startsWith('/discussions') ||
     _href.startsWith('/entity') ||
-    _href.startsWith('/math/wiki/') ||
+    _href.startsWith('/math/wiki/') || //temporary
+    _href.startsWith('/ref/') || // temporary
     _href.startsWith('/page') ||
     _href.startsWith('/taxonomy') ||
     _href.startsWith('/unsubscribe') ||
     _href.startsWith('/user/profile/') ||
     _href.startsWith('/subscription/update') ||
-    _href.includes('.serlo.org') //e.g. community.serlo.org or different language
+    _href.includes('.serlo.org') // e.g. community.serlo.org or different language
   )
 }
 
