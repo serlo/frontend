e5a46a6671b491cf910498afaef4b9b2d0616769
Entkenntnis <Entkenntnis@users.noreply.github.com>
2021-09-18 11:10:54 +0200

    use pure tailwind

--- src/components/content/equations.tsx
+++ src/components/content/equations.tsx
@@ -1,5 +1,6 @@
+import clsx from 'clsx'
 import { shade } from 'polished'
-import { Fragment } from 'react'
+import React, { Fragment } from 'react'
 
 import { FrontendContentNode, Sign } from '@/data-types'
 import { RenderNestedFunction } from '@/schema/article-renderer'
@@ -36,43 +37,46 @@ export function Equations({ steps, renderNested }: EquationProps) {
 
     return (
       <Fragment key={i}>
-        <style jsx>{`
-          .formula {
-            @apply align-baseline text-lg;
-          }
-          tr > td {
-            padding: 3px 3px 13px 3px;
-          }
-        `}</style>
         <tr>
-          <td className="text-right formula">
-            {step.left ? renderStepFormula('left') : null}
-          </td>
-          <td className="text-center formula">
-            {renderFormula(renderSignToString(step.sign), 'sign')}
-          </td>
-          <td className="text-left formula">
-            {step.right ? renderStepFormula('right') : null}
-          </td>
-          <td className="formula">
-            {step.transform ? (
+          {renderTD(step.left ? renderStepFormula('left') : null, 'text-right')}
+          {renderTD(
+            renderFormula(renderSignToString(step.sign), 'sign'),
+            'text-center'
+          )}
+          {renderTD(
+            step.right ? renderStepFormula('right') : null,
+            'text-left'
+          )}
+          {renderTD(
+            step.transform ? (
               <span className="border-l border-black pl-1">
                 {renderStepFormula('transform')}
               </span>
-            ) : null}
-          </td>
+            ) : null
+          )}
         </tr>
         {hasExplanation ? (
           <tr className="whitespace-normal" style={{ color: explanationColor }}>
             <td />
             {renderDownArrow()}
-            <td colSpan={2}>
+            <td colSpan={2} className="px-1 pt-1 pb-3">
               {renderNested(step.explanation, `step${i}`, 'explaination')}
             </td>
           </tr>
         ) : null}
       </Fragment>
     )
+
+    function renderTD(
+      content: JSX.Element | React.ReactNode[] | null,
+      align?: 'text-left' | 'text-right' | 'text-center'
+    ) {
+      return (
+        <td className={clsx('align-baseline text-lg px-1 pt-1 pb-3', align)}>
+          {content}
+        </td>
+      )
+    }
 
     function renderStepFormula(key: 'transform' | 'left' | 'right') {
       return renderFormula('\\displaystyle ' + step[key], key)
