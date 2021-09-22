794f584cdbd2de311ecc26dc748f6ec9203a6414
Entkenntnis <Entkenntnis@users.noreply.github.com>
2021-09-22 10:22:22 +0200

    refactor mobile menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
           DEFAULT: brand,
           light: tint(0.3, brand), // <- lightBlue
           lighter: tint(0.55, brand), // <- lighterBlue
+          300: tint(0.73, brand),
           150: tint(0.85, brand), // <-lightBlueBackground
           100: tint(0.94, brand), // <- bluewhite
           50: tint(0.96, brand), // <- lightBackground
