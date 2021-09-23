f63e654694e0d214ef5f164d33c9036abbd750dd
Entkenntnis <Entkenntnis@users.noreply.github.com>
2021-09-23 14:45:39 +0200

    refactor share modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -78,6 +78,7 @@ module.exports = {
         brand: `0 0 10px ${brand}, 0 0 5px ${brand}`,
         menu: 'rgba(0, 0, 0, 0.2) 0px 2px 4px;',
         modal: '0px 0px 10px 0px rgba(0, 0, 0, 0.25)',
+        input: `0 0 4px 0 ${brand}`,
       },
       fontFamily: {
         serlo: 'Karmilla, sans-serif',
