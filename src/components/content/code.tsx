3087f05d8841512fbeb73442d37c2c3e501609db
Entkenntnis <Entkenntnis@users.noreply.github.com>
2021-09-28 11:18:49 +0200

    use cjs instead of esm

--- src/components/content/code.tsx
+++ src/components/content/code.tsx
@@ -1,22 +1,20 @@
 /* eslint-disable import/no-internal-modules */
 import * as React from 'react'
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+import c from 'react-syntax-highlighter/dist/cjs/languages/prism/c'
+import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
+import java from 'react-syntax-highlighter/dist/cjs/languages/prism/java'
+import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
+import latex from 'react-syntax-highlighter/dist/cjs/languages/prism/latex'
+import markup from 'react-syntax-highlighter/dist/cjs/languages/prism/markup'
+import python from 'react-syntax-highlighter/dist/cjs/languages/prism/python'
 import coy from 'react-syntax-highlighter/dist/cjs/styles/prism/coy'
-import xml from 'react-syntax-highlighter/dist/esm/languages/hljs/xml'
-import c from 'react-syntax-highlighter/dist/esm/languages/prism/c'
-import css from 'react-syntax-highlighter/dist/esm/languages/prism/css'
-import java from 'react-syntax-highlighter/dist/esm/languages/prism/java'
-import javascript from 'react-syntax-highlighter/dist/esm/languages/prism/javascript'
-import latex from 'react-syntax-highlighter/dist/esm/languages/prism/latex'
-import markup from 'react-syntax-highlighter/dist/esm/languages/prism/markup'
-import python from 'react-syntax-highlighter/dist/esm/languages/prism/python'
 
 SyntaxHighlighter.registerLanguage('java', java)
 SyntaxHighlighter.registerLanguage('javascript', javascript)
 SyntaxHighlighter.registerLanguage('python', python)
 SyntaxHighlighter.registerLanguage('css', css)
 SyntaxHighlighter.registerLanguage('latex', latex)
-SyntaxHighlighter.registerLanguage('html', xml)
 SyntaxHighlighter.registerLanguage('markup', markup)
 SyntaxHighlighter.registerLanguage('c', c)
 
