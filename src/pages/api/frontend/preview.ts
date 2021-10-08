5d8bd25ed04829bf0a51fa7ebc619f732a82fea0
Entkenntnis <Entkenntnis@users.noreply.github.com>
2021-10-08 23:31:26 +0200

    clean up

--- src/pages/api/frontend/preview.ts
+++ src/pages/api/frontend/preview.ts
@@ -1,14 +1,14 @@
-// Path=/;
-
 import type { NextApiRequest, NextApiResponse } from 'next'
+
+const cookie = '__serlo_preview=1; Path=/;'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   res.setPreviewData({})
   const cookies = res.getHeader('Set-Cookie')
   if (Array.isArray(cookies)) {
-    res.setHeader('Set-Cookie', [...cookies, '__serlo_preview=1; Path=/;'])
+    res.setHeader('Set-Cookie', [...cookies, cookie])
   } else {
-    res.setHeader('Set-Cookie', '__serlo_preview=1; Path=/;')
+    res.setHeader('Set-Cookie', cookie)
   }
   res.end('Preview mode enabled')
 }
