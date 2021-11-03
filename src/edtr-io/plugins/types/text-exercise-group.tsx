ca1c88eabf588f629a608037339a9ba0249d1b28
Knorrke <knorr.b@gmx.de>
2021-11-03 21:29:00 +0100

    remove react-virtual

--- src/edtr-io/plugins/types/text-exercise-group.tsx
+++ src/edtr-io/plugins/types/text-exercise-group.tsx
@@ -2,7 +2,6 @@
 import { AddButton } from '@edtr-io/editor-ui/internal'
 import { boolean, EditorPlugin, EditorPluginProps, list } from '@edtr-io/plugin'
 import * as React from 'react'
-import { useVirtual } from 'react-virtual'
 
 import { SemanticSection } from '../helpers/semantic-section'
 import {
@@ -15,70 +14,6 @@ import {
 import { RevisionHistoryLoader } from './helpers/revision-history-loader'
 import { ToolbarMain } from './toolbar-main/toolbar-main'
 import { useLoggedInData } from '@/contexts/logged-in-data-context'
-
-// https://github.com/tannerlinsley/react-virtual/issues/167
-function useVirtualResizeObserver<T>(options: {
-  size: number
-  parentRef: React.RefObject<T>
-  estimateSize: () => number
-}) {
-  const measureRefCacheRef = React.useRef<
-    Record<string, (el: HTMLElement | null) => void>
-  >({})
-  const elCacheRef = React.useRef<Record<number, HTMLElement | null>>({})
-
-  const resizeObserverRef = React.useRef(
-    new ResizeObserver((entries) => {
-      entries.forEach((entry) => {
-        const el = entry.target as HTMLElement
-        const index = el.getAttribute('data-index')
-        if (index !== null) measureRefCacheRef.current[index](el)
-      })
-    })
-  )
-
-  React.useEffect(() => {
-    const resizeObserver = resizeObserverRef.current
-    return () => {
-      resizeObserver.disconnect()
-    }
-  }, [])
-
-  const rowVirtualizer = useVirtual<T>(options)
-
-  const refs = React.useMemo(() => {
-    const obj: Record<number, (el: HTMLElement | null) => void> = {}
-    for (let i = 0; i < options.size; i++) {
-      obj[i] = (el: HTMLElement | null) => {
-        const currentElCache = elCacheRef.current[i]
-        if (currentElCache) {
-          resizeObserverRef.current.unobserve(currentElCache)
-        }
-
-        if (el) {
-          // sync
-          measureRefCacheRef.current[i](el)
-
-          el.setAttribute('data-index', i.toString())
-          resizeObserverRef.current.observe(el)
-        }
-
-        elCacheRef.current[i] = el
-      }
-    }
-    return obj
-  }, [options.size])
-
-  for (let i = 0; i < rowVirtualizer.virtualItems.length; i++) {
-    const item = rowVirtualizer.virtualItems[i]
-    if (item.measureRef !== refs[item.index]) {
-      measureRefCacheRef.current[item.index] = item.measureRef
-    }
-    item.measureRef = refs[item.index]
-  }
-
-  return rowVirtualizer
-}
 
 export const textExerciseGroupTypeState = entityType(
   {
@@ -104,14 +39,6 @@ function TextExerciseGroupTypeEditor(
 ) {
   const { cohesive, content, 'grouped-text-exercise': children } = props.state
   const isCohesive = cohesive.value ?? false
-
-  const virtualParent = React.useRef(null)
-
-  const virtualizer = useVirtualResizeObserver({
-    size: children.length,
-    parentRef: virtualParent,
-    estimateSize: React.useCallback(() => 35, []),
-  })
 
   const loggedInData = useLoggedInData()
   if (!loggedInData) return null
@@ -142,53 +69,20 @@ function TextExerciseGroupTypeEditor(
           {contentRendered}
         </SemanticSection>
       </section>
-      <div
-        ref={virtualParent}
-        style={{
-          height: '100%',
-          overflow: 'auto',
-        }}
-      >
-        <div
-          style={{
-            height: `${virtualizer.totalSize}px`,
-            width: '100%',
-            position: 'relative',
-          }}
-        >
-          {virtualizer.virtualItems.map((virtualRow) => {
-            const child = children[virtualRow.index]
-            return (
-              <div
-                key={virtualRow.index}
-                ref={virtualRow.measureRef}
-                style={{
-                  position: 'absolute',
-                  top: 0,
-                  left: 0,
-                  width: '100%',
-                  transform: `translateY(${virtualRow.start}px)`,
-                }}
-              >
-                <section className="row" key={child.id}>
-                  <div className="col-sm-1 hidden-xs">
-                    <em>{getExerciseIndex(virtualRow.index)})</em>
-                  </div>
-                  <div className="col-sm-11 col-xs-12">
-                    <OptionalChild
-                      state={child}
-                      removeLabel={
-                        editorStrings.textExerciseGroup.removeExercise
-                      }
-                      onRemove={() => children.remove(virtualRow.index)}
-                    />
-                  </div>
-                </section>
-              </div>
-            )
-          })}
-        </div>
-      </div>
+      {children.map((child, index) => (
+        <section className="row" key={child.id}>
+          <div className="col-sm-1 hidden-xs">
+            <em>{getExerciseIndex(index)})</em>
+          </div>
+          <div className="col-sm-11 col-xs-12">
+            <OptionalChild
+              state={child}
+              removeLabel={editorStrings.textExerciseGroup.removeExercise}
+              onRemove={() => children.remove(index)}
+            />
+          </div>
+        </section>
+      ))}
       <AddButton onClick={() => children.insert()}>
         {editorStrings.textExerciseGroup.addExercise}
       </AddButton>
