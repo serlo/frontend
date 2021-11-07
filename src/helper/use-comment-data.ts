207376916a11e54dec9bc980df499d3752cb08ef
Botho <1258870+elbotho@users.noreply.github.com>
2021-11-07 21:09:05 +0100

    report to sentry when fetching comments without id

--- src/helper/use-comment-data.ts
+++ src/helper/use-comment-data.ts
@@ -2,6 +2,7 @@ import { ThreadAware } from '@serlo/api'
 import { gql, GraphQLClient } from 'graphql-request'
 import useSWR from 'swr'
 
+import { triggerSentry } from './trigger-sentry'
 import { endpoint } from '@/api/endpoint'
 
 const query = gql`
@@ -36,6 +37,7 @@ const query = gql`
   }
 `
 export function useCommentData(id: number) {
+  if (!id) triggerSentry({ message: 'trying to fetch comments without id' })
   const client = new GraphQLClient(endpoint)
   const fetcher = () => client.request(query, { id })
   const resp = useSWR<{ uuid: ThreadAware }, object>(
