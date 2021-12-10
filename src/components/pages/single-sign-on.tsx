bd949359838a416dd57cb3be877ba320814ffa78
Botho <1258870+elbotho@users.noreply.github.com>
2021-12-10 09:39:12 +0100

    refactor component

--- src/components/pages/single-sign-on.tsx
+++ src/components/pages/single-sign-on.tsx
@@ -1,6 +1,8 @@
 import { useKeycloak } from '@react-keycloak/ssr'
+import clsx from 'clsx'
 import type { KeycloakInstance, KeycloakTokenParsed } from 'keycloak-js'
 
+import { PartnerListNew } from '../landing/rework/partner-list-new'
 import { HeadTags } from '@/components/head-tags'
 import { Logo } from '@/components/navigation/logo'
 
@@ -15,85 +17,106 @@ export function SingleSignOn() {
   const { keycloak } = useKeycloak<KeycloakInstance>()
 
   const parsedToken: ParsedToken | undefined = keycloak?.tokenParsed
-
-  const loggedinState = keycloak?.authenticated ? (
-    <span className="text-success">angemeldet</span>
-  ) : (
-    <span className="text-danger">nicht angemeldet</span>
-  )
-
-  const greeting =
-    keycloak?.authenticated || (keycloak && parsedToken)
-      ? `Willkommen zurück ${parsedToken?.preferred_username ?? ''}!`
-      : 'Willkommen Visitor. Melde dich bitte an.'
+  const username = parsedToken?.preferred_username ?? ''
+  const isLoggedIn = keycloak?.authenticated
 
   return (
     <>
-      <HeadTags data={{ title: 'Single-Sign-On – Serlo.org' }} />
+      <HeadTags data={{ title: 'Single Sign-On – serlo.org' }} />
       <header className="px-side pt-12 bg-brand-100 sm:text-center pb-5">
         <div className="pb-6">
           <Logo subline="" noLink />
         </div>
-        {keycloak?.authenticated ? (
-          <>
-            <button
-              type="button"
-              className="serlo-button serlo-make-interactive-transparent-blue"
-              onClick={() => {
-                if (keycloak) {
-                  window.location.href = keycloak.createAccountUrl()
-                }
-              }}
-            >
-              Mein Konto
-            </button>
+        {renderButtons()}
+      </header>
+      <section
+        className={clsx(
+          'serlo-responsive-margin mt-5 mb-10',
+          'md:mt-12 md:px-8 lg:block text-center min-h-1/2'
+        )}
+      >
+        <div className="max-w-xl sm:mx-auto text-lg">
+          <h1 className="serlo-h1 text-brand mt-16 mb-5">
+            👋 Willkommen{username ? ` zurück ${username}` : ''}!<br />
+          </h1>
+          <p className="serlo-p text-2xl font-bold tracking-tight">
+            Du bist {isLoggedIn ? '' : 'nicht'} angemeldet{' '}
+            <small>{isLoggedIn ? '🟢' : '⚪️'}</small>
+          </p>
+        </div>
+      </section>
+      <footer className="partner font-bold text-center serlo-responsive-padding pb-1">
+        <h2 className="font-bold pt-16 pb-12 text-center">
+          Partner und Unterstützer
+        </h2>
+        <PartnerListNew />
+        <nav>
+          <a
+            href="https://de.serlo.org/serlo"
+            target="_blank"
+            rel="noreferrer"
+            className="serlo-link"
+          >
+            Über Serlo
+          </a>
+          {' • '}
+          <a
+            href="https://de.serlo.org/privacy"
+            target="_blank"
+            rel="noreferrer"
+            className="serlo-link"
+          >
+            Datenschutz
+          </a>
+          {' • '}
+          <a
+            href="https://de.serlo.org/imprint"
+            target="_blank"
+            rel="noreferrer"
+            className="serlo-link"
+          >
+            Impressum
+          </a>
+        </nav>
+        <style jsx>
+          {`
+            .partner {
+              background: url('/_assets/img/landing/about-container.svg');
+              border-bottom: 2rem solid #ffefda;
+              background-size: cover;
+            }
+          `}
+        </style>
+      </footer>
+    </>
+  )
 
-            <button
-              type="button"
-              className="serlo-button serlo-make-interactive-transparent-blue"
-              onClick={() => {
-                if (keycloak) {
-                  window.location.href = keycloak.createLogoutUrl()
-                }
-              }}
-            >
+  function renderButtons() {
+    if (!keycloak) return null
+    const btnClass = 'serlo-button serlo-make-interactive-transparent-blue'
+
+    return (
+      <nav>
+        {keycloak.authenticated ? (
+          <>
+            <a className={btnClass} href={keycloak.createAccountUrl()}>
+              Mein Konto
+            </a>
+            <a className={btnClass} href={keycloak.createLogoutUrl()}>
               Abmelden
-            </button>
+            </a>
           </>
         ) : (
           <>
-            <button
-              type="button"
-              className="serlo-button serlo-make-interactive-transparent-blue"
-              onClick={() => {
-                if (keycloak) {
-                  window.location.href = keycloak.createRegisterUrl()
-                }
-              }}
-            >
+            <a className={btnClass} href={keycloak.createRegisterUrl()}>
               Registrieren
-            </button>
-
-            <button
-              type="button"
-              className="serlo-button serlo-make-interactive-transparent-blue"
-              onClick={() => {
-                if (keycloak) {
-                  window.location.href = keycloak.createLoginUrl()
-                }
-              }}
-            >
+            </a>
+            <a className={btnClass} href={keycloak.createLoginUrl()}>
               Anmelden
-            </button>
+            </a>
           </>
         )}
-      </header>
-      <p className="italic pt-5 text-center text-brand font-handwritten text-3xl landing-button-with-wings landing-button-with-wink p-with-wink">
-        {greeting}
-      </p>
-      <h1 className="text-center text-5xl font-extrabold tracking-tight max-w-2xl mt-3 mb-6 mx-auto">
-        Du bist {loggedinState}
-      </h1>
-    </>
-  )
+      </nav>
+    )
+  }
 }
