69dc999a49c78f3df8f917a09b9065b4f8a3c782
Botho <1258870+elbotho@users.noreply.github.com>
2021-12-10 10:19:02 +0100

    use env to set kc client id

--- env.d.ts
+++ env.d.ts
@@ -12,6 +12,8 @@ declare namespace NodeJS {
     NEXT_PUBLIC_GA_TRACKING_ID: string | undefined
     // The GitHub SHA of the commit the deployment was triggered by (defined in preview & production)
     NEXT_PUBLIC_COMMIT_SHA: string | undefined
+    // Keycloak client id
+    NEXT_PUBLIC_KEYCLOAK_CLIENT_ID: string
 
     // Hydra client id for production environment (defined in preview & production, only exposed to server)
     HYDRA_CLIENT_ID_PRODUCTION: string | undefined
