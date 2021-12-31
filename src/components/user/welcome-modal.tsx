0ce6565965529a7be03f3df01c1e5151d41284f0
Stephan Kulla <git.mail@kulla.me>
2021-12-31 19:24:09 +0100

    user-journey: Fix reading of query data

--- src/components/user/welcome-modal.tsx
+++ src/components/user/welcome-modal.tsx
@@ -1,37 +1,26 @@
 import { MouseEvent, useState } from 'react'
 
 import { ModalWithCloseButton } from '../modal-with-close-button'
+import { endpointEnmeshed } from '@/api/endpoint'
 import { LoadingSpinner } from '@/components/loading/loading-spinner'
+import { triggerSentry } from '@/helper/trigger-sentry'
 import { useEnmeshed } from '@/helper/use-enmeshed'
-import { useEnmeshedQR } from '@/helper/use-enmeshed-qr'
 
 const sessionStorageKey = 'session-id-enmeshed'
-
-function getSessionId() {
-  if (typeof window === 'undefined') return 'foo'
-
-  const stored = sessionStorage.getItem(sessionStorageKey)
-  if (stored) return stored
-
-  const newId = (Math.random() + 1).toString(36).substr(2, 13) //random string
-
-  sessionStorage.setItem(sessionStorageKey, newId)
-  return newId
-}
 
 export function WelcomeModal({ callback }: { callback: () => void }) {
   const [showModal, setShowModal] = useState(false)
 
   const sessionId = getSessionId()
 
-  const [qrCodeSrc, getQrCode] = useEnmeshedQR(sessionId)
+  const [qrCodeSrc, setQrCodeSrc] = useState('')
   const [enmeshedAttributes, queryAttributes, writeAttribute] =
     useEnmeshed(sessionId)
 
   const handleOnClick = (event: MouseEvent) => {
     event.preventDefault()
     setShowModal(true)
-    getQrCode()
+    fetchQRCode()
   }
 
   const handleMockLoad = () => {
@@ -110,4 +99,90 @@ export function WelcomeModal({ callback }: { callback: () => void }) {
       </ModalWithCloseButton>
     </>
   )
+
+  function fetchQRCode() {
+    //TODO: get name and session id from user
+
+    fetch(
+      `${endpointEnmeshed}/init?sessionId=${sessionId}&name=Botho%20Willer`,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'image/png',
+        },
+      }
+    )
+      .then((res) => res.blob())
+      .then((res) => {
+        const urlCreator = window.URL || window.webkitURL
+        setQrCodeSrc(urlCreator.createObjectURL(res))
+        // TODO: When the workflow has been defined in the future we should revoke the object URL when done with:
+        // urlCreator.revokeObjectUrl(qrCode)
+      })
+      .then(fetchAttributes)
+      .catch((e) => {
+        // eslint-disable-next-line no-console
+        console.log(JSON.stringify(e))
+
+        triggerSentry({
+          message: `error while creating qr code: ${JSON.stringify(e)}`,
+        })
+      })
+  }
+
+  function fetchAttributes() {
+    fetch(`${endpointEnmeshed}/attributes?sessionId=${sessionId}`, {})
+      .then((res) => res.json())
+      .then((body: EnmeshedResponse) => {
+        console.log(body)
+        if (body.status === 'pending') {
+          setTimeout(fetchAttributes, 1000)
+        }
+        if (body.status === 'success') {
+          console.log(body.attributes)
+          setShowModal(false)
+          callback()
+        }
+      })
+      .catch((e) => {
+        // eslint-disable-next-line no-console
+        console.log(JSON.stringify(e))
+        // triggerSentry({
+        //   message: `${JSON.stringify(e)}`,
+        // })
+      })
+  }
 }
+
+function getSessionId() {
+  if (typeof window === 'undefined') return 'foo'
+
+  const stored = sessionStorage.getItem(sessionStorageKey)
+  if (stored) return stored
+
+  const newId = (Math.random() + 1).toString(36).substr(2, 13) //random string
+
+  sessionStorage.setItem(sessionStorageKey, newId)
+  return newId
+}
+
+export interface EnmeshedErrorResponse {
+  status: 'error'
+  message: string
+}
+
+export interface EnmeshedPendingResponse {
+  status: 'pending'
+}
+
+export interface EnmeshedSuccessResponse {
+  status: 'success'
+  attributes: EnmeshedAttributes
+}
+
+export type EnmeshedResponse =
+  | EnmeshedErrorResponse
+  | EnmeshedPendingResponse
+  | EnmeshedSuccessResponse
+
+export type EnmeshedAttributes = { name: string; value: string }[] | false
