1434de9e75707848d5694ea49ccaba9ca3436a1a
Botho <1258870+elbotho@users.noreply.github.com>
2022-01-28 14:53:17 +0100

    change quickbar ui, close with esc & on blur

--- src/components/navigation/quickbar.tsx
+++ src/components/navigation/quickbar.tsx
@@ -1,7 +1,9 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import clsx from 'clsx'
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect, KeyboardEvent } from 'react'
+
+import { Link } from '../content/link'
 
 interface QuickbarDataEntry {
   title: string
@@ -18,6 +20,7 @@ type QuickbarData = QuickbarDataEntry[]
 export function Quickbar({ subject }: { subject?: string }) {
   const [data, setData] = useState<QuickbarData | null>(null)
   const [query, setQuery] = useState('')
+  const [open, setOpen] = useState(false)
   const inputRef = useRef<HTMLInputElement>(null)
 
   const [sel, setSel] = useState(-1)
@@ -39,9 +42,9 @@ export function Quickbar({ subject }: { subject?: string }) {
           const filteredData = subject
             ? data.filter(
                 (entry) =>
-                  entry.title.toLowerCase().includes(subject) ||
+                  entry.titleLower.includes(subject) ||
                   (entry.path.length > 0 &&
-                    entry.path[0].toLowerCase().includes(subject))
+                    entry.pathLower[0].includes(subject))
               )
             : data
           setData(filteredData)
@@ -51,7 +54,8 @@ export function Quickbar({ subject }: { subject?: string }) {
 
   useEffect(() => {
     setSel(-1)
-  }, [query])
+    setOpen(query && data ? true : false)
+  }, [query, data])
 
   let results: { entry: QuickbarDataEntry; score: number }[] = []
 
@@ -106,104 +110,123 @@ export function Quickbar({ subject }: { subject?: string }) {
     }
   }
 
+  function close() {
+    setOpen(false)
+  }
+
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    {
+      if (e.key == 'Escape') {
+        setQuery('')
+      }
+      if (e.key == 'ArrowDown' || e.key == 'ArrowUp' || e.key == 'Enter') {
+        if (e.key == 'ArrowDown' && sel < results.length) {
+          setSel(sel + 1)
+        }
+        if (e.key == 'ArrowUp' && sel >= 0) {
+          setSel(sel - 1)
+        }
+        if (e.key == 'Enter') {
+          if (sel == results.length) {
+            window.location.href = `//${host}/search?q=${encodeURIComponent(
+              query
+            )}`
+          }
+          if (sel >= 0 && sel < results.length) {
+            window.location.href = `//${host}/${results[sel].entry.id}`
+          }
+        }
+        e.preventDefault()
+        return false
+      }
+    }
+  }
+
   return (
     <div className="max-w-2xl mx-auto px-4 text-left font-normal">
       <div className="relative">
-        <input
-          type="text"
-          className="border-2 rounded-3xl pl-5 pr-12 h-12 w-full text-lg hover:shadow focus:shadow outline-none"
-          value={query}
-          onChange={(value) => setQuery(value.target.value)}
-          placeholder="... heute lerne ich"
-          ref={inputRef}
-          onBlur={() => {
-            setSel(-1)
-          }}
-          onKeyDown={(e) => {
-            if (
-              e.key == 'ArrowDown' ||
-              e.key == 'ArrowUp' ||
-              e.key == 'Enter'
-            ) {
-              if (e.key == 'ArrowDown' && sel < results.length) {
-                setSel(sel + 1)
-              }
-              if (e.key == 'ArrowUp' && sel >= 0) {
-                setSel(sel - 1)
-              }
-              if (e.key == 'Enter') {
-                if (sel == results.length) {
-                  window.location.href = `//${host}/search?q=${encodeURIComponent(
-                    query
-                  )}`
-                }
-                if (sel >= 0 && sel < results.length) {
-                  window.location.href = `//${host}/${results[sel].entry.id}`
-                }
-              }
-              e.preventDefault()
-              return false
-            }
-          }}
-        />
-        {query && (
-          <div
-            className="absolute top-0 right-0 bottom-0 flex items-center justify-center w-12 cursor-pointer text-gray-300"
-            onClick={() => {
-              setQuery('')
-              setTimeout(() => {
-                inputRef.current?.focus()
-              }, 0)
-            }}
-          >
-            <FontAwesomeIcon icon={faTimes} />
-          </div>
-        )}
-        {data && query && (
-          <div className="px-5 pb-2 border rounded-xl shadow absolute top-14 w-full bg-white z-20">
-            {results.map((x, i) => (
-              <p key={i} className={clsx('my-2', { 'bg-brand-50': i == sel })}>
-                <span className="text-sm text-gray-700">
-                  {x.entry.path.join(' > ')}
-                  {x.entry.path.length > 0 ? ' > ' : ''}
-                </span>
-                <a
-                  className="cursor-pointer"
-                  href={`//${host}/${x.entry.id}`}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span className="text-lg text-brand hover:underline">
-                    {x.entry.isTax ? (
-                      <>{x.entry.title}&nbsp;&gt;</>
-                    ) : (
-                      x.entry.title
-                    )}
-                  </span>
-                </a>
-              </p>
-            ))}
-            <p
-              className={clsx('text-lg mt-2 text-gray-800', {
-                'bg-brand-50': sel == results.length,
-              })}
-            >
-              <a
-                href={`//${host}/search?q=${encodeURIComponent(query)}`}
-                target="_blank"
-                rel="noreferrer"
-                className="cursor-pointer hover:text-black"
-              >
-                Auf Serlo nach{' '}
-                <i>
-                  <strong>{query}</strong>
-                </i>{' '}
-                suchen ...
-              </a>
-            </p>
-          </div>
-        )}
+        {renderInput()}
+        {query && renderResetButton()}
+        {open && renderOverlay()}
       </div>
     </div>
   )
+
+  function renderInput() {
+    return (
+      <input
+        type="text"
+        className="border-2 rounded-3xl pl-5 pr-12 h-12 w-full text-lg hover:shadow focus:shadow outline-none"
+        value={query}
+        onChange={(value) => setQuery(value.target.value)}
+        placeholder="... heute lerne ich"
+        ref={inputRef}
+        onBlur={() => {
+          setTimeout(() => {
+            close()
+          }, 200)
+        }}
+        onFocus={() => {
+          if (query && data) setOpen(true)
+        }}
+        onKeyDown={onKeyDown}
+      />
+    )
+  }
+  function renderResetButton() {
+    return (
+      <div
+        className="absolute top-0 right-0 bottom-0 flex items-center justify-center w-12 cursor-pointer text-gray-300"
+        onClick={() => {
+          setQuery('')
+          setTimeout(() => {
+            inputRef.current?.focus()
+          }, 0)
+        }}
+      >
+        <FontAwesomeIcon icon={faTimes} />
+      </div>
+    )
+  }
+  function renderOverlay() {
+    return (
+      <div className="px-5 pb-2 border rounded-xl shadow absolute top-14 w-full bg-white z-20">
+        {results.map((x, i) => (
+          <Link
+            key={i}
+            className="hover:no-underline group"
+            href={`/${x.entry.id}`}
+          >
+            <p className={clsx('my-2', { 'bg-brand-50': i == sel })}>
+              <span className="text-sm text-gray-700">
+                {x.entry.path.join(' > ')}
+                {x.entry.path.length > 0 ? ' > ' : ''}
+              </span>
+
+              <span className="text-lg text-brand group-hover:underline">
+                {x.entry.isTax ? <>{x.entry.title}&nbsp;&gt;</> : x.entry.title}
+              </span>
+            </p>
+          </Link>
+        ))}
+        <p
+          className={clsx('text-lg mt-2 text-gray-800', {
+            'bg-brand-50': sel == results.length,
+          })}
+        >
+          <a
+            href={`//${host}/search?q=${encodeURIComponent(query)}`}
+            rel="noreferrer"
+            className="cursor-pointer hover:text-black"
+          >
+            Auf Serlo nach{' '}
+            <i>
+              <strong>{query}</strong>
+            </i>{' '}
+            suchen ...
+          </a>
+        </p>
+      </div>
+    )
+  }
 }
