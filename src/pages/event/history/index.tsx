fe81f148d38f4b5e1b469e561cdd7b7f00cc3aec
Entkenntnis <Entkenntnis@users.noreply.github.com>
2022-03-08 14:44:14 +0100

    remove unused props

--- src/pages/event/history/index.tsx
+++ src/pages/event/history/index.tsx
@@ -2,10 +2,9 @@ import { PageTitle } from '@/components/content/page-title'
 import { FrontendClientBase } from '@/components/frontend-client-base'
 import { Events } from '@/components/user/events'
 import { useInstanceData } from '@/contexts/instance-context'
-import { SlugProps_OLD } from '@/data-types'
 import { renderedPageNoHooks } from '@/helper/rendered-page'
 
-export default renderedPageNoHooks<SlugProps_OLD>(() => {
+export default renderedPageNoHooks(() => {
   return (
     <FrontendClientBase>
       <Content />
