4f4a68fc2d4d0e7bf106f42a23382f5a2a6fd207
Botho <1258870+elbotho@users.noreply.github.com>
2022-03-15 00:29:34 +0100

    also use folder icon on mobile

--- src/components/navigation/breadcrumbs.tsx
+++ src/components/navigation/breadcrumbs.tsx
@@ -96,7 +96,10 @@ function BreadcrumbEntries({
             path={['breadcrumbs', i]}
           >
             <span className="sm:hidden pt-0.25 pr-1">
-              <FontAwesomeIcon icon={faArrowCircleLeft} size="1x" />
+              <FontAwesomeIcon
+                icon={asBackButton ? faArrowCircleLeft : faFolderOpen}
+                size="1x"
+              />
             </span>
             {!isTaxonomy && (
               <span className="hidden sm:inline text-base pt-0.25 pr-1">
