071957de58782709a0d0c49d22f4306ff0bb47c2
Botho <1258870+elbotho@users.noreply.github.com>
2022-03-16 21:23:51 +0100

    New translations index.ts (Hindi)

--- src/data/hi/index.ts
+++ src/data/hi/index.ts
@@ -90,7 +90,7 @@ export const instanceData = {
       nonFree: "इस सामग्री का उपयोग हमारी अन्य सामग्री की तुलना में अधिक प्रतिबंधित हो सकता है।"
     },
     course: {
-      showPages: "पाठ्यक्रम का अवलोकन करें",
+      showPages: 'Course overview',
       pages: "पाठ्यक्रम अवलोकन",
       next: "अगला",
       back: 'Back',
