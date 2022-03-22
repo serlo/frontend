b0d31b7cb3a5734e6dca256e3654536129b6a659
Botho <1258870+elbotho@users.noreply.github.com>
2022-03-22 16:07:09 +0100

    fix next onbeforeunload problem

---
+++ src/helper/use-leave-confirm.ts
@@ -0,0 +1,35 @@
+import { useRouter } from 'next/router'
+import nProgress from 'nprogress'
+import * as React from 'react'
+
+import { useLoggedInData } from '@/contexts/logged-in-data-context'
+
+export function useLeaveConfirm(dirty: boolean) {
+  const Router = useRouter()
+  const loggedInData = useLoggedInData()
+
+  const onRouteChangeStart = React.useCallback(() => {
+    if (dirty) {
+      if (window.confirm(loggedInData?.strings.editor.confirmRouteChange)) {
+        return true
+      }
+      nProgress.done()
+      if (Router.asPath !== window.location.pathname) {
+        window.history.pushState('', '', Router.asPath)
+      }
+      throw "Abort route change by user's confirmation."
+    }
+  }, [dirty, loggedInData?.strings.editor.confirmRouteChange, Router.asPath])
+
+  React.useEffect(() => {
+    Router.events.on('routeChangeStart', onRouteChangeStart)
+    window.onbeforeunload = dirty ? () => true : null
+
+    return () => {
+      Router.events.off('routeChangeStart', onRouteChangeStart)
+      window.onbeforeunload = null
+    }
+  }, [onRouteChangeStart, Router.events, dirty])
+
+  return
+}
