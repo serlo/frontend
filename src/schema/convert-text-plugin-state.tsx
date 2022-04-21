28522d88a6c50eac28b72010487defc4bfed3b9f
Entkenntnis <Entkenntnis@users.noreply.github.com>
2022-04-21 13:27:04 +0200

    thats the basic idea

--- src/schema/convert-text-plugin-state.tsx
+++ src/schema/convert-text-plugin-state.tsx
@@ -4,9 +4,7 @@ import { sanitizeLatex } from './sanitize-latex'
 import type {
   FrontendContentNode,
   FrontendLiNode,
-  FrontendMathNode,
   FrontendTextColor,
-  FrontendTextNode,
 } from '@/data-types'
 
 const colors: FrontendTextColor[] = ['blue', 'green', 'orange']
@@ -31,7 +29,7 @@ export function convertTextPluginState(
 
 export function convertSlateBlock(node: NewElement): FrontendContentNode[] {
   if (node.type === 'p') {
-    return handleSemistructedContentOfP(convertTextPluginState(node.children))
+    return [{ type: 'p', children: convertTextPluginState(node.children) }]
   }
   if (node.type === 'a') {
     const children = convertTextPluginState(node.children)
@@ -124,9 +122,10 @@ export function convertSlateBlock(node: NewElement): FrontendContentNode[] {
     ]
   }
   if (node.type === 'list-item') {
-    const children = handleSemistructedContentOfP(
-      convertTextPluginState(node.children)
-    )
+    const children: FrontendContentNode[] = [
+      { type: 'p', children: convertTextPluginState(node.children) },
+    ]
+
     return [
       {
         type: 'li',
@@ -153,66 +152,4 @@ export function convertTextNode(node: NewText): FrontendContentNode[] {
       code: node.code,
     },
   ]
-}
-
-export function unwrapSingleMathInline(children: FrontendContentNode[]) {
-  return children.map((child) => {
-    if (
-      child.type == 'p' &&
-      child.children?.length == 1 &&
-      child.children[0].type == 'inline-math'
-    ) {
-      // force conversion
-      ;(child.children[0] as unknown as FrontendMathNode).type = 'math'
-      return child.children[0]
-    }
-    return child
-  })
-}
-
-export function handleSemistructedContentOfP(input: FrontendContentNode[]) {
-  // generate children, split text blocks at new lines
-  const children = input.flatMap((child) => {
-    if (child.type == 'text' && child.text.includes('\n')) {
-      return child.text.split('\n').flatMap((text, i) => {
-        const value: FrontendTextNode[] = []
-        if (i != 0) {
-          value.push({ type: 'text', text: '%%%BARRIER%%%' })
-        }
-        if (text) {
-          value.push({ type: 'text', text })
-        }
-        return value
-      })
-    }
-    return child
-  })
-
-  // group inline nodes together in p, don't merge if barrier is present
-  const result: FrontendContentNode[] = []
-  let resultAppendable = false
-  children.forEach((child) => {
-    if (
-      child.type == 'text' ||
-      child.type == 'a' ||
-      child.type == 'inline-math'
-    ) {
-      const last = result[result.length - 1]
-      if (child.type == 'text' && child.text == '%%%BARRIER%%%') {
-        resultAppendable = false
-        return
-      }
-      if (resultAppendable && last && last.type == 'p') {
-        last.children!.push(child)
-      } else {
-        result.push({ type: 'p', children: [child] })
-        resultAppendable = true
-      }
-    } else {
-      result.push(child)
-      resultAppendable = false
-    }
-  })
-
-  return unwrapSingleMathInline(result)
 }
