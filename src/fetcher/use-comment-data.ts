15bed757b6537255e1f9f48f531b0672c988f065
Entkenntnis <Entkenntnis@users.noreply.github.com>
2022-05-09 14:28:03 +0200

    fixing typings

--- src/fetcher/use-comment-data.ts
+++ src/fetcher/use-comment-data.ts
@@ -1,11 +1,9 @@
 import { gql, GraphQLClient } from 'graphql-request'
 import useSWR from 'swr'
 
-import {
-  GetCommentsQuery,
-  GetCommentsThreads_Article_Fragment,
-} from './graphql-types/operations'
+import { GetCommentsQuery } from './graphql-types/operations'
 import { endpoint } from '@/api/endpoint'
+import { hasOwnPropertyTs } from '@/helper/has-own-property-ts'
 
 const query = gql`
   query getComments($id: Int!) {
@@ -42,12 +40,10 @@ const query = gql`
   }
 `
 
-export type GetCommentsQueryUuid = Exclude<
+export type GetCommentsNode = Extract<
   GetCommentsQuery['uuid'],
-  { __typename?: 'Comment' }
->
-export type GetCommentsNode =
-  GetCommentsThreads_Article_Fragment['threads']['nodes'][number]
+  { threads: any }
+>['threads']['nodes'][number]
 
 export function useCommentData(id: number) {
   const client = new GraphQLClient(endpoint)
@@ -58,19 +54,28 @@ export function useCommentData(id: number) {
 
   const { data, error } = resp
 
-  const uuid = data?.uuid as GetCommentsQueryUuid
-  const threads = uuid?.threads.nodes
-
-  const activeThreads = threads?.filter((thread) => !thread.archived)
-  const archivedThreads = threads?.filter((thread) => thread.archived)
-
-  const commentData = { active: activeThreads, archived: archivedThreads }
-  const commentCount = threads?.reduce((acc, thread) => {
-    return acc + thread.comments.nodes.length
-  }, 0)
+  const uuid = data?.uuid
 
   // eslint-disable-next-line no-console
   if (error) console.log(error)
 
-  return { commentData, commentCount, error }
+  if (uuid && hasOwnPropertyTs(uuid, 'threads')) {
+    const threads = uuid?.threads.nodes
+
+    const activeThreads = threads?.filter((thread) => !thread.archived)
+    const archivedThreads = threads?.filter((thread) => thread.archived)
+
+    const commentData = { active: activeThreads, archived: archivedThreads }
+    const commentCount = threads?.reduce((acc, thread) => {
+      return acc + thread.comments.nodes.length
+    }, 0)
+
+    return { commentData, commentCount, error }
+  } else {
+    return {
+      commentData: { active: undefined, archived: undefined },
+      commentCount: undefined,
+      error,
+    }
+  }
 }
