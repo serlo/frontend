6591a7cf08ed5eb8696a68589c32374df15e6ddc
Botho <1258870+elbotho@users.noreply.github.com>
2022-05-11 18:58:31 +0200

    New translations index.ts (Tamil)

--- src/data/ta/index.ts
+++ src/data/ta/index.ts
@@ -406,7 +406,7 @@ export const loggedInData = {
       directLink: 'Direct link to this content'
     },
     notifications: {
-      hide: 'Hide notifications for this content.',
+      hide: 'Deactivate new notifications for this content.',
       setToRead: 'Set notification to read.',
       setAllToRead: 'Set all visible to read',
       showNew: 'New',
