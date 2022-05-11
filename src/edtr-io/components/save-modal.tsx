69a550933e395301b6f913b2882a312073f01742
Entkenntnis <Entkenntnis@users.noreply.github.com>
2022-05-11 12:40:13 +0200

    commited to wrong branch, revert
    Revert "use state in modal to store changes text and only update plugin state when saving"
    
    This reverts commit 053d7b2748b17fee52d0f7b94f933c0017c14abc.

--- src/edtr-io/components/save-modal.tsx
+++ src/edtr-io/components/save-modal.tsx
@@ -43,8 +43,6 @@ export function SaveModal({
   const buttonDisabled = !maySave || pending
   const isOnlyText = !showSkipCheckout && !subscriptions && !license && !changes
 
-  const [changesText, setChangesText] = useState(changes?.value ?? '')
-
   const loggedInData = useLoggedInData()
   if (!loggedInData) return null
   const { edtrIo } = loggedInData.strings.editor
@@ -90,7 +88,6 @@ export function SaveModal({
         </button>
         <button
           onClick={() => {
-            changes?.set(changesText)
             handleSave(
               notificationSubscription,
               emailSubscription,
@@ -141,10 +138,10 @@ export function SaveModal({
       <label className="font-bold">
         {edtrIo.changes}
         <textarea
-          value={changesText}
+          value={changes.value}
           onChange={(e) => {
             const { value } = e.target as HTMLTextAreaElement
-            setChangesText(value)
+            changes.set(value)
           }}
           className={clsx(
             'mt-1 mb-7 flex items-center rounded-2xl w-full p-2',
