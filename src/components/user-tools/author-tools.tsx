b9c597a5bfe4814e55bcb98e0a338f71810d48c2
Botho <1258870+elbotho@users.noreply.github.com>
2022-05-27 12:04:32 +0200

    add link to analytics data for reviewers

--- src/components/user-tools/author-tools.tsx
+++ src/components/user-tools/author-tools.tsx
@@ -41,6 +41,7 @@ export enum Tool {
   SortEntities = 'sortEntities',
   Trash = 'trash',
   DirectLink = 'directLink',
+  AnalyticsLink = 'analyticsLink',
 }
 
 interface ToolConfig {
@@ -177,6 +178,11 @@ export function AuthorTools({ tools, entityId, data }: AuthorToolsProps) {
       title: loggedInStrings.authorMenu.directLink,
       url: `/${data.id}`,
       canDo: true,
+    },
+    analyticsLink: {
+      title: loggedInStrings.authorMenu.analyticsLink,
+      url: `https://simpleanalytics.com/${lang}.serlo.org${data.alias ?? ''}`,
+      canDo: canDo(Entity.checkoutRevision) && data.alias,
     },
   } as ToolsConfig
 
