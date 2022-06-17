7e3c616746bc355f4c15235a0d89c33dd22c2a2b
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-06-17 14:37:37 +0200

    chore(kratos): add script for creating a user

---
+++ scripts/kratos/new-user.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+curl --request POST -sL \
+  --header "Content-Type: application/json" \
+  --data "{
+  \"traits\": {
+    \"username\": \"$1\",
+    \"email\": \"$2\"
+  }, 
+  \"credentials\": { 
+    \"password\": {
+        \"config\": {
+            \"password\": \"$3\"
+        }
+    }
+ }
+}" http://127.0.0.1:4434/identities
