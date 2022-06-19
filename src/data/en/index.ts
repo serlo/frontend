78628536e97f3720085526d69bdd6b58d3324214
Botho <1258870+elbotho@users.noreply.github.com>
2022-06-19 14:48:18 +0200

    image: alt fallback i18n

--- src/data/en/index.ts
+++ src/data/en/index.ts
@@ -122,6 +122,7 @@ export const instanceData = {
         'There is no content here. Please edit or delete.',
       picture: 'Picture',
       previewImage: 'Preview Image',
+      imageAltFallback: 'Image',
       exercisesTitle: 'Exercises',
       moreExercises: 'You can find more exercises in the following folder:',
       relatedContentTitle: 'Still want more?',
