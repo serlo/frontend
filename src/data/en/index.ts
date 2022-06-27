459eac87d14ef16302bd380edcaf3f447e0e0738
Botho <1258870+elbotho@users.noreply.github.com>
2022-06-27 17:30:20 +0200

    i18n try

--- src/data/en/index.ts
+++ src/data/en/index.ts
@@ -2,7 +2,7 @@
 import { Instance } from '@/fetcher/graphql-types/operations'
 import { headerData, footerData, landingSubjectsData,secondaryMenus } from './menu-data'
 export const instanceData = {
-  lang: Instance.En,
+  lang: Instance["En"],
   headerData: headerData,
   footerData: footerData,
   secondaryMenus: secondaryMenus,
@@ -364,7 +364,7 @@ export const instanceData = {
   },
 }
 export const instanceLandingData = {
-  lang: Instance.En,
+  lang: Instance["En"],
   subjectsData: landingSubjectsData,
   strings: {
     vision:
