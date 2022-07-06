9a6d23c869bd89ff9d9f0c89bdb4a56c8ece7549
Botho <1258870+elbotho@users.noreply.github.com>
2022-07-06 21:00:06 +0200

    cleanup

--- scripts/check-api-version.mjs
+++ scripts/check-api-version.mjs
@@ -11,23 +11,14 @@ const query = gql`
 `
 
 const require = createRequire(import.meta.url)
-// const { devDependencies } = require('../package.json')
 
 const getVersion = async (domain) => {
   const response = await request(`https://api.${domain}/graphql`, query)
   return response.metadata.version
 }
 
-// console.log(devDependencies)
-
 const stagingVersion = await getVersion('serlo-staging.dev')
 const productionVersion = await getVersion('serlo.org')
-// const dependencyVersion = devDependencies['@serlo/api'].replace('^', '')
 
-// console.log(`frontend needs: \x1b[32m ${dependencyVersion}\x1b[0m`)
 console.log(`staging is: \x1b[32m ${stagingVersion}\x1b[0m`)
 console.log(`production is: \x1b[32m ${productionVersion}\x1b[0m`)
-
-// if (productionVersion !== dependencyVersion) {
-//   throw '\x1b[33m ⚠️ \x1b[31m production does not match expected version!'
-// }
