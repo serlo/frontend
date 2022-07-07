9325659c361023ede5f7fabe7e1a826865170c31
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-07-07 11:08:36 +0200

    debug(kratos): send 400 to see if it is shown in frontend

--- src/pages/api/hydra/login.ts
+++ src/pages/api/hydra/login.ts
@@ -6,9 +6,11 @@ async function login(req: NextApiRequest, res: NextApiResponse) {
   const session = await kratos
     .toSession(undefined, String(req.headers.cookie))
     .then(({ data }) => data)
-    .catch((error) => {
-      res.status(500)
-      res.send(error)
+    .catch(() => {
+      res.status(400)
+      // TODO
+      res.send('error getting kratos sessions')
+      // res.send(error)
     })
   const query = req.query
   const challenge = String(query.login_challenge)
@@ -33,14 +35,14 @@ async function login(req: NextApiRequest, res: NextApiResponse) {
             res.redirect(body.redirect_to)
           })
           .catch(() => {
-            res.status(500)
+            res.status(400)
             // TODO
             res.send('Error when accepting login request')
             // res.send(error)
           })
       })
       .catch(() => {
-        res.status(500)
+        res.status(400)
         // TODO
         res.send('Error when getting login request')
         // res.send(error)
