4bdb88964edf512a4cb7bb2743647b9a31ff1099
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-07-21 00:01:38 +0200

    chore(kratos): fix env var in api for local dev and minor changes

--- scripts/kratos/prepare.sh
+++ scripts/kratos/prepare.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo
 echo 'Registering new client in hydra'
+echo
 docker-compose -f scripts/kratos/docker-compose.yml run hydra clients delete frontend.serlo.org --endpoint http://hydra:4445
 
 docker-compose -f scripts/kratos/docker-compose.yml run \
@@ -16,9 +18,15 @@ docker-compose -f scripts/kratos/docker-compose.yml run \
   --post-logout-callbacks http://localhost:3000/api/auth/logout-callback \
   --token-endpoint-auth-method client_secret_post
 
+echo
 echo 'Creating the user dev in Kratos'
+echo
 yarn kratos:newuser dev serlo@dev.org 123456
 
+echo
 echo 'Making authentication in api possible'
+echo
 cp -f scripts/kratos/graphql-fetch-cloudflare-auth.template src/api/graphql-fetch.ts
+echo
 echo "\033[0;31mImportant:\033[0m the file src/api/graphql-fetch.ts was modified in order to imitate the authentication made by the cloudflare worker. DO NOT COMMIT this change."
+echo
