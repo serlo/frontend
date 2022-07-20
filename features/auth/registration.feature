dcbcd7e369945e06e3bf846fb9c5e47e7593398c
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-07-21 00:00:54 +0200

    test(kratos): add registration test

---
+++ features/auth/registration.feature
@@ -0,0 +1,24 @@
+Feature: Registration
+  @registration
+  Scenario: User registers successfully
+    When I visit page '/auth/login'
+    # FIXME: typo
+    And I click on 'Neuen Account registeren'
+    And I click on 'Neuen Account registrieren' 
+
+    Then I should be on page '/auth/registration'
+    
+    When I fill in 'traits.email' with 'user@serlo.org'
+    And I fill in 'password' with '123456'
+    When I fill in 'traits.username' with 'serlouser'
+    And I click on the button 'Sign up'
+
+    When I click on 'Anmelden'
+    And I wait 3 seconds
+    And I fill in 'identifier' with 'serlouser'
+    And I fill in 'password' with '123456'
+    And I click on the button 'Sign in'
+
+    Then I should be on page '/auth/login-check'
+    And I wait 5 seconds
+    And I should see the text 'serlouser'
--- features/support/world.js
+++ features/support/world.js
@@ -1,5 +1,7 @@
 const { setWorldConstructor, Before, After } = require('cucumber')
 const { browser } = require('./browser')
+const { exec } = require('child_process')
+const { Configuration, V0alpha2Api } = require('@ory/kratos-client')
 
 class World {
   constructor() {
@@ -17,4 +19,30 @@ Before(async function () {
 
 After(async function () {
   browser.close()
+})
+After({ tags: '@registration' }, async function () {
+  const kratos = new V0alpha2Api(
+    new Configuration({
+      basePath: `http://localhost:4434`,
+    })
+  )
+  const allIdentities = await kratos
+    .adminListIdentities()
+    .then(({ data }) => data)
+  if (allIdentities) {
+    for (const identity of allIdentities) {
+      await kratos.adminDeleteIdentity(identity.id)
+    }
+  }
+  // TODO: find a way of deleting user in legacy db
+  // exec(`docker-compose -f scripts/kratos/docker-compose.yml exec mysql mysql --user=root --password=secret serlo  --execute="DELETE FROM user WHERE username = 'serlouser'"`, (error, stdout, stderr) => {
+  //   if (error) {
+  //       console.log(`error: ${error.message}`);
+  //       return;
+  //   }
+  //   if (stderr) {
+  //       console.log(`stderr: ${stderr}`);
+  //       return;
+  //   }
+  //   console.log(`stdout: ${stdout}`); })
 })
