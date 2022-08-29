5d99ff7a32785cdf9a0b66347b25a85187edbe99
Stephan Kulla <git.mail@kulla.me>
2022-08-29 12:31:48 +0200

    image-upload: Refactor src/edtr-io/plugins/image.tsx

--- src/edtr-io/plugins/image.tsx
+++ src/edtr-io/plugins/image.tsx
@@ -40,56 +40,12 @@ export interface FileError {
   message: string
 }
 
-function matchesAllowedExtensions(fileName: string) {
-  const extension = fileName.toLowerCase().slice(fileName.lastIndexOf('.') + 1)
-  return allowedExtensions.includes(extension)
-}
-
-function handleErrors(errors: FileErrorCode[]): FileError[] {
-  return errors.map((error) => ({
-    errorCode: error,
-    message: errorCodeToMessage(error),
-  }))
-}
-
-function onError(errors: FileError[]): void {
-  alert(errors.map((error) => error.message).join('\n'))
-}
-
-function errorCodeToMessage(error: FileErrorCode) {
-  switch (error) {
-    case FileErrorCode.TOO_MANY_FILES:
-      return 'You can only upload one file'
-    case FileErrorCode.NO_FILE_SELECTED:
-      return 'No file selected'
-    case FileErrorCode.BAD_EXTENSION:
-      return 'Not an accepted file type'
-    case FileErrorCode.FILE_TOO_BIG:
-      return 'Filesize is too big'
-    case FileErrorCode.UPLOAD_FAILED:
-      return 'Error while uploading'
-  }
-}
-
-export const validateFile: UploadValidator<FileError[]> = (file) => {
-  let uploadErrors: FileErrorCode[] = []
-
-  if (!file) {
-    uploadErrors = [...uploadErrors, FileErrorCode.NO_FILE_SELECTED]
-  } else if (!matchesAllowedExtensions(file.name)) {
-    uploadErrors = [...uploadErrors, FileErrorCode.BAD_EXTENSION]
-  } else if (file.size > maxFileSize) {
-    uploadErrors = [...uploadErrors, FileErrorCode.FILE_TOO_BIG]
-  } else {
-    return {
-      valid: true,
-    }
-  }
-
-  return {
-    valid: false,
-    errors: handleErrors(uploadErrors),
-  }
+export function createImagePlugin() {
+  return createCoreImagePlugin({
+    upload: createUploadImageHandler(),
+    validate: validateFile,
+    secondInput: 'description',
+  })
 }
 
 export function createUploadImageHandler() {
@@ -159,12 +115,51 @@ export function createReadFile() {
   }
 }
 
-export function createImagePlugin() {
-  return createCoreImagePlugin({
-    upload: createUploadImageHandler(),
-    validate: validateFile,
-    secondInput: 'description',
-  })
+export const validateFile: UploadValidator<FileError[]> = (file) => {
+  let uploadErrors: FileErrorCode[] = []
+
+  if (!file) {
+    uploadErrors = [...uploadErrors, FileErrorCode.NO_FILE_SELECTED]
+  } else if (!matchesAllowedExtensions(file.name)) {
+    uploadErrors = [...uploadErrors, FileErrorCode.BAD_EXTENSION]
+  } else if (file.size > maxFileSize) {
+    uploadErrors = [...uploadErrors, FileErrorCode.FILE_TOO_BIG]
+  } else {
+    return { valid: true }
+  }
+
+  return { valid: false, errors: handleErrors(uploadErrors) }
+}
+
+function matchesAllowedExtensions(fileName: string) {
+  const extension = fileName.toLowerCase().slice(fileName.lastIndexOf('.') + 1)
+  return allowedExtensions.includes(extension)
+}
+
+function handleErrors(errors: FileErrorCode[]): FileError[] {
+  return errors.map((error) => ({
+    errorCode: error,
+    message: errorCodeToMessage(error),
+  }))
+}
+
+function onError(errors: FileError[]): void {
+  alert(errors.map((error) => error.message).join('\n'))
+}
+
+function errorCodeToMessage(error: FileErrorCode) {
+  switch (error) {
+    case FileErrorCode.TOO_MANY_FILES:
+      return 'You can only upload one file'
+    case FileErrorCode.NO_FILE_SELECTED:
+      return 'No file selected'
+    case FileErrorCode.BAD_EXTENSION:
+      return 'Not an accepted file type'
+    case FileErrorCode.FILE_TOO_BIG:
+      return 'Filesize is too big'
+    case FileErrorCode.UPLOAD_FAILED:
+      return 'Error while uploading'
+  }
 }
 
 const uploadUrlQuery = gql`
