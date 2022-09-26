434c217836289d50d5f5ef03df1016425fa21d39
Botho <1258870+elbotho@users.noreply.github.com>
2022-09-26 16:58:20 +0200

    try proxy for graphql requests

--- src/api/graphql-fetch.ts
+++ src/api/graphql-fetch.ts
@@ -4,7 +4,7 @@ import { RefObject } from 'react'
 import { endpoint } from '@/api/endpoint'
 import { AuthenticationPayload } from '@/auth/auth-provider'
 
-interface ParsedArgs {
+export interface ParsedArgs {
   query: RequestDocument
   variables: unknown
 }
@@ -26,18 +26,28 @@ export function createGraphqlFetch() {
 export function createAuthAwareGraphqlFetch(
   auth: RefObject<AuthenticationPayload>
 ) {
-  return async function fetch(args: string) {
-    const { query, variables } = JSON.parse(args) as ParsedArgs
+  return async function _fetch(args: string) {
+    // TODO: only use this proxy on localhost
+    // const { query, variables } = JSON.parse(args) as ParsedArgs
     if (auth.current === null) throw new Error('unauthorized')
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return await executeQuery()
+    const result = await fetch('/api/graphql', {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(args),
+    })
+    return (await result.json()) as unknown
+    // // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    // return await executeQuery()
 
-    function executeQuery() {
-      const client = new GraphQLClient(endpoint, {
-        credentials: 'include',
-      })
-      return client.request(query, variables)
-    }
+    // function executeQuery() {
+    //   const client = new GraphQLClient(endpoint, {
+    //     credentials: 'include',
+    //   })
+    //   return client.request(query, variables)
+    // }
   }
 }
