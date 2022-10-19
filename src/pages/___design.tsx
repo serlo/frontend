97246fe03729de248ff5be1e7bc0cca7b0ae08d4
Botho <1258870+elbotho@users.noreply.github.com>
2022-10-19 15:16:27 +0200

    init design guide at /___design

---
+++ src/pages/___design.tsx
@@ -0,0 +1,29 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+import DesignGuideSVG from '@/assets-webkit/img/design-guide/design-guide.svg'
+
+import { FrontendClientBase } from '@/components/frontend-client-base'
+
+const ContentPage: NextPage = () => {
+  return (
+    <FrontendClientBase
+      noHeaderFooter
+      noContainers
+      showNav={false}
+      authorization={{}}
+    >
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <DesignGuideSVG className="design-guide mx-auto max-w-5xl h-auto" />
+      <style jsx global>{`
+        .design-guide {
+          text {
+            white-space: normal !important;
+          }
+        }
+      `}</style>
+    </FrontendClientBase>
+  )
+}
+export default ContentPage
