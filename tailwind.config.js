01ac4dac39d6cc37da42ea5360f93c2425e315b1
Botho <1258870+elbotho@users.noreply.github.com>
2022-10-21 16:48:04 +0200

    yarn

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,7 @@ module.exports = {
           // 700: '#6D8A13',
           // 800: '#5B7310',
           // 900: '#45570C',
-          muted: 'rgb(207, 224, 151)',
+          muted: '#cfe097',
         },
         truegray: colors.neutral,
         berry: '#857189',
