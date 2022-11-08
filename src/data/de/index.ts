bed2df7c41c3d56eb61dc8329be61988de0ccbae
Botho <1258870+elbotho@users.noreply.github.com>
2022-11-08 17:19:27 +0100

    New translations index.ts (German)

--- src/data/de/index.ts
+++ src/data/de/index.ts
@@ -9,7 +9,12 @@ export const instanceData = {
     header: {
       slogan: "Die freie Lernplattform",
       search: "Suche",
-      login: "Anmelden"
+      login: "Anmelden",
+      skipLinks: {
+        sentence: 'Skip to %content% or %footer%',
+        content: 'content',
+        footer: 'footer'
+      }
     },
     search: {
       privacy: "Die Suche wird von Google bereitgestellt. Schau dir unsere %privacypolicy% an, um herauszufinden, welche Informationen verarbeitet werden.",
