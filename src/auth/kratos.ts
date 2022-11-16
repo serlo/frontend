7bafdadc78ad26ebcd550060230552f44de1b810
Hugo Tiburtino <hugotiburtino@protonmail.com>
2022-11-16 23:16:21 +0100

    refactor(kratos): fix cors error in vercel

--- src/auth/kratos.ts
+++ src/auth/kratos.ts
@@ -1,9 +1,7 @@
 import { Configuration as KratosConfig, V0alpha2Api } from '@ory/client'
 
-import { frontendOrigin } from '../helper/urls/frontent-origin'
-
 export const kratos = new V0alpha2Api(
   new KratosConfig({
-    basePath: `${frontendOrigin}/api/.ory`,
+    basePath: `/api/.ory`,
   })
 )
