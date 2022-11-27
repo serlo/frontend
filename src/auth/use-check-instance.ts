1161157e9b1788bdd362e627d9efdef3cde7ad40
Botho <1258870+elbotho@users.noreply.github.com>
2022-11-27 11:14:03 +0100

    use cookie to make it work across subdomains

--- src/auth/use-check-instance.ts
+++ src/auth/use-check-instance.ts
@@ -1,14 +1,20 @@
+import Cookies from 'js-cookie'
 import { useRouter } from 'next/router'
+
+import { COOKIE_DOMAIN } from './kratos'
 
 // we currently only use this for kratos auth flows
 // if a user is following a link in a kratos mail the user is redirected to de.serlo.org by default
 // this hook forwards them to the right instance if possible
 
+const cookieName = 'previousInstance'
+
 export function useCheckInstance() {
   const { locale: current } = useRouter()
 
   return ({ redirect }: { redirect?: boolean }) => {
-    const previous = localStorage.getItem('instance')
+    const previous = Cookies.get(cookieName)
+
     if (redirect && previous && current && previous !== current) {
       if (window.location.hostname === 'localhost') {
         window.location.pathname =
@@ -23,7 +29,8 @@ export function useCheckInstance() {
       throw new Error('wrong instance, redirecting …')
     } else {
       // store current value for the next auth page
-      if (current) localStorage.setItem('instance', current)
+      if (current)
+        Cookies.set(cookieName, current, { expires: 3, domain: COOKIE_DOMAIN })
     }
   }
 }
