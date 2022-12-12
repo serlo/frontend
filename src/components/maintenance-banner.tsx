48440dbf512e529fc98fc116d09f24feca7ae987
Botho <1258870+elbotho@users.noreply.github.com>
2022-12-12 10:37:38 +0100

    change time

--- src/components/maintenance-banner.tsx
+++ src/components/maintenance-banner.tsx
@@ -2,7 +2,7 @@ export function MaintenanceBanner() {
   return (
     <div className="hidden sm:block absolute top-0 left-0 right-0 bg-truegray-200 h-10">
       <p className="text-right p-2">
-        Vorsicht: Ab 22:00 arbeiten wir am Login-System. Lernen könnt ihr
+        Vorsicht: Ab 20:30 arbeiten wir am Login-System. Lernen könnt ihr
         weiter, aber einloggen/bearbeiten geht heute Nacht nicht. 🦉
       </p>
       <style jsx global>
