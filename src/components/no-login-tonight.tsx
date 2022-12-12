6c46a90c592e62722da715e502311b01f8021019
Botho <1258870+elbotho@users.noreply.github.com>
2022-12-12 12:12:19 +0100

    add timezone

--- src/components/no-login-tonight.tsx
+++ src/components/no-login-tonight.tsx
@@ -7,8 +7,9 @@ export function NoLoginTonight() {
     <div>
       <PageTitle title="Tut uns keid, keine Logins heute Nacht" />
       <p className="serlo-p">
-        Heute Nacht (12.12. ab 20:30) arbeiten wir am Login-System, deswegen ist
-        das Einloggen und Registrieren bis morgen früh nicht möglich.
+        Heute Nacht (12.12. ab 20:30 GMT+1) arbeiten wir am Login-System,
+        deswegen ist das Einloggen und Registrieren bis morgen früh nicht
+        möglich.
         <br />
         Wenn alles klappt ist morgen alles wieder aktiv (und schöner und
         schneller).
