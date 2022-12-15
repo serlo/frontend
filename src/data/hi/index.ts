2b34b9ab049f1a89143a81a2edcc31de7af14bc5
Botho <1258870+elbotho@users.noreply.github.com>
2022-12-15 12:51:45 +0100

    New translations index.ts (Hindi)

--- src/data/hi/index.ts
+++ src/data/hi/index.ts
@@ -376,9 +376,11 @@ export const instanceData = {
         code4000008: 'The provided authentication code is invalid, please try again.',
         code4000010: 'Have you already verified your email address?.%break% %verificationLinkText%',
         code4060004: 'The recovery link is not valid or has already been used. Please try requesting an email again',
-        code4070001: 'The verification link is not valid or has already been used. Please try requesting an email again.'
+        code4070001: 'The verification link is not valid or has already been used. Please try requesting an email again.',
+        code4070005: 'Sorry, this verification link is not valid any more. Please try requesting an email again.'
       },
       usernameInvalid: 'Your username may only contain letters, digits, underscores (_) and hyphens (-).',
+      usernameTooLong: 'Sorry, this username is too long, make sure it\'s 32 characters or less',
       passwordTooShort: 'Sorry, this password is too short. Please choose one that is at least 8 characters long.',
       passwordTooLong: 'Sorry, this password is too long. Please choose one that has a maximum of 72 characters.',
       passwordTooSimilar: 'Sorry, this password is too similar to your email or username.',
@@ -514,7 +516,6 @@ export const loggedInData = {
       edit: "संपादित",
       editTax: 'Edit Title & Text',
       unrevisedEdit: 'Show unrevised revisions',
-      organize: "आयोजन",
       moveToGrouped: 'Move content to other grouped-text-exercise',
       moveToTextExercise: "सामग्री को अन्य पाठ-व्यायाम में ले जाएँ",
       sortEntities: "सामग्री को क्रमबद्ध करें",
