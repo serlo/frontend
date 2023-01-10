43a7aec1feeca8499481f5413d053151af4ba754
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-01-10 15:15:54 +0100

    remove unused id

--- src/components/content/thank-you.tsx
+++ src/components/content/thank-you.tsx
@@ -2,14 +2,12 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
 
 import { FaIcon } from '../fa-icon'
-import { useEntityId } from '@/contexts/entity-id-context'
 import { useInstanceData } from '@/contexts/instance-context'
 import { Instance } from '@/fetcher/graphql-types/operations'
 import { submitEvent } from '@/helper/submit-event'
 
 export function ThankYou() {
   const { lang } = useInstanceData()
-  const id = useEntityId()
   const [submitted, setSubmitted] = useState(false)
 
   if (lang !== Instance.De) return null // de only
