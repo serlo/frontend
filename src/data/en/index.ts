8b26433fada9c093a2c971a1525df20c7ccf6cc1
Botho <1258870+elbotho@users.noreply.github.com>
2023-02-21 21:55:33 +0100

    Update source file index.ts

--- src/data/en/index.ts
+++ src/data/en/index.ts
@@ -205,8 +205,8 @@ export const instanceData = {
       copyLink: 'Copy comment link',
       commentsOverviewExplanation:
         'Here you can see all comments that were written to content on %instance%.serlo.org. %break% Answer questions or find content you could improve. %break% The link above the comment brings you to the relevant entity.',
-      edit: 'Edit',
-      abort: 'Abort',
+      edit: 'Edit comment',
+      cancelEdit: 'Cancel',
     },
     revisions: {
       toOverview: 'Back to overview',
