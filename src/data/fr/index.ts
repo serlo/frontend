d8717ced1a6dd32aa55059351115bdf8ee7f708c
Botho <1258870+elbotho@users.noreply.github.com>
2023-02-21 17:16:47 +0100

    New translations index.ts (French)

--- src/data/fr/index.ts
+++ src/data/fr/index.ts
@@ -195,7 +195,9 @@ export const instanceData = {
       hideReplies: 'Hide',
       showArchived: "Afficher les %threads% affichés",
       copyLink: "Copier le lien du commentaire",
-      commentsOverviewExplanation: 'Here you can see all comments that were written to content on %instance%.serlo.org. %break% Answer questions or find content you could improve. %break% The link above the comment brings you to the relevant entity.'
+      commentsOverviewExplanation: 'Here you can see all comments that were written to content on %instance%.serlo.org. %break% Answer questions or find content you could improve. %break% The link above the comment brings you to the relevant entity.',
+      edit: 'Edit',
+      abort: 'Abort'
     },
     revisions: {
       toOverview: "Retour à la vue d'ensemble",
@@ -958,14 +960,14 @@ export const kratosMailStrings = {
       subject: '👉 Access to your Serlo account',
       'body.plaintext': `👋 Hi {{ .Identity.traits.username }},
 Are you trying to get access to your account at serlo.org? If not please just ignore this mail.
- 
+
 To reset your password please open the following link in your browser:
 {{ .RecoveryURL }}
 
 Best of luck from your Serlo team`,
       body: `<p>👋 Hi <b>{{ .Identity.traits.username }}</b>,</p>
 <p>Are you trying to get access to your account at serlo.org? If not please just ignore this mail.</p>
- 
+
 <p>To reset your password please open the following link in your browser:
 <a href="{{ .RecoveryURL }}">{{ .RecoveryURL }}</a><br/><br/>Best of luck from your Serlo team</p>`
     },
