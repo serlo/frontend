97bbbd710f8adc444c6393b0b90b4bf119b00b46
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-03-01 11:46:21 +0100

    fix

--- src/pages/api/.ory/[...paths].ts
+++ src/pages/api/.ory/[...paths].ts
@@ -9,11 +9,10 @@ const KRATOS_HOSTS = {
   local: process.env.KRATOS_HOST_LOCAL,
 }
 
+// if env is not set, it's a production build running on localhost, use staging as default
 const KRATOS_HOST =
   KRATOS_HOSTS[
-    process.env.NEXT_PUBLIC_VERCEL_URL && process.env.NEXT_PUBLIC_ENV
-      ? process.env.NEXT_PUBLIC_ENV
-      : 'local'
+    process.env.NEXT_PUBLIC_ENV ? process.env.NEXT_PUBLIC_ENV : 'staging'
   ]
 
 // TODO: this should probably be handled in CF Worker instead since it changes independent of Frontend.
