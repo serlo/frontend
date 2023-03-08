e687af6c8f89faf7a418890571ebe2e259a6627e
Botho <1258870+elbotho@users.noreply.github.com>
2023-03-08 11:57:02 +0100

    simplify styling, remove styled

--- src/edtr-io/plugins/highlight/editor.tsx
+++ src/edtr-io/plugins/highlight/editor.tsx
@@ -1,31 +1,8 @@
-import {
-  EditorCheckbox,
-  EditorInput,
-  EditorInlineSettings,
-} from '@edtr-io/editor-ui'
-import { styled } from '@edtr-io/ui'
 import { useState } from 'react'
 
 import { HighlightProps } from '.'
 
-const Textarea = styled.textarea({
-  height: '250px',
-  width: '100%',
-  margin: 'auto',
-  padding: '10px',
-  resize: 'none',
-  fontFamily: 'Menlo, Monaco, "Courier New", monospace',
-  border: 'none',
-  outline: 'none',
-  boxShadow: '0 1px 1px 0 rgba(0,0,0,0.50)',
-  '&::-webkit-input-placeholder': {
-    color: 'rgba(0,0,0,0.5)',
-  },
-})
-
-const CheckboxContainer = styled.div({
-  float: 'right',
-})
+const languages = ['text', 'c', 'javascript', 'jsx', 'markup', 'java', 'python']
 
 export function HighlightEditor(props: HighlightProps) {
   const { config, state, focused, editable } = props
@@ -42,9 +19,10 @@ export function HighlightEditor(props: HighlightProps) {
       setEditThrottled(true)
     }
   }
+
   return throttledEdit || edit ? (
     <>
-      <Textarea
+      <textarea
         value={state.code.value}
         name="text"
         placeholder={i18n.code.placeholder}
@@ -53,36 +31,11 @@ export function HighlightEditor(props: HighlightProps) {
         }}
         // make sure editor does not create new plugin on enter etc
         onKeyDown={(e) => e.stopPropagation()}
+        className="h-32 w-full m-auto p-side resize-y font-mono shadow-menu border-none"
       >
         {state.code.value}
-      </Textarea>
-      <EditorInlineSettings>
-        <EditorInput
-          list="available-languages"
-          label="Language:"
-          value={state.language.value}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            state.language.set(e.target.value)
-          }}
-          placeholder={i18n.language.placeholder}
-        />
-        <datalist id="available-languages">
-          {['c', 'javascript', 'jsx', 'markup', 'java', 'python'].map(
-            (language) => {
-              return <option key={language} value={language} />
-            }
-          )}
-        </datalist>
-        <CheckboxContainer>
-          <EditorCheckbox
-            label={i18n.showLineNumbers.label}
-            onChange={() => {
-              state.showLineNumbers.set(!state.showLineNumbers.value)
-            }}
-            checked={state.showLineNumbers.value}
-          />
-        </CheckboxContainer>
-      </EditorInlineSettings>
+      </textarea>
+      {renderSettings()}
     </>
   ) : (
     <Renderer
@@ -92,4 +45,42 @@ export function HighlightEditor(props: HighlightProps) {
       code={state.code.value}
     />
   )
+
+  function renderSettings() {
+    return (
+      <div className="mt-2 flex justify-between">
+        <label>
+          Language:{' '}
+          <select
+            onChange={(e) => state.language.set(e.target.value)}
+            className="cursor-pointer"
+          >
+            {languages.map((language) => {
+              return (
+                <option
+                  key={language}
+                  value={language}
+                  selected={
+                    state.language.value === language ? true : undefined
+                  }
+                >
+                  {language}
+                </option>
+              )
+            })}
+          </select>
+        </label>
+        <label className="cursor-pointer">
+          {i18n.showLineNumbers.label}:{' '}
+          <input
+            type="checkbox"
+            onChange={() => {
+              state.showLineNumbers.set(!state.showLineNumbers.value)
+            }}
+            checked={state.showLineNumbers.value}
+          />
+        </label>
+      </div>
+    )
+  }
 }
