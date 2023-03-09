d3e4c53795754b4342a55b4bb97490caa261e15c
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-03-09 08:03:04 +0100

    rework h5p plugin

--- src/components/content/h5p.tsx
+++ src/components/content/h5p.tsx
@@ -6,15 +6,20 @@ export interface H5pProps {
   url: string
 }
 
+export function parseH5pUrl(url: string) {
+  const result = /https:\/\/app\.lumi\.education\/run\/(\w+)/i.exec(url)
+  return result ? result[1] : null
+}
+
 export function H5p({ url }: H5pProps) {
-  const id = /https:\/\/app\.lumi\.education\/run\/(.+)/i.exec(url)
+  const id = parseH5pUrl(url)
   const { strings } = useInstanceData()
 
   if (!id) {
     return <p className="serlo-p">{strings.errors.defaultMessage}</p>
   }
 
-  const src = `/api/frontend/lumi/embed/${id[1]}`
+  const src = `/api/frontend/lumi/embed/${id}`
 
   return (
     <div className="mx-side mb-block">
