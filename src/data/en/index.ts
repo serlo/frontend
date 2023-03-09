bf0b94ba41b42060f9c26afea9d2a5dee411a0e7
Botho <1258870+elbotho@users.noreply.github.com>
2023-03-09 20:25:17 +0100

    Update source file index.ts

--- src/data/en/index.ts
+++ src/data/en/index.ts
@@ -184,8 +184,7 @@ export const instanceData = {
       text: 'By clicking on image or button above you agree that external content from %provider% will be loaded. Also personal data may be transferred to this service in accordance with our %privacypolicy%.',
       video: 'Play Video from %provider%',
       applet: 'Load Applet from %provider%',
-      twingle: 'Load Donation Form',
-      h5p: 'Load Interactive Content'
+      twingle: 'Load Donation Form'
     },
     comments: {
       question: 'Do you have a question?',
