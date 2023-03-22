e3e89ea8105466eee522ac9689c436fde5e25a55
Mikey Stengel <mikey@serlo.org>
2023-03-22 12:51:36 +0100

    Add comma thanks to Peters feedback

--- src/data/de/index.ts
+++ src/data/de/index.ts
@@ -400,7 +400,7 @@ export const instanceData = {
       passwordTooLong: "Sorry, dieses Passwort ist zu lang. Bitte wähle ein Passwort, das höchstens 72 Zeichen lang ist.",
       passwordTooSimilar: "Sorry, dieses Passwort ist deiner Mailadresse oder deinem Benutzernamen zu ähnlich.",
       emailInvalid: "Sorry, das ist keine gültige E-Mail-Adresse. Schau nach, ob du dich vertippt hast.",
-      registrationAgreement: "Mit deinem Klick auf %signup% stimmst du der %privacypolicy% und %terms% zu. Du könntest E-Mail Benachtigungen von uns bekommen von denen du dich jederzeit abmelden kannst.",
+      registrationAgreement: "Mit deinem Klick auf %signup% stimmst du der %privacypolicy% und %terms% zu. Du könntest E-Mail Benachtigungen von uns bekommen, von denen du dich jederzeit abmelden kannst.",
       registrationCheckboxAgreement:
       'Ich stimme der %privacypolicy% und den %terms% zu. Ich könnte E-Mail Benachtigungen von Serlo bekommen von denen ich mich jederzeit abmelden kann.',
       terms: "Nutzungsbedingungen",
