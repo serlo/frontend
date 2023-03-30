fce0e2a32407e2f9d59271304c9634e3f8b7f755
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-03-31 00:56:58 +0200

    setup for accessing edtr-io codebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -79,6 +79,7 @@ module.exports = {
           'msw/*',
           'next/*',
           'public/_assets/**/*',
+          'test-edtr-io/**',
         ],
       },
     ],
---
+++ additional.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="test-edtr-io/internal__plugin-toolbar" />
+/// <reference types="test-edtr-io/internal__document-editor" />
+/// <reference types="test-edtr-io/internal__plugin-state" />
+/// <reference types="test-edtr-io/internal__dev-expression" />
+/// <reference types="test-edtr-io/ui" />
+/// <reference types="test-edtr-io/internal__plugin" />
