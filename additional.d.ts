37ded7a20a19c81a83fe367a74d96b1e18bc77ed
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-03-31 08:54:35 +0200

    current status quo

--- additional.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="test-edtr-io/internal__plugin-toolbar" />
-/// <reference types="test-edtr-io/internal__document-editor" />
-/// <reference types="test-edtr-io/internal__plugin-state" />
-/// <reference types="test-edtr-io/internal__dev-expression" />
-/// <reference types="test-edtr-io/ui" />
-/// <reference types="test-edtr-io/internal__plugin" />
