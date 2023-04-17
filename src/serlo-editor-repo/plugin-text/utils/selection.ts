64e75a32c22b5f3419946440f8890ac0660e4df7
Vitomir Budimir <budimirvitomir@gmail.com>
2023-04-17 14:22:51 +0200

    fix(plugin-text): fix isSelectionAtEnd and add isSelectionAtStart utils

--- src/serlo-editor-repo/plugin-text/utils/selection.ts
+++ src/serlo-editor-repo/plugin-text/utils/selection.ts
@@ -54,7 +54,13 @@ export function trimSelection(editor: SlateEditor): Partial<Range> | null {
   return trimmedSelection
 }
 
+export function isSelectionAtStart(editor: SlateEditor, selection: BaseRange) {
+  const firstPointInEditor = SlateEditor.start(editor, [0])
+  return Point.compare(firstPointInEditor, selection.focus) > -1
+}
+
 export function isSelectionAtEnd(editor: SlateEditor, selection: BaseRange) {
-  const lastPointInEditor = SlateEditor.end(editor, [selection.anchor.path[0]])
+  const childrenCount = editor.children.length
+  const lastPointInEditor = SlateEditor.end(editor, [childrenCount - 1])
   return Point.compare(lastPointInEditor, selection.focus) < 1
 }
