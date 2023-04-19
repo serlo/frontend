e5631f43c9b1fe3b59acecfc2eaeb0c46d89cd73
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-04-19 21:48:28 +0200

    don't add dummy data

--- src/pages/___test_planetscale.tsx
+++ src/pages/___test_planetscale.tsx
@@ -8,16 +8,6 @@ interface Data {
 
 export const getServerSideProps: GetServerSideProps<Data> = async () => {
   const count = await prisma.exerciseSubmission.count()
-  await prisma.exerciseSubmission.create({
-    data: {
-      path: '/test',
-      entityId: 1,
-      revisionId: 1,
-      type: 'input',
-      result: 'correct',
-      sessionId: '123',
-    },
-  })
   return { props: { count } }
 }
 
@@ -25,7 +15,9 @@ const Page: NextPage<Data> = ({ count }) => {
   return (
     <>
       <div className="w-full">
-        <div className="mx-auto text-xl pt-24 w-fit">Anzahl Aufrufe</div>
+        <div className="mx-auto text-xl pt-24 w-fit">
+          Anzahl Interaktive Aufgaben geklickt
+        </div>
         <div className="w-fit mx-auto text-5xl mt-24 font-bold pb-24">
           {count}
         </div>
