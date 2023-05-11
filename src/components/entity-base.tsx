fd3647dbb519ef168ccc8752dabeb6df9ed9d2f9
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-05-11 12:53:17 +0200

    visual prototype

--- src/components/entity-base.tsx
+++ src/components/entity-base.tsx
@@ -1,10 +1,12 @@
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import dynamic from 'next/dynamic'
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 
 import { CommentAreaEntityProps } from './comments/comment-area-entity'
 import { HSpace } from './content/h-space'
 import { Horizon } from './content/horizon'
 import { Lazy } from './content/lazy'
+import { FaIcon } from './fa-icon'
 import { HeadTags } from './head-tags'
 import { JsonLd } from './json-ld'
 import { Breadcrumbs } from './navigation/breadcrumbs'
@@ -38,6 +40,18 @@ const DonationsBanner = dynamic<DonationsBannerProps>(() =>
 )
 
 export function EntityBase({ children, page, entityId }: EntityBaseProps) {
+  const [survey, setSurvey] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSurvey(true)
+    }, 20000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+
   const noComments =
     page.kind === 'single-entity' &&
     (page.entityData.typename === UuidType.Page ||
@@ -45,6 +59,34 @@ export function EntityBase({ children, page, entityId }: EntityBaseProps) {
 
   return (
     <>
+      {survey && (
+        <div className="fixed inset-0 bg-black/30 z-[1000] flex justify-center items-center">
+          <div className="h-[300px] w-[500px] bg-white z-[1200] rounded-xl relative mx-side">
+            <button className="-right-3 -top-3 w-12 h-12 rounded-full absolute serlo-button-blue flex justify-center items-center">
+              <FaIcon icon={faTimes} className="text-2xl text-white"></FaIcon>
+            </button>
+            <p className="text-almost-black mx-side mt-4">
+              Wir stellen regelmäßig Fragen auf der Plattform, um unser
+              Lernangebot für dich weiter zu verbessern.
+            </p>
+            <p className="serlo-p italic text-2xl mt-6">
+              Bekommst du zu Hause Hilfe, wenn du beim Lernen nicht
+              weiterkommst?
+            </p>
+
+            <p className="flex justify-around">
+              <button className="serlo-button-blue w-24">JA</button>
+              <button className="serlo-button-blue w-24">NEIN</button>
+            </p>
+
+            <p className="text-center mt-8">
+              <button className="hover:underline">
+                Ich bin keine Schüler*in.
+              </button>
+            </p>
+          </div>
+        </div>
+      )}
       {page.secondaryMenuData && (
         <SecondaryMenu data={page.secondaryMenuData} />
       )}
