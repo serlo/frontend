47fbe61090e723b81ade6e4e9cd6945d1d8e44f6
Botho <1258870+elbotho@users.noreply.github.com>
2023-05-11 14:08:06 +0200

    feat(editor): add simple tooltip to text-toolbar

---
+++ src/serlo-editor-repo/editor-ui/editor-tooltip.tsx
@@ -0,0 +1,34 @@
+import { useInstanceData } from '@/contexts/instance-context'
+import { isMac } from '@/helper/client-detection'
+
+export function EditorTooltip({
+  text,
+  hotkeys,
+}: {
+  text?: string
+  hotkeys?: string
+}) {
+  const { strings } = useInstanceData()
+  if (!text && !hotkeys) return null
+
+  const hotkeysTranslated = hotkeys?.replace(
+    '%ctrlOrCmd%',
+    isMac ? '⌘' : strings.keys.ctrl
+  )
+
+  return (
+    <span
+      className="serlo-tooltip pointer-events-none opacity-0 transition-opacity hover:block absolute cursor-default bottom-full pb-[0.7rem]"
+      onClick={(e) => e.stopPropagation()}
+      onMouseDown={(e) => e.stopPropagation()}
+      onMouseUp={(e) => e.stopPropagation()}
+    >
+      <span className="block text-sm font-bold bg-almost-black py-1.5 px-2 rounded z-50 text-center text-white w-80 max-w-fit ">
+        {text}
+        {hotkeys ? (
+          <span className="block text-gray-400">{hotkeysTranslated}</span>
+        ) : null}
+      </span>
+    </span>
+  )
+}
