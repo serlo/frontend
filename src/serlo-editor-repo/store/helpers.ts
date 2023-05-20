342679ea5c7f8315ef1f8967a83179ed56dffe03
Vitomir Budimir <budimirvitomir@gmail.com>
2023-05-20 19:06:24 +0200

    refactor(editor): increase cache size for custom selector creators

--- src/serlo-editor-repo/store/helpers.ts
+++ src/serlo-editor-repo/store/helpers.ts
@@ -1,12 +1,15 @@
 import * as R from 'ramda'
 import { createSelectorCreator, defaultMemoize } from 'reselect'
 
-export const createDeepEqualSelector = createSelectorCreator(
-  defaultMemoize,
-  R.equals
-)
+export const createDeepEqualSelector = createSelectorCreator(defaultMemoize, {
+  resultEqualityCheck: R.equals,
+  maxSize: 50,
+})
 
 export const createJsonStringifySelector = createSelectorCreator(
   defaultMemoize,
-  (a, b) => JSON.stringify(a) === JSON.stringify(b)
+  {
+    resultEqualityCheck: (a, b) => JSON.stringify(a) === JSON.stringify(b),
+    maxSize: 50,
+  }
 )
