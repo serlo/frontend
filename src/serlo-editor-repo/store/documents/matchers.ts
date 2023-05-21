81a85a8e6e7f9334156ce8094f64ae2192505b91
Vitomir Budimir <budimirvitomir@gmail.com>
2023-05-21 15:37:56 +0200

    refactor(editor): fix redux toolkit action matchers types

--- src/serlo-editor-repo/store/documents/matchers.ts
+++ src/serlo-editor-repo/store/documents/matchers.ts
@@ -1,19 +1,16 @@
 import { AnyAction } from '@reduxjs/toolkit'
 
 import { pureInsert, pureReplaceText } from './slice'
-import type {
-  PureInsertActionPayload,
-  PureReplaceTextActionPayload,
-} from './types'
+import type { PureInsertAction, PureReplaceTextAction } from './types'
 
 export function isPureInsertAction(
   action: AnyAction
-): action is PureInsertActionPayload {
+): action is PureInsertAction {
   return action.type === pureInsert.type
 }
 
 export function isPureReplaceTextAction(
   action: AnyAction
-): action is PureReplaceTextActionPayload {
+): action is PureReplaceTextAction {
   return action.type === pureReplaceText.type
 }
