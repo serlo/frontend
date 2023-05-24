21873ed7d65fa538d8ce182fbf152a89ace025df
Lars Rasmussen <lars@lars-rasmussen.de>
2023-05-24 17:12:39 +0200

    format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  presets: [
-    require('./shared/tailwind-base.cjs')
-  ],
+  presets: [require('./shared/tailwind-base.cjs')],
   mode: 'jit',
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './shared/tailwind-base.cjs']
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './shared/tailwind-base.cjs'],
 }
