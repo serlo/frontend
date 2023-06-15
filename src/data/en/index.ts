1d7eb9fa37cb0abedd2c9d3508d2635ea1657398
Botho <1258870+elbotho@users.noreply.github.com>
2023-06-15 18:01:16 +0200

    build merged geogebra renderer

--- src/data/en/index.ts
+++ src/data/en/index.ts
@@ -183,7 +183,8 @@ export const instanceData = {
       text: 'By clicking on image or button above you agree that external content from %provider% will be loaded. Also personal data may be transferred to this service in accordance with our %privacypolicy%.',
       video: 'Play Video from %provider%',
       applet: 'Load Applet from %provider%',
-      twingle: 'Load Donation Form'
+      twingle: 'Load Donation Form',
+      general: 'Activate'
     },
     comments: {
       question: 'Do you have a question?',
--- src/serlo-editor/plugins/geogebra/editor.tsx
+++ src/serlo-editor/plugins/geogebra/editor.tsx
@@ -1,33 +1,58 @@
 import { GeogebraProps } from '.'
 import { EditorInput, EditorInlineSettings } from '../../editor-ui'
-import { GeogebraRenderer } from './renderer'
+import { GeogebraRenderer, parseId } from './renderer'
+import { FaIcon } from '@/components/fa-icon'
 import { useLoggedInData } from '@/contexts/logged-in-data-context'
+import { entityIconMapping } from '@/helper/icon-by-entity-type'
+import { EmbedWrapper } from '@/serlo-editor/editor-ui/embed-wrapper'
 
 export function GeogebraEditor(props: GeogebraProps) {
   const { focused, editable, state } = props
 
   const editorStrings = useLoggedInData()!.strings.editor
 
-  if (!editable) return <GeogebraRenderer {...props} />
+  const { cleanId, url } = parseId(state.value)
+  const couldBeValidId = cleanId.length === 8
 
   return (
     <>
-      <GeogebraRenderer {...props} disableCursorEvents={editable} />
-      {focused ? (
-        <EditorInlineSettings>
-          <EditorInput
-            label={editorStrings.geogebra.urlOrId}
-            placeholder="12345"
-            value={state.value}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              state.set(e.target.value)
-            }}
-            inputWidth="70%"
-            width="100%"
-            ref={props.autofocusRef}
+      {editable && focused ? renderInput() : null}
+      {couldBeValidId ? (
+        <EmbedWrapper
+          type="applet"
+          provider="GeoGebra"
+          embedUrl={url}
+          className={focused ? '' : 'pointer-events-none'}
+        >
+          <GeogebraRenderer url={url} />
+        </EmbedWrapper>
+      ) : (
+        <div className="rounded-lg bg-editor-primary-50 py-32 text-center">
+          <FaIcon
+            icon={entityIconMapping['applet']}
+            className="text-7xl text-editor-primary-200"
           />
-        </EditorInlineSettings>
-      ) : null}
+        </div>
+      )}
     </>
   )
+
+  function renderInput() {
+    return (
+      <EditorInlineSettings className="mb-3">
+        <EditorInput
+          label={`${editorStrings.geogebra.urlOrId}: `}
+          placeholder="z.B. N5ktHvtW"
+          value={state.value}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            state.set(e.target.value)
+          }}
+          inputWidth="40%"
+          width="100%"
+          ref={props.autofocusRef}
+          className="ml-1"
+        />
+      </EditorInlineSettings>
+    )
+  }
 }
