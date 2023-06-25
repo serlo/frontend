544ab7a7bc8cbfefdf7d31c4db402a45cab881bb
Botho <1258870+elbotho@users.noreply.github.com>
2023-06-25 23:35:32 +0200

    simplify article related content

--- src/serlo-editor/plugins/article/article-related-content.tsx
+++ src/serlo-editor/plugins/article/article-related-content.tsx
@@ -1,11 +1,4 @@
-import { faGripVertical, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
-import { flatten, values } from 'ramda'
-import {
-  DragDropContext,
-  Draggable,
-  DraggableProvidedDragHandleProps,
-  Droppable,
-} from 'react-beautiful-dnd'
+import { faCircleArrowUp, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 import { ArticleProps, buttonClass } from '.'
 import { FaIcon } from '@/components/fa-icon'
@@ -29,127 +22,72 @@ export function ArticleRelatedContent({
   const { strings } = useInstanceData()
   const articleStrings = useEditorStrings().templatePlugins.article
 
-  const allItems = flatten(values(data))
-  if (!editable && allItems.length === 0) return null
+  const allItemsEmpty =
+    data.articles.length + data.courses.length + data.videos.length === 0
+  if (!editable && allItemsEmpty) return null
 
   return (
     <>
       <h2>{articleStrings.stillWantMore}</h2>
       <p className="mb-4">{articleStrings.moreOnTopic}:</p>
-      {categories.map((type) => {
-        return (
-          <div className="mb-2" key={type}>
-            {renderRelatedContentSection(type)}
-          </div>
-        )
-      })}
+      {categories.map((type) => (
+        <div className="mb-2" key={type}>
+          {renderRelatedContentSection(type)}
+        </div>
+      ))}
     </>
   )
 
   function renderRelatedContentSection(category: typeof categories[number]) {
     if (data[category].length === 0) return null
 
-    if (!editable) {
-      return (
-        <>
-          {renderHeader()}
-          {data[category].map((item, index) => {
-            return (
-              <div key={index}>
-                <a href={`/${item.id.value}`}>{item.title.value}</a>
-              </div>
-            )
-          })}
-        </>
-      )
-    }
-
     return (
       <>
-        {renderHeader()}
-        <DragDropContext
-          onDragEnd={(result) => {
-            const { source, destination } = result
-            if (!destination) return
-            data[category].move(source.index, destination.index)
-          }}
-        >
-          <Droppable droppableId="default">
-            {(provided) => {
-              return (
-                <div ref={provided.innerRef} {...provided.droppableProps}>
-                  {renderDraggables()}
-                  {provided.placeholder}
-                </div>
-              )
-            }}
-          </Droppable>
-        </DragDropContext>
-      </>
-    )
-
-    function renderHeader() {
-      return (
         <div className="mb-1">
           <FaIcon icon={categoryIconMapping[category]} />{' '}
           {getTranslatedType(strings, category)}
         </div>
-      )
-    }
-
-    function renderDraggables() {
-      return data[category].map((item, index) => {
-        return (
-          <Draggable
-            key={index}
-            draggableId={`${category}-${index}`}
-            index={index}
-          >
-            {(provided) => {
-              return (
-                <div
-                  ref={provided.innerRef}
-                  {...provided.draggableProps}
-                  className="flex"
-                >
-                  <a
-                    href={`/${item.id.value}`}
-                    className="flex-grow"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {item.title.value}
-                  </a>
-                  {renderButtons(index, category, provided.dragHandleProps)}
-                </div>
-              )
-            }}
-          </Draggable>
-        )
-      })
-    }
+        {data[category].map((item, index) => {
+          return editable ? (
+            <div key={item.id.value} className="flex">
+              <a
+                href={`/${item.id.value}`}
+                className="flex-grow"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {item.title.value}
+              </a>
+              {renderButtons(index, category)}
+            </div>
+          ) : (
+            <div key={item.id.value}>
+              <a href={`/${item.id.value}`}>{item.title.value}</a>
+            </div>
+          )
+        })}
+      </>
+    )
   }
 
-  function renderButtons(
-    index: number,
-    category: typeof categories[number],
-    dragHandleProps?: DraggableProvidedDragHandleProps
-  ) {
+  function renderButtons(index: number, category: typeof categories[number]) {
     return (
       <>
+        {index === 0 ? null : (
+          <button
+            className={`${buttonClass} serlo-tooltip-trigger`}
+            onClick={() => data[category].move(index, index - 1)}
+          >
+            <EditorTooltip text={articleStrings.dragLabel} />
+            <FaIcon icon={faCircleArrowUp} aria-hidden="true" />
+          </button>
+        )}
         <button
           className={`${buttonClass} serlo-tooltip-trigger`}
           onClick={() => data[category].remove(index)}
         >
           <EditorTooltip text={articleStrings.removeLabel} />
           <FaIcon icon={faTrashAlt} aria-hidden="true" />
-        </button>
-        <button
-          {...dragHandleProps}
-          className={`${buttonClass} serlo-tooltip-trigger`}
-        >
-          <EditorTooltip text={articleStrings.dragLabel} />
-          <FaIcon icon={faGripVertical} aria-hidden="true" />
         </button>
       </>
     )
