9f6e06ec92adcf9b9b08f0d580a2f2dc0c2784ba
Botho <1258870+elbotho@users.noreply.github.com>
2023-06-27 14:11:57 +0200

    simplify exercise

--- src/serlo-editor/plugins/exercise/editor.tsx
+++ src/serlo-editor/plugins/exercise/editor.tsx
@@ -1,59 +1,18 @@
 import { faRandom, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
-import { PropsWithChildren, useState } from 'react'
+import { ReactNode, useState } from 'react'
 
 import { ExerciseProps } from '.'
 import { FaIcon } from '@/components/fa-icon'
-import {
-  EditorStrings,
-  useEditorStrings,
-} from '@/contexts/logged-in-data-context'
+import { useEditorStrings } from '@/contexts/logged-in-data-context'
 import { AddButton } from '@/serlo-editor/editor-ui'
 import { PluginToolbarButton } from '@/serlo-editor/plugin/plugin-toolbar'
 import { store, selectDocument } from '@/serlo-editor/store'
 
-const interactivePlugins: {
-  name: 'scMcExercise' | 'inputExercise' | 'h5p'
-  addLabel: (exStrings: EditorStrings['templatePlugins']['exercise']) => string
-  title: (exStrings: EditorStrings['templatePlugins']['exercise']) => string
-}[] = [
-  {
-    name: 'scMcExercise',
-    addLabel(exStrings) {
-      return exStrings.addChoiceExercise
-    },
-    title(exStrings) {
-      return exStrings.choiceExercise
-    },
-  },
-  {
-    name: 'inputExercise',
-    addLabel(exStrings) {
-      return exStrings.addInputExercise
-    },
-    title(exStrings) {
-      return exStrings.inputExercise
-    },
-  },
-  {
-    name: 'h5p',
-    addLabel(exStrings) {
-      return exStrings.addH5pExercise
-    },
-    title(exStrings) {
-      return exStrings.h5pExercise
-    },
-  },
-]
-
-function InlineOptions(props: PropsWithChildren<{}>) {
-  return (
-    <div className="absolute left-6 -top-3 z-10 whitespace-nowrap pl-3">
-      <div className="rounded-md bg-[rgba(255,255,255,0.95)] p-2 shadow-menu">
-        {props.children}
-      </div>
-    </div>
-  )
-}
+const interactiveExerciseTypes = [
+  'scMcExercise',
+  'inputExercise',
+  'h5p',
+] as const
 
 export function ExerciseEditor({ editable, state }: ExerciseProps) {
   const { content, interactive } = state
@@ -63,89 +22,75 @@ export function ExerciseEditor({ editable, state }: ExerciseProps) {
   return (
     <>
       {content.render()}
-      {renderInteractive()}
-    </>
-  )
-
-  function renderInteractive() {
-    if (interactive.defined) {
-      return interactive.render({
-        renderToolbar(children) {
-          return (
-            <>
-              <div
-                className="relative"
-                onMouseLeave={() => setShowOptions(false)}
-              >
-                <PluginToolbarButton
-                  icon={<FaIcon icon={faRandom} />}
-                  label={exStrings.changeInteractive}
-                  onClick={() => setShowOptions(true)}
-                />
-                <PluginToolbarButton
-                  icon={<FaIcon icon={faTrashAlt} />}
-                  label={exStrings.removeInteractive}
-                  onClick={() => interactive.remove()}
-                />
-                {showOptions ? (
-                  <InlineOptions>
-                    {interactivePlugins
-                      .filter(
-                        (plugin) =>
-                          !interactive ||
-                          plugin.name !== getCurrentInteractivePlugin()
-                      )
-                      .map((plugin) => {
-                        return (
-                          <div
-                            key={plugin.name}
-                            className="w-full min-w-[150px] cursor-pointer px-2.5 py-1.5 hover:text-[rgb(70,155,255)]"
-                            onClick={() => {
-                              interactive.replace(plugin.name)
-                              setShowOptions(false)
-                            }}
-                          >
-                            {plugin.title(exStrings)}
-                          </div>
-                        )
-                      })}
-                  </InlineOptions>
-                ) : null}
-              </div>
-              {children}
-            </>
-          )
-        },
-      })
-    }
-
-    if (editable) {
-      return (
+      {interactive.defined ? (
+        interactive.render({ renderToolbar })
+      ) : editable ? (
         <>
-          <p>
-            <em>{exStrings.addOptionalInteractiveEx}</em>
+          <p className="mb-2 text-gray-400">
+            {exStrings.addOptionalInteractiveEx}
           </p>
-          <div className="flex">
-            {interactivePlugins.map((plugin) => {
+          <div className="-ml-1.5 flex">
+            {interactiveExerciseTypes.map((type) => {
               return (
                 <AddButton
-                  key={plugin.name}
-                  onClick={() => {
-                    interactive.create({
-                      plugin: plugin.name,
-                    })
-                  }}
+                  key={type}
+                  onClick={() => interactive.create({ plugin: type })}
                 >
-                  {plugin.addLabel(exStrings)}
+                  {exStrings[type]}
                 </AddButton>
               )
             })}
           </div>
         </>
-      )
-    }
+      ) : null}
+    </>
+  )
 
-    return null
+  function renderToolbar(children: ReactNode) {
+    return (
+      <>
+        <div className="relative" onMouseLeave={() => setShowOptions(false)}>
+          <PluginToolbarButton
+            icon={<FaIcon icon={faRandom} />}
+            label={exStrings.changeInteractive}
+            onClick={() => setShowOptions(true)}
+          />
+          <PluginToolbarButton
+            icon={<FaIcon icon={faTrashAlt} />}
+            label={exStrings.removeInteractive}
+            onClick={() => {
+              if (interactive.defined) interactive.remove()
+            }}
+          />
+          {showOptions ? (
+            <div className="absolute left-6 -top-3 z-10 whitespace-nowrap pl-3">
+              <div className="rounded-md bg-[rgba(255,255,255,0.95)] p-2 shadow-menu">
+                {interactiveExerciseTypes
+                  .filter(
+                    (type) =>
+                      !interactive || type !== getCurrentInteractivePlugin()
+                  )
+                  .map((type) => {
+                    return (
+                      <div
+                        key={type}
+                        className="w-full min-w-[150px] cursor-pointer px-2.5 py-1.5 hover:text-[rgb(70,155,255)]"
+                        onClick={() => {
+                          if (interactive.defined) interactive.replace(type)
+                          setShowOptions(false)
+                        }}
+                      >
+                        {exStrings[type]}
+                      </div>
+                    )
+                  })}
+              </div>
+            </div>
+          ) : null}
+        </div>
+        {children}
+      </>
+    )
   }
 
   function getCurrentInteractivePlugin() {
