cf409909b3a8fc81c3a354aea101554b8e6ddbd6
Stephan Kulla <git.mail@kulla.me>
2023-07-03 14:27:56 +0000

    New translations index.ts (Spanish)

--- src/data/es/index.ts
+++ src/data/es/index.ts
@@ -747,6 +747,7 @@ export const loggedInData = {
         text: {
           title: "Texto",
           description: "Redacta el contenido usando texto enriquecido y fórmulas matemáticas.",
+          placeholder: 'Write something or add elements with ⊕.',
           quote: "Cita",
           setColor: "Elegir color",
           resetColor: "Restablecer color",
