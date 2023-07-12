4dc013872b6dad1cc38df64b21d27619f2ec896b
Botho <1258870+elbotho@users.noreply.github.com>
2023-07-12 17:50:01 +0200

    add parent type

--- src/serlo-editor/core/plugin-toolbar/plugin-toolbar.tsx
+++ src/serlo-editor/core/plugin-toolbar/plugin-toolbar.tsx
@@ -1,22 +1,30 @@
 import { ReactElement } from 'react'
 
 import { PluginToolbarDropdownMenu } from './plugin-toolbar-dropdown-menu'
+import { selectDocument, selectParent, store } from '@/serlo-editor/store'
 
+// TODO: use enum from https://github.com/serlo/frontend/pull/2564 when that is merged
 export enum PluginType {
   TEXT = 'Text',
 }
 
 interface PluginToolbarProps {
+  pluginId: string
   pluginType: PluginType
   contentControls: ReactElement
   pluginControls: ReactElement
 }
 
 export function PluginToolbar({
+  pluginId,
   pluginType,
   contentControls,
   pluginControls,
 }: PluginToolbarProps) {
+  const state = store.getState()
+  const parent = selectParent(state, pluginId)
+  const parentType = parent ? selectDocument(state, parent.id)?.plugin : null
+
   return (
     <div className="absolute -top-8 left-0 right-0 z-50 flex h-8 w-full items-center justify-between bg-editor-primary-100">
       {/* Content controls */}
@@ -34,8 +42,8 @@ export function PluginToolbar({
       </div>
 
       {/* Plugin ancestry indicator */}
-      <div className="absolute -top-5 right-0 h-5 bg-editor-primary-100 px-1 text-sm">
-        Parent plugin type
+      <div className="absolute -top-6 right-0 h-6 rounded-t-md bg-gray-200 px-2 pt-0.5 text-sm font-bold capitalize">
+        {parentType}
       </div>
     </div>
   )
