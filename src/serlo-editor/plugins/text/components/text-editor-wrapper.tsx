44a2960a703990551bf9381de5de6437f7123e82
Vitomir Budimir <budimirvitomir@gmail.com>
2023-07-19 10:42:53 +0200

    chore(text-editor): add open questions in a todo

--- src/serlo-editor/plugins/text/components/text-editor-wrapper.tsx
+++ src/serlo-editor/plugins/text/components/text-editor-wrapper.tsx
@@ -4,6 +4,13 @@ import { useTextConfig } from '../hooks/use-text-config'
 
 export type TextEditorWrapperProps = TextEditorProps | InlineTextEditorProps
 
+/**
+ * TODO: This is just a quick solution. Things to be reconsidered:
+ * 1. Should this be a component or just a function
+ * 2. The naming of the components (or function)
+ * 3. The naming of the config property (`controls`)
+ * 4. Is it worth having a separate component for the inline editor?
+ */
 export function TextEditorWrapper(props: TextEditorWrapperProps) {
   const config = useTextConfig(props.config)
 
