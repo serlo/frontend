9c16a65e7b928c6958e421b312f62634dbdb2aaf
Botho <1258870+elbotho@users.noreply.github.com>
2023-07-19 13:07:36 +0200

    replace styled with tw

--- src/serlo-editor/core/plugin-toolbar/text-controls/icons/color-text-icon.tsx
+++ src/serlo-editor/core/plugin-toolbar/text-controls/icons/color-text-icon.tsx
@@ -1,36 +1,17 @@
-import styled from 'styled-components'
-
 import { edtrColorText, EdtrIcon } from '@/serlo-editor/editor-ui'
 
 interface ColorTextIconProps {
   color: string
 }
 
-const ColoredText = styled.span({
-  position: 'relative',
-  verticalAlign: 'middle',
-  display: 'inline-block',
-})
-
-const FlexContainer = styled.span({
-  display: 'flex',
-  alignItems: 'center',
-  flexDirection: 'column',
-})
-
-const Line = styled.span<ColorTextIconProps>(({ color }) => ({
-  border: `2px solid ${color}`,
-  borderRadius: '4px',
-  bottom: '0',
-  width: '80%',
-  position: 'absolute',
-}))
-
 export const ColorTextIcon = ({ color }: ColorTextIconProps) => (
-  <ColoredText>
-    <FlexContainer>
+  <span className="relative inline-block align-middle">
+    <span className="flex flex-col items-center">
       <EdtrIcon icon={edtrColorText} />
-      <Line color={color} />
-    </FlexContainer>
-  </ColoredText>
+      <span
+        className="absolute bottom-0 w-[80%] rounded-[4px] border-2"
+        style={{ borderColor: color }}
+      />
+    </span>
+  </span>
 )
