22faecf38eeb46ce0fc1ff6576ac7fbded24a4c0
Botho <1258870+elbotho@users.noreply.github.com>
2023-08-14 12:36:46 +0200

    implement suggestions from @hejtful

--- src/serlo-editor/plugins/text/components/text-editor.tsx
+++ src/serlo-editor/plugins/text/components/text-editor.tsx
@@ -181,23 +181,31 @@ export function TextEditor(props: TextEditorProps) {
           }
         }
 
-        // Create a new Slate instance on "enter" key
+        // Special behaviours when creating new lines
         if (isHotkey(['enter', 'shift+enter'], event) && !isListActive) {
-          // prevent two consecutive empty lines
-          // wrapped in setTimeout to let slates build in function run first
+          // Prevent two consecutive empty lines.
+          // Wrapped in `setTimeout` to let Slate's built-in function to run first
           setTimeout(() => {
             const { path } = selection.focus
-            const node = Node.get(editor, path)
-            if (!Node.string(node).length) {
-              if (path[0] > 0) {
-                const previousNode = Node.get(editor, [path[0] - 1, 0])
-                if (!Node.string(previousNode).length)
-                  editor.deleteBackward('block')
-              } else editor.deleteBackward('block')
+            const currentLine = Node.get(editor, path)
+
+            // If not an empty line, do nothing
+            if (Node.string(currentLine).length) return
+            const nodeLineIndex = path[0]
+
+            // If first line is empty: do not allow a new line by deleting new line
+            if (nodeLineIndex === 0) {
+              editor.deleteBackward('block')
+              return
+            }
+            // If current and previous line are empty:  do not allow a new line by deleting new line
+            const previousLine = Node.get(editor, [nodeLineIndex - 1, 0])
+            if (!Node.string(previousLine).length) {
+              editor.deleteBackward('block')
             }
           })
 
-          // no newlines in headings and start new block as paragraph instead of heading again
+          // Prevent newlines in headings. Instead, add a new paragraph as the next block.
           const fragmentChild = editor.getFragment()[0]
           const isHeading =
             Object.hasOwn(fragmentChild, 'type') && fragmentChild.type === 'h'
@@ -205,7 +213,6 @@ export function TextEditor(props: TextEditorProps) {
           if (isHeading) {
             event.preventDefault()
             Transforms.insertNodes(editor, emptyDocumentFactory().value)
-            return
           }
         }
 
