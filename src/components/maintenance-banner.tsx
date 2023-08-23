9dad75978c7f48f3c460b569bc1ccb8c4376224f
Botho <1258870+elbotho@users.noreply.github.com>
2023-08-24 00:33:31 +0200

    remove buggy logic

--- src/components/maintenance-banner.tsx
+++ src/components/maintenance-banner.tsx
@@ -1,12 +1,9 @@
 export function MaintenanceBanner() {
-  const today = new Date().toISOString().slice(0, 10)
-  const relativeDay = today === '2023-08-24' ? 'Heute' : 'Am Donnerstag 24.08.'
-
   return (
     <div className="absolute left-0 right-0 top-0 z-20 hidden h-10 bg-brand-200 sm:block">
       <p className="p-2 text-right">
-        <b>Vorsicht:</b> {relativeDay} ab 22:30 wird Serlo gewartet und für
-        einige Stunden nicht verfügbar sein 🧑‍🔧
+        <b>Vorsicht:</b> Heute ab 22:30 wird Serlo gewartet und für einige
+        Stunden nicht verfügbar sein 🧑‍🔧
       </p>
       <style jsx global>
         {`
