1f6e143e4ce39cb9f9bf7e026a2c241bfc322019
Botho <1258870+elbotho@users.noreply.github.com>
2023-08-23 08:37:27 +0200

    simplify message

--- src/components/maintenance-banner.tsx
+++ src/components/maintenance-banner.tsx
@@ -5,8 +5,7 @@ export function MaintenanceBanner() {
   return (
     <div className="bg-truegray-200 absolute left-0 right-0 top-0 hidden h-10 sm:block">
       <p className="p-2 text-right">
-        Vorsicht: {relativeDay} ab 22:30 machen wir Wartungsarbeit. Serlo wird
-        für einige Stunden nicht verfügbar sein 🧑‍🔧
+        Vorsicht: {relativeDay} ab 22:30 wird Serlo gewartet und für einige Stunden nicht verfügbar sein 🧑‍🔧
       </p>
       <style jsx global>
         {`
