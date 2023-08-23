385217476a8603446485fdb5a5c0dc45cac04ba7
Botho <1258870+elbotho@users.noreply.github.com>
2023-08-23 08:52:40 +0200

    improve styling

--- src/components/maintenance-banner.tsx
+++ src/components/maintenance-banner.tsx
@@ -3,14 +3,18 @@ export function MaintenanceBanner() {
   const relativeDay = today === '2023-08-24' ? 'Heute' : 'Am Donnerstag 24.08.'
 
   return (
-    <div className="bg-truegray-200 absolute left-0 right-0 top-0 hidden h-10 sm:block">
+    <div className="absolute left-0 right-0 top-0 z-20 hidden h-10 bg-brand-200 sm:block">
       <p className="p-2 text-right">
-        Vorsicht: {relativeDay} ab 22:30 wird Serlo gewartet und für einige Stunden nicht verfügbar sein 🧑‍🔧
+        <b>Vorsicht:</b> {relativeDay} ab 22:30 wird Serlo gewartet und für
+        einige Stunden nicht verfügbar sein 🧑‍🔧
       </p>
       <style jsx global>
         {`
           body {
             margin-top: 2.5rem;
+          }
+          #__next > header .flex-wrap > button.serlo-button-green {
+            margin-top: 2rem;
           }
         `}
       </style>
