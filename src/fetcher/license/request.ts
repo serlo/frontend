140ccbf32091109face8cffa87cdc33cfb3b87ff
Botho <1258870+elbotho@users.noreply.github.com>
2023-08-28 22:32:51 +0200

    remove conversion from license fetcher

--- src/fetcher/license/request.ts
+++ src/fetcher/license/request.ts
@@ -1,13 +1,14 @@
 import { request } from 'graphql-request'
 
 import { licenseDetailsQuery } from './query'
-import { convertState } from '../convert-state'
 import {
   LicenseDetailsQuery,
   LicenseDetailsQueryVariables,
 } from '../graphql-types/operations'
 import { endpoint } from '@/api/endpoint'
 import { LicenseDetailPage, PageNotFound } from '@/data-types'
+import { FrontendContentNode } from '@/frontend-node-types'
+import { ConvertNode, convert } from '@/schema/convert-edtr-io-state'
 
 export async function requestLicensePage(
   id: number
@@ -17,18 +18,21 @@ export async function requestLicensePage(
     LicenseDetailsQueryVariables
   >(endpoint, licenseDetailsQuery, { id })
 
-  const license = result.license.license
+  const { license } = result.license
 
-  if (!license) {
-    return { kind: 'not-found' }
-  }
+  if (!license) return { kind: 'not-found' }
+
+  // hack to remove undefined pluginId value, can be removed when we move to app directory
+  const content = JSON.parse(
+    JSON.stringify(convert(JSON.parse(license.content) as ConvertNode))
+  ) as FrontendContentNode[]
 
   return {
     kind: 'license-detail',
     licenseData: {
       id,
       title: license.title,
-      content: convertState(license.content),
+      content,
       isDefault: license.default,
     },
   }
