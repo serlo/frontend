baa46d18a28023cf2793c125e0aeac419a7c6db0
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-09-08 11:52:49 +0200

    fix wordings

--- src/components/comments/comment-area-all-threads.tsx
+++ src/components/comments/comment-area-all-threads.tsx
@@ -26,8 +26,12 @@ export function CommentAreaAllThreads() {
 
   // eslint-disable-next-line @typescript-eslint/unbound-method
   const { commentData, error, loading, loadMore } = useCommentDataAll(
-    filter,
-    (status ? status : undefined) as CommentStatus
+    filter === 'all' ? undefined : filter,
+    (status === 'all'
+      ? undefined
+      : status
+      ? status
+      : undefined) as CommentStatus
   )
   const { lang, strings } = useInstanceData()
   const auth = useAuthentication()
@@ -55,9 +59,13 @@ export function CommentAreaAllThreads() {
               onChange={(e) => {
                 setFilter(e.target.value)
               }}
-              className="ml-3 cursor-pointer appearance-none rounded-lg bg-brand-400 p-3 pr-9 [&>option:selected]:bg-brand-100 [&>option]:bg-white"
+              className={clsx(
+                filter ? 'bg-brand-400' : 'bg-gray-200',
+                'ml-3 cursor-pointer appearance-none rounded-lg  p-3 pr-9 [&>option:selected]:bg-brand-100 [&>option]:bg-white'
+              )}
             >
-              <option value="">Alle Fächer</option>
+              <option value="">Fach auswählen</option>
+              <option value="all">alle Fächer</option>
               <option value="czU=">Mathematik</option>
               <option value="czE3NzQ0">Nachhaltigkeit</option>
               <option value="czIzMzYy">Biologie</option>
@@ -66,15 +74,26 @@ export function CommentAreaAllThreads() {
               <option value="czEwNjA4MQ==">Fächer im Aufbau</option>
               <option value="czE4MTg4Mw==">Lerntipps</option>
             </select>
+          </span>
+          <span
+            className={clsx(
+              "after:absolute after:-ml-7 after:mt-1.5 after:text-2xl after:text-black after:content-['▾']",
+              'ml-12 inline-block border-solid after:pointer-events-none'
+            )}
+          >
             <select
               value={status ?? ''}
               onChange={(e) => {
                 setStatus(e.target.value)
                 setShowRefresh(false)
               }}
-              className="ml-10 cursor-pointer appearance-none rounded-lg bg-brand-400 p-3 pr-9 [&>option:selected]:bg-brand-100 [&>option]:bg-white"
+              className={clsx(
+                status ? 'bg-brand-400' : 'bg-gray-200',
+                'cursor-pointer appearance-none rounded-lg bg-brand-400 p-3 pr-9 [&>option:selected]:bg-brand-100 [&>option]:bg-white'
+              )}
             >
-              <option value="">jeder Status</option>
+              <option value="">Status auswählen</option>
+              <option value="all">alle Status</option>
               <option value={CommentStatus.Open}>offen</option>
               <option value={CommentStatus.Done}>abgeschlossen</option>
               <option value={CommentStatus.NoStatus}>kein Status</option>
