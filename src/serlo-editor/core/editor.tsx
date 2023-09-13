d6f1f8139c74e0631e23a80be60f1a120c5f77ab
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-09-13 12:17:28 +0200

    attempt 1

--- src/serlo-editor/core/editor.tsx
+++ src/serlo-editor/core/editor.tsx
@@ -1,7 +1,11 @@
 import { useMemo, useEffect, ReactNode, useRef, useState } from 'react'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
-import { HotkeysProvider, useHotkeys } from 'react-hotkeys-hook'
+import {
+  HotkeysProvider,
+  useHotkeys,
+  useHotkeysContext,
+} from 'react-hotkeys-hook'
 import { Provider } from 'react-redux'
 
 import { EditableContext, PreferenceContextProvider } from './contexts'
@@ -17,6 +21,7 @@ import {
   useAppDispatch,
   DocumentState,
   selectSerializedDocument,
+  focus,
 } from '../store'
 import { ROOT } from '../store/root/constants'
 
@@ -36,6 +41,16 @@ export function Editor(props: EditorProps) {
     </Provider>
   )
 }
+
+const pluginIds = [
+  '2866db2d-6210-4579-a6cf-6e01b282bbbf',
+  '7d59264e-b2e5-45a9-92a7-dc0a123949a6',
+  '649d16bc-6189-43c1-a7c0-ba2effc6fca7',
+  '44cf6a69-d925-420a-8b0e-998b152b7200',
+  'c6b1f38f-40b6-4aa2-bb75-571945d6fa25',
+  'bc070871-4e7c-41a9-91d8-8546a4a130a5',
+  '3db8191b-9954-4ce2-b1d2-fb2cf2ee83af',
+]
 
 function InnerDocument({
   children,
@@ -63,6 +78,22 @@ function InnerDocument({
       }
     })
   }, [onChange])
+
+  const hotkeysContext = useHotkeysContext()
+
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    setTimeout(() => setCounter(counter + 1), 2000)
+    if (counter > 0) {
+      const toFocus = pluginIds[counter % pluginIds.length]
+      dispatch(focus(toFocus))
+
+      console.log('toggle scope')
+      hotkeysContext.enableScope('test')
+      hotkeysContext.disableScope('test')
+    }
+  }, [counter, dispatch])
 
   useEffect(() => {
     dispatch(runInitRootSaga({ initialState: props.initialState }))
