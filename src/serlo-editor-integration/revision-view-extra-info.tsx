c4bebcddac7846dc72e65cc611d14a2ebbce4c7d
Lars Rasmussen <lars@lars-rasmussen.de>
2023-10-05 16:34:44 +0200

    Display info on image revision view

--- src/serlo-editor-integration/revision-view-extra-info.tsx
+++ src/serlo-editor-integration/revision-view-extra-info.tsx
@@ -5,7 +5,7 @@ export function RevisionViewExtraInfo({
   children: React.ReactNode
 }) {
   return (
-    <span className="break-all bg-editor-primary-100 px-1 text-sm">
+    <span className="m-1 break-all bg-editor-primary-100 px-1 text-sm">
       {children}
     </span>
   )
--- src/serlo-editor-integration/serlo-plugin-wrappers/image-serlo-static-renderer.tsx
+++ src/serlo-editor-integration/serlo-plugin-wrappers/image-serlo-static-renderer.tsx
@@ -1,16 +1,27 @@
 import { useRouter } from 'next/router'
+import { useContext } from 'react'
 
+import { RevisionViewExtraInfo } from '../revision-view-extra-info'
 import { EditorImagePlugin } from '../types/editor-plugins'
+import { RevisionViewContext } from '@/contexts/revision-view-context'
 import { ImageStaticRenderer } from '@/serlo-editor/plugins/image/static'
 
-// TODO: revision extra info
-// extraInfo={
-//   isRevisionView ? (
-//     <ExtraRevisionViewInfo element={element} />
-//   ) : undefined
-// }
-
 export function ImageSerloStaticRenderer(props: EditorImagePlugin) {
+  const isRevisionView = useContext(RevisionViewContext)
   const pathNameBase = useRouter().asPath.split('/').pop()
-  return <ImageStaticRenderer {...props} pathNameBase={pathNameBase} />
+  return (
+    <>
+      <ImageStaticRenderer {...props} pathNameBase={pathNameBase} />
+      {isRevisionView ? (
+        <>
+          {props.state.alt ? (
+            <RevisionViewExtraInfo>{`Alt: ${props.state.alt}`}</RevisionViewExtraInfo>
+          ) : null}
+          {props.state.link?.href ? (
+            <RevisionViewExtraInfo>{`Link: ${props.state.link?.href}`}</RevisionViewExtraInfo>
+          ) : null}
+        </>
+      ) : null}
+    </>
+  )
 }
