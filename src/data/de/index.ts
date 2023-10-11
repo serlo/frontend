817cb6dd9da3606d0e2fd50d0eee4f9a84847284
Stephan Kulla <git.mail@kulla.me>
2023-10-11 17:33:08 +0200

    New translations index.ts (German)

--- src/data/de/index.ts
+++ src/data/de/index.ts
@@ -889,6 +889,7 @@ export const loggedInData = {
           unit: "Einheit (optional)",
           addAnswer: "Antwort hinzufügen",
           enterTheValue: "Gib hier die Antwort ein",
+          feedbackPlaceholder: 'Add a feedback message for this answer',
           yourSolution: "Deine Lösung",
           types: {
             'input-string-normalized-match-challenge': "Text (genau, z.B. 'Tiger')",
