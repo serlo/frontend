f61fb079aaad768a6dae88ffd808b15eadbd4351
Mikey Stengel <mikey@serlo.org>
2023-11-22 20:21:06 +0100

    Constrain width and force top style of revision-history-loader

--- src/serlo-editor-integration/components/save-modal.tsx
+++ src/serlo-editor-integration/components/save-modal.tsx
@@ -83,7 +83,7 @@ export function SaveModal({
       title={editorStrings.save}
       className={clsx(
         !isOnlyText &&
-          '!top-[2rem] max-h-full w-auto !-translate-x-1/2 translate-y-0 overflow-y-auto pb-20'
+          '!top-[2rem] max-h-full !w-[900px] !-translate-x-1/2 translate-y-0 overflow-y-auto pb-20'
       )}
     >
       <div className="mx-side">
