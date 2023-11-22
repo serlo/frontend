3a01753df53d5c5e216d2256f5fe3354b5f3d598
Stephan Kulla <git.mail@kulla.me>
2023-11-22 22:22:52 +0100

    New translations index.ts (Spanish)

--- src/data/es/index.ts
+++ src/data/es/index.ts
@@ -1093,8 +1093,7 @@ export const loggedInData = {
           chooseOption: "Choose an option",
           multipleChoice: "Multiple Choice",
           singleChoice: "Single Choice",
-          inputExercise: "Ejercicio de respuesta escrita",
-          exerciseGroupNeedsTwoOrMoreSubtasks: 'Within an exercise group, you must create two or more exercises.'
+          inputExercise: "Ejercicio de respuesta escrita"
         },
         difficulty: {
           title: 'What is the %difficulty% level of the exercise and learning goal?',
