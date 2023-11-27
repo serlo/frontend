8ed8472fa4bf478576ae143078f14a61d223d7ca
Mikey Stengel <mikey@serlo.org>
2023-11-27 14:51:37 -0500

    Disable AI feature in locales other than German

--- src/components/exercise-generation/use-ai-features.tsx
+++ src/components/exercise-generation/use-ai-features.tsx
@@ -1,11 +1,14 @@
 import { Ai } from '@serlo/authorization'
+import { useRouter } from 'next/router'
 
 import { useCanDo } from '@/auth/use-can-do'
 import { isProduction } from '@/helper/is-production'
 
 export function useAiFeatures() {
   const canDo = useCanDo()
-  const canUseAiFeatures = canDo(Ai.executePrompt)
+  const { locale } = useRouter()
+  // For now, we only allow the ai feature in the German locale
+  const canUseAiFeatures = canDo(Ai.executePrompt) && locale === 'de'
 
   // To be deleted when enabling AI features in production
   const canUseAiFeaturesOutsideProduction = canUseAiFeatures && !isProduction
