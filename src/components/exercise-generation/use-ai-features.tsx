ea8126178fa61bbdc4a6863b01a8038eccc85122
Mikey Stengel <mikey.stengel96@gmail.com>
2023-11-27 16:49:20 -0500

    Update src/components/exercise-generation/use-ai-features.tsx
    Co-authored-by: Botho <1258870+elbotho@users.noreply.github.com>

--- src/components/exercise-generation/use-ai-features.tsx
+++ src/components/exercise-generation/use-ai-features.tsx
@@ -6,7 +6,7 @@ import { isProduction } from '@/helper/is-production'
 
 export function useAiFeatures() {
   const canDo = useCanDo()
-  const { locale } = useRouter()
+  const { lang } = useInstanceData()
   // For now, we only allow the ai feature in the German locale
   const canUseAiFeatures = canDo(Ai.executePrompt) && locale === 'de'
 
