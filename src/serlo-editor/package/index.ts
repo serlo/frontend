6c3f4aafa730f454baf8ac53da56915fa43b4816
Stephan Kulla <git.mail@kulla.me>
2023-12-13 20:13:19 +0100

    refactor(editor-npm): Remove unused exports

--- src/serlo-editor/package/index.ts
+++ src/serlo-editor/package/index.ts
@@ -1,13 +1,3 @@
-import IconBox from '@/serlo-editor/editor-ui/assets/plugin-icons/icon-box.svg'
-import IconEquation from '@/serlo-editor/editor-ui/assets/plugin-icons/icon-equation.svg'
-import IconGeogebra from '@/serlo-editor/editor-ui/assets/plugin-icons/icon-geogebra.svg'
-import IconHighlight from '@/serlo-editor/editor-ui/assets/plugin-icons/icon-highlight.svg'
-import IconMultimedia from '@/serlo-editor/editor-ui/assets/plugin-icons/icon-multimedia.svg'
-import IconSpoiler from '@/serlo-editor/editor-ui/assets/plugin-icons/icon-spoiler.svg'
-import IconTable from '@/serlo-editor/editor-ui/assets/plugin-icons/icon-table.svg'
-import IconText from '@/serlo-editor/editor-ui/assets/plugin-icons/icon-text.svg'
-import IconVideo from '@/serlo-editor/editor-ui/assets/plugin-icons/icon-video.svg'
-
 export { SerloEditor, type SerloEditorProps } from './editor'
 
 export { editorPlugins } from '@/serlo-editor/plugin/helpers/editor-plugins'
@@ -25,19 +15,6 @@ export {
   instanceData as instanceDataEn,
   loggedInData as loggedInDataEn,
 } from '@/data/en'
-
-// plugins
-export { createBoxPlugin } from '@/serlo-editor/plugins/box'
-export { equationsPlugin } from '@/serlo-editor/plugins/equations'
-export { geoGebraPlugin } from '@/serlo-editor/plugins/geogebra'
-export { createHighlightPlugin } from '@/serlo-editor/plugins/highlight'
-export { createMultimediaPlugin } from '@/serlo-editor/plugins/multimedia'
-export { createRowsPlugin } from '@/serlo-editor/plugins/rows'
-export { createSerloTablePlugin } from '@/serlo-editor/plugins/serlo-table'
-export { createSpoilerPlugin } from '@/serlo-editor/plugins/spoiler'
-export { createTextPlugin } from '@/serlo-editor/plugins/text'
-export { unsupportedPlugin } from '@/serlo-editor/plugins/unsupported'
-export { videoPlugin } from '@/serlo-editor/plugins/video'
 
 export { ROOT } from '@/serlo-editor/store/root/constants'
 export {
@@ -63,19 +40,6 @@ export type {
   EditorPluginProps,
   PrettyStaticState,
 } from '@/serlo-editor/plugin'
-
-// icons
-export {
-  IconBox,
-  IconEquation,
-  IconGeogebra,
-  IconHighlight,
-  IconMultimedia,
-  IconSpoiler,
-  IconTable,
-  IconText,
-  IconVideo,
-}
 
 export { FaIcon } from '@/components/fa-icon'
 
