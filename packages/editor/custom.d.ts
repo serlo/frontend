5556765f14ec5d316e865113b19fa329f3958b5d
Stephan Kulla <git.mail@kulla.me>
2023-12-16 14:26:56 +0100

    chore(editor): Split declation and augmentation file

--- packages/editor/custom.d.ts
+++ packages/editor/custom.d.ts
@@ -3,29 +3,3 @@ declare module '*.svg' {
   const content: FC<SVGAttributes<SVGElement>>
   export default content
 }
-
-// from https://github.com/microsoft/TypeScript/issues/44253
-// let's keep an eye on this to see if they merge it at some point
-
-/** Extract from T those types that has K keys  */
-type ExtractByKey<T, K extends keyof any> = T extends infer R
-  ? K extends keyof R
-    ? R
-    : never
-  : never
-
-type KeyofUnion<T> = T extends infer R ? keyof R : never
-
-declare global {
-  interface ObjectConstructor {
-    /**
-     * Determines whether an object has a property with the specified name.
-     * @param o An object.
-     * @param v A property name.
-     */
-    hasOwn<T extends Record<keyof any, any>, K extends KeyofUnion<T>>(
-      o: T,
-      v: K
-    ): o is ExtractByKey<T, K>
-  }
-}
