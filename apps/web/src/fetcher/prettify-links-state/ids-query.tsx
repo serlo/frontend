026639af985c89add37de4b671992e250704df13
Entkenntnis <Entkenntnis@users.noreply.github.com>
2023-12-16 11:39:20 +0100

    adding /mathe-aufgaben, top 150 added

--- apps/web/src/fetcher/prettify-links-state/ids-query.tsx
+++ apps/web/src/fetcher/prettify-links-state/ids-query.tsx
@@ -1,20 +1,23 @@
 import { gql } from 'graphql-request'
 
-export const idsQuery = (ids: number[]) => {
+export const idsQuery = (ids: number[], opts?: { withTitle?: boolean }) => {
   const queries = ids.map(
     (id) => gql` 
     uuid${id}: uuid(id:${id}) {
         ... on AbstractEntity {
           alias
           instance
+          ${opts?.withTitle ? 'title' : ''}
         }
         ... on Page {
           alias
           instance
+          ${opts?.withTitle ? 'title' : ''}
         }
         ... on TaxonomyTerm {
           alias
           instance
+          ${opts?.withTitle ? 'title' : ''}
         }
       }
     `
