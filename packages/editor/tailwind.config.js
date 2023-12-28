c345835b38a919119eb2e00bf8508871060fbb7a
Vitomir Budimir <budimirvitomir@gmail.com>
2023-12-28 15:50:51 +0100

    fix(editor): include frontend in tailwind config content

--- packages/editor/tailwind.config.js
+++ packages/editor/tailwind.config.js
@@ -3,5 +3,8 @@ import sharedTailwindBase from '@serlo/frontend/shared/tailwind-base.js'
 /** @type {import('tailwindcss').Config} */
 export default {
   presets: [sharedTailwindBase],
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    '../../apps/web/src/**/*.{js,ts,jsx,tsx}',
+  ],
 }
