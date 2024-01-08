ab1c9681b753aa67fbb71f40068c5443b8cb37b0
Lars Rasmussen <lars@lars-rasmussen.de>
2024-01-08 12:43:30 +0100

    fix spacing issue

--- src/serlo-editor/plugins/text-area-exercise/renderer.tsx
+++ src/serlo-editor/plugins/text-area-exercise/renderer.tsx
@@ -3,7 +3,7 @@ export function TextAreaExerciseRenderer() {
     <textarea
       rows={3}
       spellCheck={false}
-      className="my-5 w-full resize-y rounded-xl border border-brand bg-brand-50 p-2"
+      className="mx-side my-5 w-full resize-y rounded-xl border border-brand bg-brand-50 p-2"
     ></textarea>
   )
 }
