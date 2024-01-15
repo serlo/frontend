f98645a15a9bba979f53a4f85e12730fe7866f1a
Botho <1258870+elbotho@users.noreply.github.com>
2024-01-15 17:42:37 +0100

    feat(editor): add info notice to plugin copy tool

--- packages/editor/src/editor-ui/plugin-toolbar/plugin-tool-menu/plugin-copy-tool.tsx
+++ packages/editor/src/editor-ui/plugin-toolbar/plugin-tool-menu/plugin-copy-tool.tsx
@@ -2,7 +2,7 @@ import { selectStaticDocumentWithoutIds, store } from '@editor/store'
 import { faCopy } from '@fortawesome/free-solid-svg-icons'
 import { shouldUseFeature } from '@serlo/frontend/src/components/user/profile-experimental'
 import { useInstanceData } from '@serlo/frontend/src/contexts/instance-context'
-// import { useEditorStrings } from '@serlo/frontend/src/contexts/logged-in-data-context'
+import { useEditorStrings } from '@serlo/frontend/src/contexts/logged-in-data-context'
 import { showToastNotice } from '@serlo/frontend/src/helper/show-toast-notice'
 import { useCallback } from 'react'
 
@@ -15,7 +15,7 @@ interface PluginCopyToolProps {
  * experimental plugin to copy plugin's editor state to the clipboard
  */
 export function PluginCopyTool({ pluginId }: PluginCopyToolProps) {
-  // const editorStrings = useEditorStrings()
+  const editorStrings = useEditorStrings()
   const { strings } = useInstanceData()
 
   const handleOnClick = useCallback(() => {
@@ -24,8 +24,12 @@ export function PluginCopyTool({ pluginId }: PluginCopyToolProps) {
 
     void navigator.clipboard.writeText(JSON.stringify(rowsDocument))
     showToastNotice(strings.share.copySuccess, 'success', 2000)
-    // TODO: add explanation
-  }, [pluginId, strings.share.copySuccess])
+    showToastNotice(
+      '👉 ' + editorStrings.edtrIo.pluginCopyInfo,
+      undefined,
+      4000
+    )
+  }, [pluginId, strings, editorStrings])
 
   if (!navigator.clipboard || !shouldUseFeature('editorPluginCopyTool')) {
     return null
