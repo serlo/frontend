667d23154d25b336930bb4f4370021c998e33a0b
Botho <1258870+elbotho@users.noreply.github.com>
2024-01-29 16:44:02 +0100

    feat(plugin-exercise-group): prepare frontend for db migration

--- packages/editor/src/plugins/serlo-template-plugins/exercise-group/text-exercise-group.tsx
+++ packages/editor/src/plugins/serlo-template-plugins/exercise-group/text-exercise-group.tsx
@@ -6,6 +6,8 @@ import {
   type EditorPluginProps,
   list,
   PrettyStaticState,
+  optional,
+  child,
 } from '@editor/plugin'
 import { selectStaticDocument, store } from '@editor/store'
 import { EditorPluginType } from '@editor/types/editor-plugin-type'
@@ -29,19 +31,19 @@ import { ContentLoaders } from '../helpers/content-loaders/content-loaders'
 import { TextExerciseTypePluginState } from '../text-exercise'
 import { ToolbarMain } from '../toolbar-main/toolbar-main'
 
-// text-exercises also include interactive exercises, we keep the naming to avoid db-migration
+// "text-exercises" also include interactive exercises, we keep the naming to avoid db-migration
 
 export const textExerciseGroupTypeState = entityType(
   {
     ...entity,
     content: editorContent(EditorPluginType.Rows),
-    cohesive: boolean(false),
     /* cohesive field would indicate whether the children of a grouped exercise are cohesive
     this field might be used in the future, but currently it has no effect and can not be changed
     */
+    cohesive: boolean(false),
+    exercises: optional(list(child({ plugin: EditorPluginType.Exercise }))),
   },
   {
-    // I think this is not correct because it meant for strings?
     'grouped-text-exercise': list(serializedChild('type-text-exercise')),
   }
 )
@@ -60,11 +62,13 @@ function TextExerciseGroupTypeEditor(
 ) {
   const {
     content,
-    'grouped-text-exercise': children,
+    'grouped-text-exercise': legacyChildren,
+    exercises,
     id,
     revision,
     replaceOwnState,
   } = props.state
+
   const { canUseAiFeatures } = useAiFeatures()
   const router = useRouter()
   const currentPath = router.asPath.toLowerCase()
@@ -95,41 +99,87 @@ function TextExerciseGroupTypeEditor(
         />
       </div>
       <article className="exercisegroup mt-32">
-        <TextExerciseGroupTypeRenderer
-          content={<>{content.render()}</>}
-          exercises={children.map((child, index) => {
-            return {
-              id: child.id,
-              element: (
-                <>
-                  <nav className="flex justify-end">
-                    <button
-                      className="serlo-button-editor-secondary serlo-tooltip-trigger mr-2"
-                      onClick={() => children.remove(index)}
-                    >
-                      <EditorTooltip text={exGroupStrings.removeExercise} />
-                      <FaIcon icon={faTrashAlt} />
-                    </button>
-                    <ContentLoaders
-                      id={staticExercises[index].id}
-                      currentRevision={staticExercises[index].revision}
-                      onSwitchRevision={(data) =>
-                        child.replace(TemplatePluginType.TextExercise, data)
-                      }
-                      entityType={UuidType.GroupedExercise}
-                    />
-                  </nav>
-                  {child.render()}
-                </>
-              ),
+        {exercises.defined ? renderMigratedChildren() : renderLegacyChildren()}
+        <AddButton
+          onClick={() => {
+            if (exercises.defined) {
+              exercises.insert(undefined, {
+                plugin: EditorPluginType.Exercise,
+              })
+            } else {
+              legacyChildren.insert()
             }
-          })}
-        />
-        <AddButton onClick={() => children.insert()}>
+          }}
+        >
           {exGroupStrings.addExercise}
         </AddButton>
+
         <ToolbarMain showSubscriptionOptions {...props.state} />
       </article>
     </>
   )
+
+  function renderMigratedChildren() {
+    if (!exercises.defined) return
+    return (
+      <TextExerciseGroupTypeRenderer
+        content={<>{content.render()}</>}
+        exercises={exercises.map((exercise, index) => {
+          return {
+            id: exercise.id,
+            element: (
+              <>
+                <nav className="flex justify-end">
+                  {/* TODO: check if we can easily fix the default toolbar delete entry to work in this case as well */}
+                  <button
+                    className="serlo-button-editor-secondary serlo-tooltip-trigger mr-2"
+                    onClick={() => legacyChildren.remove(index)}
+                  >
+                    <EditorTooltip text={exGroupStrings.removeExercise} />
+                    <FaIcon icon={faTrashAlt} />
+                  </button>
+                </nav>
+                {exercise.render()}
+              </>
+            ),
+          }
+        })}
+      />
+    )
+  }
+
+  function renderLegacyChildren() {
+    return (
+      <TextExerciseGroupTypeRenderer
+        content={<>{content.render()}</>}
+        exercises={legacyChildren.map((child, index) => {
+          return {
+            id: child.id,
+            element: (
+              <>
+                <nav className="flex justify-end">
+                  <button
+                    className="serlo-button-editor-secondary serlo-tooltip-trigger mr-2"
+                    onClick={() => legacyChildren.remove(index)}
+                  >
+                    <EditorTooltip text={exGroupStrings.removeExercise} />
+                    <FaIcon icon={faTrashAlt} />
+                  </button>
+                  <ContentLoaders
+                    id={staticExercises[index].id}
+                    currentRevision={staticExercises[index].revision}
+                    onSwitchRevision={(data) =>
+                      child.replace(TemplatePluginType.TextExercise, data)
+                    }
+                    entityType={UuidType.GroupedExercise}
+                  />
+                </nav>
+                {child.render()}
+              </>
+            ),
+          }
+        })}
+      />
+    )
+  }
 }
