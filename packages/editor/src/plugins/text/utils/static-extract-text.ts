ffc3c2363e6655089caa75d80b27c170ba15f3fc
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-02-12 14:40:57 +0100

    fix(sorting): retrieve preview string from multimedia

--- packages/editor/src/plugins/text/utils/static-extract-text.ts
+++ packages/editor/src/plugins/text/utils/static-extract-text.ts
@@ -1,5 +1,9 @@
 import { AnyEditorDocument } from '@editor/types/editor-plugins'
-import { isTextDocument } from '@editor/types/plugin-type-guards'
+import {
+  isMultimediaDocument,
+  isRowsDocument,
+  isTextDocument,
+} from '@editor/types/plugin-type-guards'
 import type { Descendant } from 'slate'
 
 export function extractDescendant(node: Descendant): string {
@@ -9,7 +13,19 @@ export function extractDescendant(node: Descendant): string {
   return node.text ? node.text.trim() : ''
 }
 
-export function extractStringFromTextDocument(document?: AnyEditorDocument) {
+export function extractStringFromTextDocument(
+  document?: AnyEditorDocument
+): string {
+  if (
+    document &&
+    isMultimediaDocument(document) &&
+    isRowsDocument(document.state.explanation)
+  ) {
+    return document.state.explanation.state
+      .map(extractStringFromTextDocument)
+      .join(' ')
+      .trim()
+  }
   if (document && isTextDocument(document) && document.state.length > 0) {
     return document.state.map(extractDescendant).join(' ').trim()
   }
