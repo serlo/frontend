ef9ec0dd604a4bdfd647412a859d3dc1511e1874
Mikey Stengel <mikey@serlo.org>
2024-02-12 14:01:14 +0100

    fix(dnd): Add touch backend conditionally to react-dnd

--- packages/editor/src/core/components/dnd-wrapper.tsx
+++ packages/editor/src/core/components/dnd-wrapper.tsx
@@ -1,14 +1,29 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { TouchBackend } from 'react-dnd-touch-backend'
 
 export const DndWrapper = ({ children }: { children: ReactNode }) => {
+  const [hasTouchScreen, setHasTouchScreen] = useState(false)
+
+  // ensure it runs on client (could the same be accomplished with 'use client'
+  // directive instead of using the effect?)
+  useEffect(() => {
+    setHasTouchScreen(isTouchDevice())
+  }, [])
+
+  const backend = hasTouchScreen ? TouchBackend : HTML5Backend
+
   return (
     <DndProvider
-      backend={HTML5Backend}
+      backend={backend}
       context={typeof window === 'undefined' ? undefined : window}
     >
       {children}
     </DndProvider>
   )
+}
+
+function isTouchDevice(): boolean {
+  return 'ontouchstart' in window || navigator.maxTouchPoints > 0
 }
