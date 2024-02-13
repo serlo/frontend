44d898269f2f867d73765326246f566781c8027f
Mikey Stengel <mikey@serlo.org>
2024-02-13 13:26:07 +0100

    chore(dnd): Remove unused className

--- packages/editor/src/core/components/custom-drag-layer.tsx
+++ packages/editor/src/core/components/custom-drag-layer.tsx
@@ -49,9 +49,7 @@ export function CustomDragLayer() {
 
   return (
     <div className="pointer-events-none fixed left-0 top-0 z-50">
-      <div style={{ transform }} className="">
-        {renderItem()}
-      </div>
+      <div style={{ transform }}>{renderItem()}</div>
     </div>
   )
 }
