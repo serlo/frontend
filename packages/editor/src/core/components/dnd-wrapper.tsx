d16bc9c43458542e803ecd018c41af7dbbd00922
Mikey Stengel <mikey@serlo.org>
2024-02-13 12:29:59 +0100

    refactor(dnd): Use multi backend package

--- packages/editor/src/core/components/dnd-wrapper.tsx
+++ packages/editor/src/core/components/dnd-wrapper.tsx
@@ -1,31 +1,20 @@
+import { HTML5toTouch } from 'rdndmb-html5-to-touch'
 import { ReactNode } from 'react'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { TouchBackend } from 'react-dnd-touch-backend'
+import { DndProvider } from 'react-dnd-multi-backend'
 
 import { CustomDragLayer } from './custom-drag-layer'
 import { isTouchDevice } from '../helpers/is-touch-device'
 
-export const DndWrapper = ({ children }: { children: ReactNode }) =>
-  isTouchDevice() ? (
+export const DndWrapper = ({ children }: { children: ReactNode }) => {
+  const isTouch = isTouchDevice()
+
+  return (
     <DndProvider
-      backend={TouchBackend}
-      context={window}
-      // There is a not so encouraging warning here
-      // https://react-dnd.github.io/react-dnd/docs/backends/touch
-      // > NOTE: This is
-      // > buggy due to the difference in touchstart/touchend event propagation
-      // >compared to mousedown/mouseup/click.
-      options={{ enableMouseEvents: true }}
-    >
-      <CustomDragLayer />
-      {children}
-    </DndProvider>
-  ) : (
-    <DndProvider
-      backend={HTML5Backend}
+      options={HTML5toTouch}
       context={typeof window === 'undefined' ? undefined : window}
     >
+      {isTouch && <CustomDragLayer />}
       {children}
     </DndProvider>
   )
+}
