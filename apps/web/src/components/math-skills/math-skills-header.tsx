d2816a76a28e9c4ae69a7a36e232e5335eed2153
Botho <1258870+elbotho@users.noreply.github.com>
2024-02-25 23:01:58 +0100

    sessionStorage for name, change landing screen

--- apps/web/src/components/math-skills/math-skills-header.tsx
+++ apps/web/src/components/math-skills/math-skills-header.tsx
@@ -5,37 +5,41 @@ import { cn } from '@/helper/cn'
 
 export function MathSkillsHeader() {
   const router = useRouter()
+
   const isExercise = !!router.query.exercise
   const grade = String(router.query.grade)
-
   const isGradePage = !!router.query.grade
+  const isNotLanding = isExercise || isGradePage
 
   return (
-    <div className="flex h-14 items-center justify-between bg-yellow-100 px-4 text-center">
-      <Link
-        href="/meine-mathe-skills"
-        className={cn(
-          'block cursor-pointer text-2xl text-almost-black',
-          isExercise || isGradePage ? '' : 'w-full text-center'
-        )}
-      >
-        Meine Mathe-Skills
-      </Link>
-      {isExercise ? (
-        <Link
-          href={`/meine-mathe-skills/${String(router.query.grade)}`}
-          className="my-3 block rounded bg-brand-200 px-2 py-1 text-lg !no-underline hover:bg-brand-300"
-        >
-          zur {grade.replace('klasse', '')}. Klasse
-        </Link>
-      ) : isGradePage ? (
+    <>
+      {/* {isNotLanding ? <CheckName /> : null} */}
+      <div className="flex h-14 items-center justify-between bg-yellow-100 px-4 text-center">
         <Link
           href="/meine-mathe-skills"
-          className="my-3 block rounded bg-brand-200 px-2 py-1 text-lg !no-underline hover:bg-brand-300"
+          className={cn(
+            'block cursor-pointer text-2xl text-almost-black',
+            isNotLanding ? '' : 'w-full text-center'
+          )}
         >
-          zur Startseite
+          Meine Mathe-Skills
         </Link>
-      ) : null}
-    </div>
+        {isExercise ? (
+          <Link
+            href={`/meine-mathe-skills/${String(router.query.grade)}`}
+            className="my-3 block rounded bg-brand-200 px-2 py-1 text-lg !no-underline hover:bg-brand-300"
+          >
+            zur {grade.replace('klasse', '')}. Klasse
+          </Link>
+        ) : isGradePage ? (
+          <Link
+            href="/meine-mathe-skills"
+            className="my-3 block rounded bg-brand-200 px-2 py-1 text-lg !no-underline hover:bg-brand-300"
+          >
+            zur Startseite
+          </Link>
+        ) : null}
+      </div>
+    </>
   )
 }
