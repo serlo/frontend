ff9e3f7e4fb69cddfbb3315cfa50ac4a8fd5ad18
Botho <1258870+elbotho@users.noreply.github.com>
2024-02-25 23:01:58 +0100

    sessionStorage for name, change landing screen

--- apps/web/src/pages/meine-mathe-skills/[grade]/[exercise].tsx
+++ apps/web/src/pages/meine-mathe-skills/[grade]/[exercise].tsx
@@ -6,6 +6,13 @@ import { HeadTags } from '@/components/head-tags'
 import { MathSkillsHeader } from '@/components/math-skills/math-skills-header'
 import { NumberInputExercise } from '@/components/math-skills/number-input-exercise/number-input-exercise'
 import { NumberLineExercise } from '@/components/math-skills/number-line-exercise/number-line-exercise'
+import { useEffect, useState } from 'react'
+import {
+  MathSkillsProvider,
+  MathSkillsStorageData,
+  getStored,
+  updateStored,
+} from '@/components/math-skills/utils/math-skills-data-context'
 
 const ContentPage: NextPage = () => {
   const router = useRouter()
@@ -40,31 +47,24 @@ function Content() {
   const router = useRouter()
   const exercise = String(router.query.exercise)
 
+  const [data, setData] = useState<MathSkillsStorageData | undefined>(undefined)
+  useEffect(() => setData(getStored()), [])
+
+  function updateData(updates: Partial<MathSkillsStorageData>) {
+    setData(updateStored(updates))
+  }
+
   if (!exercise) return null
 
-  if (exercise === 'zahlen-finden') {
-    return (
-      <>
-        <MathSkillsHeader />
-        <div className="min-h-[70vh] sm:mx-auto sm:max-w-xl">
-          <NumberLineExercise />
-        </div>
-      </>
-    )
-  }
-
-  if (exercise === 'potenzieren') {
-    return (
-      <>
-        <MathSkillsHeader />
-        <div className="min-h-[70vh] sm:mx-auto sm:max-w-xl">
-          <NumberInputExercise />
-        </div>
-      </>
-    )
-  }
-
-  return null
+  return (
+    <MathSkillsProvider value={{ data, updateData }}>
+      <MathSkillsHeader />
+      <div className="min-h-[70vh] sm:mx-auto sm:max-w-xl">
+        {exercise === 'zahlen-finden' ? <NumberLineExercise /> : null}
+        {exercise === 'potenzieren' ? <NumberInputExercise /> : null}
+      </div>
+    </MathSkillsProvider>
+  )
 }
 
 export default ContentPage
--- apps/web/src/pages/meine-mathe-skills/index.tsx
+++ apps/web/src/pages/meine-mathe-skills/index.tsx
@@ -1,10 +1,20 @@
 /* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import { NextPage } from 'next'
+import { useEffect, useState } from 'react'
 
 import { Link } from '@/components/content/link'
 import { FrontendClientBase } from '@/components/frontend-client-base'
 import { HeadTags } from '@/components/head-tags'
 import { MathSkillsHeader } from '@/components/math-skills/math-skills-header'
+import { NameInput } from '@/components/math-skills/name-input'
+import {
+  MathSkillsProvider,
+  MathSkillsStorageData,
+  deleteStored,
+  getStored,
+  updateStored,
+} from '@/components/math-skills/utils/math-skills-data-context'
+import { isProduction } from '@/helper/is-production'
 
 const ContentPage: NextPage = () => {
   return (
@@ -37,52 +47,94 @@ const ContentPage: NextPage = () => {
 }
 
 function Content() {
+  const [data, setData] = useState<MathSkillsStorageData | undefined>(undefined)
+  const name = data?.name
+  useEffect(() => setData(getStored()), [])
+
+  function updateData(updates: Partial<MathSkillsStorageData>) {
+    setData(updateStored(updates))
+  }
+
   return (
-    <div className="flex h-full flex-col justify-between">
-      <div>
-        <MathSkillsHeader />
-        <div className="mx-4 justify-center sm:mt-10 sm:flex sm:flex-row-reverse">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="/_assets/img/meine-mathe-skills/educated-lion.svg"
-            className="mx-auto mt-5 w-48 p-4 sm:mx-0 sm:mt-0"
-          />
-          <div className="mx-auto mb-6 mt-4 max-w-md text-lg sm:mx-0 sm:leading-loose">
-            <b>Mein Geheimnis?</b>
-            <br />
-            Ich liebe es, Dinge zu können und das zu zeigen.
-            <br /> Das dürfen auch kleine Sachen sein.
-            <br />
-            Jeder Skill gibt mir das Vertrauen, weiterzulernen.
-            <br />
-            <i>Schritt für Schritt.</i>
+    <MathSkillsProvider value={{ data, updateData }}>
+      <div className="flex h-full flex-col justify-between">
+        <div>
+          <MathSkillsHeader />
+          <div className="mx-4 justify-center sm:mt-10 sm:flex sm:flex-row-reverse sm:items-center">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="/_assets/img/meine-mathe-skills/educated-lion.svg"
+              className="mx-auto mt-5 w-52 self-start p-4 sm:mx-0 sm:-mt-1"
+            />
+            <div className="mx-auto mb-6 mt-4 max-w-md text-lg sm:mx-0 sm:leading-relaxed">
+              {name ? (
+                <>
+                  Willkommen <b>{name}</b>,
+                  <br />
+                  schön, dass du hier bist!
+                  <br />
+                  <br />
+                  Bist du bereit für ein neues Lernabenteuer?
+                  {/* Du hast schon … */}
+                </>
+              ) : (
+                <>
+                  <b>Willkommen!</b>
+                  <br />
+                  <br />
+                  Hier geht es darum Dinge zu können und das auch zu zeigen. Das
+                  dürfen auch kleine Sachen sein.
+                  <br />
+                  Immer Schritt für Schritt!
+                  <br />
+                  <br />
+                  <b>Wie heißt du denn?</b>
+                  <NameInput />
+                </>
+              )}
+            </div>
+          </div>
+          <div className="mx-4 mt-10 text-2xl font-bold mobile:flex mobile:justify-center">
+            <h2>Wähle eine Klassenstufe</h2>
+          </div>
+          <div className="mt-6 flex justify-center text-2xl font-bold sm:mt-8">
+            <Link
+              href="/meine-mathe-skills/klasse5"
+              className="flex h-36 w-36 items-center justify-center rounded-full bg-brand-600 text-center  text-white !no-underline transition-colors hover:bg-brand-500	"
+            >
+              <p className="text-2xl">5. Klasse</p>
+            </Link>
           </div>
         </div>
-        <div className="mx-4 mt-16 text-2xl font-bold mobile:flex mobile:justify-center">
-          <h2>Wähle eine Klassenstufe</h2>
-        </div>
-        <div className="mt-6 flex justify-center text-2xl font-bold sm:mt-8">
-          <Link
-            href="/meine-mathe-skills/klasse5"
-            className="flex h-36 w-36 items-center justify-center rounded-full bg-brand-600 text-center  text-white !no-underline transition-colors hover:bg-brand-500	"
+        <div className="mb-3 ml-3 px-3 text-right text-gray-700">
+          {isProduction ? null : (
+            <button
+              onClick={() => {
+                deleteStored()
+                setData(undefined)
+              }}
+            >
+              (daten löschen)
+            </button>
+          )}{' '}
+          <a className="hover:underline " href="https://de.serlo.org/legal">
+            Impressum
+          </a>
+          <a
+            className="ml-4 hover:underline"
+            href="https://de.serlo.org/privacy"
           >
-            <p className="text-2xl">5. Klasse</p>
-          </Link>
+            Datenschutz
+          </a>
+          <span className="ml-4 opacity-50 hover:underline">
+            <a href="https://www.freepik.com/free-vector/collection-cute-animals-wearing-graduation-cap-cartoon-style-vector_3780561.htm#query=animal%20illustration&position=1&from_view=keyword&track=ais&uuid=7991b85d-fd28-4da4-8e08-29af073982df">
+              Bild von rawpixel.com
+            </a>{' '}
+            (Freepik){' '}
+          </span>
         </div>
       </div>
-      <div className="mb-3 ml-3 px-3 text-right text-gray-700">
-        <a href="https://de.serlo.org/legal">Impressum</a>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <a href="https://de.serlo.org/privacy">Datenschutz</a>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <span className="opacity-50">
-          <a href="https://www.freepik.com/free-vector/collection-cute-animals-wearing-graduation-cap-cartoon-style-vector_3780561.htm#query=animal%20illustration&position=1&from_view=keyword&track=ais&uuid=7991b85d-fd28-4da4-8e08-29af073982df">
-            Tierbilder von rawpixel.com
-          </a>{' '}
-          (Freepik){' '}
-        </span>
-      </div>
-    </div>
+    </MathSkillsProvider>
   )
 }
 
