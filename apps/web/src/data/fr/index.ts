2436d8b0959733adb151d034375bbe6b8639728c
Stephan Kulla <git.mail@kulla.me>
2024-02-27 15:54:36 +0100

    New translations index.ts (French)

--- apps/web/src/data/fr/index.ts
+++ apps/web/src/data/fr/index.ts
@@ -687,7 +687,8 @@ export const loggedInData = {
           illegalInjectionFound: "Injection illégale trouvée",
           serloEntitySrc: "Entité Serlo {{src}}",
           serloId: 'Serlo ID',
-          placeholder: 'Serlo ID (e.g. 1565)'
+          placeholder: 'Serlo ID (e.g. /1565)',
+          invalidStateWarning: "Please use a valid Serlo ID (just numbers). E.g. '/1555'"
         },
         multimedia: {
           title: "Contenu multimédia avec un texte",
