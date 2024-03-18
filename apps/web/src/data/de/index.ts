f45593502819e6e848c09efdd4ef5b18aa107c69
Stephan Kulla <git.mail@kulla.me>
2024-03-18 17:49:44 +0100

    New translations index.ts (German)

--- apps/web/src/data/de/index.ts
+++ apps/web/src/data/de/index.ts
@@ -842,8 +842,8 @@ export const loggedInData = {
           title: "Lückentext",
           description: "Ein Text mit Lücken zum Ausfüllen",
           placeholder: "Schreibe einen Text und füge Lücken ein",
-          chooseType: "Wähle den Antworttyp",
-          chooseChildPluginType: 'Choose the answer type',
+          chooseType: "Wähle den Aufgabentyp",
+          chooseChildPluginType: "Wähle den Antworttyp",
           modes: {
             typing: "Tippen",
             'drag-and-drop': 'Drag & Drop'
