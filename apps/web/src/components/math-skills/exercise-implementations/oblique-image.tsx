8ff45109c9d01c2de28c591ac4d2a4102c4546d9
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-03-20 08:33:27 +0100

    Bodies added in exercise 'oblique-image'

--- apps/web/src/components/math-skills/exercise-implementations/oblique-image.tsx
+++ apps/web/src/components/math-skills/exercise-implementations/oblique-image.tsx
@@ -15,17 +15,26 @@ interface PyraData {
   bd: number
   koerper: string
   w: number
+  ac: number
+  cd: number
 }
 
-export function ObliqueImagePyramide() {
+export function ObliqueImage() {
   return (
     <SelfEvaluationExercise
       generator={() => {
         const factor = randomItemFromArray([0.5, 1])
-        const ab = randomIntBetween(1, 10) * 2
-        const me = randomIntBetween(2, 15)
-        const bd = randomIntBetween(2, 10)
-        const koerper = randomItemFromArray(['pyra', 'quader'])
+        const me = randomIntBetween(6, 12)
+        const ab = randomIntBetween(8, 12)
+        const bd = randomIntBetween(6, 10)
+        const ac = randomIntBetween(7, 10)
+        const cd = randomIntBetween(7, 10)
+        const koerper = randomItemFromArray([
+          'pyra',
+          'quader',
+          'prisma3',
+          'pyra3',
+        ])
         const w = randomItemFromArray([30, 45, 60])
         const data: PyraData = {
           ab,
@@ -33,6 +42,8 @@ export function ObliqueImagePyramide() {
           bd,
           koerper,
           w,
+          ac,
+          cd,
         }
         return { data }
       }}
@@ -42,9 +53,15 @@ export function ObliqueImagePyramide() {
             <h2 className="text-2xl">
               Das ist {data.koerper === 'pyra' ? 'die Pyramide ABCDE.' : null}
               {data.koerper === 'quader' ? 'der Quader ABCDEFGH.' : null}
+              {data.koerper === 'prisma3' ? 'der Prisma ABCDEF.' : null}
+              {data.koerper === 'pyra3' ? 'die Dreieckspyramide ABCD.' : null}
             </h2>
             {data.koerper === 'pyra' ? <SubComponent data={data} /> : null}
             {data.koerper === 'quader' ? <ComponentQ data={data} /> : null}
+            {data.koerper === 'prisma3' ? <ComponentP data={data} /> : null}
+            {data.koerper === 'pyra3' ? (
+              <ComponentPyraDrei data={data} />
+            ) : null}
             <small className="mb-6 block">
               Skizze ist nicht maßstabsgetreu
             </small>
@@ -240,6 +257,187 @@ function ComponentQ({ data }: { data: PyraData }) {
     })
 
     b.create('text', [4.5, 2, `${data.me} cm`], {})
+    setBoard(b)
+
+    return () => {
+      if (board) JXG.JSXGraph.freeBoard(board)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data])
+
+  return (
+    <div
+      onClick={(e) => {
+        e.preventDefault()
+      }}
+    >
+      <div
+        id="jxgbox"
+        className="jxgbox pointer-events-none mb-2 mt-6 h-[300px] w-[300px] rounded-2xl border border-gray-200"
+      ></div>
+      <style jsx global>
+        {`
+          .JXGtext {
+            font-family: Karla, sans-serif !important;
+            font-weight: bold !important;
+            font-size: 18px !important;
+          }
+        `}
+      </style>
+    </div>
+  )
+}
+function ComponentP({ data }: { data: PyraData }) {
+  const [board, setBoard] = useState<ReturnType<
+    typeof JXG.JSXGraph.initBoard
+  > | null>(null)
+
+  useEffect(() => {
+    const b = JXG.JSXGraph.initBoard('jxgbox', {
+      boundingbox: [-1, 6, 7, -2],
+      showNavigation: false,
+      showCopyright: false,
+    })
+
+    const pointA = b.create('point', [1, 0], {
+      name: 'A',
+      fixed: true,
+      label: { autoPosition: true },
+    })
+    const pointB = b.create('point', [5, 0], { name: 'B', fixed: true })
+    const pointC = b.create('point', [3.5, 0.5], { name: 'C', fixed: true })
+    const pointD = b.create('point', [1, 4], { name: 'D', fixed: true })
+    const pointE = b.create('point', [5, 4], { name: 'E', fixed: true })
+    const pointF = b.create('point', [3.5, 4.5], { name: 'F', fixed: true })
+
+    const poly1 = b.create('polygon', [pointA, pointC, pointF, pointD], {
+      name: 'Polygon 1',
+      withLabel: false,
+      color: 'yellow',
+    })
+    const poly2 = b.create('polygon', [pointA, pointB, pointC], {
+      name: 'Polygon 2',
+      withLabel: false,
+      color: 'blue',
+    })
+    const poly3 = b.create('polygon', [pointA, pointB, pointE, pointD], {
+      name: 'Polygon 3',
+      withLabel: false,
+      color: 'yellow',
+    })
+
+    const poly4 = b.create('polygon', [pointB, pointC, pointF, pointE], {
+      name: 'Polygon 4',
+      withLabel: false,
+    })
+    const poly5 = b.create('polygon', [pointD, pointE, pointF], {
+      name: 'Polygon 5',
+      withLabel: false,
+      color: 'blue',
+    })
+
+    b.create('text', [3.5, 0, `${data.ab} cm`], {
+      anchorX: 'middle',
+      anchorY: 'top',
+    })
+    b.create('text', [2.2, 1, `${data.ac} cm`], {
+      anchorX: 'middle',
+      anchorY: 'top',
+    })
+    b.create('text', [5.2, 1, `${data.bd} cm`], {
+      anchorX: 'middle',
+      anchorY: 'top',
+    })
+
+    b.create('text', [5.2, 3, `${data.me} cm`], {})
+    setBoard(b)
+
+    return () => {
+      if (board) JXG.JSXGraph.freeBoard(board)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data])
+
+  return (
+    <div
+      onClick={(e) => {
+        e.preventDefault()
+      }}
+    >
+      <div
+        id="jxgbox"
+        className="jxgbox pointer-events-none mb-2 mt-6 h-[300px] w-[300px] rounded-2xl border border-gray-200"
+      ></div>
+      <style jsx global>
+        {`
+          .JXGtext {
+            font-family: Karla, sans-serif !important;
+            font-weight: bold !important;
+            font-size: 18px !important;
+          }
+        `}
+      </style>
+    </div>
+  )
+}
+function ComponentPyraDrei({ data }: { data: PyraData }) {
+  const [board, setBoard] = useState<ReturnType<
+    typeof JXG.JSXGraph.initBoard
+  > | null>(null)
+
+  useEffect(() => {
+    const b = JXG.JSXGraph.initBoard('jxgbox', {
+      boundingbox: [-1, 6, 7, -2],
+      showNavigation: false,
+      showCopyright: false,
+    })
+
+    const pointA = b.create('point', [1, 0], {
+      name: 'A',
+      fixed: true,
+      label: { autoPosition: true },
+    })
+    const pointB = b.create('point', [5, 0], { name: 'B', fixed: true })
+    const pointC = b.create('point', [3.5, 0.5], { name: 'C', fixed: true })
+    const pointD = b.create('point', [3.2, 4], { name: 'D', fixed: true })
+    const pointM = b.create('point', [3.2, 0.2], { name: '', fixed: true })
+
+    const poly1 = b.create('polygon', [pointA, pointC, pointD], {
+      name: 'Polygon 1',
+      withLabel: false,
+      color: 'yellow',
+    })
+    const poly2 = b.create('polygon', [pointA, pointB, pointC], {
+      name: 'Polygon 2',
+      withLabel: false,
+      color: 'blue',
+    })
+    const poly3 = b.create('polygon', [pointA, pointB, pointD], {
+      name: 'Polygon 3',
+      withLabel: false,
+      color: 'yellow',
+    })
+    const line1 = b.create('line', [pointM, pointD], {
+      name: 'Hoehe',
+      withLabel: false,
+      straightFirst: false,
+      straightLast: false,
+    })
+
+    b.create('text', [3.5, 0, `${data.ab} cm`], {
+      anchorX: 'middle',
+      anchorY: 'top',
+    })
+    b.create('text', [2.2, 1, `${data.ac} cm`], {
+      anchorX: 'middle',
+      anchorY: 'top',
+    })
+    b.create('text', [5.2, 1, `${data.bd} cm`], {
+      anchorX: 'middle',
+      anchorY: 'top',
+    })
+
+    b.create('text', [3.5, 2.6, `${data.me} cm`], {})
     setBoard(b)
 
     return () => {
--- apps/web/src/components/math-skills/exercises/middle-school-final-exam.tsx
+++ apps/web/src/components/math-skills/exercises/middle-school-final-exam.tsx
@@ -6,7 +6,7 @@ import { ExponentialFunctionTime } from '../exercise-implementations/exponential
 import { LogarithmExercise1 } from '../exercise-implementations/logarithm-exercise-1'
 import { LogarithmExercise2 } from '../exercise-implementations/logarithm-exericse-2'
 import { NormalformParabola } from '../exercise-implementations/normalform-parabola'
-import { ObliqueImagePyramide } from '../exercise-implementations/oblique-image'
+import { ObliqueImage } from '../exercise-implementations/oblique-image'
 import { PlotFunction } from '../exercise-implementations/plot-function-1'
 import { PlotFunction2 } from '../exercise-implementations/plot-function-2'
 import { RootEquations } from '../exercise-implementations/root-equations'
@@ -136,6 +136,6 @@ export const middleSchoolFinalExam = {
     subtitle: 'Geometrie',
     useCalculator: false,
     track: 1,
-    component: <ObliqueImagePyramide />,
+    component: <ObliqueImage />,
   },
 } as const
