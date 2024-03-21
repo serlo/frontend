bf45e4ff930ec266c5280e7b9b7e1055d743fec8
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-03-21 10:15:19 +0100

    Hint added on 'mapping-graps-2'

--- apps/web/src/components/math-skills/exercise-implementations/mapping-graphs-2.tsx
+++ apps/web/src/components/math-skills/exercise-implementations/mapping-graphs-2.tsx
@@ -196,234 +196,39 @@ export function AbbildungGraphen2() {
         const y_Ende_1 = data.d + data.y_offset
         const y_Ende_2 = data.d - data.y_offset
 
-        if (data.function_type === 1)
-          return (
-            <>
-              Der Funktionsterm von <i>g</i> lautet:
-              <br />
-              <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
-                y = {data.a === -1 ? '-' : null}{' '}
-                {buildFrac(
-                  <>{data.a !== -1 ? data.a : -data.a}</>,
-                  <>
-                    {data.b === 0 || data.c === -1 ? null : '('}x{' '}
-                    {data.b > 0 ? '+' : null} {data.b !== 0 ? data.b : null}{' '}
-                    {data.x_dir === 'links'
-                      ? '+ ' + data.x_offset
-                      : '- ' + data.x_offset}
-                    {data.b === 0 || data.c === -1 ? null : ')'}
-                    <sup>{data.c !== -1 ? -data.c : null}</sup>
-                  </>
-                )}{' '}
-                {data.d > 0 && data.d !== 0 ? '+' : null}{' '}
-                {data.d !== 0 ? data.d : null}{' '}
-                {data.y_dir === 'oben'
-                  ? '+ ' + data.y_offset
-                  : '- ' + data.y_offset}
-              </span>
-              <br />
-              <br />
-              Zusammengefasst:
-              <br />
-              <span className="mt-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
-                y = {data.a === -1 ? '-' : null}{' '}
-                {buildFrac(
-                  <>{data.a !== -1 ? data.a : -data.a}</>,
-                  <>
-                    {data.b === 0 || data.c === -1 ? null : '('}x{' '}
-                    {data.x_dir === 'links' && x_Ende_1 > 0
-                      ? '+ ' + x_Ende_1
-                      : null}
-                    {data.x_dir === 'links' && x_Ende_1 === 0 ? null : null}
-                    {data.x_dir === 'links' && x_Ende_1 < 0
-                      ? '- ' + x_Ende_1
-                      : null}
-                    {data.x_dir === 'rechts' && x_Ende_2 > 0
-                      ? '+ ' + x_Ende_2
-                      : null}
-                    {data.x_dir === 'rechts' && x_Ende_2 === 0 ? null : null}
-                    {data.x_dir === 'rechts' && x_Ende_2 < 0
-                      ? ' ' + x_Ende_2
-                      : null}
-                    {data.b === 0 || data.c === -1 ? null : ')'}
-                    <sup>{data.c !== -1 ? -data.c : null}</sup>
-                  </>
-                )}{' '}
-                {data.y_dir === 'oben' && y_Ende_1 > 0 ? '+ ' + y_Ende_1 : null}
-                {data.y_dir === 'oben' && y_Ende_1 === 0 ? null : null}
-                {data.y_dir === 'oben' && y_Ende_1 < 0 ? '- ' + y_Ende_1 : null}
-                {data.y_dir === 'unten' && y_Ende_2 > 0
-                  ? '+ ' + y_Ende_2
-                  : null}
-                {data.y_dir === 'unten' && y_Ende_2 === 0 ? null : null}
-                {data.y_dir === 'unten' && y_Ende_2 < 0 ? ' ' + y_Ende_2 : null}
-              </span>
-            </>
-          )
-        if (data.function_type === 2)
-          return (
-            <>
-              Der Funktionsterm von <i>g</i> lautet:
-              <br />
-              <span className="bg-bg-gray-300 mt-3 inline-block rounded-md bg-opacity-20 p-1 px-3 text-2xl">
-                y = (x {data.x_s > 0 ? '+ ' : null}
-                {data.x_s === 0 ? null : null}
-                {data.x_s !== 0 ? data.x_s : null}
-                {data.x_dir === 'links' ? ' + ' + data.x_offset : null}
-                {data.x_dir === 'rechts' ? ' - ' + data.x_offset : null})
-                <sup>2</sup> {data.y_s > 0 ? '+ ' + data.y_s : null}
-                {data.y_s === 0 ? null : null}
-                {data.y_s < 0 ? data.y_s : null}
-                {data.y_dir === 'oben' ? ' + ' + data.y_offset : null}
-                {data.y_dir === 'unten' ? ' - ' + data.y_offset : null}
-              </span>
-              <br />
-              <br />
-              Zusammengefasst:
-              <br />
-              <span className="mt-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
-                y ={' '}
-                {data.x_dir === 'links' && data.x_s + data.x_offset !== 0
-                  ? '('
-                  : null}
-                {data.x_dir === 'rechts' && data.x_s - data.x_offset !== 0
-                  ? '('
-                  : null}
-                x{' '}
-                {data.x_dir === 'links' && data.x_s + data.x_offset > 0
-                  ? '+ '
-                  : null}
-                {data.x_dir === 'rechts' && data.x_s - data.x_offset > 0
-                  ? '+ '
-                  : null}
-                {data.x_dir === 'links' && data.x_s + data.x_offset !== 0
-                  ? data.x_s + data.x_offset
-                  : null}
-                {data.x_dir === 'rechts' && data.x_s - data.x_offset !== 0
-                  ? data.x_s - data.x_offset
-                  : null}
-                {data.x_dir === 'links' && data.x_s + data.x_offset !== 0
-                  ? ')'
-                  : null}
-                {data.x_dir === 'rechts' && data.x_s - data.x_offset !== 0
-                  ? ')'
-                  : null}
-                <sup>2</sup>{' '}
-                {data.y_dir === 'oben' && data.y_s + data.y_offset > 0
-                  ? '+ '
-                  : null}
-                {data.y_dir === 'unten' && data.y_s - data.y_offset > 0
-                  ? '+ '
-                  : null}
-                {data.y_dir === 'oben' && data.y_s + data.y_offset !== 0
-                  ? data.y_s + data.y_offset
-                  : null}
-                {data.y_dir === 'unten' && data.y_s - data.y_offset !== 0
-                  ? data.y_s - data.y_offset
-                  : null}
-              </span>
-            </>
-          )
-        if (data.function_type === 3)
-          if (data.exp_offset !== 0)
-            return (
-              <>
-                Der Funktionsterm von <i>g</i> lautet:
-                <br />
-                <span className="my-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
-                  y = {data.Anfangswert} ·{' '}
-                  {data.Faktor.toString().replace('.', ',')}
-                  <sup>
-                    x {data.x_dir === 'links' ? ' + ' + data.x_offset : null}
-                    {data.x_dir === 'rechts' ? ' - ' + data.x_offset : null}
-                  </sup>
-                  {data.exp_offset > 0 ? '+ ' + data.exp_offset : null}
-                  {data.exp_offset === 0 ? null : null}{' '}
-                  {data.exp_offset < 0 ? data.exp_offset : null}
-                  {data.y_dir === 'oben' ? ' + ' + data.y_offset : null}
-                  {data.y_dir === 'unten' ? ' - ' + data.y_offset : null}
-                </span>
-                <br />
-                <br />
-                Zusammengefasst:
-                <br />
-                <span className="my-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
-                  y = {data.Anfangswert} ·{' '}
-                  {data.Faktor.toString().replace('.', ',')}
-                  <sup>
-                    x {data.x_dir === 'links' ? ' + ' + data.x_offset : null}
-                    {data.x_dir === 'rechts' ? ' - ' + data.x_offset : null}
-                  </sup>{' '}
-                  {data.y_dir === 'oben' && data.exp_offset + data.y_offset > 0
-                    ? '+ '
-                    : null}
-                  {data.y_dir === 'unten' && data.exp_offset - data.y_offset > 0
-                    ? '+ '
-                    : null}
-                  {data.y_dir === 'oben' &&
-                  data.exp_offset + data.y_offset !== 0
-                    ? data.exp_offset + data.y_offset
-                    : null}
-                  {data.y_dir === 'unten' &&
-                  data.exp_offset - data.y_offset !== 0
-                    ? data.exp_offset - data.y_offset
-                    : null}
-                </span>
-              </>
-            )
-        if (data.exp_offset === 0)
-          return (
-            <>
-              Der Funktionsterm von <i>g</i> lautet:
-              <br />
-              <span className="my-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
-                y = {data.Anfangswert} ·{' '}
-                {data.Faktor.toString().replace('.', ',')}
-                <sup>
-                  x {data.x_dir === 'links' ? ' + ' + data.x_offset : null}
-                  {data.x_dir === 'rechts' ? ' - ' + data.x_offset : null}
-                </sup>{' '}
-                {data.y_dir === 'oben' && data.exp_offset + data.y_offset > 0
-                  ? '+ '
-                  : null}
-                {data.y_dir === 'unten' && data.exp_offset - data.y_offset > 0
-                  ? '+ '
-                  : null}
-                {data.y_dir === 'oben' ? data.exp_offset + data.y_offset : null}
-                {data.y_dir === 'unten'
-                  ? data.exp_offset - data.y_offset
-                  : null}
-              </span>{' '}
-            </>
-          )
+        if (data.function_type === 1) return <></>
+        if (data.function_type === 2) return <></>
+        if (data.function_type === 3) return <></>
         return <></>
       }}
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       renderHint={({ data }) => {
         return (
           <>
-            Eine Verschiebung in x-Richtung wird durch Addieren/Subtrahieren des
-            Wertes von x ausgeführt.
+            Für eine Spiegelung an der x-Achse wird der Funktionsterm mit (-1)
+            multipliziert:
             <br />
             <br />
-            Beispiel für eine Verschiebung um 1 nach rechts:
-            <br />
-            <br />
-            <span className="my-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
-              y = (x - 2)<sup>2</sup> + 1 ↦ y = (x - 1 - 2)<sup>2</sup> + 1
-            </span>
-            <br />
-            <br />
-            Eine Verschiebung in y-Richtung wird durch Addieren/Subtrahieren des
-            Wertes vom Funktionsterm ausgeführt.
-            <br />
-            <br />
-            Beispiel für eine Verschiebung um 2 nach unten:
-            <br />
+            Beispiel:
             <br />
             <span className="my-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
-              y = 20 · 2<sup>x</sup> + 5 ↦ y = 20 · 2<sup>x</sup> + 5 - 2
+              y = (x - 2)<sup>2</sup> + 1 ↦ y = (-1) · ((x - 2)<sup>2</sup> + 1)
             </span>
+            <br />
+            Vereinfache anschließend den Term.
+            <br />
+            <br />
+            F+r eine Spiegelung an der y-Achse wird &quot;x&quot; durch
+            &quot;-x&quot; ersetzt:
+            <br />
+            <br />
+            Beispiel:
+            <br />
+            <span className="my-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
+              y = 20 · 2<sup>x</sup> + 5 ↦ y = 20 · 2<sup>-x</sup> + 5
+            </span>
+            <br />
+            Vereinfache anschließend den Term.
           </>
         )
       }}
