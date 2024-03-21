67a1c935b75719bae6f99ddb91e76dc973378472
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-03-21 15:35:39 +0100

    Exercise 'modelling-parabolas' created

--- apps/web/src/components/math-skills/exercise-implementations/modelling-parabolax.tsx
+++ apps/web/src/components/math-skills/exercise-implementations/modelling-parabolax.tsx
@@ -4,121 +4,198 @@ import { useEffect, useState } from 'react'
 
 import { SelfEvaluationExercise } from './self-evaluation-exercise'
 import { buildFrac } from '../utils/math-builder'
+import { useMathSkillsStorage } from '../utils/math-skills-data-context'
 import { randomIntBetween } from '@/helper/random-int-between'
 import { randomItemFromArray } from '@/helper/random-item-from-array'
 
 // JXG.Options.label.autoPosition = true
 
 export function ModellingParabola() {
+  const { data } = useMathSkillsStorage()
+
   return (
     <SelfEvaluationExercise
       generator={() => {
         return {
-          b: randomIntBetween(2, 9),
+          b: randomIntBetween(7, 15),
           c: randomIntBetween(2, 9),
           isPlus: randomItemFromArray([true, false]),
           isPlus_2: randomItemFromArray([true, false]),
+          context: randomIntBetween(1, 4),
         }
       }}
-      renderTask={({ b, isPlus, isPlus_2, c }) => {
+      renderTask={({ context, b, isPlus, isPlus_2, c }) => {
         return (
           <>
-            <h2 className="text-2xl">Bestimme die Scheitelform der Parabel:</h2>
+            <h2 className="text-2xl">
+              {data.name ? `${data.name} ` : 'Sophie '}
+              {context === 1
+                ? 'macht einen Weitsprung. Der Sprung kann mit dem Graphen der Funktion '
+                : null}
+              {context === 2
+                ? 'sieht beim Autofahren den Eingang eines Tunnels. Der Eingang kann mit dem Graphen der Funktion '
+                : null}
+              {context === 3
+                ? 'wirft einen Stein in einen See. Der Wurf kann mit dem Graphen der Funktion '
+                : null}
+              {context === 4
+                ? 'beobachtet einen Einsatz der Feuerwehr. Der Wasserstrahl, der ein brennendes Auto löscht kann mit dem Graphen der Funktion '
+                : null}
+            </h2>
             <span className="mt-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
-              y = x<sup>2</sup> {isPlus ? '+' : '-'} {b}x {isPlus_2 ? '+' : '-'}{' '}
-              {c}
+              y = - x<sup>2</sup> +{' '}
+              {context === 1
+                ? (b / 2).toString().replace('.', ',')
+                : b.toString().replace('.', ',')}
+              x
             </span>
-
+            <br />
+            <br />
+            <h2 className="text-2xl">beschrieben werden.</h2>
+            <br />
+            <br />
+            <h2 className="text-2xl">
+              {context === 1 ? 'Berechne die Sprungweite und wo ' : null}
+              {data.name && context === 1 ? `${data.name} ` : null}
+              {data.name === '' ? `Sophie` : null}
+              {context === 1 ? 'am höchsten in der Luft war.' : null}
+              {context === 2
+                ? 'Berechne die maximale Breite und Höhe des Eingangs.'
+                : null}
+              {context === 3
+                ? 'Berechne die maximale Weite des Wurfs und wie hoch der Stein am höchsten Punkt war.'
+                : null}
+              {context === 4
+                ? 'Berechne wie weit der Strahl maximal kommt und wie hoch er am höchsten Punkt war.'
+                : null}
+            </h2>
             <br />
             <br />
             <i>Rechne am Besten mit Stift und Papier.</i>
           </>
         )
       }}
-      renderSolution={({ b, isPlus, isPlus_2, c }) => {
+      renderSolution={({ context, b, isPlus, isPlus_2, c }) => {
         return (
           <>
-            Aufgabenstellung: <br />
+            Für die {context === 2 ? 'maximale Breite' : 'maximale Weite'}{' '}
+            müssen die Nullstellen der Parabel berechnet werden:
+            <br />
             <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
-              y = x<sup>2</sup> {isPlus ? '+' : '-'} {b}x {isPlus_2 ? '+' : '-'}
-              {c}
+              0 = - x<sup>2</sup> +{' '}
+              {context === 1
+                ? (b / 2).toString().replace('.', ',')
+                : b.toString().replace('.', ',')}
+              x
             </span>
             <br />
             <br />
-            Wir führen eine quadratische Ergänzung durch. <br />
-            Dazu addieren wir den Term{' '}
-            <span className="text-1xl mt-3 inline-block rounded-md bg-yellow bg-opacity-20 p-1 px-1">
-              <span className="inline-block scale-y-[3]">(</span>
-              {buildFrac(<>{b}</>, <>2</>)}
-              <span className="inline-block scale-y-[3]">)</span>
-              <sup>2</sup>
-            </span>{' '}
-            und subtrahieren ihn zum Schluss wieder:
+            Wir lösen die Gleichung mit dem Satz des Nullprodukts. Dazu klammern
+            wir - x aus:
             <br />
             <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
-              y = x<sup>2</sup>
-              {isPlus ? '+' : '-'} {b}x +{' '}
-              <span className="inline-block scale-y-[3]">(</span>
-              {buildFrac(<>{b}</>, <>2</>)}
-              <span className="inline-block scale-y-[3]">)</span>
-              <sup>2</sup> {isPlus_2 ? '+' : '-'} {c} -{' '}
-              <span className="inline-block scale-y-[3] ">(</span>
-              {buildFrac(<>{b}</>, <>2</>)}
-              <span className="inline-block scale-y-[3]">)</span>
-              <sup>2</sup>
+              0 = - x · (x -{' '}
+              {context === 1
+                ? (b / 2).toString().replace('.', ',')
+                : b.toString().replace('.', ',')}
+              )
+            </span>
+            <br />
+            Am ersten Faktor erkennen wir die erste Lösung:{' '}
+            <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
+              x<sub>1</sub> = 0
+            </span>
+            <br />
+            Der zweite Faktor - die Klammer - ist dann 0, wenn:{' '}
+            <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
+              x<sub>2</sub> ={' '}
+              {context === 1
+                ? (b / 2).toString().replace('.', ',')
+                : b.toString().replace('.', ',')}
             </span>
             <br />
             <br />
-            Den vorderen Teil fassen wir zu einem Binom Zusammen:
-            <br />
-            <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
-              y = <span className="inline-block scale-y-[3]">(</span>x{' '}
-              {isPlus ? '+' : '-'} {buildFrac(<>{b}</>, <>2</>)}{' '}
-              <span className="inline-block scale-y-[3]">)</span>
-              <sup>2</sup> {isPlus_2 ? '+' : '-'} {c} -{' '}
-              <span className="inline-block scale-y-[3] ">(</span>
-              {buildFrac(<>{b}</>, <>2</>)}
-              <span className="inline-block scale-y-[3]">)</span>
-              <sup>2</sup>
+            Die maximale {context === 2
+              ? 'maximale Breite'
+              : 'maximale Weite'}{' '}
+            ist damit: <br />
+            <span className="mt-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
+              x<sub>2</sub> - x<sub>1</sub> ={' '}
+              {context === 1
+                ? (b / 2).toString().replace('.', ',')
+                : b.toString().replace('.', ',')}
             </span>
             <br />
             <br />
-            Die Zahlen hinter der Klammer werden auch zusammengefasst:
+            Für die maximale Höhe muss der Scheitelpunkt der Parabel berechnet
+            werden. Da die Nullstellen bekannt sind, können wir die Mitte davon
+            als x-Wert des Scheitels bestimmen:
+            <br />
+            <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
+              x<sub>s</sub> ={' '}
+              {buildFrac(
+                <>
+                  x<sub>2</sub> - x<sub>1</sub>
+                </>,
+                <>2</>
+              )}{' '}
+              ={' '}
+              {context === 1
+                ? (b / 4).toString().replace('.', ',')
+                : (b / 2).toString().replace('.', ',')}
+            </span>
+            <br />
+            <br />
+            Die Höhe ist damit:
             <br />
             <span className="mt-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
-              y = <span className="inline-block scale-y-[1.5]">(</span>x{' '}
-              {isPlus ? '+' : '-'} {(b / 2).toString().replace('.', ',')}
-              <span className="inline-block scale-y-[1.5]">)</span>
-              <sup>2</sup>{' '}
-              {c - (b / 2) * (b / 2) === 0 ? (
-                <></>
-              ) : (
-                <>
-                  {isPlus_2 && c - (b / 2) * (b / 2) > 0 ? '+' : ''}{' '}
-                  {isPlus_2
-                    ? (c - (b / 2) * (b / 2)).toString().replace('.', ',')
-                    : (-c - (b / 2) * (b / 2)).toString().replace('.', ',')}
-                </>
-              )}
+              y<sub>s</sub> = -{' '}
+              {context === 1
+                ? (b / 4).toString().replace('.', ',')
+                : (b / 2).toString().replace('.', ',')}
+              <sup>2</sup> +{' '}
+              {context === 1
+                ? (b / 2).toString().replace('.', ',')
+                : b.toString().replace('.', ',')}{' '}
+              ·{' '}
+              {context === 1
+                ? (b / 4).toString().replace('.', ',')
+                : (b / 2).toString().replace('.', ',')}{' '}
+              ={' '}
+              {context === 1
+                ? (-(b / 4) * (b / 4) + (b / 2) * (b / 4))
+                    .toString()
+                    .replace('.', ',')
+                : (-(b / 2) * (b / 2) + (b / 2) * b)
+                    .toString()
+                    .replace('.', ',')}
             </span>
           </>
         )
       }}
       // eslint-disable-next-line no-empty-pattern
-      renderHint={({ b }) => {
+      renderHint={({ context, b }) => {
         return (
           <>
-            Führe eine quadratische Ergänzung durch mit dem Term:
+            Für die {context === 2 ? 'maximale Breite' : 'maximale Weite'}{' '}
+            müssen die Nullstellen der Parabel berechnet werden:
             <br />
-            <span className="text-1xl mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-1">
-              <span className="inline-block scale-y-[3]">(</span>
-              {buildFrac(<>{b}</>, <>2</>)}
-              <span className="inline-block scale-y-[3]">)</span>
-              <sup>2</sup>
+            <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
+              0 = - x<sup>2</sup> +{' '}
+              {context === 1
+                ? (b / 2).toString().replace('.', ',')
+                : b.toString().replace('.', ',')}
+              x
             </span>
             <br />
             <br />
-            Fasse den vorderen Teil des Terms zu einem Binom zusammen.
+            Verwende dazu den Satz des Nullprodukts.
+            <br />
+            <br />
+            Die maximale Höhe befindet sich im Scheitelpunkt der Parabel.
+            <br />
+            <br />
+            Bestimme dazu den y-Wert in der Mitte der Nullstellen.
           </>
         )
       }}
