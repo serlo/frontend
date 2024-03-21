0ccb8295010ed157626cb0765a3db2ace6c78c40
Vitomir Budimir <budimirvitomir@gmail.com>
2024-03-21 15:40:33 +0100

    feat(editor): WIP plugins config object

--- packages/editor/src/plugin/helpers/editor-plugins.tsx
+++ packages/editor/src/plugin/helpers/editor-plugins.tsx
@@ -1,3 +1,8 @@
+import {
+  createBasicPlugins,
+  type CreateBasicPluginsConfig,
+} from '@editor/editor-integration/create-basic-plugins'
+
 import { EditorPlugin } from '../internal-plugin'
 
 export interface PluginWithData {
@@ -9,6 +14,11 @@ export interface PluginWithData {
 
 export type PluginsWithData = PluginWithData[]
 
+export interface CreatePluginsConfig {
+  basicPlugins: CreateBasicPluginsConfig
+  customPlugins: PluginsWithData
+}
+
 export const editorPlugins = (function () {
   let allPlugins: PluginsWithData | null = null
 
@@ -16,6 +26,18 @@ export const editorPlugins = (function () {
     if (allPlugins) return // only initialize once
 
     allPlugins = plugins
+
+    // Ensure the highest integrity level that JS provides
+    Object.freeze(allPlugins)
+  }
+
+  function initTemp(config: CreatePluginsConfig) {
+    if (allPlugins) return // only initialize once
+
+    allPlugins = [
+      ...createBasicPlugins(config.basicPlugins),
+      ...config.customPlugins,
+    ]
 
     // Ensure the highest integrity level that JS provides
     Object.freeze(allPlugins)
