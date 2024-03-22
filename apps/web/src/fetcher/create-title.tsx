e2a865a5123669a0277a1fca52a6b63d498e8693
Botho <1258870+elbotho@users.noreply.github.com>
2024-03-23 00:56:45 +0100

    refactor: simplify subscriptions query and create-title

--- apps/web/src/fetcher/create-title.tsx
+++ apps/web/src/fetcher/create-title.tsx
@@ -21,7 +21,7 @@ export function createTitle(uuid: MainUuidType, instance: Instance): string {
 
 // subscriptions need the raw title, so it's easier to reuse their type
 export function getRawTitle(
-  uuid: SubscriptionNode['object'],
+  uuid: SubscriptionNode['object'] | MainUuidType,
   instance: Instance
 ): string | null {
   const { strings } = getInstanceDataByLang(instance)
@@ -35,44 +35,14 @@ export function getRawTitle(
   }
 
   if (uuid.__typename === UuidType.TaxonomyTerm) {
-    const term = uuid
-    if (term.type === TaxonomyTermType.Topic) {
-      return `${term.name} (${strings.entities.topic})`
+    if (uuid.type === TaxonomyTermType.Topic) {
+      return `${uuid.title} (${strings.entities.topic})`
     }
-    if (term.type === TaxonomyTermType.Subject) {
-      return `${term.name} - ${strings.entities.subject}`
+    if (uuid.type === TaxonomyTermType.Subject) {
+      return `${uuid.title} - ${strings.entities.subject}`
     }
     // missing: special behaviour on curriculum term
-    return `${term.name}`
   }
 
-  if (
-    uuid.__typename === UuidType.Exercise ||
-    uuid.__typename === UuidType.ExerciseGroup
-  ) {
-    const subject = uuid.subject?.taxonomyTerm.name
-    const typenameString = getTranslatedType(strings, uuid.__typename)
-    if (!subject) return typenameString
-    return subject + ' ' + typenameString
-  }
-
-  if (uuid.__typename === UuidType.User) {
-    return uuid.username
-  }
-  if (
-    uuid.__typename === UuidType.Page ||
-    uuid.__typename === UuidType.Article ||
-    uuid.__typename === UuidType.Video ||
-    uuid.__typename === UuidType.Applet ||
-    uuid.__typename === UuidType.CoursePage ||
-    uuid.__typename === UuidType.Course ||
-    uuid.__typename === UuidType.Event
-  ) {
-    if (uuid.currentRevision?.title) {
-      return uuid.currentRevision.title
-    }
-  }
-
-  //fallback
-  return null
+  return Object.hasOwn(uuid, 'title') ? uuid.title : null
 }
--- apps/web/src/pages/subscriptions/manage.tsx
+++ apps/web/src/pages/subscriptions/manage.tsx
@@ -149,60 +149,17 @@ export const subscriptionsQuery = gql`
             __typename
             id
             alias
-            ... on User {
-              username
-            }
+            title
+
             ... on TaxonomyTerm {
               type
-              name
             }
-            ... on Exercise {
+
+            ... on AbstractEntity {
               subject {
                 taxonomyTerm {
                   name
                 }
-              }
-            }
-            ... on ExerciseGroup {
-              subject {
-                taxonomyTerm {
-                  name
-                }
-              }
-            }
-            ... on Page {
-              currentRevision {
-                title
-              }
-            }
-            ... on Article {
-              currentRevision {
-                title
-              }
-            }
-            ... on Video {
-              currentRevision {
-                title
-              }
-            }
-            ... on Applet {
-              currentRevision {
-                title
-              }
-            }
-            ... on CoursePage {
-              currentRevision {
-                title
-              }
-            }
-            ... on Course {
-              currentRevision {
-                title
-              }
-            }
-            ... on Event {
-              currentRevision {
-                title
               }
             }
           }
