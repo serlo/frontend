45260ceb00cebc118feb7aec228be5e935ad9293
Botho <1258870+elbotho@users.noreply.github.com>
2024-03-23 00:23:54 +0100

    fix: cleanup remaining GroupedExercise types after codegen

--- apps/web/src/pages/___content.tsx
+++ apps/web/src/pages/___content.tsx
@@ -14,7 +14,6 @@ const entities = [
   ['TaxonomyTerm (lvl)', 1386],
   ['Exercise', 54210],
   ['ExerciseGroup', 53205],
-  ['GroupedExercise', 53209], // TODO: remove after migration
   ['Course', 51979],
   ['Event', 145590],
 ]
