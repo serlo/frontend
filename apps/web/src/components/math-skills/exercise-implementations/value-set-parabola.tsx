68df156a7104d0ba80aace8b150a2e5d68260e84
FelixEcca <163112991+FelixEcca@users.noreply.github.com>
2024-03-22 08:15:28 +0100

    Fixed presentation of 'value-set-parabola'

--- apps/web/src/components/math-skills/exercise-implementations/value-set-parabola.tsx
+++ apps/web/src/components/math-skills/exercise-implementations/value-set-parabola.tsx
@@ -39,42 +39,78 @@ export function ValueSetParabola() {
         )
       }}
       renderSolution={({ a, b, c }) => {
-        return (
-          <>
-            Wertemenge der Parabel: <br />
-            <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
-              y = {a ? null : '-'} x<sup>2</sup> + bx + c
-            </span>
-            <br />
-            <br />
-            Wir schreiben die Parabel in der Scheitelpunktform:
-            <br />
-            <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
-              y = {a ? null : '-'} (x - {b < 0 ? '(' : null}
-              {b}
-              {b < 0 ? ')' : null})<sup>2</sup> +{c < 0 ? '(' : null} {a}
-              {c < 0 ? ')' : null}
-            </span>
-            <br />
-            <br />
-            Zusammengefasst: <br />
-            <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
-              y = {a ? null : '-'} (x {b > 0 ? '- ' + b : '+ ' + -b})
-              <sup>2</sup> {c > 0 ? '+ ' + c : null}
-              {c < 0 ? '- ' + -c : null}
-            </span>
-            <br />
-            <br />
-            Da die Parabel nach {a === true ? 'oben' : 'unten'} geöfnet ist, ist
-            der Wertebereich die Menge der reellen Zahlen{' '}
-            {a === true ? 'oberhalb' : 'unterhalb'} des Scheitels:
-            <br />
-            <span className="mt-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
-              W = {'{'} y | y {a === true ? '≥' : '≤'}{' '}
-              {c > 0 && c !== 0 ? c : '- ' + -c} {'}'}
-            </span>
-          </>
-        )
+        if (b < 0 || c < 0 || b === 0 || c === 0)
+          return (
+            <>
+              Wertemenge der Parabel: <br />
+              <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
+                y = {a ? null : '-'} x<sup>2</sup> + bx + c
+              </span>
+              <br />
+              <br />
+              Wir schreiben die Parabel in der Scheitelpunktform:
+              <br />
+              <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
+                y = {a ? null : '-'} (x - {b < 0 ? '(' : null}
+                {b}
+                {b < 0 ? ')' : null})<sup>2</sup> + {c < 0 ? '(' : null}
+                {c}
+                {c < 0 ? ')' : null}
+              </span>
+              <br />
+              <br />
+              Zusammengefasst: <br />
+              <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
+                y = {a ? null : '-'}
+                {b !== 0 ? '(' : null}x {b > 0 ? '- ' + b : null}
+                {b < 0 ? '+ ' + -b : null}
+                {b !== 0 ? ')' : null}
+                <sup>2</sup> {c > 0 ? '+ ' + c : null}{' '}
+                {c < 0 ? '- ' + -c : null}
+              </span>
+              <br />
+              <br />
+              Da die Parabel nach {a === true ? 'oben' : 'unten'} geöfnet ist,
+              ist der Wertebereich die Menge der reellen Zahlen{' '}
+              {a === true ? 'oberhalb' : 'unterhalb'} des Scheitels:
+              <br />
+              <span className="mt-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
+                W = {'{'} y | y {a === true ? '≥' : '≤'}{' '}
+                {c > 0 && c !== 0 ? c : '- ' + -c} {'}'}
+              </span>
+            </>
+          )
+        if (b > 0 && c > 0)
+          return (
+            <>
+              Wertemenge der Parabel: <br />
+              <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
+                y = {a ? null : '-'} x<sup>2</sup> + bx + c
+              </span>
+              <br />
+              <br />
+              Wir schreiben die Parabel in der Scheitelpunktform:
+              <br />
+              <span className="mt-3 inline-block rounded-md bg-gray-300 bg-opacity-20 p-1 px-3 text-2xl">
+                y = {a ? null : '-'} (x - {b < 0 ? '(' : null}
+                {b}
+                {b < 0 ? ')' : null})<sup>2</sup> + {c < 0 ? '(' : null}
+                {c}
+                {c < 0 ? ')' : null}
+              </span>
+              <br />
+              <br />
+              Da die Parabel nach {a === true ? 'oben' : 'unten'} geöfnet ist,
+              ist der Wertebereich die Menge der reellen Zahlen{' '}
+              {a === true ? 'oberhalb' : 'unterhalb'} des Scheitels:
+              <br />
+              <span className="mt-3 inline-block rounded-md bg-newgreen bg-opacity-20 p-1 px-3 text-2xl">
+                W = {'{'} y | y {a === true ? '≥' : '≤'}{' '}
+                {c > 0 && c !== 0 ? c : '- ' + -c} {'}'}
+              </span>
+            </>
+          )
+        return <></>
       }}
       // eslint-disable-next-line no-empty-pattern
       renderHint={({ a, c, b }) => {
