12ec3965367b1e3021ea9bc6e5e6076c2f072408
Botho <1258870+elbotho@users.noreply.github.com>
2024-03-28 12:43:10 +0100

    fix(plugin-exercise-group): no comments in solutions within exercise-group

--- apps/web/src/serlo-editor-integration/serlo-plugin-wrappers/solution-serlo-static-renderer.tsx
+++ apps/web/src/serlo-editor-integration/serlo-plugin-wrappers/solution-serlo-static-renderer.tsx
@@ -1,13 +1,17 @@
 import { StaticSolutionRenderer } from '@editor/plugins/solution/static'
 import type { EditorSolutionDocument } from '@editor/types/editor-plugins'
+import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
 import { useContext } from 'react'
 
 import type { CommentAreaEntityProps } from '@/components/comments/comment-area-entity'
 import { Lazy } from '@/components/content/lazy'
+import { FaIcon } from '@/components/fa-icon'
 import { isPrintMode, printModeSolutionVisible } from '@/components/print-mode'
 import { useAB } from '@/contexts/ab'
+import { ExerciseGroupContext } from '@/contexts/exercise-group-context'
+import { useInstanceData } from '@/contexts/instance-context'
 import { RevisionViewContext } from '@/contexts/revision-view-context'
 import { useEntityId } from '@/contexts/uuids-context'
 import { exerciseSubmission } from '@/helper/exercise-submission'
@@ -22,7 +26,9 @@ const CommentAreaEntity = dynamic<CommentAreaEntityProps>(() =>
 export function SolutionSerloStaticRenderer(props: EditorSolutionDocument) {
   const { asPath } = useRouter()
   const ab = useAB()
+  const commentStrings = useInstanceData().strings.comments
   const isRevisionView = useContext(RevisionViewContext)
+  const isInExerciseGroup = useContext(ExerciseGroupContext)
   const context = props.serloContext
 
   const exerciseUuid = useEntityId()
@@ -38,11 +44,26 @@ export function SolutionSerloStaticRenderer(props: EditorSolutionDocument) {
         : window.location.href.includes('#comment-')
 
   const afterSlot =
-    exerciseUuid && !isRevisionView ? (
+    isRevisionView || !exerciseUuid ? null : isInExerciseGroup ? (
+      <>
+        <h2 className="serlo-h2 mt-10 border-b-0">
+          <FaIcon className="text-2xl text-brand-400" icon={faQuestionCircle} />{' '}
+          {commentStrings.question}
+        </h2>
+        <p className="serlo-p">
+          <a
+            href="#comment-area-begin-scrollpoint"
+            className="serlo-button-light"
+          >
+            {commentStrings.questionLink} 👇
+          </a>
+        </p>
+      </>
+    ) : (
       <Lazy>
         <CommentAreaEntity entityId={exerciseUuid} />
       </Lazy>
-    ) : null
+    )
 
   function onSolutionOpen() {
     {
