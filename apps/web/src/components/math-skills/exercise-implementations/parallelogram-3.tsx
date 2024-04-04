c1b22e946afcf3446f669c1f569c3bf84c2384dd
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-04-04 16:02:55 +0200

    feat: finish parallelogramm-3

--- apps/web/src/components/math-skills/exercise-implementations/parallelogram-3.tsx
+++ apps/web/src/components/math-skills/exercise-implementations/parallelogram-3.tsx
@@ -1,116 +1,177 @@
 import { SelfEvaluationExercise } from './self-evaluation-exercise'
 import { buildVec, buildVec2 } from '../utils/math-builder'
+import { randomIntBetween } from '@/helper/random-int-between'
+import { randomItemFromArray } from '@/helper/random-item-from-array'
 
-// Flexibilisierungen
-
-// - Austausch von T und R (also abhängiger Punkt)
-// - sin/cos, Quadrat machen, Zahlen
-// VariablenameSätze ABC, RST, PQR, EFG
-
-// Frage: Wie gehe ich mit Positionierungen um? Negative Zahlen? Auch nicht so wichtig
 export function Parallelogram3() {
   const phi = <span className="font-mono">ϕ</span>
   return (
     <SelfEvaluationExercise
-      generator={() => null}
-      renderTask={() => (
+      generator={() => {
+        const fixed = randomItemFromArray([0, 2])
+        const names = randomItemFromArray(['ABC', 'RST', 'PQR', 'EFG']).split(
+          ''
+        )
+        const x = randomIntBetween(-12, -1)
+        const y = randomIntBetween(1, 12)
+
+        const x_off = randomIntBetween(-x + 1, -x + 10)
+        const w = randomIntBetween(2, 6)
+        const h = randomIntBetween(2, 10)
+
+        const func1 = randomItemFromArray(['sin', 'cos'])
+        const func2 = func1 === 'sin' ? 'cos' : 'sin'
+
+        const hasSqrt1 = randomItemFromArray([true, false])
+        const hasSqrt2 = randomItemFromArray([false, !hasSqrt1])
+
+        return {
+          fixed,
+          names,
+          x,
+          y,
+          x_off,
+          w,
+          h,
+          func1,
+          func2,
+          hasSqrt1,
+          hasSqrt2,
+        }
+      }}
+      renderTask={(data) => (
         <>
           <p className="serlo-main-task">
-            Seien die Pfeile {buildVec('OT')} = {buildVec2(-2, 4)} und{' '}
+            Seien die Pfeile {buildVec(<>O{data.names[data.fixed]}</>)} ={' '}
+            {buildVec2(data.x, data.y)} und{' '}
             {buildVec(
               <>
-                OR<sub>n</sub>
+                O{data.names[data.fixed === 0 ? 2 : 0]}
+                <sub>n</sub>
               </>
             )}{' '}
             ={' '}
             {buildVec2(
-              <>5 + 5 · sin {phi}</>,
               <>
-                6 · cos<sup>2</sup> {phi}
+                {data.x_off} + {data.w} · {data.func1}
+                {data.hasSqrt1 && <sup>2</sup>} {phi}
+              </>,
+              <>
+                {data.h} · {data.func2}
+                {data.hasSqrt2 && <sup>2</sup>} {phi}
               </>
             )}{' '}
             gegeben.
           </p>
           <p className="serlo-main-task">
             Diese spannen mit dem Ursprung O ( 0 | 0 ) für {phi} ∈ [0°; 90°]
-            Parallelogramme OR<sub>n</sub>S<sub>n</sub>T auf.
+            Parallelogramme O{data.names[0]}
+            {data.fixed !== 0 && <sub>n</sub>}
+            {data.names[1]}
+            <sub>n</sub>
+            {data.names[2]}
+            {data.fixed !== 2 && <sub>n</sub>} auf.
           </p>
           <p className="serlo-main-task">
             Weisen Sie durch Rechnung nach, dass sich die Koordinaten der Punkte
-            S<sub>n</sub> in Abhängigkeit von {phi} wie folgt darstellen lassen:
+            {data.names[1]}
+            <sub>n</sub> in Abhängigkeit von {phi} wie folgt darstellen lassen:
           </p>
           <p className="serlo-highlight-gray">
-            S<sub>n</sub> ( 3 + 5 · sin {phi} | 6 · cos<sup>2</sup>
-            {phi} + 4)
+            {data.names[1]}
+            <sub>n</sub> ( {data.x + data.x_off} + {data.w} · {data.func1}
+            {data.hasSqrt1 && <sup>2</sup>} {phi} | {data.h} · {data.func2}
+            {data.hasSqrt2 && <sup>2</sup>} {phi} + {data.y})
           </p>
         </>
       )}
-      renderSolution={() => (
+      renderSolution={(data) => (
         <>
           <p>
-            Die Punkte S<sub>n</sub> lassen sich durch eine Vektoraddition
-            darstellen. Nutze{' '}
-            {buildVec(
-              <>
-                R<sub>n</sub>S<sub>n</sub>
-              </>
-            )}{' '}
-            = {buildVec(<>OT</>)}. Das ist möglich, weil im Parallelogramm
-            gegenüberliegende Seiten parallel und gleich lang sind:
+            Die Punkte {data.names[1]}
+            <sub>n</sub> lassen sich durch eine Vektoraddition darstellen:
           </p>
           <p className="serlo-highlight-gray">
             {buildVec(
               <>
-                OS<sub>n</sub>
+                O{data.names[1]}
+                <sub>n</sub>
               </>
             )}{' '}
             ={' '}
             {buildVec(
               <>
-                OR<sub>n</sub>
+                O{data.names[data.fixed === 2 ? 0 : 2]}
+                <sub>n</sub>
               </>
             )}{' '}
             ⊕{' '}
             {buildVec(
               <>
-                R<sub>n</sub>S<sub>n</sub>
+                {data.names[data.fixed === 2 ? 0 : 2]}
+                <sub>n</sub>
+                {data.names[1]}
+                <sub>n</sub>
               </>
             )}{' '}
             ={' '}
             {buildVec(
               <>
-                OR<sub>n</sub>
+                O{data.names[data.fixed === 2 ? 0 : 2]}
+                <sub>n</sub>
               </>
             )}{' '}
-            ⊕ {buildVec(<>OT</>)}
+            ⊕ {buildVec(<>O{data.names[data.fixed]}</>)}
           </p>
-          <p>Setze die passenen Terme ein und berechne:</p>
+          <p className="mt-1">
+            Dabei wurde{' '}
+            {buildVec(
+              <>
+                {data.names[data.fixed === 0 ? 2 : 0]}
+                <sub>n</sub>
+                {data.names[1]}
+                <sub>n</sub>
+              </>
+            )}{' '}
+            = {buildVec(<>O{data.names[data.fixed]}</>)} genutzt. Das ist
+            möglich, weil im Parallelogramm gegenüberliegende Seiten parallel
+            und gleich lang sind. Setze die passenen Terme ein und berechne:
+          </p>
           <p className="serlo-highlight-gray">
             {buildVec(
               <>
-                OS<sub>n</sub>
+                O{data.names[data.fixed]}
+                <sub>n</sub>
               </>
             )}{' '}
             ={' '}
             {buildVec2(
-              <>5 + 5 · sin {phi}</>,
               <>
-                6 · cos<sup>2</sup> {phi}
+                {data.x_off} + {data.w} · {data.func1}
+                {data.hasSqrt1 && <sup>2</sup>} {phi}
+              </>,
+              <>
+                {data.h} · {data.func2}
+                {data.hasSqrt2 && <sup>2</sup>} {phi}
               </>
             )}{' '}
-            ⊕ {buildVec2(-2, 4)} ={' '}
+            ⊕ {buildVec2(data.x, data.y)} ={' '}
             {buildVec2(
-              <>3 + 5 · sin {phi}</>,
               <>
-                6 · cos<sup>2</sup>
-                {phi} + 4
+                {data.x + data.x_off} + {data.w} · {data.func1}
+                {data.hasSqrt1 && <sup>2</sup>} {phi}
+              </>,
+              <>
+                {data.h} · {data.func2}
+                {data.hasSqrt2 && <sup>2</sup>} {phi} + {data.y}
               </>
             )}
           </p>
           <p>Schreibe den Ortsvektor schließlich in Koordinaten:</p>
           <p className="serlo-highlight-green">
-            S<sub>n</sub> ( 3 + 5 · sin {phi} | 6 · cos<sup>2</sup>
-            {phi} + 4)
+            S<sub>n</sub> ( {data.x + data.x_off} + {data.w} · {data.func1}
+            {data.hasSqrt1 && <sup>2</sup>} {phi} | {data.h} · {data.func2}
+            {data.hasSqrt2 && <sup>2</sup>} {phi} + {data.y})
           </p>
         </>
       )}
