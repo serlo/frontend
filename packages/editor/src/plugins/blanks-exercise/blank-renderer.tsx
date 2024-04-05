b7d7ec91cfb470651454ed8b04d6d64d1223b474
Lars <lars@serlo.org>
2024-04-05 18:44:09 +0200

    wip: Not delete blanks when they are empty on blur

--- packages/editor/src/plugins/blanks-exercise/blank-renderer.tsx
+++ packages/editor/src/plugins/blanks-exercise/blank-renderer.tsx
@@ -65,13 +65,6 @@ export function BlankRenderer(props: BlankRendererProps) {
     return () => document.removeEventListener('keydown', handleDocumentKeydown)
   }, [editor, slateFocused, inputRef, selected])
 
-  // remove empty blanks when plugin looses focus
-  useEffect(() => {
-    if (!focused && !correctAnswers[0]?.answer.trim().length) {
-      removeBlanks(editor)
-    }
-  }, [focused, editor, correctAnswers])
-
   const context = useContext(BlanksContext)
   if (context === null) return null
 
