33749d80a4030c97a916c967ec31d5efa1a7ce2a
Botho <1258870+elbotho@users.noreply.github.com>
2024-04-05 14:53:42 +0200

    user Events component with username instead of id

--- apps/web/src/pages/event/history/user/profile/[username].tsx
+++ apps/web/src/pages/event/history/user/profile/[username].tsx
@@ -1,8 +1,6 @@
-import request, { gql } from 'graphql-request'
-import { GetStaticPaths, GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 
-import { endpoint } from '@/api/endpoint'
 import { useAuthentication } from '@/auth/use-authentication'
 import { PageTitle } from '@/components/content/page-title'
 import { FrontendClientBase } from '@/components/frontend-client-base'
@@ -12,33 +10,31 @@ import { UserUnrevisedRevisions } from '@/components/user/user-unrevised-revisio
 import { useInstanceData } from '@/contexts/instance-context'
 import { renderedPageNoHooks } from '@/helper/rendered-page'
 
-export interface UserEventHistoryProps {
-  id: number
-  username: string
-}
-
-export default renderedPageNoHooks<UserEventHistoryProps>((props) => {
+export default renderedPageNoHooks(() => {
   return (
     <FrontendClientBase>
-      <Content {...props} />
+      <Content />
     </FrontendClientBase>
   )
 })
 
-function Content({ username, id }: UserEventHistoryProps) {
+function Content() {
   const { strings } = useInstanceData()
+  const router = useRouter()
+  const username = String(router.query.username)
   const auth = useAuthentication()
-  const [isOwn, setIsOwn] = useState(false)
+  const isOwn = auth?.username === username
 
-  useEffect(() => {
-    setIsOwn(auth?.username === username)
-  }, [auth?.username, username])
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => setMounted(true), [])
 
   const title = strings.pageTitles[
     isOwn ? 'userEditsMine' : 'userEdits'
   ].replace('%user%', username)
 
   const backUrl = `/user/profile/${username}`
+
+  if (!mounted) return <>…</>
 
   return (
     <>
@@ -49,52 +45,7 @@ function Content({ username, id }: UserEventHistoryProps) {
       <h2 className="serlo-h3" id="activities">
         {strings.eventLog.currentEvents}
       </h2>
-      <Events userId={id} perPage={10} moreButton />
+      <Events username={username} perPage={10} moreButton />
     </>
   )
 }
-
-// events query still relies on user uuid
-export const getStaticProps: GetStaticProps<UserEventHistoryProps> = async (
-  context
-) => {
-  const usernameParam = String(context.params?.username)
-  if (!usernameParam) return { notFound: true }
-
-  const data = await requestUserId(usernameParam)
-  return data
-    ? {
-        props: { ...data },
-        revalidate: 1,
-      }
-    : { notFound: true }
-}
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: 'blocking',
-  }
-}
-
-export async function requestUserId(
-  username: string
-): Promise<{ username: string; id: number } | undefined> {
-  const { user } = await request<{
-    user: { userByUsername: { id: number } }
-  }>(endpoint, userIdByUsernameQuery, {
-    username,
-  })
-  const id = user?.userByUsername.id
-  return id ? { username, id } : undefined
-}
-
-export const userIdByUsernameQuery = gql`
-  query userIdByUsername($username: String!) {
-    user {
-      userByUsername(username: $username) {
-        id
-      }
-    }
-  }
-`
