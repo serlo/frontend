86109f848bcebd132635b040cd3038d5b420bd33
shn-srl <shane@serlo.org>
2024-04-08 13:50:42 +0200

    Better code

--- apps/web/src/mutations/use-experiment-create-exercise-submission-mutation.ts
+++ apps/web/src/mutations/use-experiment-create-exercise-submission-mutation.ts
@@ -15,12 +15,10 @@ const mutation = gql`
 
 export function useCreateExerciseSubmissionMutation(path: string) {
   const mutationFetch = useMutationFetch()
-  const splitPath = path.split('/').filter((p) => p.length > 0)
-  const experimentIds = [30680, 23869, 66809]
-  const pathContainsExperimentId = splitPath.some((p) =>
-    experimentIds.includes(+p)
+  const isExperimentPath = [30680, 23869, 66809].find((id) =>
+    path.includes(`/${id}`)
   )
-  if (!pathContainsExperimentId) return () => Promise.resolve()
+  if (!isExperimentPath) return () => Promise.resolve()
 
   return async function (input: ExerciseSubmissionInput) {
     return await mutationFetch(mutation, input)
