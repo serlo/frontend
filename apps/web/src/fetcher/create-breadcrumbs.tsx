fb02b12e23a77bf76689e040feff0baea0437a8a
Botho <1258870+elbotho@users.noreply.github.com>
2024-04-11 11:23:25 +0200

    use secondaryMenu data to shorten breadcrumbs again

--- apps/web/src/fetcher/create-breadcrumbs.tsx
+++ apps/web/src/fetcher/create-breadcrumbs.tsx
@@ -12,64 +12,6 @@ type TaxonomyTermNodes = Extract<
   { __typename: 'Article' }
 >['taxonomyTerms']['nodes']
 
-export function taxonomyParentsToRootToBreadcrumbsData(
-  term: TaxonomyTermNodes[0] | undefined,
-  instance: Instance,
-  includeFirstParent?: boolean
-): BreadcrumbsData | undefined {
-  if (term === undefined) return undefined
-
-  const { secondaryMenus } = getInstanceDataByLang(instance)
-
-  const breadcrumbs = term.path.map((entry) => {
-    return {
-      label: entry!.title,
-      url: entry!.alias,
-      id: entry!.id,
-    }
-  })
-
-  if (includeFirstParent) {
-    breadcrumbs.push({
-      label: term.title,
-      url: term.alias,
-      id: term.id,
-    })
-  }
-
-  const subject = secondaryMenus.find(
-    (menu) => menu.rootId === breadcrumbs[0].id
-  )
-
-  if (subject) {
-    breadcrumbs[0].label = subject.rootName ?? breadcrumbs[0].label
-    breadcrumbs[0].url = subject.landingUrl ?? breadcrumbs[0].url
-  }
-
-  return breadcrumbs
-
-  // TODO: check if we still need this
-  //     // check if this breadcrumb is already part of secondary menu
-  //     const matching2 = secondaryMenus.filter((menu) =>
-  //       menu.entries.some((entry) => entry.id === current.id)
-  //     )
-  //     if (matching2.length > 0) {
-  //       const metaMenuEntry = matching2[0].entries.filter(
-  //         (menu) => menu.id === current.id
-  //       )[0]
-  //       breadcrumbs.unshift({
-  //         label: metaMenuEntry.title,
-  //         url: current.alias,
-  //         id: current.id,
-  //       })
-  //       breadcrumbs.unshift({
-  //         label: matching2[0].rootName ?? getSubjectName(current),
-  //         url: matching2[0].landingUrl,
-  //         id: matching2[0].rootId,
-  //       })
-  //       …
-}
-
 export function createBreadcrumbs(uuid: MainUuidType, instance: Instance) {
   if (uuid.__typename === UuidType.TaxonomyTerm) {
     return compat(taxonomyParentsToRootToBreadcrumbsData(uuid, instance))
@@ -91,54 +33,116 @@ export function createBreadcrumbs(uuid: MainUuidType, instance: Instance) {
   }
 
   return undefined
+}
 
-  function buildFromTaxTerms(
-    nodes: TaxonomyTermNodes | undefined,
-    instance: Instance
-  ) {
-    if (!nodes) return undefined
+export function taxonomyParentsToRootToBreadcrumbsData(
+  term: TaxonomyTermNodes[0] | undefined,
+  instance: Instance,
+  includeFirstParent?: boolean
+): BreadcrumbsData | undefined {
+  if (term === undefined) return undefined
 
-    // we select first shortest taxonomy path as main taxonomy and show it in breadcrumbs
-    // this happens directly on taxonomy parents and is not taking short-cuircuits into account
-    nodes.sort((a, b) => a.path.length - b.path.length)
-    return taxonomyParentsToRootToBreadcrumbsData(nodes[0], instance, true)
+  const { secondaryMenus } = getInstanceDataByLang(instance)
+
+  let breadcrumbs = term.path.map((entry) => {
+    return {
+      label: entry!.title,
+      url: entry!.alias,
+      id: entry!.id,
+    }
+  })
+
+  if (includeFirstParent) {
+    breadcrumbs.push({
+      label: term.title,
+      url: term.alias,
+      id: term.id,
+    })
   }
 
-  function compat(breadcrumbs?: BreadcrumbsData) {
-    if (!breadcrumbs || !breadcrumbs.length) return undefined
+  // get the subject from the secondary menu data so we link to the correct landing pages
+  const subject = secondaryMenus.find(
+    (menu) => menu.rootId === breadcrumbs[0].id
+  )
 
-    // identify final exam taxonomies or their children
-    const isOrInExamsFolder = !!breadcrumbs.find(
-      ({ id }) => id && mathExamsTaxonomies.includes(id)
+  if (subject) {
+    breadcrumbs[0].label = subject.rootName ?? breadcrumbs[0].label
+    breadcrumbs[0].url = subject.landingUrl ?? breadcrumbs[0].url
+
+    const breadcrumbIdsNoRoot = breadcrumbs.map((item) => item.id).slice(1)
+
+    // if there are other entries that exists in breadcrumbs and secondaryMenu
+    // we use that to shorten the breadcrumbs
+    // example: https://de.serlo.org/mathe/16299/klasse-11
+    const secondaryMenuEntry = subject?.entries.find((entry) =>
+      breadcrumbIdsNoRoot.includes(entry.id ?? 0)
     )
 
-    // compat: remove empty entries
-    breadcrumbs = breadcrumbs.filter(({ url, label }) => url && label)
-
-    // compat: exam breadcrumbs remove "Deutschland" Taxonomie and maybe school type
-    if (isOrInExamsFolder) {
-      const exclude =
-        breadcrumbs.length > 4 ? [...schoolTaxonomies, 16030] : [16030]
-      return breadcrumbs.filter(({ id }) => id && !exclude.includes(id))
+    if (secondaryMenuEntry) {
+      const matchingBreadcrumbIndex = breadcrumbs.findIndex(
+        (item) => item.id === secondaryMenuEntry.id
+      )
+      const matchingBreadcrumbItem = breadcrumbs[matchingBreadcrumbIndex]
+      breadcrumbs = [
+        breadcrumbs[0],
+        {
+          ...matchingBreadcrumbItem,
+          label: secondaryMenuEntry.title,
+        },
+        ...breadcrumbs.slice(matchingBreadcrumbIndex + 1),
+      ]
     }
-
-    const shortened: BreadcrumbsData = []
-    breadcrumbs.map((entry, i, arr) => {
-      const maxItems = 4
-      const overflow = arr.length > maxItems
-      const itemsToRemove = arr.length - maxItems
-      const ellipsesItem = overflow && i === 2
-
-      if (overflow && i > 2 && i < 1 + itemsToRemove) return
-      // special case
-      if (arr.length - itemsToRemove > 4 && i === 1) return
-      if (ellipsesItem) {
-        shortened.push({ label: '', ellipsis: true })
-      } else {
-        shortened.push(entry)
-      }
-    })
-
-    return shortened
   }
+
+  return breadcrumbs
+}
+
+function buildFromTaxTerms(
+  nodes: TaxonomyTermNodes | undefined,
+  instance: Instance
+) {
+  if (!nodes) return undefined
+
+  // we select first shortest taxonomy path as main taxonomy and show it in breadcrumbs
+  // this happens directly on taxonomy parents and is not taking short-cuircuits into account
+  nodes.sort((a, b) => a.path.length - b.path.length)
+  return taxonomyParentsToRootToBreadcrumbsData(nodes[0], instance, true)
+}
+
+function compat(breadcrumbs?: BreadcrumbsData) {
+  if (!breadcrumbs || !breadcrumbs.length) return undefined
+
+  // identify final exam taxonomies or their children
+  const isOrInExamsFolder = !!breadcrumbs.find(
+    ({ id }) => id && mathExamsTaxonomies.includes(id)
+  )
+
+  // compat: remove empty entries
+  breadcrumbs = breadcrumbs.filter(({ url, label }) => url && label)
+
+  // compat: exam breadcrumbs remove "Deutschland" Taxonomie and maybe school type
+  if (isOrInExamsFolder) {
+    const exclude =
+      breadcrumbs.length > 4 ? [...schoolTaxonomies, 16030] : [16030]
+    return breadcrumbs.filter(({ id }) => id && !exclude.includes(id))
+  }
+
+  const shortened: BreadcrumbsData = []
+  breadcrumbs.map((entry, i, arr) => {
+    const maxItems = 4
+    const overflow = arr.length > maxItems
+    const itemsToRemove = arr.length - maxItems
+    const ellipsesItem = overflow && i === 2
+
+    if (overflow && i > 2 && i < 1 + itemsToRemove) return
+    // special case
+    if (arr.length - itemsToRemove > 4 && i === 1) return
+    if (ellipsesItem) {
+      shortened.push({ label: '', ellipsis: true })
+    } else {
+      shortened.push(entry)
+    }
+  })
+
+  return shortened
 }
