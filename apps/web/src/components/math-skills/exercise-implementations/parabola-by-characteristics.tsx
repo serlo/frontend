ee843aef4194db3441945e39aede610bacf18975
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-05-02 08:39:26 +0200

    refactor: add helper for polynoms

--- apps/web/src/components/math-skills/exercise-implementations/parabola-by-characteristics.tsx
+++ apps/web/src/components/math-skills/exercise-implementations/parabola-by-characteristics.tsx
@@ -1,6 +1,6 @@
 import { SelfEvaluationExercise } from './self-evaluation-exercise'
-import { HighlightGreen, HighlightGray } from '../components/content-components'
-import { pp } from '../utils/pretty-print'
+import { HighlightGray } from '../components/content-components'
+import { pp, ppPolynom } from '../utils/pretty-print'
 import { randomIntBetween } from '@/helper/random-int-between'
 
 export function ParabolaCharacteristics() {
@@ -40,30 +40,19 @@ export function ParabolaCharacteristics() {
         const c = n_1 * n_2
         return (
           <>
-            Bestimme die Parameter b und c der Parabel.
-            <br />
-            <br />
-            Setze dazu die Punkte P und Q jeweils in eine Funktionsgleichung
-            ein:
-            <br />
-            Ɪ:{' '}
-            <HighlightGray>
-              {p_1_y} = {p_1_x < 0 ? '(' + p_1_x + ')' : p_1_x}
-              <sup>2</sup> {p_1_x < 0 ? p_1_x + ' · b' : null}
-              {p_1_x === 0 ? ' + 0 · b' : null}
-              {p_1_x > 0 ? ' + ' + p_1_x + ' · b' : null} + c
-            </HighlightGray>
-            <br />
-            ꞮꞮ:{' '}
-            <HighlightGray>
-              {p_2_y} = {p_2_x < 0 ? '(' + p_2_x + ')' : p_2_x}
-              <sup>2</sup> {p_2_x < 0 ? ' ' + p_2_x + ' · b' : null}
-              {p_2_x === 0 ? ' + 0 · b' : null}
-              {p_2_x > 0 ? ' + ' + p_2_x + ' · b' : null} + c
-            </HighlightGray>
-            <br />
-            <br />
-            Berechne die Potenz und fasse die rechte Seite zusammen:
+            <p>
+              Setze die Punkte P und Q in die quadratische Funktion ein und
+              stelle ein lineares Gleichungssystem auf:
+            </p>
+            <p className="serlo-highlight-gray">
+              I&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {p_1_y} ={' '}
+              {pp(p_1_x, 'embrace_neg')}
+              <sup>2</sup> {pp(p_1_x, 'merge_op')} · b + c<br />
+              II&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              {p_2_y} = {pp(p_2_x, 'embrace_neg')}
+              <sup>2</sup> {pp(p_2_x, 'merge_op')} · b + c
+            </p>
+            <p>Berechne die Potenz und fasse die rechte Seite zusammen:</p>
             <br />
             Ɪ:{' '}
             <HighlightGray>
@@ -108,12 +97,14 @@ export function ParabolaCharacteristics() {
             Löse das lineare Gleichungssystem und stelle den Funktionsterm der
             Parabel auf:
             <br />
-            <HighlightGreen>
-              y = x<sup>2</sup> {b !== 0 && b > 0 ? '+' : null}{' '}
-              {b !== 0 && b !== 1 ? b + 'x' : null}
-              {b === 1 ? 'x' : null} {c !== 0 && c > 0 ? '+' : null}
-              {c !== 0 ? c : null}
-            </HighlightGreen>
+            <p className="serlo-highlight-green">
+              y ={' '}
+              {ppPolynom([
+                [1, 'x', 2],
+                [b, 'x', 1],
+                [c, '', 0],
+              ])}
+            </p>
           </>
         )
       }}
--- apps/web/src/components/math-skills/utils/pretty-print.tsx
+++ apps/web/src/components/math-skills/utils/pretty-print.tsx
@@ -1,7 +1,54 @@
-export function pp(x: number) {
-  const isNeg = x < 0
+export function pp(
+  x: number,
+  mode: 'normal' | 'embrace_neg' | 'merge_op' = 'normal'
+): string {
+  const numStr = Math.abs(x).toLocaleString('de-De').replace(/\./g, '\xa0')
+  if (mode === 'normal') {
+    return (x < 0 ? '−' : '') + numStr
+  }
+  if (mode === 'embrace_neg') {
+    if (x >= 0) return numStr
+    return `(−${numStr})`
+  }
+  if (mode === 'merge_op') {
+    if (x >= 0) {
+      return `+ ${numStr}`
+    }
+    return `- ${numStr}`
+  }
+  return x.toString()
+}
+
+export function ppPolynom(polynom: [number, string, number][]): JSX.Element {
+  let isFirstElement = true
   return (
-    (isNeg ? '−' : '') +
-    Math.abs(x).toLocaleString('de-De').replace(/\./g, '\xa0')
+    <>
+      {polynom.map(([koeff, variable, exp]) => {
+        if (koeff === 0) return null
+        let beginWithWhitespace = true
+        if (isFirstElement) {
+          beginWithWhitespace = false
+          isFirstElement = false
+        }
+        return (
+          <>
+            {beginWithWhitespace ? ' ' : null}
+            {Math.abs(koeff) === 1
+              ? koeff > 0
+                ? beginWithWhitespace
+                  ? '+ '
+                  : ''
+                : '- '
+              : pp(koeff, 'merge_op')}
+            {exp === 0 ? null : (
+              <>
+                {variable}
+                {exp === 1 ? null : <sup>{exp}</sup>}
+              </>
+            )}
+          </>
+        )
+      })}
+    </>
   )
 }
