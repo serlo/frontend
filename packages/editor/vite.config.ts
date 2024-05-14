7963398af2aa38d4d3c0a5131684feb9504343e0
Mikey Stengel <mikey@serlo.org>
2024-05-14 15:09:30 +0200

    feat(editor): Custom code to simplify editor API to bundle styles within JS AND export

--- packages/editor/vite.config.ts
+++ packages/editor/vite.config.ts
@@ -3,57 +3,11 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 import svgr from 'vite-plugin-svgr'
-import fs from 'fs'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { existsSync, mkdirSync, writeFileSync } from 'fs'
 
 // https://vitejs.dev/guide/build.html#library-mode
 /* we use vite only for building the serlo editor package */
-
-function injectCssPlugin() {
-  return {
-    name: 'inject-css-plugin',
-    generateBundle(_, bundle) {
-      console.log({ bundle })
-      // const cssFileName = 'style.css'
-      const cssFileName = Object.keys(bundle).find((file) =>
-        file.endsWith('.css')
-      )
-
-      const jsFileName = Object.keys(bundle).find((file) =>
-        file.endsWith('.js')
-      )
-
-      if (!cssFileName || !jsFileName) {
-        throw new Error('JS or CSS bundle not found!')
-      }
-
-      const cssFile = bundle[cssFileName]
-      const jsFile = bundle[jsFileName]
-      console.log({ cssFile })
-      console.log({ jsFile })
-      if (cssFile.type !== 'asset' || jsFile.type !== 'chunk') {
-        this.error('CSS file is not an asset or JS file is not a chunk!')
-        return
-      }
-
-      const cssCode = cssFile.source
-
-      if (typeof cssCode !== 'string') {
-        this.error('CSS code is not a string!')
-        return
-      }
-
-      // Inject CSS code into the JS bundle
-      jsFile.code = `
-          (function() {
-            const style = document.createElement('style');
-            style.textContent = \`${cssCode}\`;
-            document.head.appendChild(style);
-          })();
-          ${jsFile.code}
-        `
-    },
-  }
-}
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
@@ -88,7 +42,25 @@ export default defineConfig({
       rollupTypes: true,
     }),
     svgr({ include: '**/*.svg' }),
-    injectCssPlugin(),
+    cssInjectedByJsPlugin({
+      // Tried using the injectCodeFunction, but it didn't get called.
+      // preRenderCSSCode works!
+      preRenderCSSCode: (cssCode) => {
+        try {
+          // Ensure the dist directory exists
+          const distDir = resolve(__dirname, 'dist')
+          if (!existsSync(distDir)) {
+            mkdirSync(distDir, { recursive: true })
+          }
+
+          // Write the CSS to a file. Usually, this plugin excludes css bundles
+          // from the output but we need to export it for the shadow DOM
+          writeFileSync(resolve(__dirname, 'dist', 'style.css'), cssCode)
+        } catch (e) {
+          console.error('Failed to write CSS to file', e)
+        }
+        return cssCode
+      },
+    }),
   ],
-  css: {},
 })
