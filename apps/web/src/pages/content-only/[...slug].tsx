8ed0cddff7459562717560b2c6af9eee877eec6d
Botho <1258870+elbotho@users.noreply.github.com>
2024-05-16 22:13:46 +0200

    fix import order

--- apps/web/src/pages/content-only/[...slug].tsx
+++ apps/web/src/pages/content-only/[...slug].tsx
@@ -1,7 +1,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 
-import { Entity } from '@/components/entity/entity'
 import { LazyIframeResizer } from '@/components/content/lazy-iframe-resizer'
+import { Entity } from '@/components/entity/entity'
 import { FrontendClientBase } from '@/components/frontend-client-base/frontend-client-base'
 import { HeadTags } from '@/components/head-tags'
 import { LoadingSpinner } from '@/components/loading/loading-spinner'
