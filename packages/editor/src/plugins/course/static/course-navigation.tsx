93ab45d713d57333a046aa1e75eb18f4024c1c7e
Botho <1258870+elbotho@users.noreply.github.com>
2024-05-16 23:02:32 +0200

    add title to course

--- packages/editor/src/plugins/course/static/course-navigation.tsx
+++ packages/editor/src/plugins/course/static/course-navigation.tsx
@@ -1,12 +1,15 @@
 import { EditorCourseDocument } from '@editor/types/editor-plugins'
-import { useState, MouseEvent } from 'react'
+import { useState, MouseEvent, use } from 'react'
 
 import { CourseNavigationRenderer } from '../renderer/course-navigation'
 import { Link } from '@/components/content/link'
+import { useEntityData } from '@/contexts/serlo-entity-context'
 import { cn } from '@/helper/cn'
 
 export function CourseNavigation({ pages }: EditorCourseDocument['state']) {
   const [courseNavOpen, setCourseNavOpen] = useState(pages?.length < 4)
+  const { title } = useEntityData()
+
   if (!pages) return null
 
   const openCourseNav = (e?: MouseEvent) => {
@@ -18,7 +21,7 @@ export function CourseNavigation({ pages }: EditorCourseDocument['state']) {
     <CourseNavigationRenderer
       open={courseNavOpen}
       onOverviewButtonClick={openCourseNav}
-      title="courseTitle" // TODO: get course title in here
+      title={title ?? ''}
       pages={pages.map(({ id, title }) => {
         // TODO: check if empty?
         const active = false // TODO: get currently active page
