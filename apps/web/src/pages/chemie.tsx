b5a3fd94e4d3a188c4f3e3ffe2a328531b6778d3
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-05-25 09:27:56 +0200

    Revert "hotfix: disable prerendering"
    This reverts commit b3a50e9f7c68f3e5533775367a0d5a392aa08385.

--- apps/web/src/pages/chemie.tsx
+++ apps/web/src/pages/chemie.tsx
@@ -2,7 +2,7 @@ import { GetStaticProps } from 'next'
 
 import { SubjectLanding } from '@/components/pages/subject-landing'
 import { SlugProps, TaxonomyPage } from '@/data-types'
-// import { subjectLandingGetStaticProps } from '@/fetcher/subject-landing/subject-landing-get-static-props'
+import { subjectLandingGetStaticProps } from '@/fetcher/subject-landing/subject-landing-get-static-props'
 import { renderedPageNoHooks } from '@/helper/rendered-page'
 
 const subject = 'chemie'
@@ -13,10 +13,7 @@ export default renderedPageNoHooks<{ pageData: TaxonomyPage }>(
   }
 )
 
-export const getStaticProps: GetStaticProps<
-  SlugProps
-> = async (/*context*/) => {
-  return { notFound: true }
-  // if (context.locale !== 'de') return { notFound: true }
-  // return subjectLandingGetStaticProps(context.locale, subject)
+export const getStaticProps: GetStaticProps<SlugProps> = async (context) => {
+  if (context.locale !== 'de') return { notFound: true }
+  return subjectLandingGetStaticProps(context.locale, subject)
 }
