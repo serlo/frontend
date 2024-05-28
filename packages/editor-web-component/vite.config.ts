d189fae0d7e2e9ce8c6a77e42835a97531ee3bd7
Mikey Stengel <mikey@serlo.org>
2024-05-28 18:48:45 +0200

    fix(editor-web-component): Stop bundling and applying math quill styles globally
    * Modularize code
    * Change NO_OP variable name

--- packages/editor-web-component/vite.config.ts
+++ packages/editor-web-component/vite.config.ts
@@ -47,83 +47,154 @@ const envReplacements = {
   ),
 }
 
+function parseCodeToAST(code: string): acorn.Program {
+  return acorn.parse(code, {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  })
+}
+
+function generateCodeFromAST(ast: acorn.Program) {
+  return escodegen.generate(ast)
+}
+
+type PatternMatcher = (node: acorn.Program) => boolean
+
+function removePattern(
+  ast: acorn.Program,
+  patternMatcher: PatternMatcher,
+  replacement: Record<string, unknown>
+) {
+  let foundMatch = false
+
+  estraverse.replace(ast, {
+    enter(node) {
+      if (patternMatcher(node)) {
+        foundMatch = true
+        return replacement
+      }
+    },
+  })
+
+  return { ast, foundMatch }
+}
+
+// Traverses the AST and look for the specific try-catch block that
+// looks something like this
+// try {
+//   if (typeof document < "u") {
+//     var t = document.createElement("style");
+//     t.appendChild(document.createTextNode(`.katex{font:....
+//   }
+// } catch (e) {
+//   console.error("vite-plugin-css-injected-by-js", e);
+// }
+function matchGlobalStyleInjection(node) {
+  return (
+    node.type === 'TryStatement' &&
+    node.handler &&
+    node.handler.body &&
+    node.handler.body.body.some(
+      (n) =>
+        n.type === 'ExpressionStatement' &&
+        n.expression.type === 'CallExpression' &&
+        n.expression.callee.type === 'MemberExpression' &&
+        n.expression.callee.object.name === 'console' &&
+        n.expression.callee.property.name === 'error' &&
+        n.expression.arguments.length > 0 &&
+        n.expression.arguments[0].value === 'vite-plugin-css-injected-by-js'
+    )
+  )
+}
+
+// Traverses the AST and look for the specific try-catch block that
+// looks something like this
+// function Ee() {
+//   if (document.getElementById("react-mathquill-styles") == null) {
+//     var ke = document.createElement("style");
+//     ke.setAttribute("id", "react-mathquill-styles"), ke.innerHTML = ve.Z[0][1], document.getElementsByTagName("head")[0].appendChild(ke);
+//   }
+// }
+function matchMathQuillStyleInjection(node) {
+  return (
+    node.type === 'FunctionDeclaration' &&
+    node.body.body.some(
+      (n) =>
+        n.type === 'IfStatement' &&
+        n.test.type === 'BinaryExpression' &&
+        n.test.left.type === 'CallExpression' &&
+        n.test.left.callee.type === 'MemberExpression' &&
+        n.test.left.callee.object.name === 'document' &&
+        n.test.left.callee.property.name === 'getElementById' &&
+        n.test.left.arguments[0].value === 'react-mathquill-styles' &&
+        n.test.right.type === 'Literal' &&
+        n.test.right.value === null
+    )
+  )
+}
+
+/**
+ * TODO define type here
+ */
+function getNoOpReplacement(identifier: string) {
+  return {
+    type: 'VariableDeclaration',
+    declarations: [
+      {
+        type: 'VariableDeclarator',
+        id: {
+          type: 'Identifier',
+          name: `NO_OPERATION_INSTEAD_OF_${identifier}`,
+        },
+        init: { type: 'Literal', value: null },
+      },
+    ],
+    kind: 'const',
+  }
+}
+
 // Custom Rollup plugin to remove global style injection from the @editor
 // package. This basically undos the cssInjectedByJsPlugin inside the editor
 // package. The reason why we don't remove it from there, is because we want to
 // keep the convenience of importing from the @serlo/editor package without
 // having to worry about styling.
-function removeGlobalEditorStyles(): Plugin {
+function removeGlobalStylesPlugin(): Plugin {
   return {
-    name: 'remove-global-styles',
+    name: 'remove-styles-plugin',
     generateBundle(_, bundle) {
-      let foundMatch = false
+      let globalStyleMatch = false
+      let mathQuillStyleMatch = false
 
       for (const file of Object.values(bundle)) {
         if (file.type === 'chunk') {
           const chunk = file
+          let ast = parseCodeToAST(chunk.code)
 
-          // Parse the code into an AST
-          const ast = acorn.parse(chunk.code, {
-            ecmaVersion: 2020,
-            sourceType: 'module',
-          })
-
-          // Traverses the AST and look for the specific try-catch block that
-          // looks something like this
-          // try {
-          //   if (typeof document < "u") {
-          //     var t = document.createElement("style");
-          //     t.appendChild(document.createTextNode(`.katex{font:....
-          //   }
-          // } catch (e) {
-          //   console.error("vite-plugin-css-injected-by-js", e);
-          // }
-          estraverse.replace(ast, {
-            enter(node) {
-              if (
-                node.type === 'TryStatement' &&
-                node.handler &&
-                node.handler.body &&
-                node.handler.body.body.some(
-                  (n) =>
-                    n.type === 'ExpressionStatement' &&
-                    n.expression.type === 'CallExpression' &&
-                    n.expression.callee.type === 'MemberExpression' &&
-                    n.expression.callee.object.name === 'console' &&
-                    n.expression.callee.property.name === 'error' &&
-                    n.expression.arguments.length > 0 &&
-                    n.expression.arguments[0].value ===
-                      'vite-plugin-css-injected-by-js'
-                )
-              ) {
-                foundMatch = true
-                // Replace the entire try-catch block with a NO_OP
-                return {
-                  type: 'VariableDeclaration',
-                  declarations: [
-                    {
-                      type: 'VariableDeclarator',
-                      id: {
-                        type: 'Identifier',
-                        name: 'NO_OPERATION_INSTEAD_OF_GLOBAL_STYLE',
-                      },
-                      init: { type: 'Literal', value: null },
-                    },
-                  ],
-                  kind: 'const',
-                }
-              }
-            },
-          })
+          ;({ ast, foundMatch: globalStyleMatch } = removePattern(
+            ast,
+            matchGlobalStyleInjection,
+            getNoOpReplacement('GLOBAL_EDITOR_STYLES')
+          ))
+          ;({ ast, foundMatch: mathQuillStyleMatch } = removePattern(
+            ast,
+            matchMathQuillStyleInjection,
+            getNoOpReplacement('MATH_QUILL')
+          ))
 
           // Turn AST back into code and write it into the chunk.
-          chunk.code = escodegen.generate(ast)
+          chunk.code = generateCodeFromAST(ast)
         }
       }
 
-      if (!foundMatch) {
+      if (!globalStyleMatch) {
         throw new Error(
-          'No global style injection pattern was found and removed.'
+          'No editor global style injection pattern was found and removed.'
+        )
+      }
+
+      if (!mathQuillStyleMatch) {
+        throw new Error(
+          'No MathQuill style injection pattern was found and removed.'
         )
       }
     },
@@ -152,6 +223,6 @@ export default defineConfig({
     }),
     svgr({ include: '**/*.svg' }),
     cssInjectedByJsPlugin(),
-    removeGlobalEditorStyles(),
+    removeGlobalStylesPlugin(),
   ],
 })
