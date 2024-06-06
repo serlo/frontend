aa128266fdc60af87da112770320d9c9ece9aa6d
Mikey Stengel <mikey@serlo.org>
2024-06-06 16:04:19 +0200

    fix(editor-web-component): Try to fix use blur on outside click (shadow root is null)

--- packages/editor/src/core/helpers/use-shadow-root.ts
+++ packages/editor/src/core/helpers/use-shadow-root.ts
@@ -1,11 +1,15 @@
 import { useEffect, useState } from 'react'
 
 // null while computing; false if we are certainly outside of the shadow DOM.
-type PossibleShadowRoot = ShadowRoot | null | false
+type PossibleShadowRoot = ShadowRoot | null
 
 export function useShadowRoot(
   containerRef: React.RefObject<HTMLElement>
 ): PossibleShadowRoot {
+  console.log(
+    'useShadowRoot. Checking whether container ref is part of the shadow root: ',
+    { containerRef }
+  )
   const [shadowRoot, setShadowRoot] = useState<PossibleShadowRoot>(null)
 
   useEffect(() => {
@@ -16,14 +20,22 @@ export function useShadowRoot(
     // Should be fixed in React 19, hopefully. If not for the timeout,
     // getRootNode() will not consistently return the shadow root...
     const timeout = setTimeout(() => {
+      // containerRef..current.shadowRoot is always null which is why we
+      // use getRootNode.
       const possibleShadowRoot = containerRef.current?.getRootNode()
-      if (possibleShadowRoot instanceof ShadowRoot) {
-        // containerRef..current.shadowRoot is always null which is why we
-        // use getRootNode.
+
+      console.log('Use Shadow root called. Checking if root is a shadow root', {
+        containerRef,
+        possibleShadowRoot,
+      })
+
+      if (isShadowRoot(possibleShadowRoot)) {
+        console.log('It is a shadow root! 🎉')
         setShadowRoot(possibleShadowRoot)
-      } else {
-        setShadowRoot(false)
       }
+      // else {
+      //   setShadowRoot(false)
+      // }
     }, 1)
 
     return () => {
@@ -31,11 +43,12 @@ export function useShadowRoot(
     }
   }, [containerRef])
 
+  console.log('Returning shadow root: ', shadowRoot)
   return shadowRoot
 }
 
 export function isShadowRoot(
-  shadowRoot: PossibleShadowRoot
+  shadowRoot: PossibleShadowRoot | Node | undefined
 ): shadowRoot is ShadowRoot {
   return shadowRoot instanceof ShadowRoot
 }
