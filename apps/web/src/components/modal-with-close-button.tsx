1b433e89be884f9551c3c8f2f73363ff79be0210
Mikey Stengel <mikey@serlo.org>
2024-06-06 14:54:10 +0200

    refactor(modal): Migrate from react-modal to Radix dialog

--- apps/web/src/components/modal-with-close-button.tsx
+++ apps/web/src/components/modal-with-close-button.tsx
@@ -1,27 +1,27 @@
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import * as Dialog from '@radix-ui/react-dialog'
 import { useState, type ReactNode, useCallback, useEffect } from 'react'
-import BaseModal from 'react-modal'
 
 import { FaIcon } from './fa-icon'
 import { useInstanceData } from '@/contexts/instance-context'
 import { cn } from '@/helper/cn'
 
-let isAppElementSet = false
+// let isAppElementSet = false
 
-export function useLazySetAppElement(
-  selectorOrElement: string | HTMLElement = '#serlo-root'
-) {
-  console.log('useLazySetAppElement called with', { selectorOrElement })
-  useEffect(() => {
-    if (!isAppElementSet) {
-      // const element = document.querySelector(selector)
-      // if (element) {
-      BaseModal.setAppElement(selectorOrElement)
-      isAppElementSet = true
-      // }
-    }
-  }, [selectorOrElement])
-}
+// export function useLazySetAppElement(
+//   selectorOrElement: string | HTMLElement = '#serlo-root'
+// ) {
+//   console.log('useLazySetAppElement called with', selectorOrElement)
+//   useEffect(() => {
+//     if (!isAppElementSet && selectorOrElement) {
+//       // const element = document.querySelector(selector)
+//       // if (element) {
+//       BaseModal.setAppElement(selectorOrElement)
+//       isAppElementSet = true
+//       // }
+//     }
+//   }, [selectorOrElement])
+// }
 
 interface ModalWithCloseButtonProps {
   isOpen: boolean
@@ -45,13 +45,11 @@ export function ModalWithCloseButton({
   extraTitleClassName,
   confirmCloseDescription,
   extraCloseButtonClassName,
-  parentSelector,
   appElement,
 }: ModalWithCloseButtonProps) {
+  console.log('Modal with close button', { isOpen, appElement, className })
   const { strings } = useInstanceData()
   const [showConfirmation, setShowConfirmation] = useState(false)
-
-  useLazySetAppElement(appElement)
 
   const onRequestClose = useCallback(
     () =>
@@ -60,58 +58,58 @@ export function ModalWithCloseButton({
   )
 
   return (
-    <BaseModal
-      overlayClassName={cn(defaultModalOverlayStyles, 'z-[101]')}
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      className={cn('serlo-modal', className)}
-      parentSelector={parentSelector}
-    >
-      {title ? (
-        <h2 className={cn('serlo-h2', extraTitleClassName)}>{title}</h2>
-      ) : null}
-      <button
-        onClick={onRequestClose}
-        title={title}
-        className={cn(
-          `z-2 absolute right-3.5 top-3.5 inline-flex h-9 w-9 cursor-pointer items-center
-          justify-center rounded-full border-none leading-tight
-        text-almost-black hover:bg-brand hover:text-white`,
-          extraCloseButtonClassName
-        )}
-        data-qa="modal-close-button"
-      >
-        <FaIcon icon={faXmark} className="h-5" />
-      </button>
+    <Dialog.Root open={isOpen} onOpenChange={onRequestClose}>
+      <Dialog.Portal container={appElement}>
+        <Dialog.Overlay className={defaultModalOverlayStyles} />
+        <Dialog.Content className={cn('serlo-modal', className)}>
+          {title ? (
+            <Dialog.Title className={cn('serlo-h2', extraTitleClassName)}>
+              {title}
+            </Dialog.Title>
+          ) : null}
+          <button
+            onClick={onRequestClose}
+            title={title}
+            className={cn(
+              `z-2 absolute right-3.5 top-3.5 inline-flex h-9 w-9 cursor-pointer items-center
+              justify-center rounded-full border-none leading-tight
+              text-almost-black hover:bg-brand hover:text-white`,
+              extraCloseButtonClassName
+            )}
+            data-qa="modal-close-button"
+          >
+            <FaIcon icon={faXmark} className="h-5" />
+          </button>
 
-      {children}
+          {children}
 
-      {showConfirmation && (
-        <div className="absolute inset-0 z-10 flex items-center justify-center rounded-xl bg-gray-500 bg-opacity-75 px-4">
-          <div className="rounded-xl bg-editor-primary-100 p-6 shadow-lg ">
-            <p className="px-2">{confirmCloseDescription}</p>
-            <div className="mt-4 flex space-x-4">
-              <button
-                className="serlo-button-blue-transparent mr-4"
-                onClick={onCloseClick}
-              >
-                {strings.modal.leaveNow}
-              </button>
-              <button
-                className="serlo-button-blue"
-                onClick={() => setShowConfirmation(false)}
-              >
-                {strings.modal.noStay}
-              </button>
+          {showConfirmation && (
+            <div className="absolute inset-0 z-10 flex items-center justify-center rounded-xl bg-gray-500 bg-opacity-75 px-4">
+              <div className="rounded-xl bg-editor-primary-100 p-6 shadow-lg ">
+                <p className="px-2">{confirmCloseDescription}</p>
+                <div className="mt-4 flex space-x-4">
+                  <button
+                    className="serlo-button-blue-transparent mr-4"
+                    onClick={onCloseClick}
+                  >
+                    {strings.modal.leaveNow}
+                  </button>
+                  <button
+                    className="serlo-button-blue"
+                    onClick={() => setShowConfirmation(false)}
+                  >
+                    {strings.modal.noStay}
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
-    </BaseModal>
+          )}
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
 
-// See https://github.com/reactjs/react-modal/blob/master/src/components/Modal.js#L107
 export const defaultModalOverlayStyles = cn(
-  'fixed bottom-0 left-0 right-0 top-0 bg-white bg-opacity-75'
+  'fixed bottom-0 left-0 right-0 top-0 z-[101] bg-white bg-opacity-75'
 )
