f9d933f2ea45bf7791c94e391d8bd2003d644458
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-06-11 08:36:39 +0200

    change approach and only fix serlo client

--- apps/web/src/serlo-editor-integration/serlo-plugin-wrappers/h5p-serlo-static.tsx
+++ apps/web/src/serlo-editor-integration/serlo-plugin-wrappers/h5p-serlo-static.tsx
@@ -1,10 +1,11 @@
 import { parseH5pUrl } from '@editor/plugins/h5p/renderer'
-import { H5pStaticRenderer } from '@editor/plugins/h5p/static'
 import type { EditorH5PDocument } from '@editor/types/editor-plugins'
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { useAB } from '@/contexts/ab'
+import { useInstanceData } from '@/contexts/instance-context'
 import { useEntityData } from '@/contexts/uuids-context'
 import { exerciseSubmission } from '@/helper/exercise-submission'
 import { useCreateExerciseSubmissionMutation } from '@/mutations/use-experiment-create-exercise-submission-mutation'
@@ -15,8 +16,14 @@ export function H5pSerloStaticRenderer(props: EditorH5PDocument) {
   const ab = useAB()
   const { entityId, revisionId } = useEntityData()
   const trackExperiment = useCreateExerciseSubmissionMutation(asPath)
+  const [onClient, setOnClient] = useState(false)
+  const id = parseH5pUrl(props.state)
+  const { strings } = useInstanceData()
 
   useEffect(() => {
+    requestAnimationFrame(() => {
+      setOnClient(true)
+    })
     const handleSubmissionEvent = (e: Event) => {
       const e_id = (e as CustomEvent).detail as string
       const id = parseH5pUrl(props.state)
@@ -48,5 +55,44 @@ export function H5pSerloStaticRenderer(props: EditorH5PDocument) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  return <H5pStaticRenderer {...props} />
+  if (!id) {
+    return <p className="serlo-p">{strings.errors.defaultMessage}</p>
+  }
+
+  if (!onClient) {
+    return (
+      <div
+        className="mx-side mb-block"
+        style={{ width: 727, height: 500 }}
+      ></div>
+    )
+  }
+
+  console.log('why is this renderer on the server?')
+
+  return <ClientUI id={id} />
 }
+
+function ClientUIRaw({ id }: { id: string }) {
+  return (
+    <div className="mx-side mb-block" style={{ width: 727 }}>
+      <H5PPlayerUI
+        contentId={id}
+        loadContentCallback={async (contentId) => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          const data = await (
+            await fetch(`/api/frontend/lumi/embed/${contentId}`)
+          ).json()
+          console.log(data)
+          return data
+        }}
+      />
+    </div>
+  )
+}
+
+const ClientUI = dynamic(() => Promise.resolve(ClientUIRaw), { ssr: false })
+
+const H5PPlayerUI = dynamic(() =>
+  import('@lumieducation/h5p-react').then((mod) => mod.H5PPlayerUI)
+)
