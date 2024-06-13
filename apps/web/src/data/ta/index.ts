95cc7d67c32340d2abe9ea848fb583e012a11d6a
Stephan Kulla <git.mail@kulla.me>
2024-06-13 14:46:01 +0200

    New translations index.ts (Tamil)

--- apps/web/src/data/ta/index.ts
+++ apps/web/src/data/ta/index.ts
@@ -190,7 +190,7 @@ export const instanceData = {
     },
     comments: {
       question: "உங்களுக்கு கேள்வி உள்ளதா?",
-      questionLink: 'Write it below',
+      questionLink: 'Write it here',
       commentsOne: "கருத்து",
       commentsMany: "கருத்துகள்",
       submit: "இணைக்க",
