4660dcdbcfd89ba57c36fd2be7847b5729bd3250
Vitomir Budimir <budimirvitomir@gmail.com>
2024-06-18 20:03:54 +0200

    fix(dropzone-image): answer zone answer add and settings tooltips

--- packages/editor/src/plugins/dropzone-image/components/answer-zone/answer-zone-empty.tsx
+++ packages/editor/src/plugins/dropzone-image/components/answer-zone/answer-zone-empty.tsx
@@ -1,6 +1,8 @@
+import { EditorTooltip } from '@editor/editor-ui/editor-tooltip'
 import { faCog, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 import { FaIcon } from '@/components/fa-icon'
+import { useEditorStrings } from '@/contexts/logged-in-data-context'
 
 export interface AnswerZoneEmptyProps {
   answerZoneId: string
@@ -11,19 +13,29 @@ export interface AnswerZoneEmptyProps {
 export const AnswerZoneEmpty = (props: AnswerZoneEmptyProps) => {
   const { answerZoneId, onClickSettingsButton, onClickPlusButton } = props
 
+  const dropzoneImageStrings = useEditorStrings().plugins.dropzoneImage
+
   return (
     <div className="absolute inset-0 flex items-center justify-center">
       <button
-        className="absolute right-2 top-2 rounded bg-orange-100 p-1"
+        className="serlo-tooltip-trigger absolute right-2 top-2 rounded bg-orange-100 p-1"
         onClick={() => onClickSettingsButton(answerZoneId)}
       >
+        <EditorTooltip
+          text={dropzoneImageStrings.answers.settings}
+          className="-ml-5 !pb-1"
+        />
         <FaIcon icon={faCog} />
       </button>
       <button
-        data-qa={`answer-zone-${answerZoneId}-add-answer-button`}
-        className="rounded bg-orange-100 px-3 py-2"
+        className="serlo-tooltip-trigger rounded bg-orange-100 px-3 py-2"
         onClick={() => onClickPlusButton(answerZoneId)}
+        data-qa={`answer-zone-${answerZoneId}-add-answer-button`}
       >
+        <EditorTooltip
+          text={dropzoneImageStrings.answers.add}
+          className="-ml-5 !pb-1"
+        />
         <FaIcon icon={faPlus} />
       </button>
     </div>
