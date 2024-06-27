281f503987238fcdb29d7b77a60125f46f7a5e96
shn-srl <shane@serlo.org>
2024-06-27 10:04:10 +0200

    refactor(image-plugin-redesign): Fix upload button

--- packages/editor/src/plugins/image/controls/upload-button.tsx
+++ packages/editor/src/plugins/image/controls/upload-button.tsx
@@ -3,36 +3,53 @@ import { TempFile, isTempFile } from '@editor/plugin'
 import { faRedoAlt, faUpload } from '@fortawesome/free-solid-svg-icons'
 import { FaIcon } from '@serlo/frontend/src/components/fa-icon'
 import { useEditorStrings } from '@serlo/frontend/src/contexts/logged-in-data-context'
+import { useState } from 'react'
 
 import type { ImageProps } from '..'
+import { cn } from '@/helper/cn'
 
 export function UploadButton({ config, state }: ImageProps) {
   const { src } = state
   const imageStrings = useEditorStrings().plugins.image
   const isFailed = isTempFile(src.value) && src.value.failed
 
+  const [isLabelFocused, setIsLabelFocused] = useState(false)
   return (
     <>
-      <button className="z-0 mb-4 flex w-full  items-center justify-center rounded-lg bg-editor-primary-200 font-semibold text-gray-800 hover:bg-editor-primary-300 ">
-        <label tabIndex={-1} className="z-10 h-full w-full cursor-pointer py-2">
-          <span className="mr-2 inline-block">
-            <FaIcon icon={faUpload} />
-          </span>
-          <input
-            tabIndex={-1}
-            type="file"
-            accept="image/*"
-            className="sr-only"
-            onChange={({ target }) => {
-              if (target.files && target.files.length) {
-                void src.upload(target.files[0], config.upload)
-              }
-            }}
-            data-qa="plugin-image-upload"
-          />
-          {imageStrings.upload}
-        </label>
-      </button>
+      <label
+        onFocus={(e) => {
+          e.stopPropagation()
+          setIsLabelFocused(true)
+        }}
+        onBlur={(e) => {
+          e.stopPropagation()
+          setIsLabelFocused(false)
+        }}
+        className={cn(
+          'mb-4 flex w-full py-2',
+          'items-center justify-center',
+          'cursor-pointer rounded-lg',
+          'font-semibold text-gray-800',
+          'bg-editor-primary-200 hover:bg-editor-primary-300',
+          isLabelFocused ? 'outline outline-2 outline-red-400' : ''
+        )}
+      >
+        <span className="mr-2 inline-block">
+          <FaIcon icon={faUpload} />
+        </span>
+        <input
+          type="file"
+          accept="image/*"
+          className="sr-only"
+          onChange={({ target }) => {
+            if (target.files && target.files.length) {
+              void src.upload(target.files[0], config.upload)
+            }
+          }}
+          data-qa="plugin-image-upload"
+        />
+        {imageStrings.upload}
+      </label>
 
       {isFailed ? (
         <button
