f54dda448a1bb8de36f11bf7323436f5eb2df3b1
Vitomir Budimir <budimirvitomir@gmail.com>
2024-07-19 14:25:57 +0200

    feat(auth): handle vidis bad role error

--- apps/web/src/pages/auth/error.tsx
+++ apps/web/src/pages/auth/error.tsx
@@ -5,7 +5,9 @@ import { useEffect, useState } from 'react'
 import { kratos } from '@/auth/kratos'
 import type { AxiosError } from '@/auth/types'
 import { FrontendClientBase } from '@/components/frontend-client-base/frontend-client-base'
+import { instanceData } from '@/data/en'
 import { renderedPageNoHooks } from '@/helper/rendered-page'
+import { showToastNotice } from '@/helper/show-toast-notice'
 import { triggerSentry } from '@/helper/trigger-sentry'
 
 export default renderedPageNoHooks(() => (
@@ -18,6 +20,8 @@ function Error() {
   const [error, setError] = useState<FlowError | string>()
   const router = useRouter()
   const { id } = router.query
+
+  const authStrings = instanceData.strings.auth
 
   useEffect(() => {
     if (!router.isReady || error) return
@@ -37,9 +41,15 @@ function Error() {
             void router.push('/') // Let's just redirect home!
             return Promise.reject(err)
         }
+
+        // Wrong role
+        if (err.message.includes('ERR_BAD_ROLE')) {
+          showToastNotice(authStrings.badRole, 'warning')
+        }
+
         return Promise.reject(err)
       })
-  }, [id, router, router.isReady, error])
+  }, [id, router, router.isReady, error, authStrings.badRole])
 
   return error ? <pre>{JSON.stringify(error, null, 2)}</pre> : null
 }
