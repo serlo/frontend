855d2f1d2b07702ae5c21414988b39380544bd88
Vitomir Budimir <budimirvitomir@gmail.com>
2024-07-22 15:11:43 +0200

    fix(auth): fix handling of vidis bad role error

--- apps/web/src/pages/auth/error.tsx
+++ apps/web/src/pages/auth/error.tsx
@@ -29,25 +29,25 @@ function Error() {
     kratos
       .getFlowError({ id: String(id) })
       .then(({ data }) => setError(data))
-      .catch((err: AxiosError) => {
+      .catch(({ error }: { error: AxiosError }) => {
         // eslint-disable-next-line no-console
-        console.error(err)
-        triggerSentry({ message: 'Auth error', code: err.response?.status })
+        console.error(error)
+        triggerSentry({ message: 'Auth error', code: error.response?.status })
 
-        switch (err.response?.status) {
+        switch (error.response?.status) {
           case 404: // The error id could not be found.
           case 403: // The error id could not be fetched due to e.g. a CSRF issue.
           case 410: // The error id expired.
             void router.push('/') // Let's just redirect home!
-            return Promise.reject(err)
+            return Promise.reject(error)
         }
 
         // Wrong role
-        if (err.message.includes('ERR_BAD_ROLE')) {
+        if (error.message.includes('ERR_BAD_ROLE')) {
           showToastNotice(authStrings.badRole, 'warning')
         }
 
-        return Promise.reject(err)
+        return Promise.reject(error)
       })
   }, [id, router, router.isReady, error, authStrings.badRole])
 
