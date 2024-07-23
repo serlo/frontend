73667186ec99c27c4f4e841adcd0252270efacb5
shn-srl <shane@serlo.org>
2024-07-23 11:34:24 +0200

    feat(redesign-add-plugin): Redesign button for inserting element between plugins

--- packages/editor/src/plugins/rows/components/add-row-button-floating.tsx
+++ packages/editor/src/plugins/rows/components/add-row-button-floating.tsx
@@ -1,7 +1,7 @@
-import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
-import { FaIcon } from '@serlo/frontend/src/components/fa-icon'
+import IconAddRow from '@editor/editor-ui/assets/plugin-icons/icon-add-row.svg'
 import { useEditorStrings } from '@serlo/frontend/src/contexts/logged-in-data-context'
 import { cn } from '@serlo/frontend/src/helper/cn'
+import { useState } from 'react'
 
 interface AddRowButtonFloatingProps {
   onClick: React.MouseEventHandler<HTMLButtonElement>
@@ -16,29 +16,44 @@ export function AddRowButtonFloating({
 }: AddRowButtonFloatingProps) {
   const rowsStrings = useEditorStrings().plugins.rows
 
+  const [isHovered, setIsHovered] = useState(false)
+
+  if (hide) return null
   return (
-    <div className="absolute bottom-0 z-[1] h-auto w-full translate-y-full">
-      {hide ? null : (
-        <div className="group flex justify-center">
-          <button
-            className={cn(
-              `
-              add-trigger z-20 h-7 w-7
-              rounded-full pt-1 text-center
-              text-gray-600 transition-all hover:cursor-pointer
-              hover:text-editor-primary hover:opacity-100
-              focus-visible:text-editor-primary group-hover:opacity-60
-              `,
-              focused ? 'opacity-60' : 'opacity-0'
-            )}
-            title={rowsStrings.addAnElement}
-            onClick={onClick}
-            data-qa="add-new-plugin-row-button"
-          >
-            <FaIcon icon={faCirclePlus} className="text-xl" />
-          </button>
-        </div>
+    <div
+      className={cn(
+        'group',
+        'relative flex items-center',
+        'hover:text-editor-primary-300 hover:opacity-100',
+        'focus-visible:text-editor-primary-300 group-hover:opacity-100',
+        focused ? 'opacity-100' : 'opacity-0'
       )}
+    >
+      {/* Divider line */}
+      <div className="flex-grow border-t-2 border-gray-300"></div>
+      <div
+        className={cn(
+          'group/btn relative px-4 ',
+          isHovered ? 'text-editor-primary-300' : 'text-editor-primary-200'
+        )}
+      >
+        {/* Add button */}
+        <IconAddRow />
+        {/* clickability zone, wider than button */}
+        <button
+          className="absolute inset-0 -ml-[100px] -mr-[100px] cursor-pointer"
+          aria-label="Add"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          title={rowsStrings.addAnElement}
+          onClick={onClick}
+          data-qa="add-new-plugin-row-button"
+        >
+          <div className="h-full w-full" />
+        </button>
+      </div>
+      {/* Divider line */}
+      <div className="flex-grow border-t-2 border-gray-300"></div>
     </div>
   )
 }
