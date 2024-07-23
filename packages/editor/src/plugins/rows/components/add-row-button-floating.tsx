575ead7e3a761cd470b49d28a03dd94463a056d1
shn-srl <shane@serlo.org>
2024-07-23 12:10:35 +0200

    feat(redesign-add-plugin): Improve hover behaviours, design

--- packages/editor/src/plugins/rows/components/add-row-button-floating.tsx
+++ packages/editor/src/plugins/rows/components/add-row-button-floating.tsx
@@ -1,59 +1,48 @@
 import IconAddRow from '@editor/editor-ui/assets/plugin-icons/icon-add-row.svg'
-import { useEditorStrings } from '@serlo/frontend/src/contexts/logged-in-data-context'
+import { EditorTooltip } from '@editor/editor-ui/editor-tooltip'
 import { cn } from '@serlo/frontend/src/helper/cn'
-import { useState } from 'react'
+
+import { useEditorStrings } from '@/contexts/logged-in-data-context'
 
 interface AddRowButtonFloatingProps {
-  onClick: React.MouseEventHandler<HTMLButtonElement>
+  onClick: React.MouseEventHandler<HTMLElement>
   focused?: boolean
   hide?: boolean
 }
 
 export function AddRowButtonFloating({
-  focused,
   onClick,
   hide,
 }: AddRowButtonFloatingProps) {
   const rowsStrings = useEditorStrings().plugins.rows
 
-  const [isHovered, setIsHovered] = useState(false)
-
   if (hide) return null
   return (
-    <div
-      className={cn(
-        'group',
-        'relative flex items-center',
-        'hover:text-editor-primary-300 hover:opacity-100',
-        'focus-visible:text-editor-primary-300 group-hover:opacity-100',
-        focused ? 'opacity-100' : 'opacity-0'
-      )}
-    >
-      {/* Divider line */}
-      <div className="flex-grow border-t-2 border-gray-300"></div>
+    <>
       <div
         className={cn(
-          'group/btn relative px-4 ',
-          isHovered ? 'text-editor-primary-300' : 'text-editor-primary-200'
+          'group ',
+          'opacity-0 transition-opacity duration-200',
+          'relative mb-2 flex items-center px-8',
+          'text-editor-primary-200 hover:opacity-100',
+          'focus-visible:text-editor-primary-200 group-hover:opacity-100',
+          'cursor-pointer'
         )}
+        onClick={onClick}
       >
-        {/* Add button */}
-        <IconAddRow />
-        {/* clickability zone, wider than button */}
-        <button
-          className="absolute inset-0 -ml-[100px] -mr-[100px] cursor-pointer"
-          aria-label="Add"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          title={rowsStrings.addAnElement}
-          onClick={onClick}
-          data-qa="add-new-plugin-row-button"
-        >
-          <div className="h-full w-full" />
-        </button>
+        {/* Divider line */}
+        <div className="flex-grow border-t-2 border-gray-300"></div>
+        <div className={cn('group/btn serlo-tooltip-trigger relative px-6')}>
+          {/* Add button */}
+          <IconAddRow className="" />
+          <EditorTooltip
+            text={rowsStrings.addAnElement}
+            className="align-center"
+          />
+        </div>
+        {/* Divider line */}
+        <div className="flex-grow border-t-2 border-gray-300"></div>
       </div>
-      {/* Divider line */}
-      <div className="flex-grow border-t-2 border-gray-300"></div>
-    </div>
+    </>
   )
 }
