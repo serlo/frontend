80490617c4d11a691e7adde42f2034c546a22524
shn-srl <shane@serlo.org>
2024-08-15 21:15:25 +0200

    feat(plugin-image-gallery): Basic setup of Image Gallery

---
+++ packages/editor/src/plugins/image-gallery/index.ts
@@ -0,0 +1,37 @@
+import { EditorPluginType } from '@editor/types/editor-plugin-type'
+
+import { ImageGalleryEditor } from './editor'
+import {
+  type EditorPlugin,
+  type EditorPluginProps,
+  child,
+  object,
+  list,
+} from '../../plugin'
+
+export const defaultConfig: ImageGalleryConfig = {}
+
+function createImageGalleryState() {
+  return object({
+    images: list(child({ plugin: EditorPluginType.Text })),
+  })
+}
+
+export function createImageGalleryPlugin(
+  config = defaultConfig
+): EditorPlugin<ImageGalleryPluginState, ImageGalleryConfig> {
+  return {
+    Component: ImageGalleryEditor,
+    config,
+    state: createImageGalleryState(),
+  }
+}
+
+export type ImageGalleryPluginState = ReturnType<typeof createImageGalleryState>
+
+export interface ImageGalleryConfig {}
+
+export type ImageGalleryProps = EditorPluginProps<
+  ImageGalleryPluginState,
+  ImageGalleryConfig
+>
---
+++ packages/editor/src/plugins/image-gallery/toolbar.tsx
@@ -0,0 +1,17 @@
+import { PluginToolbar } from '@editor/editor-ui/plugin-toolbar'
+import { PluginDefaultTools } from '@editor/editor-ui/plugin-toolbar/plugin-tool-menu/plugin-default-tools'
+import { EditorPluginType } from '@editor/types/editor-plugin-type'
+
+import type { ImageGalleryProps } from '.'
+
+export const ImageGalleryToolbar = (props: ImageGalleryProps) => {
+  const { id } = props
+
+  return (
+    <PluginToolbar
+      pluginType={EditorPluginType.ImageGallery}
+      // pluginTooltipText={editorStrings.plugins.image.helpTooltipText}
+      pluginControls={<PluginDefaultTools pluginId={id} />}
+    />
+  )
+}
