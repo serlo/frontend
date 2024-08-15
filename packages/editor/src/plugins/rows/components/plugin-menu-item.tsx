d7b3a27e843c95b54e7ca6c41b5753d834f94ed5
Vitomir Budimir <budimirvitomir@gmail.com>
2024-08-15 11:03:30 +0200

    fix(plugin-rows): align plugin menu item shadow and outline

--- packages/editor/src/plugins/rows/components/plugin-menu-item.tsx
+++ packages/editor/src/plugins/rows/components/plugin-menu-item.tsx
@@ -41,37 +41,38 @@ export const PluginMenuItem = forwardRef<
     : ''
 
   return (
-    <button
-      data-qa={`plugin-suggestion-${pluginType}`}
-      data-active={selected}
-      ref={ref}
-      onClick={() => onInsertPlugin(pluginType)}
-      onKeyDown={(e) => {
-        if (e.key === 'Enter') {
-          onInsertPlugin(pluginType)
-          e.preventDefault()
-        }
-      }}
-      onFocus={onFocus}
-      onBlur={onBlur}
-      onMouseMove={onMouseMove}
-      onMouseLeave={onMouseLeave}
-      className={cn(
-        'group serlo-tooltip-trigger flex cursor-auto flex-col items-center rounded-md border border-0 border-transparent pb-0'
-      )}
-    >
-      <EditorTooltip className={tooltipClassName} text={description} />
-      <span
+    <div>
+      <button
+        data-qa={`plugin-suggestion-${pluginType}`}
+        ref={ref}
+        onClick={() => onInsertPlugin(pluginType)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            onInsertPlugin(pluginType)
+            e.preventDefault()
+          }
+        }}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onMouseMove={onMouseMove}
+        onMouseLeave={onMouseLeave}
         className={cn(
-          'w-full cursor-pointer rounded-md p-2 hover:shadow-xl',
-          selected && 'shadow-xl'
+          'group serlo-tooltip-trigger flex w-full cursor-auto flex-col items-center rounded-md border border-0 border-transparent pb-0'
         )}
       >
-        <span className="flex w-full items-center justify-center rounded-md">
-          {icon || <IconFallback className="h-full w-full" />}
+        <EditorTooltip className={tooltipClassName} text={description} />
+        <span
+          className={cn(
+            'w-full cursor-pointer rounded-md p-2 hover:shadow-xl',
+            selected && 'shadow-xl'
+          )}
+        >
+          <span className="flex w-full items-center justify-center rounded-md">
+            {icon || <IconFallback className="h-full w-full" />}
+          </span>
+          <h5 className="mt-2 text-center text-sm font-bold">{title}</h5>
         </span>
-        <h5 className="mt-2 text-center text-sm font-bold">{title}</h5>
-      </span>
-    </button>
+      </button>
+    </div>
   )
 })
