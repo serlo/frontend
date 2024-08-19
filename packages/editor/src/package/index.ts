e52b4a6967aa2635b938c09f9905c5dae8aba291
Lars <lars@serlo.org>
2024-08-19 13:25:09 +0200

    doc(editor-package): Add comment about `defaultPlugins` package export

--- packages/editor/src/package/index.ts
+++ packages/editor/src/package/index.ts
@@ -1,6 +1,7 @@
 export { SerloEditor, type SerloEditorProps } from './editor'
 export { SerloRenderer, type SerloRendererProps } from './serlo-renderer'
 
+// Exported only so that integrations like serlo-editor-for-edusharing can customize available plugins based on the default plugins
 export { defaultPlugins } from './config'
 
 export type { BaseEditor } from '@editor/core'
