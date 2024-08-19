2c40a3135350140572c95965d235a1d12869aa97
Vitomir Budimir <budimirvitomir@gmail.com>
2024-08-19 12:06:09 +0200

    feat(plugin-rows): larger last add plugin button

--- packages/editor/src/plugins/rows/components/add-row-button-large.tsx
+++ packages/editor/src/plugins/rows/components/add-row-button-large.tsx
@@ -11,11 +11,11 @@ export function AddRowButtonLarge({ onClick }: AddRowButtonLargeProps) {
 
   return (
     <button
-      className="serlo-button-editor-secondary mx-auto mt-24 block"
+      className="serlo-button-editor-secondary mx-auto mt-24 flex items-center rounded-lg px-5 py-2.5 text-xl"
       onClick={onClick}
       data-qa="add-new-plugin-row-button"
     >
-      <FaIcon icon={faCirclePlus} className="text-xl" />{' '}
+      <FaIcon icon={faCirclePlus} className="mr-3 text-3xl" />
       <span className="text-almost-black">{rowsStrings.addAnElement}</span>
     </button>
   )
