589f07164e83d81ac748b5e2023a8acfcbb07fe5
shn-srl <shane@serlo.org>
2024-08-21 18:27:40 +0200

    chore(plugin-image-gallery): Fix data-qa identifier

--- packages/editor/src/plugins/image-gallery/components/add-images.tsx
+++ packages/editor/src/plugins/image-gallery/components/add-images.tsx
@@ -5,7 +5,7 @@ export function AddImages() {
     <div className="mx-auto rounded-md bg-yellow-50 p-8 shadow-md">
       <button
         className="mx-auto my-20 block rounded-md"
-        data-qa="plugin-image-empty-wrapper"
+        data-qa="plugin-image-gallery-add-images-button"
       >
         <IconChoose className=" text-editor-primary-100 hover:cursor-pointer hover:text-editor-primary-200" />
       </button>
