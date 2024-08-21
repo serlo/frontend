4209a5dd3f698b4d358267ca7c944031323f6156
Stephan Kulla <git.mail@kulla.me>
2024-08-21 12:14:10 +0200

    fixes

--- packages/editor/src/plugins/datenraum-integration/components/search-panel.tsx
+++ packages/editor/src/plugins/datenraum-integration/components/search-panel.tsx
@@ -36,12 +36,16 @@ const NostrEvent = t.type({
   tags: t.array(t.array(t.string)),
 })
 
-const NostrResource = t.type({
-  id: t.string,
-  image: t.string,
-  name: t.string,
-  license: t.string,
-})
+const NostrResource = t.intersection([
+  t.type({
+    id: t.string,
+    image: t.string,
+    name: t.string,
+  }),
+  t.partial({
+    license: t.string,
+  }),
+])
 
 export function SearchPanel({ onSelect }: SearchPanelProps) {
   const [query, setQuery] = useState('')
@@ -111,6 +115,8 @@ export function SearchPanel({ onSelect }: SearchPanelProps) {
             event.tags.map(([x, y]) => [x, y])
           )
 
+          console.log(basicResource)
+
           if (!NostrResource.is(basicResource)) return
 
           if (resourcesFromRelay.find((x) => x.url === basicResource.id)) return
@@ -123,7 +129,6 @@ export function SearchPanel({ onSelect }: SearchPanelProps) {
             license: basicResource.license
               ? getLicense(basicResource.license)
               : License.OTHER,
-            licenseUrl: basicResource.license,
             url: basicResource.id,
           })
         }
@@ -134,7 +139,6 @@ export function SearchPanel({ onSelect }: SearchPanelProps) {
             image:
               'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/H5P_Logo.png/240px-H5P_Logo.png',
             license: License.CC_BY,
-            licenseUrl: 'https://creativecommons.org/licenses/by/3.0/',
             url: 'https://app.lumi.education/run/J3j0eR',
           })
 
@@ -150,7 +154,7 @@ export function SearchPanel({ onSelect }: SearchPanelProps) {
 
   return (
     <div className="min-h-[100vh]">
-      <h1 className="serlo-h4 mt-4">Datenraum Suche</h1>
+      <h1 className="serlo-h4 mt-4">Bildungs-Feed Suche</h1>
       <div className="relative mt-8">
         <input
           type="text"
@@ -222,7 +226,6 @@ export interface LearningResource {
   name: string
   image: string
   license: License
-  licenseUrl: string
 }
 
 function getLicense(license: string): License {
