3735c2e3cb0692ac3bdc06a0041f1bef4b411fe4
shn-srl <shane@serlo.org>
2024-08-21 22:05:40 +0200

    feat(plugin-image-gallery): Add Image Grid in static view

--- packages/editor/src/plugins/image-gallery/static.tsx
+++ packages/editor/src/plugins/image-gallery/static.tsx
@@ -1,3 +1,52 @@
-export function ImageGalleryStaticRenderer() {
-  return <></>
+import {
+  EditorImageDocument,
+  EditorImageGalleryDocument,
+} from '@editor/types/editor-plugins'
+import { useEffect, useState } from 'react'
+import { Photo, RowsPhotoAlbum } from 'react-photo-album'
+
+// eslint-disable-next-line import/no-unassigned-import
+import 'react-photo-album/rows.css'
+// eslint-disable-next-line import/order
+import { loadGalleryPhotos } from './util/helpers'
+
+export function ImageGalleryStaticRenderer({
+  state,
+}: EditorImageGalleryDocument) {
+  const imagesFromState = state.images as EditorImageDocument[]
+  const images = imagesFromState.map(({ id, state }) => ({
+    id: id as string,
+    src: state.src as string,
+  }))
+
+  const [photos, setPhotos] = useState<Photo[]>([])
+
+  useEffect(() => {
+    const orderedIds = state.orderedIds
+      ? (JSON.parse(state.orderedIds) as string[])
+      : []
+
+    const loadPhotos = async () => {
+      try {
+        const sortedPhotos = await loadGalleryPhotos(images, orderedIds)
+        setPhotos(sortedPhotos)
+      } catch (error) {
+        console.error('Failed to load photos:', error)
+      }
+    }
+
+    void loadPhotos()
+  }, [images, state.orderedIds])
+
+  return (
+    <div className="p-4">
+      <RowsPhotoAlbum
+        spacing={8}
+        padding={0}
+        photos={photos}
+        rowConstraints={{ maxPhotos: 2 }}
+        targetRowHeight={200}
+      />
+    </div>
+  )
 }
