d612d4ec93d7c86151d9669bcc3a6dd9d2493093
shn-srl <shane@serlo.org>
2024-08-21 17:38:36 +0200

    e2e(editor): Fix failing test relating to suggestion menu modal

--- e2e-tests/tests/400-serlo-editor.ts
+++ e2e-tests/tests/400-serlo-editor.ts
@@ -58,11 +58,11 @@ Scenario('Add new plugins', async ({ I }) => {
   I.see('(optionaler Titel)')
 })
 
-Scenario('Close plugin selection modal', async ({ I }) => {
+Scenario.only('Close plugin selection modal', async ({ I }) => {
   I.amOnPage('/entity/create/Article/1377')
   I.click('Füge ein Element hinzu')
-  const menuModal = 'Exercises'
-  I.see(menuModal)
+  const menuModal = '$plugin-menu-search-input'
+  I.seeNumberOfElements(menuModal, 1)
 
   I.pressKey('Escape')
   // Modal should be closed
