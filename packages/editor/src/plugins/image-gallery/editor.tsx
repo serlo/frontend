9a790fa097c2402af7b0e6977d3e9ba5ecb0a2d0
shn-srl <shane@serlo.org>
2024-08-22 19:08:45 +0200

    refactor(plugin-image-gallery): Dont use render function

--- packages/editor/src/plugins/image-gallery/editor.tsx
+++ packages/editor/src/plugins/image-gallery/editor.tsx
@@ -7,7 +7,7 @@ import { AddImages } from './components/add-images'
 import { SingleImageModal } from './components/single-image-modal'
 import { ImageGalleryToolbar } from './toolbar'
 
-export enum ImageGalleryPluginViewType {
+enum ImageGalleryPluginViewType {
   INIT = 'INIT', // Initial state, no images
   ADD_IMAGES = 'ADD_IMAGES', // Image selection screen
   GALLERY = 'GALLERY', // Image grid
@@ -34,40 +34,33 @@ export function ImageGalleryEditor(props: ImageGalleryProps) {
     }
   }
 
-  const renderContent = () => {
-    switch (currentView) {
-      case ImageGalleryPluginViewType.INIT:
-        return (
-          <AddImages
-            onAddImages={() => {
-              state.images.insert(0, {
-                plugin: EditorPluginType.Image,
-              })
-              setCurrentView(ImageGalleryPluginViewType.ADD_IMAGES)
-              setCurrentImageIndex(0)
-            }}
-            {...props}
-          />
-        )
-      case ImageGalleryPluginViewType.ADD_IMAGES:
-        return (
-          <SingleImageModal
-            {...props}
-            currentImageIndex={currentImageIndex}
-            onAddImage={() => {
-              setCurrentView(ImageGalleryPluginViewType.INIT)
-            }}
-            handleMultipleImageUpload={handleMultipleImageUpload}
-          />
-        )
-      default:
-        return null
-    }
-  }
   return (
     <div data-qa="plugin-image-gallery-wrapper">
       {focused ? <ImageGalleryToolbar {...props} /> : null}
-      {renderContent()}
+
+      {currentView === ImageGalleryPluginViewType.INIT && (
+        <AddImages
+          onAddImages={() => {
+            state.images.insert(0, {
+              plugin: EditorPluginType.Image,
+            })
+            setCurrentView(ImageGalleryPluginViewType.ADD_IMAGES)
+            setCurrentImageIndex(0)
+          }}
+          {...props}
+        />
+      )}
+
+      {currentView === ImageGalleryPluginViewType.ADD_IMAGES && (
+        <SingleImageModal
+          {...props}
+          currentImageIndex={currentImageIndex}
+          onAddImage={() => {
+            setCurrentView(ImageGalleryPluginViewType.INIT)
+          }}
+          handleMultipleImageUpload={handleMultipleImageUpload}
+        />
+      )}
     </div>
   )
 }
