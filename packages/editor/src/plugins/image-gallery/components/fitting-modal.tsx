f97b3045f5fa7bdf6312d0ab4002a4dd1f9f78a7
shn-srl <shane@serlo.org>
2024-08-22 19:18:34 +0200

    fix(plugin-image-gallery): Fix Dialog.title a11y issue

--- packages/editor/src/plugins/image-gallery/components/fitting-modal.tsx
+++ packages/editor/src/plugins/image-gallery/components/fitting-modal.tsx
@@ -15,6 +15,7 @@ export function FittingModal({
   return (
     <Dialog.Root open={open} onOpenChange={onOpenChange}>
       <Dialog.Portal>
+        <Dialog.Title className="sr-only" />
         <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-40" />
         <Dialog.Content className="fixed bottom-0 left-1/2 top-[40%] h-fit w-[500px] max-w-[85%] -translate-x-1/2 -translate-y-1/2 pb-10 outline-none">
           <div>{children}</div>
