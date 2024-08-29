75a9554182c4dfe66b224515efd18c8746b61eec
Lars <lars@serlo.org>
2024-08-29 11:48:29 +0200

    chore(editor-package): Remove unused package exports

--- packages/editor/src/package/index.ts
+++ packages/editor/src/package/index.ts
@@ -1,24 +1,10 @@
 export { SerloEditor, type SerloEditorProps } from './editor'
 export { SerloRenderer, type SerloRendererProps } from './serlo-renderer'
 
-// Exported only so that integrations like serlo-editor-for-edusharing can customize available plugins based on the default plugins
-export { defaultPlugins } from './config'
-
 export type { BaseEditor } from '@editor/core'
 
 export { EditorPluginType } from '@editor/types/editor-plugin-type'
-export { TemplatePluginType } from '@editor/types/template-plugin-type'
 export { type EditorVariant } from '@editor/package/storage-format'
 
-export { string, object, optional, number } from '@editor/plugin'
-export type {
-  EditorPlugin,
-  EditorPluginProps,
-  PrettyStaticState,
-} from '@editor/plugin'
-
-export { FaIcon } from '@/components/fa-icon'
-
-export { EditorInput, PreviewOverlay } from '@editor/editor-ui'
-export { PluginToolbar } from '@editor/editor-ui/plugin-toolbar'
-export { PluginDefaultTools } from '@editor/editor-ui/plugin-toolbar/plugin-tool-menu/plugin-default-tools'
+// Exported only so that integrations like serlo-editor-for-edusharing can customize available plugins based on the default plugins
+export { defaultPlugins } from './config'
