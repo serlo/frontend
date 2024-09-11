419ad79c13ebb298ffe6b3eb37cc6623fd3a5318
Stephan Kulla <git.mail@kulla.me>
2024-09-11 21:53:08 +0200

    fix(editor): Documents not having a state can be migrated

--- packages/editor/__tests__/migration-state.ts
+++ packages/editor/__tests__/migration-state.ts
@@ -1,3 +1,11 @@
-test("Hello World", () => {
+import { migrate } from '@editor/package/storage-format'
 
+test('A document not being in the storage format can be migrated', () => {
+  const { stateChanged, migratedState } = migrate({ plugin: 'rows' }, 'unknown')
+
+  expect(stateChanged).toBe(true)
+  expect(migratedState).toMatchObject({
+    type: 'https://serlo.org/editor',
+    document: { plugin: 'rows' },
+  })
 })
