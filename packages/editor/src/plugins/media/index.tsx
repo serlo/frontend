bba68fc59e6433dfa6276d32a04628f32de2efa1
Stephan Kulla <git.mail@kulla.me>
2024-09-13 18:29:27 +0200

    wip: Add first sketch to load image data

--- packages/editor/src/plugins/media/index.tsx
+++ packages/editor/src/plugins/media/index.tsx
@@ -1,9 +1,96 @@
-import { object } from '@editor/plugin'
+import { EditorPluginProps, object, scalar } from '@editor/plugin'
 
-const state = object({})
+const state = object({
+  resourceLocation: scalar<Resource | null>(null),
+})
+type MediaState = typeof state
+type MediaProps = EditorPluginProps<MediaState>
+
+enum Hosting {
+  CDN = 'cdn',
+}
+
+enum Embedding {
+  Image = 'image',
+}
 
 export const mediaPlugin = {
   state,
   config: {},
-  Component: () => <div>Media Plugin</div>,
+  Component: MediaPlugin,
 }
+
+function MediaPlugin(props: MediaProps) {
+  const { state } = props
+  const resource = state.resourceLocation.value
+
+  console.log(resource)
+
+  if (!resource) {
+    return (
+      <div>
+        No media selected:{' '}
+        <button
+          className="serlo-button"
+          onClick={() =>
+            state.resourceLocation.set({
+              hostingService: Hosting.CDN,
+              contentUrl:
+                'https://cdn.pixabay.com/photo/2023/09/29/14/58/road-8284023_640.jpg',
+              embeddingType: Embedding.Image,
+            })
+          }
+        >
+          Add dummy data
+        </button>
+      </div>
+    )
+  }
+
+  const embedding = embeddingResolver[resource.hostingService](resource)
+
+  if (embedding.type === Embedding.Image) {
+    return <img src={embedding.contentUrl} />
+  }
+}
+
+const embeddingResolver: ResourceResolver = {
+  [Hosting.CDN]: (resource) => {
+    return {
+      resourceLocation: resource,
+      type: resource.embeddingType,
+      contentUrl: resource.contentUrl,
+    }
+  },
+}
+
+type ResourceResolver = {
+  [H in Hosting]: (resource: Resource<H>) => EmbeddingType<H>
+}
+
+interface ResourceTypes {
+  [Hosting.CDN]: CreateResourceType<
+    Hosting.CDN,
+    { embeddingType: Embedding; contentUrl: string }
+  >
+}
+
+type Resource<H extends Hosting = Hosting> = ResourceTypes[H]
+
+type CreateResourceType<H extends Hosting, AdditionalInformation> = {
+  hostingService: H
+} & AdditionalInformation
+
+interface EmbeddingTypesAdditonalInformation {
+  [Embedding.Image]: { contentUrl: string }
+}
+
+type EmbeddingType<
+  Hosts extends Hosting = Hosting,
+  Types extends Embedding = Embedding,
+> = {
+  [T in Types]: EmbeddingTypesAdditonalInformation[T] & {
+    type: T
+    resourceLocation: Resource<Hosts>
+  }
+}[Types]
