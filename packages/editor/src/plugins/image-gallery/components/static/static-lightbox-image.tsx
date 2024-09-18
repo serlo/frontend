8be4e01b75c316674c9a903ac350536212174d35
Vitomir Budimir <budimirvitomir@gmail.com>
2024-09-18 10:43:05 +0200

    fix(image-gallery): lightbox desktop caption below image

--- packages/editor/src/plugins/image-gallery/components/static/static-lightbox-image.tsx
+++ packages/editor/src/plugins/image-gallery/components/static/static-lightbox-image.tsx
@@ -1,7 +1,6 @@
 import { StaticSlate } from '@editor/plugins/text/static-components/static-slate'
 
 import { GridImage } from '../../types'
-import { cn } from '@/helper/cn'
 
 interface StaticLightboxImageProps {
   image: GridImage
@@ -13,17 +12,15 @@ export function StaticLightboxImage({ image }: StaticLightboxImageProps) {
   }
 
   return (
-    <div className="relative">
-      <img src={image.src} className="max-h-[60vh] bg-white" />
-      <div
-        className={cn(
-          'absolute inset-0 flex items-end justify-center p-3 italic text-white',
-          'bg-gradient-to-b from-transparent via-[rgba(0,0,0,0.15)] via-70% to-[rgba(0,0,0,0.8)] to-85%'
-        )}
-      >
-        <div className="text-center [&_a]:text-brand-400">
-          <StaticSlate element={image.caption} />
-        </div>
+    <div className="text-center">
+      <img
+        src={image.src}
+        // TODO: get actual alt text or fallback
+        alt={`Image ${image.src}`}
+        className="max-h-[60vh] bg-white"
+      />
+      <div className="mt-3 italic text-gray-100 [&_a]:text-brand-400">
+        <StaticSlate element={image.caption} />
       </div>
     </div>
   )
