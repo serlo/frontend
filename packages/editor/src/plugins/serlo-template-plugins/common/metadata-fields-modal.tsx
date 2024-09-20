b42aa45f51a7f0e8efbf8b2f37e42c25950da6c2
Botho <1258870+elbotho@users.noreply.github.com>
2024-09-20 10:29:10 +0200

    refactor(editor): move up setting modals

--- packages/editor/src/plugins/serlo-template-plugins/common/metadata-fields-modal.tsx
+++ packages/editor/src/plugins/serlo-template-plugins/common/metadata-fields-modal.tsx
@@ -29,7 +29,7 @@ export function MetadataFieldsModal({
       <ModalWithCloseButton
         isOpen={showSettingsModal}
         setIsOpen={setShowSettingsModal}
-        className="top-8 max-w-xl translate-y-0 sm:top-1/3"
+        className="top-8 max-w-xl translate-y-0 sm:top-24"
         title="Metadata"
         extraTitleClassName="sr-only"
       >
