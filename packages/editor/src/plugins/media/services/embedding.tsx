b5cce3a21139166e49faa2bd2807f66355c110e8
Stephan Kulla <git.mail@kulla.me>
2024-09-26 20:35:39 +0200

    feat(media): Add spinner

--- packages/editor/src/plugins/media/services/embedding.tsx
+++ packages/editor/src/plugins/media/services/embedding.tsx
@@ -1,3 +1,4 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { useEffect, useState } from 'react'
 
 import { GeogebraRenderer } from './geogebra'
@@ -5,6 +6,7 @@ import { ImageRenderer } from './html-image'
 import { VideoRenderer } from './html-video'
 import { resolveEmbedding } from './resolve-embedding'
 import { Embed, Resource, EmbeddingProp } from './types'
+import { FaIcon } from '@/components/fa-icon'
 
 export function Embedding({ resource }: { resource: Resource }) {
   const embeddingResult = resolveEmbedding(resource)
@@ -21,7 +23,13 @@ export function Embedding({ resource }: { resource: Resource }) {
   }, [embeddingResult, embeddingResultIsPromise, setEmbedding])
 
   if (embedding === null) {
-    return <div>Load embedding...</div>
+    // TODO: Design spinner
+    return (
+      <div className="flex min-h-32 w-full items-center justify-center rounded-b bg-orange-100">
+        <FaIcon icon={faSpinner} className="mr-2 animate-spin-slow" />
+        Loading...
+      </div>
+    )
   } else if (embedding.type === Embed.HTMLImage) {
     return <ImageRenderer {...embedding} />
   } else if (embedding.type === Embed.HTMLVideo) {
