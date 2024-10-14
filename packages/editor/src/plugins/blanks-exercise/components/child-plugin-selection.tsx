9453c9a35295d1dfb4d2ae239598aa51cac391ec
Botho <1258870+elbotho@users.noreply.github.com>
2024-10-14 10:20:41 +0200

    feat(blanks): hide exercise task until child plugin is selected, focus child plugin after selection

--- packages/editor/src/plugins/blanks-exercise/components/child-plugin-selection.tsx
+++ packages/editor/src/plugins/blanks-exercise/components/child-plugin-selection.tsx
@@ -2,6 +2,7 @@ import IconTable from '@editor/editor-ui/assets/plugin-icons/icon-table.svg'
 import IconText from '@editor/editor-ui/assets/plugin-icons/icon-text.svg'
 import { SelectionCard } from '@editor/editor-ui/selection-card'
 import { useEditStrings } from '@editor/i18n/edit-strings-provider'
+import { useAppDispatch, focus } from '@editor/store'
 import { EditorPluginType } from '@editor/types/editor-plugin-type'
 
 import type { BlanksExerciseProps } from '..'
@@ -16,15 +17,18 @@ export function ChildPluginSelection({
   const pluginStrings = useEditStrings().plugins
   const description = pluginStrings.blanksExercise.childPluginSelection
 
+  const dispatch = useAppDispatch()
+
   function handleClick(plugin: EditorPluginType) {
     setShowSelection(false)
     childPlugin.replace(plugin)
+    dispatch(focus(childPlugin.id))
   }
 
   return (
     <>
       <div className="py-6 text-center text-gray-500">{description}</div>
-      <div className="flex justify-center gap-6 [&_svg]:min-w-20">
+      <div className="blanks-child-plugin-selection flex justify-center gap-6 [&_svg]:min-w-20">
         <SelectionCard
           onClick={() => handleClick(EditorPluginType.Text)}
           icon={<IconText />}
