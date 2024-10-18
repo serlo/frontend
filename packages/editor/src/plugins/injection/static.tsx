d68fbea9f8734613b2a54a1b2ba36c6dc88b5a94
Botho <1258870+elbotho@users.noreply.github.com>
2024-10-19 00:21:10 +0200

    refactor(injection): clean code, allow staging for now

--- packages/editor/src/plugins/injection/static.tsx
+++ packages/editor/src/plugins/injection/static.tsx
@@ -6,10 +6,12 @@ import {
 } from '@editor/types/editor-plugins'
 import { useEffect, useState } from 'react'
 
-const fetchHost =
-  process.env.NODE_ENV === 'development'
+function getHost(currentHost: string) {
+  if (currentHost.endsWith('serlo-staging.dev')) return currentHost
+  return process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
     : 'https://serlo.org'
+}
 
 export function InjectionStaticRenderer({
   state: href,
@@ -19,14 +21,8 @@ export function InjectionStaticRenderer({
     AnyEditorDocument[] | 'loading' | 'error'
   >('loading')
 
-  const [base, hash] = href.split('#')
-
-  const cleanedHref = base.startsWith('/') ? base : `/${base}`
-
   useEffect(() => {
-    if (!cleanedHref) return
-
-    const url = `${fetchHost}/api/frontend/injection-content?path=${encodeURIComponent(cleanedHref)}&hash=${hash ? encodeURIComponent(hash) : ''}`
+    if (!href) return
 
     function handleError(error: unknown) {
       // eslint-disable-next-line no-console
@@ -34,34 +30,24 @@ export function InjectionStaticRenderer({
       setContent('error')
     }
 
-    try {
-      fetch(url)
-        .then((res) => {
-          if (!res.ok) {
-            void res
-              .text()
-              .then((text) => {
-                throw new Error(text)
-              })
-              .catch(handleError)
-          } else {
-            void res
-              .json()
-              .then((data) => {
-                // eslint-disable-next-line no-console
-                console.log(data)
-                setContent(data as AnyEditorDocument[])
-              })
-              .catch(handleError)
-          }
-        })
-        .catch(handleError)
-    } catch (e) {
-      handleError(e)
-    }
-  }, [cleanedHref, hash])
+    async function fetchSerloContent() {
+      const host = getHost(window.location.host)
+      const url = `${host}/api/frontend/injection-content?href=${encodeURIComponent(href)}`
+      const res = await fetch(url)
+      const data = (await res.json()) as string | AnyEditorDocument[]
 
-  if (!href || !cleanedHref) return null
+      if (!res.ok) return handleError(data)
+      setContent(data as AnyEditorDocument[])
+    }
+
+    try {
+      void fetchSerloContent()
+    } catch (error) {
+      handleError(error)
+    }
+  }, [href])
+
+  if (!href) return null
 
   if (content === 'loading') return <LoadingSpinner />
   if (content === 'error') return errorBox ?? null
