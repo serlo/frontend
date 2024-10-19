008c5078df3d215038ec6e87fa98f9bc74a6d2d0
Botho <1258870+elbotho@users.noreply.github.com>
2024-10-19 17:03:49 +0200

    refactor(editor): remove skipControls config

--- packages/editor/src/plugins/serlo-template-plugins/text-exercise.tsx
+++ packages/editor/src/plugins/serlo-template-plugins/text-exercise.tsx
@@ -6,7 +6,6 @@ import {
 } from '@editor/plugin'
 import { selectStaticDocument, useStore } from '@editor/store'
 import { EditorPluginType } from '@editor/types/editor-plugin-type'
-import { cn } from '@editor/utils/cn'
 import { UuidType } from '@serlo/frontend/src/data-types'
 import { ContentLoaders } from '@serlo/frontend/src/serlo-editor-integration/components/content-loaders/content-loaders'
 
@@ -23,20 +22,16 @@ export const textExerciseTypeState = entityType(
 export type TextExerciseTypePluginState = typeof textExerciseTypeState
 
 /** Exercise with an optional solution spoiler */
-export const textExerciseTypePlugin: EditorPlugin<
-  TextExerciseTypePluginState,
-  { skipControls: boolean }
-> = {
-  Component: TextExerciseTypeEditor,
-  state: textExerciseTypeState,
-  config: { skipControls: false },
-}
+export const textExerciseTypePlugin: EditorPlugin<TextExerciseTypePluginState> =
+  {
+    Component: TextExerciseTypeEditor,
+    state: textExerciseTypeState,
+  }
 
 function TextExerciseTypeEditor({
   state,
-  config,
   id,
-}: EditorPluginProps<TextExerciseTypePluginState, { skipControls: boolean }>) {
+}: EditorPluginProps<TextExerciseTypePluginState>) {
   const { content, licenseId, changes } = state
 
   const store = useStore()
@@ -47,23 +42,18 @@ function TextExerciseTypeEditor({
 
   return (
     <>
-      {config.skipControls ? null : (
-        <div className="absolute right-0 -mt-20 mr-side flex flex-row gap-4">
-          <ContentLoaders
-            id={state.id.value}
-            currentRevision={state.revision.value}
-            onSwitchRevision={state.replaceOwnState}
-            entityType={UuidType.Exercise}
-          />
-        </div>
-      )}
-      <article
-        className={cn('text-exercise', config.skipControls ? 'mt-12' : 'mt-32')}
-      >
+      <div className="absolute right-0 -mt-20 mr-side flex flex-row gap-4">
+        <ContentLoaders
+          id={state.id.value}
+          currentRevision={state.revision.value}
+          onSwitchRevision={state.replaceOwnState}
+          entityType={UuidType.Exercise}
+        />
+      </div>
+      <article className="text-exercise mt-32">
         {content.render()}
-        {config.skipControls ? null : (
-          <SaveButton changes={changes} licenseId={licenseId} />
-        )}
+
+        <SaveButton changes={changes} licenseId={licenseId} />
       </article>
     </>
   )
