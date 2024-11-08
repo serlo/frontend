97bd693c217c484562f09ef5041d0036bc8d8dce
Lars <lars@serlo.org>
2024-11-08 09:58:10 +0100

    fix(plugin-edusharing): display embeds correctly

--- packages/editor/src/plugins/edusharing-asset/renderer.tsx
+++ packages/editor/src/plugins/edusharing-asset/renderer.tsx
@@ -1,5 +1,4 @@
 import EdusharingIcon from '@editor/editor-ui/assets/edusharing.svg'
-import DOMPurify from 'dompurify'
 import IframeResizer from 'iframe-resizer-react'
 import * as t from 'io-ts'
 import { memo, useEffect, useState } from 'react'
@@ -31,6 +30,12 @@ const EmbedJson = t.type({
     ]),
   }),
 })
+
+const iframeResizerHtml =
+  '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.9/iframeResizer.contentWindow.min.js"></script>'
+
+// Set some styles to default values to create consistency between different browsers similar to a CSS reset, see https://meyerweb.com/eric/tools/css/reset/
+const defaultStyle = 'padding: 0; margin: 0; border: 0;'
 
 export function EdusharingAssetRenderer(props: {
   nodeId?: string
@@ -77,14 +82,13 @@ export function EdusharingAssetRenderer(props: {
       }
 
       // HTML snipped returned by edu-sharing cannot be used as it is.
-      const { embedType, html, defineContainerHeight } =
+      const { embedType, htmlSnippet, defineContainerHeight } =
         getEmbedHtml(responseJson)
 
-      // Prevent common XSS methods
-      const sanitizedHtml = DOMPurify.sanitize(html)
+      const html = buildHtml(htmlSnippet, defineContainerHeight)
 
       setEmbedType(embedType)
-      setEmbedHtml(sanitizedHtml)
+      setEmbedHtml(html)
       setDefineContainerHeight(defineContainerHeight)
     }
 
@@ -105,9 +109,21 @@ export function EdusharingAssetRenderer(props: {
     </figure>
   )
 
+  function buildHtml(htmlSnippet: string, defineContainerHeight: boolean) {
+    // Hack: Some learning apps size themselves to be a little bit too tall and a scroll bar appears -> 97% height to prevent this
+    return `
+      <html style="${defaultStyle}${defineContainerHeight ? 'height: 97%;' : ''}">
+        <body style="${defaultStyle}${defineContainerHeight ? 'height: 100%;' : ''}">
+          ${htmlSnippet}
+          ${defineContainerHeight ? '' : iframeResizerHtml}
+        </body>
+      </html> 
+    `
+  }
+
   function getEmbedHtml(content: t.TypeOf<typeof EmbedJson>): {
     embedType: EmbedType
-    html: string
+    htmlSnippet: string
     defineContainerHeight: boolean
   } {
     let { detailsSnippet } = content
@@ -135,14 +151,14 @@ export function EdusharingAssetRenderer(props: {
       if (!linkElement) {
         return {
           embedType: 'unknown',
-          html: '<div>Fehler beim Einbinden des Inhalts</div>',
+          htmlSnippet: '<div>Fehler beim Einbinden des Inhalts</div>',
           defineContainerHeight: false,
         }
       }
 
       return {
         embedType: isLink ? 'link' : isBrockhaus ? 'brockhaus' : 'unknown',
-        html: `<a class="serlo-link" target="_blank" rel="noopener noreferrer" href="${
+        htmlSnippet: `<a class="serlo-link" target="_blank" rel="noopener noreferrer" href="${
           linkElement.href
         }">${
           linkElement.innerText ? linkElement.innerText : linkElement.href
@@ -190,7 +206,7 @@ export function EdusharingAssetRenderer(props: {
 
       return {
         embedType: 'pixabay',
-        html: imageSnippet + emptyStringOrJumpToSource,
+        htmlSnippet: imageSnippet + emptyStringOrJumpToSource,
         defineContainerHeight: false,
       }
     }
@@ -201,7 +217,7 @@ export function EdusharingAssetRenderer(props: {
       const imageSnippet = buildImageSnippet(image)
       return {
         embedType: 'edusharing-image',
-        html: appendIframeResizer(imageSnippet),
+        htmlSnippet: imageSnippet,
         defineContainerHeight: false,
       }
     }
@@ -216,7 +232,7 @@ export function EdusharingAssetRenderer(props: {
         .replace('height="0"', '')
       return {
         embedType: 'file',
-        html: detailsSnippet,
+        htmlSnippet: detailsSnippet,
         defineContainerHeight: false,
       }
     }
@@ -233,7 +249,7 @@ export function EdusharingAssetRenderer(props: {
 
       return {
         embedType: 'audio',
-        html: appendIframeResizer(detailsSnippet),
+        htmlSnippet: detailsSnippet,
         defineContainerHeight: false,
       }
     }
@@ -256,7 +272,7 @@ export function EdusharingAssetRenderer(props: {
         `
       return {
         embedType: 'video',
-        html: appendIframeResizer(detailsSnippet),
+        htmlSnippet: detailsSnippet,
         defineContainerHeight: false,
       }
     }
@@ -268,38 +284,18 @@ export function EdusharingAssetRenderer(props: {
     if (isH5P) {
       return {
         embedType: 'h5p',
-        html: appendIframeResizer(detailsSnippet),
+        htmlSnippet: detailsSnippet,
         defineContainerHeight: false,
       }
     }
 
+    // Learning apps & PDFs
     const isPdf = iframe?.id === 'docFrame'
-    if (isPdf) {
-      // Do not adjust height based on container size
-      iframe.style.height = 'auto'
+    const isLearningApp = iframe && detailsSnippet.includes('learningapps.org/')
+    if (isLearningApp || isPdf) {
       return {
-        embedType: 'pdf',
-        html: htmlDocument.body.innerHTML,
-        defineContainerHeight: true,
-      }
-    }
-
-    // Learning apps
-    if (detailsSnippet.includes('learningapps.org/')) {
-      const iframeHtmlElement = htmlDocument.querySelector('iframe')
-      if (!iframeHtmlElement) {
-        return {
-          embedType: 'unknown',
-          html: 'Error. Please contact support. Details: Could not find iframe in learningapp embed html.',
-          defineContainerHeight: false,
-        }
-      }
-      const iframeHtml = iframeHtmlElement.outerHTML
-        .replace('width="95%"', 'width="100%"')
-        .replace('height: 80vh', '')
-      return {
-        embedType: 'learning-app',
-        html: iframeHtml,
+        embedType: isLearningApp ? 'learning-app' : isPdf ? 'pdf' : 'unknown',
+        htmlSnippet: `<iframe style="${defaultStyle} height: 100%; width: 100%;" src="${iframe.src}"></iframe>`,
         defineContainerHeight: true,
       }
     }
@@ -307,7 +303,7 @@ export function EdusharingAssetRenderer(props: {
     // Backup when content type could not be determined above
     return {
       embedType: 'unknown',
-      html: appendIframeResizer(detailsSnippet),
+      htmlSnippet: detailsSnippet,
       defineContainerHeight: false,
     }
   }
@@ -333,14 +329,20 @@ export function EdusharingAssetRenderer(props: {
         {defineContainerHeight ? (
           <iframe
             srcDoc={embedHtml}
-            style={{ width: '100%', height: '100%' }}
+            style={{
+              width: '100%',
+              height: '100%',
+            }}
           />
         ) : (
           <MemoizedIframeResizer
             heightCalculationMethod="lowestElement"
             checkOrigin={false}
             srcDoc={embedHtml}
-            style={{ width: '1px', minWidth: '100%' }}
+            style={{
+              width: '1px',
+              minWidth: '100%',
+            }}
           />
         )}
       </div>
@@ -370,13 +372,6 @@ function getImageOrUndefined(
 
 function buildImageSnippet(image: HTMLImageElement): string {
   return `<img style="width: 100%; object-fit: contain;" src="${image.getAttribute('src')}" alt="${image.getAttribute('alt')}" title="${image.getAttribute('title')}">`
-}
-
-function appendIframeResizer(htmlSnippet: string) {
-  return (
-    htmlSnippet +
-    '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/4.3.9/iframeResizer.contentWindow.min.js"></script>'
-  )
 }
 
 // Only re-render if `srcDoc` prop changed. We do not want to re-render the Iframe every time when EdusharingAssetRenderer is re-rendered because the state within the iframe is lost.
