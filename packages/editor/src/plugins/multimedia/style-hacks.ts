20172f409d596e8545490cd901c57da923d4aaad
Vitomir Budimir <budimirvitomir@gmail.com>
2024-11-18 17:23:31 +0100

    fix(editor): class specificity 2nd try

--- packages/editor/src/plugins/multimedia/style-hacks.ts
+++ packages/editor/src/plugins/multimedia/style-hacks.ts
@@ -39,8 +39,8 @@ export function getStyleHacks(
 
     // put top child toolbars in position of multimedia toolbar (first rows child)
     `
-    [&_.explanation-wrapper>.rows-child:first-child_.plugin-toolbar]:left-[7px]
     [&_.explanation-wrapper_.rows-child:first-child_.plugin-toolbar]:!-top-[72px]
+    [&_.explanation-wrapper_.rows-child:first-child_.plugin-toolbar]:left-[7px]
     `,
 
     // make multimedia explanation children left aligned
