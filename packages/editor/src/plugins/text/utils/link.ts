7ba07984c53cf389155e4f84c330acb29fe656e8
Mikey Stengel <mikey.stengel96@gmail.com>
2024-11-18 17:55:58 +0100

    fix(editor): Implement feedback of Botho, renaming variable and early exiting

--- packages/editor/src/plugins/text/utils/link.ts
+++ packages/editor/src/plugins/text/utils/link.ts
@@ -1,24 +1,22 @@
 export function getCleanUrl(
   inputUrl: string,
   instance?: string,
-  yieldAbsoluteUrl: boolean = false
+  isSerlo: boolean = true
 ) {
   const isJustId = /^\/[1-9][0-9]*$/.test(inputUrl)
-  if (isJustId && yieldAbsoluteUrl) {
+  if (isJustId && !isSerlo) {
     const domain = instance ? `${instance}.serlo.org` : 'serlo.org'
     return `https://${domain}${inputUrl}`
   }
+
   const serloContentMatch = inputUrl.match(
     /https?:\/\/([a-z]{2}\.)?serlo\.org(\/[a-z]+)?\/([1-9]?[0-9]+)/
   )
   const serloId = parseInt(serloContentMatch?.[3] ?? 'NaN')
-
   const hashPart = inputUrl.split('#')[1]
   const hash = hashPart ? `#${hashPart}` : ''
 
-  // If yieldAbsoluteUrl is true and it's a Serlo content URL, we want to keep
-  // the absolute URL instead of replacing it with a relative one!
-  if (yieldAbsoluteUrl && !isNaN(serloId)) {
+  if (!isSerlo && !isNaN(serloId)) {
     return (serloContentMatch ? serloContentMatch[0] : inputUrl) + hash
   }
 
