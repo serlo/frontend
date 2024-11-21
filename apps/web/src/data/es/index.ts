f47430e60300d230e4b0ae50368fce139c7629d6
Stephan Kulla <git.mail@kulla.me>
2024-11-22 00:54:15 +0100

    New translations index.ts (Spanish)

--- apps/web/src/data/es/index.ts
+++ apps/web/src/data/es/index.ts
@@ -366,6 +366,7 @@ export const instanceData = {
         code4000007: "Ya existe una cuenta con el mismo correo electrónico o nombre de usuario.",
         code4000008: "El código de autentificación proporcionado no es válido, por favor, inténtalo de nuevo.",
         code4000010: "¿Has verificado ya tu dirección de correo electrónico?%break%%verificationLinkText%",
+        code4000032: "You inserted less than 8 characters.",
         code4060004: "El enlace de recuperación no es válido o ya ha sido utilizado. Por favor, intenta solicitar un correo electrónico de nuevo",
         code4070001: "El enlace de verificación no es válido o ya ha sido utilizado. Por favor, intenta solicitar un correo electrónico de nuevo.",
         code4070005: "Lo sentimos, este enlace de verificación ya no es válido. Por favor, intenta solicitar un correo electrónico de nuevo."
