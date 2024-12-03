daad3d986ade9c6eb7d6e49154f278c0620644a1
Mikey Stengel <mikey@serlo.org>
2024-12-03 18:50:03 +0100

    Try to fix nested math issue

--- packages/editor/src/math/editor.tsx
+++ packages/editor/src/math/editor.tsx
@@ -163,7 +163,11 @@ export function MathEditor(props: MathEditorProps) {
     const isShadowRootNode = isShadowRoot(root)
     const target =
       (isShadowRootNode || isDocument
-        ? root.querySelector<HTMLDivElement>('.toolbar-controls-target')
+        ? root.querySelector<HTMLDivElement>(
+            // Either equations and toolbar, or plugin-text and toolbar (for
+            // nested math plugins)
+            '.plugin-text .toolbar-controls-target, .plugin-equations .toolbar-controls-target'
+          )
         : document.body) ?? document.body
 
     return createPortal(children, target)
