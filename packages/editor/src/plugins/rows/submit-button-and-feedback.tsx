c9d213ecbdee30c2f0427417443e0fcc7aee4641
Lars <lars@serlo.org>
2024-12-04 14:19:02 +0100

    feat(microadaptivity): add hardcoded ai feedback

--- packages/editor/src/plugins/rows/submit-button-and-feedback.tsx
+++ packages/editor/src/plugins/rows/submit-button-and-feedback.tsx
@@ -1,7 +1,48 @@
+import { FaIcon } from '@editor/editor-ui/fa-icon'
 import { cn } from '@editor/utils/cn'
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons'
 import { useState } from 'react'
 
 import { AnimateChangeInHeight } from '../text-area-exercise/animate-change-in-height'
+
+const aiFeedback = {
+  feedbackStart:
+    'Dein Text zeigt, dass du eine klare Meinung hast, und du hast schon gute Ansätze gezeigt, deine Argumente zu formulieren.',
+  feedback: [
+    {
+      title: '"Write between 50-75 words"',
+      feedback: [
+        'Dein Text umfasst ungefähr 40 Wörter und ist damit etwas zu kurz. Versuche, ein paar Sätze hinzuzufügen, um zwischen 50 und 75 Wörtern zu erreichen.',
+      ],
+      // suggestion:
+      //   'Du könntest mehr Details zu deinen Argumenten hinzufügen, z. B. warum Hausaufgaben langweilig sind oder warum Hobbys so wichtig sind.',
+      isCorrect: false,
+    },
+    {
+      title: '"Use 3-5 linking words"',
+      feedback: [
+        'Du hast einige Verbindungen genutzt, aber es fehlen Verbindungswörter, um die Sätze besser zu verbinden.',
+      ],
+      suggestion:
+        "Nutze Wörter wie 'because', 'however', oder 'and' öfter, um deine Gedanken flüssiger darzustellen.",
+      isCorrect: false,
+    },
+    {
+      title: 'Language',
+      feedback: [
+        'Achte darauf, dass deine Sätze vollständig und präzise sind. Zum Beispiel könnte ',
+        {
+          type: 'link',
+          text: "'Students must time for hobbies'",
+          href: '#students-must-time-for-hobbies',
+        },
+        " umformuliert werden zu: 'Students need time for hobbies because they are important.'",
+      ],
+      suggestion: '',
+      isCorrect: false,
+    },
+  ],
+}
 
 export function SubmitButtonAndFeedback() {
   const [showFeedback, setShowFeedback] = useState(false)
@@ -26,11 +67,42 @@ export function SubmitButtonAndFeedback() {
           <div className="fixed bottom-0 left-0 z-50 flex w-full flex-col items-center p-3">
             <div className="flex max-w-[50rem] flex-row gap-3 rounded-md bg-purple-200 p-3 shadow-plugin-focus">
               <img src="/_assets/img/birdie.svg" className="max-w-10" />
-              <div>
-                Feedback text Feedback text Feedback text Feedback text Feedback
-                text Feedback text Feedback text Feedback text Feedback text
-                Feedback text Feedback text Feedback text Feedback text Feedback
-                text Feedback text Feedback text Feedback text{' '}
+              <div className="flex flex-col gap-3">
+                <div>{aiFeedback.feedbackStart}</div>
+                <div className="flex flex-col gap-3">
+                  {aiFeedback.feedback.map((entry, index) => (
+                    <div key={index}>
+                      <div className="flex flex-row items-center gap-1 ">
+                        <FaIcon icon={faCircleXmark} className="text-red-400" />
+                        <a
+                          href="#feedback-criteria"
+                          className="text-brand-700"
+                        >{`${entry.title}`}</a>
+                      </div>
+                      <div>
+                        {entry.feedback.map((elem, index) => {
+                          if (typeof elem === 'object' && 'type' in elem) {
+                            return (
+                              <a
+                                key={index}
+                                href={elem.href}
+                                className="text-brand-700"
+                              >
+                                {elem.text}
+                              </a>
+                            )
+                          }
+                          return <span key={index}>{elem}</span>
+                        })}
+                      </div>
+                      {entry.suggestion ? (
+                        <div className="flex flex-row gap-1 ">
+                          ⮕ {entry.suggestion}
+                        </div>
+                      ) : null}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
