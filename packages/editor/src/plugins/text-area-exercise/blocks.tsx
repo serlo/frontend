abbd8179cb1a426c01a06a0500e6476c15458222
Mikey Stengel <mikey@serlo.org>
2024-12-05 18:44:20 +0100

    Make border thicker

--- packages/editor/src/plugins/text-area-exercise/blocks.tsx
+++ packages/editor/src/plugins/text-area-exercise/blocks.tsx
@@ -26,7 +26,7 @@ export function Blocks() {
 
   return (
     <div className="">
-      <div className="flex w-full flex-col gap-3 rounded-xl border border-brand bg-brand-50 p-2">
+      <div className="flex w-full flex-col gap-3 rounded-xl border-2 border-brand bg-brand-50 p-2">
         {blocks.map((block) =>
           block.type === 'text' ? (
             <TextBlock key={block.id} {...block} />
