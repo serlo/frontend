60a607aa83efaf641ea66e21ac7350bbb0cfa9af
Botho <1258870+elbotho@users.noreply.github.com>
2024-12-05 16:04:03 +0100

    refactor, add exercise ui

---
+++ apps/web/src/components/pages/lernpfad_prototype/exercises/exercise-wrapper.tsx
@@ -0,0 +1,65 @@
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+import { StickyHeaderLearner } from '../sticky-header-learner'
+import type { ExerciseProps } from '../types'
+import { FaIcon } from '@/components/fa-icon'
+
+export function ExerciseWrapper({
+  id,
+  data,
+  onSubmitClick,
+  onNextExerciseClick,
+  onBackToMapClick,
+  children,
+}: ExerciseProps & { children: JSX.Element }) {
+  if (id === null) return null
+
+  return (
+    <div className="flex flex-row">
+      <aside className="relative flex-shrink flex-grow basis-0">
+        <button
+          className="sticky top-0 flex flex-row items-center gap-3 p-7"
+          onClick={onBackToMapClick}
+        >
+          <FaIcon icon={faArrowLeft} />
+          <span className="text-lg">Back to Path</span>
+        </button>
+      </aside>
+      {/* To change content width, change both max-w and basis */}
+      <main
+        id="content"
+        className="mb-[50%] max-w-[min(100%,50rem)] flex-shrink flex-grow basis-[50rem]"
+      >
+        <StickyHeaderLearner />
+        {children}
+      </main>
+      <aside className="flex-shrink flex-grow basis-0">
+        {/* <GetAiFeedbackButton /> */}
+
+        {data.done ? (
+          <div className="text-center">
+            <p>Exercise done!</p>
+            <div className="align-center flex justify-center gap-2">
+              {data.nextExercises?.map((nextId) => (
+                <button
+                  key={nextId}
+                  className="serlo-button-edit-primary"
+                  onClick={() => onNextExerciseClick(nextId)}
+                >
+                  Go to {nextId}
+                </button>
+              ))}
+            </div>
+          </div>
+        ) : (
+          <button
+            className="serlo-button-edit-primary"
+            onClick={() => onSubmitClick(id)}
+          >
+            Submit
+          </button>
+        )}
+      </aside>
+    </div>
+  )
+}
--- apps/web/src/components/pages/lernpfad_prototype/exercises/recap_easy.tsx
+++ apps/web/src/components/pages/lernpfad_prototype/exercises/recap_easy.tsx
@@ -1,37 +1,18 @@
 import type { ExerciseProps } from '../types'
-import { BackLink } from './back-link'
-import { DoneState } from './done'
+import { ExerciseWrapper } from './exercise-wrapper'
 import { EditorRenderer } from '@/serlo-editor-integration/editor-renderer'
 
 // TODO: Structure somehow?
 
 export function RecapEasy(props: ExerciseProps) {
-  const { id, data, onSubmitClick } = props
-
-  if (id === null) return null
-
   return (
-    <div className="mx-auto max-w-2xl">
-      <BackLink {...props} />
-
-      <div className="flex h-full flex-col items-center justify-center">
-        <h1>{data.title}</h1>
+    <ExerciseWrapper {...props}>
+      <>
         <EditorRenderer document={editorContent1} />
         <EditorRenderer document={editorContent2} />
         <EditorRenderer document={editorContent3} />
-
-        {data.done ? (
-          <DoneState {...props} />
-        ) : (
-          <button
-            className="serlo-button-edit-primary"
-            onClick={() => onSubmitClick(id)}
-          >
-            Submit
-          </button>
-        )}
-      </div>
-    </div>
+      </>
+    </ExerciseWrapper>
   )
 }
 
