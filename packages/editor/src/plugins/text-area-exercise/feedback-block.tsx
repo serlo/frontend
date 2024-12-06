5218dbfeed5a946e7cf6370b4c4b03b747826996
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-12-06 13:27:34 +0100

    add x to close

--- packages/editor/src/plugins/text-area-exercise/feedback-block.tsx
+++ packages/editor/src/plugins/text-area-exercise/feedback-block.tsx
@@ -1,16 +1,35 @@
+import { FaIcon } from '@editor/editor-ui/fa-icon'
 import { cn } from '@editor/utils/cn'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 import { AnimateChangeInHeight } from './animate-change-in-height'
 import { PrototypeStateStore } from './prototype-state'
 import { Feedback } from './types'
+import { usePluginId } from './use-plugin-id'
 
 export function FeedbackBlock({ id, content }: Feedback) {
   const silentmode = PrototypeStateStore.useState((s) => s.silentMode)
+  const pluginId = usePluginId()
   return (
     <div
       id={id}
-      className={cn('m-3 rounded-md bg-purple-200 p-3', silentmode && 'hidden')}
+      className={cn(
+        'relative m-3 rounded-md bg-purple-200 p-3',
+        silentmode && 'hidden'
+      )}
     >
+      <button
+        className="absolute -right-1 -top-3 h-6 w-6 rounded-full bg-blue-200 text-center"
+        onClick={() => {
+          PrototypeStateStore.update((s) => {
+            s.textAreaPlugins[pluginId].textAreaBlocks = s.textAreaPlugins[
+              pluginId
+            ].textAreaBlocks.filter((x) => x.id !== id)
+          })
+        }}
+      >
+        <FaIcon icon={faTimes} />
+      </button>
       <AnimateChangeInHeight>
         {/* {isCorrect ? (
           <FaIcon icon={faCheck} className="text-purple-400" />
