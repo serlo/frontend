1f24a37c75d70b0dff7401a9e88215d91a401b98
Vitomir Budimir <budimirvitomir@gmail.com>
2024-12-09 10:48:02 +0100

    fix: ai button text

--- apps/web/src/components/pages/lernpfad_prototype/map-navigation.tsx
+++ apps/web/src/components/pages/lernpfad_prototype/map-navigation.tsx
@@ -23,7 +23,7 @@ export function MapNavigation() {
       </div>
 
       <button className="mr-4 flex flex-row items-center gap-3 rounded-md bg-purple-200 px-6 py-3 font-bold hover:cursor-pointer hover:bg-purple-300">
-        KI Tutor
+        Sprich mit Birdie
         <img src="/_assets/img/birdie.svg" className="max-w-6" />
       </button>
     </nav>
