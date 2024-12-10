ae8b8501f51efdcda9346ad7573d2b43a5175cde
Botho <1258870+elbotho@users.noreply.github.com>
2024-12-10 17:32:47 +0100

    feat(plugin-ai-generation): add mocked logic

--- packages/editor/src/plugins/ai-generation/editor.tsx
+++ packages/editor/src/plugins/ai-generation/editor.tsx
@@ -1,9 +1,79 @@
 import { EditorModal } from '@editor/editor-ui/editor-modal'
+import { showToastNotice } from '@editor/editor-ui/show-toast-notice'
 import { useEditStrings } from '@editor/i18n/edit-strings-provider'
+import { EditorPluginType } from '@editor/package'
+import {
+  insertPluginChildBefore,
+  removePluginChild,
+  selectChildTreeOfParent,
+  selectStaticDocument,
+  useAppDispatch,
+  useStore,
+} from '@editor/store'
 import { cn } from '@editor/utils/cn'
+import { either as E } from 'fp-ts'
+import { useState } from 'react'
 
-export function AiGenerationEditor() {
+import { AiGenerationPluginProps } from '.'
+import { StateDecoder } from './decoder'
+import { mocked } from './mocked'
+
+export function AiGenerationEditor(props: AiGenerationPluginProps) {
   const aiStrings = useEditStrings().plugins.aiGeneration
+  const [prompt, setPrompt] = useState('')
+
+  const store = useStore()
+  const dispatch = useAppDispatch()
+
+  // TODO: i18n
+  function throwError(error?: unknown) {
+    showToastNotice('⚠️ Sorry, something is wrong with the data.', 'warning')
+    // eslint-disable-next-line no-console
+    console.error(error)
+    throw new Error(
+      'JSON input data is not a valid editor-state or contains unsupported plugins'
+    )
+  }
+
+  function handleSubmit() {
+    console.log(prompt)
+
+    // TODO: fetch, validate etc
+
+    setPrompt('')
+
+    const decoded = StateDecoder.decode(mocked)
+
+    if (E.isLeft(decoded)) return throwError()
+
+    const content = decoded.right
+
+    const parentPlugin = selectChildTreeOfParent(store.getState(), props.id)
+
+    // for now make sure we only use it in rows plugin until we provide a list of allowed plugins
+    if (
+      parentPlugin === null ||
+      selectStaticDocument(store.getState(), parentPlugin.id)?.plugin !==
+        EditorPluginType.Rows
+    ) {
+      const msg = 'Ai generation can only be used inside a rows plugin!'
+      showToastNotice(msg)
+      // eslint-disable-next-line no-console
+      console.error(msg)
+      return
+    }
+
+    for (const document of content.state) {
+      dispatch(
+        insertPluginChildBefore({
+          parent: parentPlugin.id,
+          sibling: props.id,
+          document,
+        })
+      )
+    }
+    dispatch(removePluginChild({ parent: parentPlugin.id, child: props.id }))
+  }
 
   return (
     <EditorModal
@@ -13,28 +83,24 @@ export function AiGenerationEditor() {
       className="top-8 max-w-xl translate-y-0 sm:top-24"
       extraTitleClassName="serlo-h3 mt-4"
     >
-      <textarea
-        className={cn(`ml-side w-[calc(100%-32px)] rounded-xl border-2 border-editor-primary-100
+      <form>
+        <textarea
+          className={cn(`ml-side w-[calc(100%-32px)] rounded-xl border-2 border-editor-primary-100
           bg-editor-primary-100 px-2.5 py-2 text-almost-black
           focus:border-editor-primary focus:outline-none`)}
-        // value={prompt}
-        rows={5}
-        placeholder={aiStrings.placeholder}
-        // onChange={(e) => setPrompt(e.target.value)}
-        // onKeyDown={(e) => {
-        //   if (e.key === 'Enter') {
-        //     onEnter(prompt)
-        //   }
-        // }}
-      />
-      <button
-        className="serlo-button-edit-primary mx-side mt-4 px-4"
-        // onClick={() => {
-        //   onEnter(prompt)
-        // }}
-      >
-        {aiStrings.buttonText}
-      </button>
+          value={prompt}
+          rows={5}
+          placeholder={aiStrings.placeholder}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="serlo-button-edit-primary mx-side mt-4 px-4"
+          onClick={handleSubmit}
+        >
+          {aiStrings.buttonText}
+        </button>
+      </form>
     </EditorModal>
   )
 }
---
+++ packages/editor/src/plugins/ai-generation/mocked.ts
@@ -0,0 +1,30 @@
+export const mocked = {
+  plugin: 'rows',
+  state: [
+    {
+      plugin: 'text',
+      state: [
+        {
+          type: 'p',
+          children: [
+            { text: 'In Arabic numerals, the number 123 is written as "١٢٣".' },
+          ],
+        },
+        {
+          type: 'p',
+          children: [
+            {
+              text: 'Arabic numerals are the ten digits: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, which are used in most of the world today. However, the term "Arabic numerals" can also refer to the numeral system used in the Arab world, which is different from the Western Arabic numerals.',
+            },
+          ],
+        },
+      ],
+      id: '3ac1e39b-10d3-422d-8de1-5a25dbefb49f',
+    },
+    {
+      plugin: 'highlight',
+      state: { code: '123 123', language: 'text', showLineNumbers: false },
+      id: 'e6a38748-fbfb-48df-b873-b2c62e31941c',
+    },
+  ],
+}
