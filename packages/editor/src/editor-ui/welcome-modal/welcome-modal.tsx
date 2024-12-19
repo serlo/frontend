68bf9a4117721cda3996663d8874ddd9a42e303e
Vitomir Budimir <budimirvitomir@gmail.com>
2024-12-19 10:31:34 +0100

    feat(editor-ui): add welcome modal video and text content

--- packages/editor/src/editor-ui/welcome-modal/welcome-modal.tsx
+++ packages/editor/src/editor-ui/welcome-modal/welcome-modal.tsx
@@ -1,4 +1,5 @@
 import { EditorModal } from '@editor/editor-ui/editor-modal'
+import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons'
 import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
 
@@ -17,27 +18,16 @@ export function WelcomeModal() {
     })
   }
 
-  // TODO: Placeholder strings until final strings are provided
-  const welcomeModalStrings = {
-    title: 'Welcome',
-    firstStepText:
-      "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    thirdStepText:
-      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-    nextButton: 'Next',
-    nextButtonLastStep: "Let's go!",
-  }
-
   return (
     <EditorModal
       // width should match slideWidth const
-      className="top-1/2 flex w-[700px] max-w-[95%] flex-col gap-16 px-0 pt-20"
+      className="top-1/2 flex w-[600px] max-w-[95%] flex-col gap-16 px-0 pt-20"
       extraTitleClassName="sr-only"
-      title={welcomeModalStrings.title}
+      title="Herzlich Willkommen beim Serlo Editor"
       isOpen={isOpen}
       setIsOpen={(isOpen) => !isOpen && onClose()}
     >
-      <div className="flex h-[350px] overflow-hidden">
+      <div className="flex h-[400px] overflow-hidden">
         <div
           className="flex items-center transition-transform"
           style={{
@@ -46,18 +36,34 @@ export function WelcomeModal() {
         >
           <div {...slideProps}>
             <h1 className="serlo-h1">
-              {welcomeModalStrings.title}{' '}
-              <span className="text-brand-600">Serlo Editor</span>
+              Herzlich Willkommen! <br />
+              <span className="text-brand-600">beim Serlo Editor</span>
             </h1>
-            <p className="serlo-p">{welcomeModalStrings.firstStepText}</p>
+            <p className="serlo-p">
+              Der Serlo Editor hilft Dir,{' '}
+              <b>Texte, Bilder und interaktive Aufgaben</b>
+              direkt zu bearbeiten und sofort sehen, wie sie später aussehen.
+            </p>
+            <p className="serlo-p">
+              So kannst Du Lernmaterialien <b>einfach</b> erstellen,{' '}
+              <b>ohne technische Vorkenntnisse</b> zu benötigen.
+            </p>
           </div>
 
           <div {...slideProps}>
-            <h1 className="serlo-h1">Video</h1>
-          </div>
-
-          <div {...slideProps}>
-            <p className="serlo-p">{welcomeModalStrings.thirdStepText}</p>
+            <div className="px-4">
+              <video controls>
+                <source
+                  src="https://storage.googleapis.com/assets.serlo.org/wikimedia/Der_Menstruationszyklus.webm"
+                  type="video/webm"
+                />
+              </video>
+            </div>
+            <p className="serlo-p mb-0 mt-4">
+              Weitere <b>Erklärungen und Videos</b> findest du jeweils in der
+              Toolbar der einzelnen Plugins. Einfach auf das{' '}
+              <FaIcon icon={faCircleQuestion} /> klicken.
+            </p>
           </div>
         </div>
       </div>
@@ -76,9 +82,7 @@ export function WelcomeModal() {
           className="serlo-button-learner-primary"
           onClick={handleNextButtonClick}
         >
-          {currentStep === 3
-            ? welcomeModalStrings.nextButtonLastStep
-            : welcomeModalStrings.nextButton}{' '}
+          {currentStep === steps.length ? "Los geht's!" : 'Weiter'}{' '}
           <FaIcon icon={faArrowCircleRight} />
         </button>
       </div>
@@ -86,9 +90,9 @@ export function WelcomeModal() {
   )
 }
 
-const steps = [1, 2, 3]
+const steps = [1, 2]
 
-const slideWidth = 700
+const slideWidth = 600
 
 const slideProps = {
   style: { width: slideWidth },
@@ -98,5 +102,4 @@ const slideProps = {
 const translateXValuesMap: Record<number, string> = {
   1: '0',
   2: `-${slideWidth}px`,
-  3: `-${slideWidth * 2}px`,
 }
