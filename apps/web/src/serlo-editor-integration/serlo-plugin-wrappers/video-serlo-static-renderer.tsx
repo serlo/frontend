eca90040e30c561b1d37ea6ffc19f27b93b1b7f4
Botho <1258870+elbotho@users.noreply.github.com>
2024-12-19 16:39:34 +0100

    fix(plugin-video): remove isTempFile check from static renderers

--- apps/web/src/serlo-editor-integration/serlo-plugin-wrappers/video-serlo-static-renderer.tsx
+++ apps/web/src/serlo-editor-integration/serlo-plugin-wrappers/video-serlo-static-renderer.tsx
@@ -1,4 +1,3 @@
-import { isTempFile } from '@editor/plugin'
 import { parseVideoUrl, VideoType } from '@editor/plugins/video/renderer'
 import { VideoStaticRenderer } from '@editor/plugins/video/static'
 import { EditorVideoDocument } from '@editor/types/editor-plugins'
@@ -16,7 +15,7 @@ const PrivacyWrapper = dynamic<PrivacyWrapperProps>(() =>
 
 export function VideoSerloStaticRenderer(props: EditorVideoDocument) {
   const { src } = props.state
-  if (!src || isTempFile(src)) return null
+  if (!src) return null
   const [iframeSrc, type] = parseVideoUrl(src)
 
   if (type === VideoType.SerloAsset) {
--- packages/editor/src/types/editor-plugins.ts
+++ packages/editor/src/types/editor-plugins.ts
@@ -200,7 +200,7 @@ export interface EditorTextDocument {
 }
 export interface EditorVideoDocument {
   plugin: EditorPluginType.Video
-  state: PrettyStaticState<VideoPluginState>
+  state: PrettyStaticState<VideoPluginState> & { src: string }
   id?: string
 }
 export interface EditorAudioDocument {
