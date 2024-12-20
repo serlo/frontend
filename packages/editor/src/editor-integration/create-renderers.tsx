2826dcbb30b0ee3fa02bed3933f15219fcc71676
Lars <lars@serlo.org>
2024-12-20 08:23:38 +0100

    fix(link-renderer): add guard against injecting javascript in link elements through href

--- packages/editor/src/editor-integration/create-renderers.tsx
+++ packages/editor/src/editor-integration/create-renderers.tsx
@@ -30,6 +30,7 @@ import { TextAreaExerciseStaticRenderer } from '@editor/plugins/text-area-exerci
 import { VideoStaticRenderer } from '@editor/plugins/video/static'
 import { EditorPluginType } from '@editor/types/editor-plugin-type'
 import { TemplatePluginType } from '@editor/types/template-plugin-type'
+import DOMPurify from 'dompurify'
 import { ComponentProps } from 'react'
 
 export function createRenderers(): InitRenderersArgs {
@@ -124,10 +125,15 @@ export function createRenderers(): InitRenderersArgs {
     ],
     mathRenderer: (element: MathElement) => <StaticMath {...element} />,
     linkRenderer: ({ href, children }: ComponentProps<LinkRenderer>) => {
+      // href can be manipulated by the user and could potentially be abused to inject malicious javascript. We use DOMPurify to validate the href.
+      // Examples:
+      // - 'javascript:doSomethingBad()' -> invalid
+      // - 'https://example.com/' -> valid
+      const isHrefValid = DOMPurify.isValidAttribute('a', 'href', href)
       return (
         <a
           className="serlo-link cursor-pointer"
-          href={href}
+          href={isHrefValid ? href : ''}
           target="_blank"
           rel="noopener noreferrer"
         >
