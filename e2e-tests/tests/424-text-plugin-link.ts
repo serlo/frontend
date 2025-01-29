2e0ef0d9d33e0b1ab8972b03286e44be5be18eb9
Vitomir Budimir <budimirvitomir@gmail.com>
2025-01-29 18:22:17 +0100

    e2e: wait for link suggestion search results

--- e2e-tests/tests/424-text-plugin-link.ts
+++ e2e-tests/tests/424-text-plugin-link.ts
@@ -68,6 +68,7 @@ Scenario('Edit existing link', async ({ I }) => {
   I.click('$plugin-toolbar-button-link')
   I.type('Mathematik Start')
   I.seeElement('$link-suggestion-0')
+  I.seeElement('$link-suggestion-1')
   I.pressKey('Enter')
   I.seeElement(locate({ css: '.editor-core a' }).withAttr({ href: '/19767' }))
 
@@ -79,6 +80,7 @@ Scenario('Edit existing link', async ({ I }) => {
   I.pressKey(['CommandOrControl', 'A'])
   I.type('geometrie')
   I.seeElement('$link-suggestion-0')
+  I.seeElement('$link-suggestion-1')
   I.pressKey('Enter')
   I.seeElement(locate({ css: '.editor-core a' }).withAttr({ href: '/1288' }))
 })
@@ -94,6 +96,7 @@ Scenario('Remove existing link', async ({ I }) => {
   I.click('$plugin-toolbar-button-link')
   I.type('Math')
   I.click('$link-suggestion-0')
+  I.click('$link-suggestion-1')
   I.seeElement({ css: '.editor-core a' })
 
   I.say('Remove link using the remove link button')
