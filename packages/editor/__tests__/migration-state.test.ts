df541cfa1cd6d9c1eedc41910b3c2544f3862f5b
Vitomir Budimir <budimirvitomir@gmail.com>
2025-02-04 12:29:48 +0100

    fix(editor): temporarily disable migration-state unit test

--- packages/editor/__tests__/migration-state.test.ts
+++ packages/editor/__tests__/migration-state.test.ts
@@ -1,7 +1,8 @@
 import { migrate } from '@editor/package/storage-format'
 import { expect, test } from 'vitest'
 
-test('A document not being in the storage format can be migrated', () => {
+// Temporarily disabled, until https://linear.app/serlo/issue/EDTR-98/ is done
+test.todo('A document not being in the storage format can be migrated', () => {
   const { stateChanged, migratedState } = migrate({ plugin: 'rows' }, 'unknown')
 
   expect(stateChanged).toBe(true)
