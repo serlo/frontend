4c83cd3a22cd4314629012c20e504f5f7ccdf687
Lars <lars@serlo.org>
2025-02-05 16:39:00 +0100

    chore: simulate cross-origin iframe in editor demo page

---
+++ packages/editor/src/___dev-demo/iframed-editor-and-preview.tsx
@@ -0,0 +1,26 @@
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')!).render(
+  <iframe
+    style={{ width: '100%', height: '100%', borderWidth: '0' }}
+    // Embedding localhost:3001 on host page localhost:3000 simulates cross-origin. Important because a lot of iframe, cookie, CSP security measures are only in effect for cross-origin iframes.
+    src="http://localhost:3001/demo/react-preview/"
+    sandbox={`
+      allow-downloads
+      allow-forms
+      allow-modals
+      allow-popups
+      allow-popups-to-escape-sandbox
+      allow-presentation
+      allow-same-origin
+      allow-scripts
+      allow-storage-access-by-user-activation
+    `}
+    allow={`
+      clipboard-read https://editor.serlo.org https://staging.editor.serlo.org https://dev.editor.serlo.org https://editor.serlo-staging.dev;
+      clipboard-write https://editor.serlo.org https://staging.editor.serlo.org https://dev.editor.serlo.org https://editor.serlo-staging.dev;
+      fullscreen https://editor.serlo.org https://staging.editor.serlo.org https://dev.editor.serlo.org https://editor.serlo-staging.dev;
+      autoplay https://editor.serlo.org https://staging.editor.serlo.org https://dev.editor.serlo.org https://editor.serlo-staging.dev
+    `}
+  ></iframe>
+)
