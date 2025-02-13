774a98748896a6079e25aef44d98045c23d27df9
Botho <1258870+elbotho@users.noreply.github.com>
2025-02-13 20:05:13 +0100

    feat(web): use swr in license update to fetch current license Id (maybe add metadata edit here later)

--- apps/web/src/pages/entity/license/update/[id].tsx
+++ apps/web/src/pages/entity/license/update/[id].tsx
@@ -1,13 +1,17 @@
+import { gql } from 'graphql-request'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { useState } from 'react'
 
+import { useGraphqlSwr } from '@/api/use-graphql-swr'
 import { PageTitle } from '@/components/content/page-title'
 import { FrontendClientBase } from '@/components/frontend-client-base/frontend-client-base'
+import { LoadingSpinner } from '@/components/loading/loading-spinner'
 import { Breadcrumbs } from '@/components/navigation/breadcrumbs'
 import { PleaseLogIn } from '@/components/user/please-log-in'
 import { useInstanceData } from '@/contexts/instance-context'
 import { useLoggedInData } from '@/contexts/logged-in-data-context'
 import { LicenseData } from '@/data-types'
+import { GetMetaAndLicenseQuery } from '@/fetcher/graphql-types/operations'
 import { renderedPageNoHooks } from '@/helper/rendered-page'
 import { useEntityUpdateLicenseMutation } from '@/mutations/use-entity-update-license-mutation'
 
@@ -23,9 +27,13 @@ export default renderedPageNoHooks<UpdateLicenseProps>((props) => (
 
 function Content({ id }: UpdateLicenseProps) {
   const updateLicense = useEntityUpdateLicenseMutation()
+
   const { strings, licenses } = useInstanceData()
   const [licenseId, setLicenseId] = useState<number>(licenses[0].id)
   const loggedInData = useLoggedInData()
+
+  const { data, error } = useFetch(id)
+
   if (!loggedInData)
     return (
       <div className="mt-12">
@@ -34,15 +42,29 @@ function Content({ id }: UpdateLicenseProps) {
     )
   const loggendInStrings = loggedInData.strings
 
+  if (!data && !error) return <LoadingSpinner noText />
+
+  if (!data || !data.uuid) {
+    return (
+      <>
+        {renderBackbutton()}
+        <h3 className="serlo-h3">{strings.errors.defaultMessage}</h3>
+      </>
+    )
+  }
+
+  const { uuid } = data
+
   return (
     <>
-      {renderBackButton()}
+      {renderBackbutton()}
       <PageTitle title={loggendInStrings.authorMenu.changeLicense} headTitle />
 
       <div className="mx-side flex">
         <select
           className="serlo-button-learner-secondary serlo-input-font-reset max-w-xl"
           onChange={(e) => setLicenseId(parseInt(e.target.value))}
+          defaultValue={uuid.licenseId}
         >
           {licenses.map(renderOption)}
         </select>
@@ -56,6 +78,15 @@ function Content({ id }: UpdateLicenseProps) {
     </>
   )
 
+  function renderBackbutton() {
+    return (
+      <Breadcrumbs
+        data={[{ label: strings.revisions.toContent, url: `/${id}` }]}
+        asBackButton
+      />
+    )
+  }
+
   function renderOption(license: LicenseData) {
     return (
       <option
@@ -67,28 +98,15 @@ function Content({ id }: UpdateLicenseProps) {
       </option>
     )
   }
-
-  function renderBackButton() {
-    return (
-      <Breadcrumbs
-        data={[{ label: strings.revisions.toContent, url: `/${id}` }]}
-        asBackButton
-      />
-    )
-  }
 }
 
 export const getStaticProps: GetStaticProps<UpdateLicenseProps> = (context) => {
   const id = parseInt(context.params?.id as string)
 
-  // get current license ID here maybe?
-
   if (isNaN(id)) return { notFound: true }
 
   return {
-    props: {
-      id,
-    },
+    props: { id },
     revalidate: 60 * 60 * 24, // 1 day
   }
 }
@@ -99,3 +117,33 @@ export const getStaticPaths: GetStaticPaths = async () => {
     fallback: 'blocking',
   }
 }
+
+type MetaAndLicenseResult = Extract<
+  GetMetaAndLicenseQuery['uuid'],
+  { licenseId: any }
+>
+
+function useFetch(id: number) {
+  return useGraphqlSwr<{ uuid: MetaAndLicenseResult }>({
+    query: metaAndLicenseQuery,
+    variables: { id },
+    config: {
+      refreshInterval: 10 * 60 * 1000, //10min
+    },
+  })
+}
+
+export const metaAndLicenseQuery = gql`
+  query getMetaAndLicense($id: Int!) {
+    uuid(id: $id) {
+      ... on AbstractEntity {
+        __typename
+        licenseId
+        currentRevision {
+          metaTitle
+          metaDescription
+        }
+      }
+    }
+  }
+`
