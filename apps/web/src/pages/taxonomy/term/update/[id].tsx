3c1dbe0816e9399628447581f5f89bab261261c3
Botho <1258870+elbotho@users.noreply.github.com>
2025-02-14 18:05:46 +0100

    fix(web): remove undefined from server side props

--- apps/web/src/pages/taxonomy/term/update/[id].tsx
+++ apps/web/src/pages/taxonomy/term/update/[id].tsx
@@ -35,8 +35,7 @@ export const getServerSideProps: GetServerSideProps<EditorPageData> = async (
   if (result.errorType === 'failed-fetch') {
     return { notFound: true }
   }
-
   return {
-    props: result,
+    props: JSON.parse(JSON.stringify(result)) as EditorPageData, // remove undefined values
   }
 }
