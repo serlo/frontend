3d011e3ea9ada16285ad7d20d0d150ecba69d47c
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2025-02-20 14:17:19 +0100

    feat(vidis): automatically forward user if idp hint is present
    Co-Authored-By: Vitomir Budimir <budimirvitomir@gmail.com>

--- apps/web/src/components/auth/login-button-vidis.tsx
+++ apps/web/src/components/auth/login-button-vidis.tsx
@@ -1,5 +1,6 @@
 import type { UiNodeInputAttributes } from '@ory/client'
-import { type FormEvent } from 'react'
+import { useRouter } from 'next/router'
+import { useEffect, useRef, type FormEvent } from 'react'
 
 import VidisLogo from '@/assets-webkit/img/auth/vidis-logo.svg'
 import { cn } from '@/helper/cn'
@@ -15,10 +16,23 @@ export function LoginButtonVidis({
   onSubmit,
   disabled,
 }: NodeProps) {
+  const router = useRouter()
+  const buttonRef = useRef<HTMLButtonElement | null>(null)
+
+  useEffect(() => {
+    if (!router.isReady) return
+
+    const { vidis_idp_hint } = router.query
+    if (vidis_idp_hint && buttonRef.current) {
+      buttonRef.current.click()
+    }
+  }, [router.isReady, router.query])
+
   return (
     <div className="-mb-8 mt-10">
       <hr />
       <button
+        ref={buttonRef}
         className={cn(`
          mt-10 w-full items-center border border-transparent px-[22px] py-2 text-center
         text-lg font-bold shadow-menu transition-all hover:border-black focus-visible:border-black
