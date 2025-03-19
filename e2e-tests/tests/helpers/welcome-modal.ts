12af0a77026175e936c680225a7cdf405533f7a7
Vitomir Budimir <budimirvitomir@gmail.com>
2025-03-19 11:43:08 +0100

    e2e: welcome modal helper fix

--- e2e-tests/tests/helpers/welcome-modal.ts
+++ e2e-tests/tests/helpers/welcome-modal.ts
@@ -1,6 +1,7 @@
 export function setWelcomeModalSeen({ I }) {
   I.amOnPage('/')
   I.executeScript(() => {
+    if (localStorage.getItem('serlo-editor::hasUserSeenWelcomeModal')) return
     localStorage.setItem('serlo-editor::hasUserSeenWelcomeModal', 'true')
   })
 }
