559e8a275fb93a8da54473ee23300a191362495d
Botho <1258870+elbotho@users.noreply.github.com>
2025-03-30 12:00:13 +0200

    temporarily load images from botho.cc, lazy load

--- packages/editor/src/plugins/image/components/editor-image.tsx
+++ packages/editor/src/plugins/image/components/editor-image.tsx
@@ -9,11 +9,20 @@ import { ImgHTMLAttributes } from 'react'
  */
 export function EditorImage(props: ImgHTMLAttributes<HTMLImageElement>) {
   const isSerlo = useIsSerlo()
-  return <img {...props} src={getSrc(isSerlo, props.src)} />
+  return <img {...props} src={getSrc(isSerlo, props.src)} loading="lazy" />
 }
 
 function getSrc(isSerlo?: boolean, src?: string) {
   if (!isSerlo || !src) return src
+
+  const isAsset = src.startsWith('https://assets.serlo.org/')
+  if (isAsset) {
+    return src
+      .replace('assets.serlo.org/', 'root.botho.cc/serlo-astro/assets/')
+      .replace('.jpg', '.webp')
+      .replace('.jpeg', '.webp')
+      .replace('.png', '.webp')
+  }
 
   const isAllowed =
     src.match(/^https:\/\/[a-z]+.(serlo|serlo-staging).(org|dev)\//) ||
