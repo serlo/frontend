efda5712c171714eefd78dfae546c6330bdb0479
Botho <1258870+elbotho@users.noreply.github.com>
2025-04-09 11:48:30 +0200

    fix(e2e): increase waits

--- e2e-tests/tests/000-general.ts
+++ e2e-tests/tests/000-general.ts
@@ -235,9 +235,9 @@ Scenario('Consent', async ({ I }) => {
 Scenario('Special Pages', ({ I }) => {
   I.amOnPage('/')
   I.click('Kontakt')
-  I.waitForText('Kontakt und Standorte', 5, 'h1')
+  I.waitForText('Kontakt und Standorte', 15, 'h1')
   I.click('Jobs')
-  I.waitForText('Digitale Bildung braucht dich', 5, 'h1')
+  I.waitForText('Digitale Bildung braucht dich', 15, 'h1')
   I.see('Unsere offenen Stellen')
   I.see('Weiterbildung')
 
@@ -245,7 +245,7 @@ Scenario('Special Pages', ({ I }) => {
   I.click('Kontakt')
 
   // Wait for next page to load before clicking team
-  I.waitForText('Kontakt und Standorte', 5, 'h1')
+  I.waitForText('Kontakt und Standorte', 15, 'h1')
   I.click('Team')
 
   // Needs to do external fetching, so wait a bit longer
