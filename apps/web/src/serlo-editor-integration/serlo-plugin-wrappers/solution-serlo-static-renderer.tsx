4bdbc38b879c8515fa1befd29753aaf36653bfe1
Botho <1258870+elbotho@users.noreply.github.com>
2025-04-22 13:02:16 +0200

    try without dynamic loading

--- apps/web/src/serlo-editor-integration/serlo-plugin-wrappers/solution-serlo-static-renderer.tsx
+++ apps/web/src/serlo-editor-integration/serlo-plugin-wrappers/solution-serlo-static-renderer.tsx
@@ -4,8 +4,9 @@ import {
 } from '@editor/package'
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
 import { useRouter } from 'next/router'
-import { lazy, Suspense, useContext } from 'react'
+import { useContext } from 'react'
 
+import { CommentAreaEntity } from '@/components/comments/comment-area-entity'
 import { Lazy } from '@/components/content/lazy'
 import { FaIcon } from '@/components/fa-icon'
 import { isPrintMode, printModeSolutionVisible } from '@/components/print-mode'
@@ -13,12 +14,6 @@ import { useEntityMetaData } from '@/contexts/entity-meta-context'
 import { ExerciseContext } from '@/contexts/exercise-context'
 import { useInstanceData } from '@/contexts/instance-context'
 import { RevisionViewContext } from '@/contexts/revision-view-context'
-
-const CommentAreaEntity = lazy(() =>
-  import('@/components/comments/comment-area-entity').then((module) => ({
-    default: module.CommentAreaEntity,
-  }))
-)
 
 // Special version for serlo.org with author tools and comments
 export function SolutionSerloStaticRenderer(props: EditorSolutionDocument) {
@@ -56,9 +51,7 @@ export function SolutionSerloStaticRenderer(props: EditorSolutionDocument) {
     if (isEntity) {
       return (
         <Lazy>
-          <Suspense>
-            <CommentAreaEntity entityId={entityId} />
-          </Suspense>
+          <CommentAreaEntity entityId={entityId} />
         </Lazy>
       )
     }
