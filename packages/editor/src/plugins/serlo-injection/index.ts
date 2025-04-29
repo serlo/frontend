d669b5e73fabb4d5f657875ec196f560b0fffbf1
Botho <1258870+elbotho@users.noreply.github.com>
2025-04-29 15:21:25 +0200

    refactor(plugin-serlo-injection): remove i18n config

--- packages/editor/src/plugins/serlo-injection/index.ts
+++ packages/editor/src/plugins/serlo-injection/index.ts
@@ -4,30 +4,13 @@ import { type EditorPlugin, type EditorPluginProps, string } from '../../plugin'
 const state = string()
 
 // as far as i know this is only used in edusharing right now
-export function createSerloInjectionPlugin(
-  config: SerloInjectionConfig = {}
-): EditorPlugin<SerloInjectionPluginState, SerloInjectionConfig> {
+export function createSerloInjectionPlugin(): EditorPlugin<SerloInjectionPluginState> {
   return {
     Component: SerloInjectionEditor,
-    config,
     state,
   }
 }
 
-export interface SerloInjectionConfig {
-  i18n?: Partial<SerloInjectionPluginConfig['i18n']>
-}
-
 export type SerloInjectionPluginState = typeof state
 
-export interface SerloInjectionPluginConfig {
-  i18n: {
-    label: string
-    placeholder: string
-  }
-}
-
-export type SerloInjectionProps = EditorPluginProps<
-  SerloInjectionPluginState,
-  SerloInjectionConfig
->
+export type SerloInjectionProps = EditorPluginProps<SerloInjectionPluginState>
